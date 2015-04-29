1c4889d7bcc961dc5319be50056405f5458379e8
Adam Smith <adamsmith@youlum.com>
2015-04-29 14:27:05 -0700

    Added Tests.hasOwnProp

---
+++ tests/pre-compiled-tests/classes/tests/hasOwnProp.js
@@ -0,0 +1,95 @@
+  /**
+   * -------------------------------------------------
+   * Public Method (Tests.hasOwnProp)
+   * -------------------------------------------------
+   * @desc Checks aIV.utils.hasOwnProp method.
+   * @type {function}
+   */
+  Tests.hasOwnProp = (function setupTestsHasOwnProp() {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Private hasOwnProp Variables
+    ////////////////////////////////////////////////////////////////////////////
+
+    /** @type {!TestResults} */
+    var results = new TestResults('Tests.hasOwnProp');
+
+    /** @type {!Object} */
+    var testObj = {
+      testProp1: true
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Public hasOwnProp Method
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * -------------------------------------------------
+     * Public Method (hasOwnProp)
+     * -------------------------------------------------
+     * @desc Checks aIV.utils.hasOwnProp method.
+     * @type {function}
+     */
+    var hasOwnProp = function() {
+
+      testCatchFalse();
+      testPassTrue();
+
+      // Save the results
+      app.results.push(results);
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Private hasOwnProp Methods
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testCatchFalse)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testCatchFalse = function() {
+
+      /** @type {boolean} */
+      var fail;
+      /** @type {string} */
+      var errorMsg;
+
+      fail = aIV.utils.hasOwnProp(testObj, 'testProp2');
+      fail = fail || aIV.utils.hasOwnProp(testObj, 'prototype');
+
+      if (fail) {
+        errorMsg = 'hasOwnProp failed to return false for invalid properties';
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testPassTrue)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testPassTrue = function() {
+
+      /** @type {boolean} */
+      var pass;
+      /** @type {string} */
+      var errorMsg;
+
+      pass = aIV.utils.hasOwnProp(testObj, 'testProp1');
+
+      if (!pass) {
+        errorMsg = 'hasOwnProp failed to return true for valid properties';
+        results.addError(errorMsg);
+      }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // The End Of The hasOwnProp Module
+    ////////////////////////////////////////////////////////////////////////////
+
+    return hasOwnProp;
+
+  })();
