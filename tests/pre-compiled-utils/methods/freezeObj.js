4464cee03b839cf512e1e052f10597023e7bfbc3
Adam Smith <adamsmith@youlum.com>
2015-04-29 01:46:58 -0700

    Added the freezeObj method

---
+++ tests/pre-compiled-utils/methods/freezeObj.js
@@ -0,0 +1,87 @@
+  /**
+   * ---------------------------------------------------
+   * Public Method (utilsModuleAPI.freezeObj)
+   * ---------------------------------------------------
+   * @desc A shortcut for the Object.freeze method with an optional
+   *   deep freeze (i.e. freezes all of an object's object properties).
+   * @param {(!Object|function)} obj - The object to freeze.
+   * @param {boolean=} deep - Deep freeze the object. The default is false.
+   * @return {(!Object|function)} The frozen object.
+   */
+  var freezeObj = (function setupFreezeObj() {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // The Public freezeObj Method
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * ---------------------------------------------------
+     * Public Method (freezeObj)
+     * ---------------------------------------------------
+     * @desc A shortcut for the Object.freeze method with an optional
+     *   deep freeze (i.e. freezes all of an object's object properties).
+     * @param {(!Object|function)} obj - The object to freeze.
+     * @param {boolean=} deep - Deep freeze the object. The default is false.
+     * @return {(!Object|function)} The frozen object.
+     */
+    var freezeObj = function(obj, deep) {
+
+      /** @type {string} */
+      var errorMsg;
+
+      if (!obj || (typeof obj !== 'object' && typeof obj !== 'function')) {
+        errorMsg = 'An aIV.utils.freezeObj call received an invalid obj ';
+        errorMsg += 'parameter.';
+        throw new TypeError(errorMsg);
+        return;
+      }
+
+      if (typeof deep !== 'boolean') {
+        deep = false;
+      }
+
+      if (deep) {
+        deepFreeze(obj);
+      }
+      else {
+        Object.freeze(obj);
+      }
+
+      return obj;
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // The Private freezeObj Methods
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * -------------------------------------------------
+     * Private Method (deepFreeze)
+     * -------------------------------------------------
+     * @desc A helper to freezeObj that recursively freezes all of its
+     *   properties.
+     * @param {(!Object|function)} obj - The object to freeze.
+     */
+    var deepFreeze = function(obj) {
+
+      /** @type {string} */
+      var prop;
+
+      Object.freeze(obj);
+
+      for (prop in obj) {
+        if (obj.hasOwnProperty(prop) && obj[ prop ] &&
+            (typeof obj[ prop ] === 'object' ||
+             typeof obj[ prop ] === 'function')) {
+          deepFreeze(obj[ prop ]);
+        }
+      }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // The End Of The freezeObj Module
+    ////////////////////////////////////////////////////////////////////////////
+
+    return freezeObj;
+
+  })();
