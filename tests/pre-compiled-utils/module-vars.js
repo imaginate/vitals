6b57a9fa9ae3da149a312f87202a5cabcb965a2c
Adam Smith <adamsmith@youlum.com>
2015-04-29 01:45:04 -0700

    Added the Module Vars

---
+++ tests/pre-compiled-utils/module-vars.js
@@ -0,0 +1,9 @@
+  /**
+   * -----------------------------------------------------
+   * Public Variable (utilsModuleAPI)
+   * -----------------------------------------------------
+   * @desc Holds the module's public properties and methods.
+   * @type {!Object<string, function>}
+   * @struct
+   */
+  var utilsModuleAPI = {};
