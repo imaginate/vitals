00633aedcf1703e5fbf55184578c0f1b01cc1333
Adam Smith <adamsmith@youlum.com>
2015-04-29 18:46:33 -0700

    Added a link to the src/methods in the Public API

--- src/pre-compiled-parts/public-api.js
+++ src/pre-compiled-parts/public-api.js
@@ -13,8 +13,8 @@
    * Global Object (aIV.utils)
    * ---------------------------------------------------
    * @desc Holds the public API for aIV's JavaScript shortcuts. For more
-   *   details on each of the methods see their definitions in the utils
-   *   module.
+   *   details on each of the methods see their complete [definitions in
+   *   the src]{@link https://github.com/imaginate/algorithmIV-javascript-shortcuts/tree/master/src/pre-compiled-parts/methods}.
    * @type {!{
    *   checkType        : function(*, string, boolean=): boolean,
    *   isValidTypeString: function(string): boolean,
