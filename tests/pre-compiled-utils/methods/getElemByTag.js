e2f5406c66d689ba2909988f3f7e382469f29ef7
Adam Smith <adamsmith@youlum.com>
2015-05-06 14:30:13 -0700

    Added the method getElemByTag

---
+++ tests/pre-compiled-utils/methods/getElemByTag.js
@@ -0,0 +1,48 @@
+  /**
+   * ---------------------------------------------------
+   * Public Method (utilsModuleAPI.getElemByTag)
+   * ---------------------------------------------------
+   * @desc A shortcut for the native DOM method -
+   *   [DOM Node].getElementsByTagName[ [index] ].
+   * @param {string} tag - The tag name of the element to select.
+   * @param {number=} index - The index of the array of found elements to
+   *   select. The default is 0.
+   * @param {(Document|Element)=} root - Limit the selections to this element's
+   *   children. The default is document or the element set with
+   *   aIV.utils.set({ getElemByTagRoot: [DOM Node] }).
+   * @return {HTMLElement} The selected DOM element.
+   */
+  utilsModuleAPI.getElemByTag = function(tag, index, root) {
+
+    /** @type {string} */
+    var errorMsg;
+    /** @type {Array<HTMLElement>} */
+    var elems;
+
+    if (!tag || typeof tag !== 'string') {
+      errorMsg = 'An aIV.utils.getElemByTag call received an invalid tag name ';
+      errorMsg += 'parameter.';
+      throw new TypeError(errorMsg);
+      return;
+    }
+
+    if (typeof index !== 'number' || index < -1) {
+      index = 0;
+    }
+    else {
+      Math.floor(index);
+    }
+
+    if (!root || typeof root !== 'object' ||
+        (!(root instanceof Element) && !(root instanceof Document))) {
+      root = defaults.getElemByTagRoot;
+    }
+
+    elems = root.getElementsByTagName(tag);
+
+    if (index < 0 || index >= elems.length) {
+      index = elems.length - 1;
+    }
+
+    return elems[ index ];
+  };
