9ed9352d36f1450af3a4da684ab157260529d264
Adam Smith <adamsmith@youlum.com>
2015-05-06 20:55:42 -0700

    Added Tests.makeElem

---
+++ tests/pre-compiled-tests/classes/tests/makeElem.js
@@ -0,0 +1,147 @@
+  /**
+   * -------------------------------------------------
+   * Public Method (Tests.makeElem)
+   * -------------------------------------------------
+   * @desc Tests the aIV.utils.makeElem method.
+   * @type {function}
+   */
+  Tests.makeElem = (function setupTests_makeElem() {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Private makeElem Variables
+    ////////////////////////////////////////////////////////////////////////////
+
+    /** @type {!TestResults} */
+    var results = new TestResults('makeElem', 3);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Public makeElem Method
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * -------------------------------------------------
+     * Public Method (makeElem)
+     * -------------------------------------------------
+     * @desc Tests the aIV.utils.makeElem method.
+     * @type {function}
+     */
+    var makeElem = function() {
+
+      testNoTag();
+      testOnlyTag();
+      testSettings();
+
+      // Save the results
+      app.results.push(results);
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Private makeElem Methods
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testNoTag)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testNoTag = function() {
+
+      /** @type {!HTMLElement} */
+      var elem;
+      /** @type {boolean} */
+      var pass;
+      /** @type {string} */
+      var errorMsg;
+
+      elem = aIV.utils.makeElem();
+
+      pass = (elem instanceof HTMLDivElement);
+
+      if (!pass) {
+        errorMsg = 'makeElem failed to create an element without a tag name';
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testOnlyTag)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testOnlyTag = function() {
+
+      /** @type {!HTMLElement} */
+      var elem;
+      /** @type {boolean} */
+      var pass;
+      /** @type {string} */
+      var errorMsg;
+
+      elem = aIV.utils.makeElem('span');
+
+      pass = (elem instanceof HTMLSpanElement);
+
+      if (!pass) {
+        errorMsg = 'makeElem failed to create an element with only a tag name';
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testSettings)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testSettings = function() {
+
+      /** @type {!HTMLElement} */
+      var elem;
+      /** @type {boolean} */
+      var pass;
+      /** @type {string} */
+      var text;
+      /** @type {string} */
+      var errorMsg;
+
+      elem = aIV.utils.makeElem({
+        tag : 'span',
+        text: 'makeElemTests',
+        id  : 'makeElemTests'
+      });
+      text = (!!elem.textContent) ? elem.textContent : elem.innerText;
+
+      pass = (elem instanceof HTMLSpanElement);
+      pass = pass && (text === 'makeElemTests');
+      pass = pass && (elem.id === 'makeElemTests');
+
+      elem = aIV.utils.makeElem({
+        tagName: 'p',
+        html   : 'makeElemTests'
+      });
+
+      pass = (elem instanceof HTMLParagraphElement);
+      pass = pass && (elem.innerHTML === 'makeElemTests');
+
+      elem = aIV.utils.makeElem({
+        className: 'makeElemTests'
+      });
+
+      pass = (elem instanceof HTMLDivElement);
+      pass = pass && (elem.className === 'makeElemTests');
+
+      if (!pass) {
+        errorMsg = 'makeElem failed to create an element with full settings';
+        results.addError(errorMsg);
+      }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // The End Of The makeElem Module
+    ////////////////////////////////////////////////////////////////////////////
+
+    return makeElem;
+
+  })();
