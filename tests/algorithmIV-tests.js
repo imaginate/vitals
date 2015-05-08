6ad05b7d8bb180d21031cb7eac82d44c7ee7077c
Adam Smith <adamsmith@youlum.com>
2015-05-08 06:48:11 -0700

    Added Tests.reset

--- tests/algorithmIV-tests.js
+++ tests/algorithmIV-tests.js
@@ -2921,6 +2921,139 @@
   })();
   /**
    * -------------------------------------------------
+   * Public Method (Tests.reset)
+   * -------------------------------------------------
+   * @desc Tests the aIV.utils.reset method.
+   * @type {function}
+   */
+  Tests.reset = (function setupTests_reset() {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Private reset Variables
+    ////////////////////////////////////////////////////////////////////////////
+
+    /** @type {!TestResults} */
+    var results = new TestResults('aIV.utils.reset', 4);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Public reset Method
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * -------------------------------------------------
+     * Public Method (reset)
+     * -------------------------------------------------
+     * @desc Tests the aIV.utils.reset method.
+     * @type {function}
+     */
+    var reset = function() {
+
+      testNoArgs();
+      testStringArg();
+      testArrArg();
+      testStringArgs();
+
+      // Save the results
+      app.results.push(results);
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Define & Setup The Private reset Methods
+    ////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testNoArgs)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testNoArgs = function() {
+
+      /** @type {string} */
+      var errorMsg;
+
+      try {
+        aIV.utils.reset();
+      }
+      catch (error) {
+        errorMsg = 'aIV.utils.reset() ';
+        errorMsg += 'failed. ' + error.toString;
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testStringArg)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testStringArg = function() {
+
+      /** @type {string} */
+      var errorMsg;
+
+      try {
+        aIV.utils.reset('checkArgsErrorMsg');
+      }
+      catch (error) {
+        errorMsg = 'aIV.utils.reset(\'checkArgsErrorMsg\') ';
+        errorMsg += 'failed. ' + error.toString;
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testArrArg)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testArrArg = function() {
+
+      /** @type {string} */
+      var errorMsg;
+
+      try {
+        aIV.utils.reset([ 'checkArgsErrorMsg', 'getElemByTagRoot' ]);
+      }
+      catch (error) {
+        errorMsg = 'aIV.utils.reset([ \'checkArgsErrorMsg\', ';
+        errorMsg += '\'getElemByTagRoot\' ]) failed. ' + error.toString;
+        results.addError(errorMsg);
+      }
+    };
+
+    /**
+     * ---------------------------------------------------
+     * Private Method (testStringArgs)
+     * ---------------------------------------------------
+     * @type {function}
+     */
+    var testStringArgs = function() {
+
+      /** @type {string} */
+      var errorMsg;
+
+      try {
+        aIV.utils.reset('checkArgsErrorMsg', 'getElemByTagRoot');
+      }
+      catch (error) {
+        errorMsg = 'aIV.utils.reset(\'checkArgsErrorMsg\', ';
+        errorMsg += '\'getElemByTagRoot\') failed. ' + error.toString;
+        results.addError(errorMsg);
+      }
+    };
+
+    ////////////////////////////////////////////////////////////////////////////
+    // The End Of The reset Module
+    ////////////////////////////////////////////////////////////////////////////
+
+    return reset;
+
+  })();
+  /**
+   * -------------------------------------------------
    * Public Method (Tests.set)
    * -------------------------------------------------
    * @desc Tests the aIV.utils.set method.
