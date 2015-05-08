e8ad7c74e0de7ecc7e357c53c6eb9dfb28f12f31
Adam Smith <adamsmith@youlum.com>
2015-05-07 23:54:14 -0700

    Added defaults.checkArgsErrorMsg

--- tests/pre-compiled-utils/module-vars.js
+++ tests/pre-compiled-utils/module-vars.js
@@ -13,10 +13,27 @@
    * Public Variable (defaults)
    * -----------------------------------------------------
    * @desc Holds each method's defaults.
-   * @type {!Object<string, *>}
+   * @type {!{
+   *   checkArgsErrorMsg  : (string|function),
+   *   getElemByClassRoot : !(Document|Element),
+   *   getElemsByClassRoot: !(Document|Element),
+   *   getElemByTagRoot   : !(Document|Element),
+   *   getElemsByTagRoot  : !(Document|Element)
+   * }}
    * @struct
    */
   var defaults = {
+    checkArgsErrorMsg  : function() {
+
+      /** @type {string} */
+      var msg;
+
+      msg = 'A ';
+      msg += utilsModuleAPI.checkArgs.caller || 'function';
+      msg +=' was called with an invalid parameter data type.';
+
+      return msg;
+    },
     getElemByClassRoot : document,
     getElemsByClassRoot: document,
     getElemByTagRoot   : document,
