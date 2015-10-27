331d6676ec01fa054e86ae4e65eb74781f01ce04
Adam Smith <adamsmith@algorithmiv.com>
2015-10-27 13:55:42 -0700

    Added js-shortcuts: cut.props & cut.str

--- src/parts/js-shortcuts/cut.js
+++ src/parts/js-shortcuts/cut.js
@@ -22,29 +22,120 @@ module.exports = cut;
 
 var is = require('node-are').is;
 var has = require('./has.js');
+var slice = require('./slice.js');
+var merge = require('./merge.js');
 
+
+/**
+ * Deletes properties from an object and returns them or deletes a pattern from
+ *   a string and returns the amended string. Note that if a source array and
+ *   numerical properties are provided splice is used on the array.
+ * @public
+ * @param {!(Object|function|Array|string)} source
+ * @param {!(string|RegExp)=} pattern - Use for a string source.
+ * @param {...*=} props - Use for an object source. Each array is considered an
+ *   array of props.
+ * @return {!(Object|string)} The values of the deleted properties or the new
+ *   string.
+ */
+function cut(source, pattern, props) {
+
+  if ( is.str(source) ) {
+    return cut.str(source, pattern);
+  }
+
+  if ( is._obj(obj) ) {
+    return cut.props.apply(null, arguments);
+  }
+
+  throw new TypeError('Invalid source param in vitals.cut call.');
+}
 
 /**
  * Deletes a property from an object and returns it.
  * @public
- * @param {!Object} obj
+ * @param {!(Object|function|Array)} obj
  * @param {*} prop
  * @return {*} The value of the deleted property.
  */
-function cut(obj, prop) {
+cut.property = function cutProperty(obj, prop) {
 
   /** @type {*} */
   var val;
 
   if ( !is._obj(obj) ) {
-    throw new TypeError('Invalid obj param in vitals.cutProp call.');
+    throw new TypeError('Invalid obj param in vitals.cut.property call.');
   }
 
   if ( !has(obj, prop) ) {
-    throw new TypeError('Missing prop in obj for vitals.cutProp call.');
+    throw new TypeError('No prop in obj for vitals.cut.property call.');
   }
 
   val = obj[prop];
-  delete obj[prop];
+  if ( is.arr(obj) && is.num(prop) ) {
+    obj.splice(prop, 1);
+  }
+  else {
+    delete obj[prop];
+  }
   return val;
-}
+};
+cut.prop = cut.property;
+
+/**
+ * Deletes properties from an object and returns an object with them.
+ * @public
+ * @param {!(Object|function|Array)} source
+ * @param {...*} props - Each array is considered an array of props.
+ * @return {!Object} The deleted properties and their values.
+ */
+cut.properties = function cutProperties(source, props) {
+
+  /** @type {string} */
+  var prop;
+  /** @type {!Object} */
+  var obj;
+  /** @type {number} */
+  var i;
+
+  if ( !is._obj(source) ) {
+    throw new TypeError('Invalid source param in vitals.cut.properties call.');
+  }
+
+  props = slice(arguments, 1);
+  obj = {};
+  i = props.length;
+  while (i--) {
+    prop = props[i];
+    if ( is.arr(prop) ) {
+      prop = cut.props(source, prop);
+      obj = merge(obj, prop);
+    }
+    else {
+      obj[prop] = cut.prop(source, prop);
+    }
+  }
+  return obj;
+};
+cut.props = cut.properties;
+
+/**
+ * Deletes a pattern from a string and returns the amended string.
+ * @public
+ * @param {string} source
+ * @param {!(string|RegExp)} pattern
+ * @return {string} The new string.
+ */
+cut.string = function cutString(source, pattern) {
+
+  if ( !is.str(source) ) {
+    throw new TypeError('Invalid source param in vitals.cut.string call.');
+  }
+
+  if ( !is('!str|regex', pattern) ) {
+    throw new TypeError('Invalid pattern param in vitals.cut.string call.');
+  }
+
+  return source.replace(pattern, '');
+};
+cut.str = cut.string;
