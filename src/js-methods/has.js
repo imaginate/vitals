bd44e2d61993aa635a85b9343f56912fa01fc620
Adam Smith <adamsmith@algorithmiv.com>
2015-10-29 17:53:18 -0700

    Rebuilt js-method: has

--- src/js-methods/has.js
+++ src/js-methods/has.js
@@ -25,61 +25,199 @@ var is = require('node-are').is;
 // HAS
 ////////////////////////////////////////////////////////////////////////////////
 
-/**
- * A shortcut for Object.prototype.hasOwnProperty that accepts null objects or a
- *   shortcut for String.prototype.includes and RegExp.prototype.test.
- * @public
- * @param {?(Object|function|string)} source
- * @param {*} prop
- * @return {boolean}
- */
-var has = (function() {
+var has = (function hasPrivateScope() {
 
   /**
+   * A shortcut for Object.prototype.hasOwnProperty (that accepts null),
+   *   String.prototype.includes, RegExp.prototype.test, and
+   *   Array.prototype.includes.
    * @public
-   * @param {?(Object|function|string)} source
-   * @param {*} prop
+   * @param {?(Object|function|string|Array)} source
+   * @param {*} key - If source is a string the following two statements apply:
+   *   For a RegExp key the source is tested for the RegExp pattern. Otherwise
+   *   the source is searched for a substring of the string-converted key.
+   *   If source is an array or arguments object the key is searched for in the
+   *   object's indexed values.
    * @return {boolean}
    */
-  function has(source, prop) {
+  function has(source, key) {
 
-    if (!source) {
-      if ( !is('?str', source) ) {
-        throw new TypeError('Invalid source param in vitals.has call.');
-      }
-      return false;
-    }
+    if (arguments.length < 2) throw _error('No key defined');
+    
+    if ( is.null(source) ) return false;
 
     if ( is.str(source) ) {
-
-      if ( is.str(prop) ) {
-        return HAS_STR_INCLUDES ?
-          source.includes(prop) : source.indexOf(prop) !== -1;
-      }
-
-      if ( is.regex(prop) ) {
-        return prop.test(source);
-      }
-
-      throw new TypeError('Invalid prop param in vitals.has call.');
+      if (!source) return !String(key);
+      if ( is.regex(key) ) return _hasPattern(source, key);
+      key = String(key);
+      return _hasStr(source, key);
     }
 
-    if ( is._obj(source) ) {
-      prop = String(prop);
-      return 'hasOwnProperty' in source ?
-        source.hasOwnProperty(prop) : prop in source;
-    }
+    if ( !is._obj(source) ) throw _typeError('source');
 
-    throw new TypeError('Invalid source param in vitals.has call.');
+    return is._arr(source) ? _hasValArr(source, key) : _hasKey(source, key);
   }
 
   /**
-   * Feature detect for String.prototype.includes.
-   * @private
-   * @type {boolean}
-   * @const
+   * A shortcut for Object.prototype.hasOwnProperty that accepts null.
+   * @public
+   * @param {?(Object|function)} source
+   * @param {*} key
+   * @return {boolean}
    */
-  var HAS_STR_INCLUDES = !!String.prototype.includes;
+  has.key = function hasKey(source, key) {
+
+    if (arguments.length < 2) throw _error('No key defined', 'key');
+
+    if ( is.null(source) ) return false;
+
+    if ( !is._obj(source) ) throw _typeError('source', 'key');
+
+    return _hasKey(source, key);
+  }
+
+  /**
+   * A shortcut that checks for a value in an object.
+   * @public
+   * @param {?(Object|function)} source
+   * @param {*} val
+   * @return {boolean}
+   */
+  has.value = function hasValue(source, val) {
+
+    if (arguments.length < 2) throw _error('No val defined', 'value');
+
+    if ( is.null(source) ) return false;
+
+    if ( !is._obj(source) ) throw _typeError('source', 'value');
+
+    return is._arr(source) ? _hasValArr(source, val) : _hasVal(source, val);
+  }
+  // define shorthand
+  has.val = has.value;
+
+  /**
+   * A shortcut for String.prototype.includes and RegExp.prototype.test.
+   * @public
+   * @param {string} source
+   * @param {*} pattern
+   * @return {boolean}
+   */
+  has.pattern = function hasPattern(source, pattern) {
+
+    if ( !is.str(source) ) throw _typeError('source', 'pattern');
+    if (arguments.length < 2) throw _error('No pattern defined', 'pattern');
+
+    if (!source) return !String(pattern);
+    if ( is.regex(pattern) ) return _hasPattern(source, pattern);
+    pattern = String(pattern);
+    return _hasStr(source, pattern);
+  }
+  // define shorthand
+  has.string = has.pattern;
+  has.str = has.pattern;
+
+  /**
+   * @private
+   * @param {!(Object|function)} source
+   * @param {*} key
+   * @return {boolean}
+   */
+  function _hasKey(source, key) {
+    return _hasOwnProperty.call(source, key);
+  }
+
+  /**
+   * @private
+   * @param {!(Object|function)} source
+   * @param {*} val
+   * @return {boolean}
+   */
+  function _hasVal(source, val) {
+
+    /** @type {string} */
+    var key;
+
+    for (key in source) {
+      if ( _hasKey(source, key) && source[key] === val ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @private
+   * @param {!Object} source
+   * @param {*} val
+   * @return {boolean}
+   */
+  function _hasValArr(source, val) {
+
+    /** @type {number} */
+    var len;
+    /** @type {number} */
+    var i;
+
+    len = source.length;
+    i = -1;
+    while (++i < len) {
+      if (source[i] === val) return true;
+    }
+    return false;
+  }
+
+  /**
+   * @private
+   * @param {string} source
+   * @param {!RegExp} pattern
+   * @return {boolean}
+   */
+  function _hasPattern(source, pattern) {
+    return pattern.test(source);
+  }
+
+  /**
+   * @private
+   * @param {string} source
+   * @param {*} str
+   * @return {boolean}
+   */
+  var _hasStr = !!String.prototype.includes
+    ? function _hasStr(source, str) { return source.includes(str); }
+    : function _hasStr(source, str) { return source.indexOf(str) !== -1; };
+
+  /**
+   * @private
+   * @param {*} key
+   * @return {boolean}
+   */
+  var _hasOwnProperty = Object.prototype.hasOwnProperty;
+
+  /**
+   * @private
+   * @param {string} param
+   * @param {string=} method
+   * @return {!TypeError} 
+   */
+  function _typeError(param, method) {
+    param += ' param';
+    method = method || '';
+    method = 'vitals.has' + ( method && '.' ) + method;
+    return new TypeError('Invalid ' + param + ' in ' + method + ' call.');
+  }
+
+  /**
+   * @private
+   * @param {string} msg
+   * @param {string=} method
+   * @return {!Error} 
+   */
+  function _error(msg, method) {
+    method = method || '';
+    method = 'vitals.has' + ( method && '.' ) + method;
+    return new Error(msg + ' for ' + method + ' call.');
+  }
 
   // END OF PRIVATE SCOPE FOR HAS
   return has;
