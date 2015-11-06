067b04a3249519834e6a680ac4f225199d0474f6
Adam Smith <adamsmith@algorithmiv.com>
2015-11-06 12:47:16 -0800

    Added vitals.js source file

---
+++ src/vitals.js
@@ -0,0 +1,18 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS
+ * -----------------------------------------------------------------------------
+ * @version 0.1.0
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
