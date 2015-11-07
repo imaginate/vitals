bf33a963216820cf6453812bee316298f10365c0
Adam Smith <adamsmith@algorithmiv.com>
2015-11-06 18:19:20 -0800

    Added the test setups

---
+++ test/base-setup.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS TESTS: BASE SETUP
+ * -----------------------------------------------------------------------------
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+// appends global helpers
+// see ../helpers/setup.js
+require('../helpers/setup')('file-system');
+
+/**
+ * @global
+ * @type {!Object}
+ */
+global.vitals = {};
+
+each(retrieve.dirpaths('test'), function(section) {
+  each(retrieve.filepaths('src/' + section, {
+    invalidNames: '_*|index',
+    validExts:    'js'
+  }), function(method) {
+    method = method.replace(/\.js$/, '');
+    global.vitals[method] = require('../src/' + section + '/' + method);
+  });
+});
---
+++ test/node-setup.js
@@ -0,0 +1,23 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS TESTS: ORG SETUP
+ * -----------------------------------------------------------------------------
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+// appends global helpers
+// see ../helpers/setup.js
+require('../helpers/setup')();
+
+// appends vitals to global
+require('../src/node-vitals.js')(1, 'all');
