a6df5b475185c369712da2b4727aea11d3996379
Adam Smith <adamsmith@algorithmiv.com>
2015-11-13 14:51:59 -0800

    Compiled & minified src with new helpers

--- src/js-methods/index.js
+++ src/js-methods/index.js
@@ -27,20 +27,51 @@ var is = require('node-are').is;
 
 
 ////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPER - IN-STR
+////////////////////////////////////////////////////////////////////////////////
+
+var _inStr = (function _inStrPrivateScope() {
+
+  /**
+   * A shortcut for String.prototype.includes.
+   * @param {string} source
+   * @param {*} str
+   * @return {boolean}
+   */
+  function _inStr(source, str) {
+    str = String(str);
+    if (!source) return !str;
+    if (!str) return true;
+    return stringIncludes(source, str);
+  }
+
+  /**
+   * @private
+   * @param {string} source
+   * @param {string} str
+   * @return {boolean}
+   */
+  var stringIncludes = !!String.prototype.includes
+    ? function stringIncludes(source, str) {
+        return source.includes(str);
+      }
+    : function stringIncludes(source, str) {
+        return source.indexOf(str) !== -1;
+      };
+
+  //////////////////////////////////////////////////////////
+  // END OF PRIVATE SCOPE FOR IN-STR
+  return _inStr;
+})();
+
+
+////////////////////////////////////////////////////////////////////////////////
 // PRIVATE HELPER - OWN
 ////////////////////////////////////////////////////////////////////////////////
 
 var _own = (function _ownPrivateScope() {
 
   /**
-   * @private
-   * @param {*} key
-   * @return {boolean}
-   */
-  var _hasOwnProperty = Object.prototype.hasOwnProperty;
-
-  /**
-   * @private
    * @param {?(Object|function)} source
    * @param {*} key
    * @return {boolean}
@@ -49,238 +80,16 @@ var _own = (function _ownPrivateScope() {
     return !!source && _hasOwnProperty.call(source, key);
   }
 
+  /**
+   * @private
+   * @param {*} key
+   * @return {boolean}
+   */
+  var _hasOwnProperty = Object.prototype.hasOwnProperty;
 
   //////////////////////////////////////////////////////////
   // END OF PRIVATE SCOPE FOR OWN
   return _own;
-})();
-
-
-////////////////////////////////////////////////////////////////////////////////
-// HAS
-////////////////////////////////////////////////////////////////////////////////
-
-var has = (function hasPrivateScope() {
-
-  //////////////////////////////////////////////////////////
-  // PUBLIC METHODS
-  // - has
-  // - has.key
-  // - has.value     (has.val)
-  // - has.pattern
-  // - has.substring (has.substr)
-  //////////////////////////////////////////////////////////
-
-  /**
-   * A shortcut for Object.prototype.hasOwnProperty (that accepts null),
-   *   String.prototype.includes, RegExp.prototype.test, and
-   *   Array.prototype.includes.
-   * @public
-   * @param {?(Object|function|string|Array)} source
-   * @param {*} key - If source is a string the following two statements apply:
-   *   For a RegExp key the source is tested for the RegExp pattern. Otherwise
-   *   the source is searched for a substring of the string-converted key.
-   *   If source is an array or arguments object the key is searched for in the
-   *   object's indexed values.
-   * @return {boolean}
-   */
-  function has(source, key) {
-
-    if (arguments.length < 2) throw _error('No key defined');
-    
-    if ( is.null(source) ) return false;
-
-    if ( is.str(source) ) return _hasPattern(source, key);
-
-    if ( !is._obj(source) ) throw _error.type('source');
-
-    return is._arr(source) ? _hasVal(source, key) : _hasKey(source, key);
-  }
-
-  /**
-   * A shortcut for Object.prototype.hasOwnProperty that accepts null.
-   * @public
-   * @param {?(Object|function)} source
-   * @param {*} key
-   * @return {boolean}
-   */
-  has.key = function hasKey(source, key) {
-
-    if (arguments.length < 2) throw _error('No key defined', 'key');
-
-    if ( is.null(source) ) return false;
-
-    if ( !is._obj(source) ) throw _error.type('source', 'key');
-
-    return _hasKey(source, key);
-  };
-
-  /**
-   * A shortcut that checks for a value in an object.
-   * @public
-   * @param {?(Object|function)} source
-   * @param {*} val
-   * @return {boolean}
-   */
-  has.value = function hasValue(source, val) {
-
-    if (arguments.length < 2) throw _error('No val defined', 'value');
-
-    if ( is.null(source) ) return false;
-
-    if ( !is._obj(source) ) throw _error.type('source', 'value');
-
-    return _hasVal(source, val);
-  };
-  // define shorthand
-  has.val = has.value;
-
-  /**
-   * A shortcut for String.prototype.includes and RegExp.prototype.test.
-   * @public
-   * @param {string} source
-   * @param {*} pattern
-   * @return {boolean}
-   */
-  has.pattern = function hasPattern(source, pattern) {
-
-    if ( !is.str(source) ) throw _error.type('source', 'pattern');
-    if (arguments.length < 2) throw _error('No pattern defined', 'pattern');
-
-    return _hasPattern(source, pattern);
-  };
-
-  /**
-   * A shortcut for String.prototype.includes.
-   * @public
-   * @param {string} source
-   * @param {*} str
-   * @return {boolean}
-   */
-  has.substring = function hasSubstring(source, str) {
-
-    if ( !is.str(source) ) throw _error.type('source', 'substring');
-    if (arguments.length < 2) throw _error('No str defined', 'substring');
-
-    return _hasSubstr(source, str);
-  };
-  // define shorthand
-  has.substr = has.substring;
-
-  //////////////////////////////////////////////////////////
-  // PRIVATE METHODS - MAIN
-  //////////////////////////////////////////////////////////
-
-  /**
-   * @private
-   * @param {!(Object|function)} source
-   * @param {*} key
-   * @return {boolean}
-   */
-  function _hasKey(source, key) {
-    return _hasOwnProperty.call(source, key);
-  }
-
-  /**
-   * @private
-   * @param {!(Object|function)} source
-   * @param {*} val
-   * @return {boolean}
-   */
-  function _hasVal(source, val) {
-    return is._arr(source) ? _valInArr(source, val) : _valInObj(source, val);
-  }
-
-  /**
-   * @private
-   * @param {string} source
-   * @param {*} pattern
-   * @return {boolean}
-   */
-  function _hasPattern(source, pattern) {
-    return is.regex(pattern)
-      ? pattern.test(source)
-      : _hasSubstr(source, pattern);
-  }
-
-  /**
-   * @private
-   * @param {string} source
-   * @param {*} str
-   * @return {boolean}
-   */
-  function _hasSubstr(source, str) {
-    str = String(str);
-    if (!source) return !str;
-    if (!str) return true;
-    return _strInStr(source, str);
-  }
-
-  //////////////////////////////////////////////////////////
-  // PRIVATE METHODS - VAL IN TESTS
-  //////////////////////////////////////////////////////////
-
-  /**
-   * @private
-   * @param {!(Object|function)} source
-   * @param {*} val
-   * @return {boolean}
-   */
-  function _valInObj(source, val) {
-
-    /** @type {string} */
-    var key;
-
-    for (key in source) {
-      if ( _own(source, key) && source[key] === val ) return true;
-    }
-    return false;
-  }
-
-  /**
-   * @private
-   * @param {!Object} source
-   * @param {*} val
-   * @return {boolean}
-   */
-  function _valInArr(source, val) {
-
-    /** @type {number} */
-    var len;
-    /** @type {number} */
-    var i;
-
-    len = source.length;
-    i = -1;
-    while (++i < len) {
-      if (source[i] === val) return true;
-    }
-    return false;
-  }
-
-  /**
-   * @private
-   * @param {string} source
-   * @param {*} str
-   * @return {boolean}
-   */
-  var _strInStr = !!String.prototype.includes
-    ? function _strInStr(source, str) { return source.includes(str); }
-    : function _strInStr(source, str) { return source.indexOf(str) !== -1; };
-
-  //////////////////////////////////////////////////////////
-  // PRIVATE METHODS - GENERAL
-  //////////////////////////////////////////////////////////
-
-  /**
-   * @private
-   * @type {!ErrorAid}
-   */
-  var _error = makeErrorAid('has');
-
-  //////////////////////////////////////////////////////////
-  // END OF PRIVATE SCOPE FOR HAS
-  return has;
 })();
 
 
@@ -680,7 +489,7 @@ var clone = (function clonePrivateScope() {
 
     if ( is.undefined(forceGlobal) ) return flags;
 
-    return has(flags, 'g')
+    return _inStr(flags, 'g')
       ? forceGlobal
         ? flags
         : flags.replace('g', '')
@@ -810,7 +619,7 @@ var amend = (function amendPrivateScope() {
 
     if ( !is.obj(obj) ) throw _error.type('obj');
 
-    if ( is.str(props) ) props = _splitProps(props);
+    if ( is.str(props) ) props = _splitKeys(props);
 
     if ( !is.obj(props) ) throw _error.type('props');
 
@@ -971,7 +780,7 @@ var amend = (function amendPrivateScope() {
 
     if ( !is.obj(obj) ) throw _error.type('obj', 'properties');
 
-    if ( is.str(props) ) props = _splitProps(props);
+    if ( is.str(props) ) props = _splitKeys(props);
 
     if ( !is.obj(props) ) throw _error.type('props', 'properties');
 
@@ -1056,24 +865,6 @@ var amend = (function amendPrivateScope() {
     }
 
     return [ val, descriptor, staticType, setter ];
-  }
-
-  /**
-   * @private
-   * @param {string} keys - One of the chars in the following list is used as
-   *   the separator (chars listed in order of use):  ", "  ","  "|"  " "
-   * @return {!Array<string>}
-   */
-  function _splitProps(keys) {
-
-    /** @type {string} */
-    var separator;
-
-    separator = _match(keys, ', ')
-      ? ', '  : _match(keys, ',')
-        ? ',' : _match(keys, '|')
-          ? '|' : ' ';
-    return keys.split(separator);
   }
 
   /**
@@ -1625,14 +1416,6 @@ var amend = (function amendPrivateScope() {
 
   /**
    * @private
-   * @param {string} source
-   * @param {*} pattern
-   * @return {boolean}
-   */
-  var _match = has.pattern;
-
-  /**
-   * @private
    * @type {!ErrorAid}
    */
   var _error = makeErrorAid('amend');
@@ -1675,7 +1458,7 @@ var create = (function createPrivateScope() {
 
     if ( !is('?obj', proto) ) throw _error.type('proto');
 
-    if ( is.str(props) ) props = _splitProps(props);
+    if ( is.str(props) ) props = _splitKeys(props);
 
     if ( !is('!obj=', props)      ) throw _error.type('props');
     if ( !is('!obj=', descriptor) ) throw _error.type('descriptor');
@@ -1703,7 +1486,7 @@ var create = (function createPrivateScope() {
 
     if ( !is('?obj', proto) ) throw _error.type('proto', 'object');
 
-    if ( is.str(props) ) props = _splitProps(props);
+    if ( is.str(props) ) props = _splitKeys(props);
 
     if ( !is('!obj=', props)      ) throw _error.type('props',      'object');
     if ( !is('!obj=', descriptor) ) throw _error.type('descriptor', 'object');
@@ -1716,21 +1499,6 @@ var create = (function createPrivateScope() {
   //////////////////////////////////////////////////////////
   // PRIVATE METHODS - MAIN
   //////////////////////////////////////////////////////////
-
-  /**
-   * @private
-   * @param {string} props - One of the chars in the following list is used as
-   *   the separator (chars listed in order of use):  ", "  ","  "|"  " "
-   * @return {!Array<string>}
-   */
-  function _splitProps(props) {
-    return props.split(
-      has(props, ', ')
-        ? ', ' : has(props, ',')
-          ? ',' : has(props, '|')
-            ? '|' : ' '
-    );
-  }
 
   /**
    * @private
@@ -2795,14 +2563,6 @@ var cut = (function cutPrivateScope() {
 
   /**
    * @private
-   * @param {string} source
-   * @param {*} pattern
-   * @return {boolean}
-   */
-  var _match = has.pattern;
-
-  /**
-   * @private
    * @type {!ErrorAid}
    */
   var _error = makeErrorAid('cut');
@@ -3190,7 +2950,7 @@ var fill = (function fillPrivateScope() {
     /** @type {number} */
     var i;
 
-    keys = _split(keys);
+    keys = _splitKeys(keys);
     len = keys.length;
     i = -1;
     while (++i < len) {
@@ -3258,31 +3018,6 @@ var fill = (function fillPrivateScope() {
   //////////////////////////////////////////////////////////
   // PRIVATE METHODS - GENERAL
   //////////////////////////////////////////////////////////
-
-  /**
-   * @private
-   * @param {string} keys
-   * @return {!Array<string>}
-   */
-  function _split(keys) {
-
-    /** @type {string} */
-    var separator;
-
-    separator = _match(keys, ', ')
-      ? ', '  : _match(keys, ',')
-        ? ',' : _match(keys, '|')
-          ? '|' : ' ';
-    return keys.split(separator);
-  }
-
-  /**
-   * @private
-   * @param {string} source
-   * @param {*} pattern
-   * @return {boolean}
-   */
-  var _match = has.pattern;
 
   /**
    * @private
@@ -4117,14 +3852,6 @@ var get = (function getPrivateScope() {
 
   /**
    * @private
-   * @param {string} source
-   * @param {*} pattern
-   * @return {boolean}
-   */
-  var _match = has.pattern;
-
-  /**
-   * @private
    * @type {!ErrorAid}
    */
   var _error = makeErrorAid('get');
@@ -4132,6 +3859,133 @@ var get = (function getPrivateScope() {
   //////////////////////////////////////////////////////////
   // END OF PRIVATE SCOPE FOR GET
   return get;
+})();
+
+
+////////////////////////////////////////////////////////////////////////////////
+// HAS
+////////////////////////////////////////////////////////////////////////////////
+
+var has = (function hasPrivateScope() {
+
+  //////////////////////////////////////////////////////////
+  // PUBLIC METHODS
+  // - has
+  // - has.key
+  // - has.value     (has.val)
+  // - has.pattern
+  // - has.substring (has.substr)
+  //////////////////////////////////////////////////////////
+
+  /**
+   * A shortcut for Object.prototype.hasOwnProperty (that accepts null),
+   *   String.prototype.includes, RegExp.prototype.test, and
+   *   Array.prototype.includes.
+   * @public
+   * @param {?(Object|function|string|Array)} source
+   * @param {*} key - If source is a string the following two statements apply:
+   *   For a RegExp key the source is tested for the RegExp pattern. Otherwise
+   *   the source is searched for a substring of the string-converted key.
+   *   If source is an array or arguments object the key is searched for in the
+   *   object's indexed values.
+   * @return {boolean}
+   */
+  function has(source, key) {
+
+    if (arguments.length < 2) throw _error('No key defined');
+    
+    if ( is.null(source) ) return false;
+
+    if ( is.str(source) ) return _match(source, key);
+
+    if ( !is._obj(source) ) throw _error.type('source');
+
+    return is._arr(source) ? _inArr(source, key) : _own(source, key);
+  }
+
+  /**
+   * A shortcut for Object.prototype.hasOwnProperty that accepts null.
+   * @public
+   * @param {?(Object|function)} source
+   * @param {*} key
+   * @return {boolean}
+   */
+  has.key = function hasKey(source, key) {
+
+    if (arguments.length < 2) throw _error('No key defined', 'key');
+
+    if ( is.null(source) ) return false;
+
+    if ( !is._obj(source) ) throw _error.type('source', 'key');
+
+    return _own(source, key);
+  };
+
+  /**
+   * A shortcut that checks for a value in an object.
+   * @public
+   * @param {?(Object|function)} source
+   * @param {*} val
+   * @return {boolean}
+   */
+  has.value = function hasValue(source, val) {
+
+    if (arguments.length < 2) throw _error('No val defined', 'value');
+
+    if ( is.null(source) ) return false;
+
+    if ( !is._obj(source) ) throw _error.type('source', 'value');
+
+    return is._arr(source) ? _inArr(source, val) : _inObj(source, val);
+  };
+  // define shorthand
+  has.val = has.value;
+
+  /**
+   * A shortcut for String.prototype.includes and RegExp.prototype.test.
+   * @public
+   * @param {string} source
+   * @param {*} pattern
+   * @return {boolean}
+   */
+  has.pattern = function hasPattern(source, pattern) {
+
+    if ( !is.str(source) ) throw _error.type('source', 'pattern');
+    if (arguments.length < 2) throw _error('No pattern defined', 'pattern');
+
+    return _match(source, pattern);
+  };
+
+  /**
+   * A shortcut for String.prototype.includes.
+   * @public
+   * @param {string} source
+   * @param {*} str
+   * @return {boolean}
+   */
+  has.substring = function hasSubstring(source, str) {
+
+    if ( !is.str(source) ) throw _error.type('source', 'substring');
+    if (arguments.length < 2) throw _error('No str defined', 'substring');
+
+    return _inStr(source, str);
+  };
+  // define shorthand
+  has.substr = has.substring;
+
+  //////////////////////////////////////////////////////////
+  // PRIVATE METHODS - GENERAL
+  //////////////////////////////////////////////////////////
+
+  /**
+   * @private
+   * @type {!ErrorAid}
+   */
+  var _error = makeErrorAid('has');
+
+  //////////////////////////////////////////////////////////
+  // END OF PRIVATE SCOPE FOR HAS
+  return has;
 })();
 
 
@@ -4914,6 +4768,85 @@ function makeErrorAid(vitalsMethod) {
   };
 
   return errorAid;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPER - IN-OBJ
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {!(Object|function)} source
+ * @param {*} val
+ * @return {boolean}
+ */
+function _inObj(source, val) {
+
+  /** @type {string} */
+  var key;
+
+  for (key in source) {
+    if ( _own(source, key) && source[key] === val ) return true;
+  }
+  return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPER - IN-ARR
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {!Object} source
+ * @param {*} val
+ * @return {boolean}
+ */
+function _inArr(source, val) {
+
+  /** @type {number} */
+  var len;
+  /** @type {number} */
+  var i;
+
+  len = source.length;
+  i = -1;
+  while (++i < len) {
+    if (source[i] === val) return true;
+  }
+  return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPER - MATCH
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * A shortcut for String.prototype.includes and RegExp.prototype.test.
+ * @param {string} source
+ * @param {*} pattern
+ * @return {boolean}
+ */
+function _match(source, pattern) {
+  return is.regex(pattern) ? pattern.test(source) : _inStr(source, pattern);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPER - SPLIT-KEYS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @param {string} keys - One of the chars in the following list is used as
+ *   the separator (chars listed in order of use):  ", "  ","  "|"  " "
+ * @return {!Array<string>}
+ */
+function _splitKeys(keys) {
+
+  /** @type {string} */
+  var separator;
+
+  separator = _inStr(keys, ', ')
+    ? ', '  : _inStr(keys, ',')
+      ? ',' : _inStr(keys, '|')
+        ? '|' : ' ';
+  return keys.split(separator);
 }
 
 
