3663363462d6f3ae706e01f7ded198720ec46708
Adam Smith <adamsmith@algorithmiv.com>
2015-11-13 12:27:24 -0800

    Added unit tests for: clone.object

--- test/js-methods/clone/clone.object.js
+++ test/js-methods/clone/clone.object.js
@@ -21,9 +21,57 @@ describe('clone.object', function() {
   //////////////////////////////////////////////
   // CLONE TESTS
 
-  title = callStr({});
+  title = callStr({ a: 1, b: { b: 2 }, c: 3 });
   it(title, function() {
-    assert(true);
+    var obj;
+    var copy;
+    obj = freeze({ a: 1, b: { b: 2 }, c: 3 }, true);
+    copy = vitals.clone.obj(obj);
+    assert(obj !== copy);
+    each(obj, function(val, key) {
+      assert( obj[key] === copy[key] );
+    });
+  });
+
+  title = callStr({ a: 1, b: { b: 2 }, c: 3 }, true);
+  it(title, function() {
+    var obj;
+    var copy;
+    obj = freeze({ a: 1, b: { b: 2 }, c: 3 }, true);
+    copy = vitals.clone.obj(obj, true);
+    assert(obj !== copy);
+    assert(obj.a === copy.a);
+    assert(obj.b !== copy.b);
+    assert(obj.c === copy.c);
+  });
+
+  title = callStr({ a: 1, b: { b: 2 }, c: 3 }, false);
+  it(title, function() {
+    var obj;
+    var copy;
+    obj = freeze({ a: 1, b: { b: 2 }, c: 3 }, true);
+    copy = vitals.clone.obj(obj, false);
+    assert(obj !== copy);
+    each(obj, function(val, key) {
+      assert( obj[key] === copy[key] );
+    });
+  });
+
+  //////////////////////////////////////////////
+  // ERROR TESTS
+
+  title = callStr(null);
+  it(title, function() {
+    assert.throws(function() {
+      vitals.clone.obj(null);
+    });
+  });
+
+  title = callStr({}, 'fail');
+  it(title, function() {
+    assert.throws(function() {
+      vitals.clone.obj({}, 'fail');
+    });
   });
 
 });
@@ -39,5 +87,5 @@ describe('clone.object', function() {
  */
 function callStr(args) {
   args = slice(arguments);
-  return testCall('clone.object', args, 3, true);
+  return testCall('clone.obj', args, 3, true);
 }
