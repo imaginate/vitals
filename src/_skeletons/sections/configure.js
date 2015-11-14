e602423aea44d8187bc5815fe0a3ab0b119a072d
Adam Smith <adamsmith@algorithmiv.com>
2015-11-14 13:09:48 -0800

    Added the section skeletons

---
+++ src/_skeletons/sections/configure.js
@@ -0,0 +1,59 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS JS - NODE VERSION - CONFIGURE JS METHODS
+ * -----------------------------------------------------------------------------
+ * @file A JavaScript utility library designed for simplicity, readability,
+ *   elegance, performance, and reliability. The configure section includes
+ *   methods for configuring objects and object properties as allowed in ES5+.
+ * @version 2.0.0
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var is = require('node-are').is;
+
+
+// *****************************************************************************
+// PRIVATE HELPERS
+// *****************************************************************************
+
+// INSERT _helpers/own.js
+// INSERT _helpers/inStr.js
+// INSERT _helpers/match.js
+// INSERT _helpers/merge.js
+// INSERT _helpers/cloneObj.js
+// INSERT _helpers/errorAid.js
+// INSERT _helpers/splitKeys.js
+
+
+// *****************************************************************************
+// SECTION: CONFIGURE JS METHODS
+// *****************************************************************************
+
+// INSERT methods/amend.js
+// INSERT methods/create.js
+// INSERT methods/freeze.js
+// INSERT methods/seal.js
+
+
+// *****************************************************************************
+// SECTION: END
+// *****************************************************************************
+
+module.exports = {
+  amend:  amend,
+  create: create,
+  freeze: freeze,
+  seal:   seal
+};
--- src/_skeletons/sections/js.js
+++ src/_skeletons/sections/js.js
@@ -1,7 +1,9 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS - JS METHODS
+ * VITALS JS - NODE VERSION - ALL JS METHODS
  * -----------------------------------------------------------------------------
+ * @file A JavaScript utility library designed for simplicity, readability,
+ *   elegance, performance, and reliability.
  * @version 2.0.0
  * @see [vitals]{@link https://github.com/imaginate/vitals}
  *
@@ -22,33 +24,49 @@ var is = require('node-are').is;
 
 
 // *****************************************************************************
-// SECTION: JS METHODS
+// PRIVATE HELPERS
 // *****************************************************************************
 
-// INSERT _helpers/inStr.js
 // INSERT _helpers/own.js
+// INSERT _helpers/inObj.js
+// INSERT _helpers/inArr.js
+// INSERT _helpers/inStr.js
+// INSERT _helpers/match.js
+// INSERT _helpers/merge.js
+// INSERT _helpers/cloneObj.js
+// INSERT _helpers/errorAid.js
+// INSERT _helpers/splitKeys.js
+
+
+// *****************************************************************************
+// SECTION: BASE JS METHODS
+// *****************************************************************************
+
 // INSERT methods/slice.js
 // INSERT methods/clone.js
-// INSERT methods/amend.js
-// INSERT methods/create.js
 // INSERT methods/cut.js
 // INSERT methods/each.js
 // INSERT methods/fill.js
-// INSERT methods/freeze.js
 // INSERT methods/fuse.js
 // INSERT methods/get.js
 // INSERT methods/has.js
 // INSERT methods/remap.js
-// INSERT methods/seal.js
 // INSERT methods/until.js
-// INSERT _helpers/errorAid.js
-// INSERT _helpers/inObj.js
-// INSERT _helpers/inArr.js
-// INSERT _helpers/match.js
-// INSERT _helpers/merge.js
-// INSERT _helpers/cloneObj.js
-// INSERT _helpers/splitKeys.js
 
+
+// *****************************************************************************
+// SECTION: CONFIGURE JS METHODS
+// *****************************************************************************
+
+// INSERT methods/amend.js
+// INSERT methods/create.js
+// INSERT methods/freeze.js
+// INSERT methods/seal.js
+
+
+// *****************************************************************************
+// SECTION: END
+// *****************************************************************************
 
 module.exports = {
   amend:  amend,
