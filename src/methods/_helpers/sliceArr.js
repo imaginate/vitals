bc57716f56bf338f84775451e9a006a05c327c13
Adam Smith <adamsmith@algorithmiv.com>
2015-11-16 17:46:34 -0800

    Fixed end parse bug in vitals helper: _sliceArr

--- src/methods/_helpers/sliceArr.js
+++ src/methods/_helpers/sliceArr.js
@@ -45,14 +45,15 @@ function _sliceArr(source, start, end) {
   len = source.length;
   start = start
     ? start < 0
-      ? len + start : start
+      ? len + start
+      : start
     : 0;
   start = start < 0 ? 0 : start;
-  end = end
-    ? end > len
-      ? len : end < 0
-        ? len + end : end
-    : 0;
+  end = is.undefined(end) || end > len
+    ? len
+    : end < 0
+        ? len + end
+        : end;
 
   if (start >= end) return [];
 
