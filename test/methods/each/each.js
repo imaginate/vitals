8325bf1341269933b471e4f3c8fc677585b89d77
Adam Smith <adamsmith@algorithmiv.com>
2015-11-21 13:04:28 -0800

    Added file for vitals.each unit tests

---
+++ test/methods/each/each.js
@@ -0,0 +1,138 @@
+/**
+ * -----------------------------------------------------------------------------
+ * TEST - VITALS - JS METHOD - EACH
+ * -----------------------------------------------------------------------------
+ * @see [vitals.each]{@link https://github.com/imaginate/vitals/blob/master/src/methods/each.js}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+describe('vitals.each (sections:js,base)', function() {
+  var title;
+
+  describe('object tests', function() {
+
+    // newObj()= {
+    //   'a':  'd',
+    //   'b':  'e',
+    //   'c':  'f',
+    //   '1':   4,
+    //   '2':   5,
+    //   '3':   6,
+    //   'a1': '1',
+    //   'b2': '2',
+    //   'c3': '3'
+    // }
+
+    title = titleStr('should ');
+    describe(title, function() {
+
+      title = callStr('<object>', '<iteratee>');
+      it(title, function() {
+        // ...
+      });
+
+    });
+
+  });
+
+  describe('array tests', function() {
+
+    // newArr()= [ "a", "b", "c", 1, 2, 3, "a1", "b2", "c3" ]
+
+    title = titleStr('should ');
+    describe(title, function() {
+
+      title = callStr('<array>', '<iteratee>');
+      it(title, function() {
+        // ...
+      });
+
+    });
+
+  });
+
+  describe('cycle tests', function() {
+
+    title = titleStr('should ');
+    describe(title, function() {
+
+      title = callStr('<object>', '<iteratee>');
+      it(title, function() {
+        // ...
+      });
+
+    });
+
+  });
+
+  describe('error tests', function() {
+    describe('should throw an error', function() {
+
+      title = callStr();
+      it(title, function() {
+        assert.throws(function() {
+          vitals.each();
+        });
+      });
+
+    });
+  });
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} shouldMsg
+ * @return {string}
+ */
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 4, true);
+}
+
+/**
+ * @private
+ * @param {...*} args
+ * @return {string}
+ */
+function callStr() {
+  return testCall('each', arguments, 5, true);
+}
+
+/**
+ * @private
+ * @return {!Object}
+ */
+function newObj() {
+  return {
+    'a':  'd',
+    'b':  'e',
+    'c':  'f',
+    '1':   4,
+    '2':   5,
+    '3':   6,
+    'a1': '1',
+    'b2': '2',
+    'c3': '3'
+  };
+}
+
+/**
+ * @private
+ * @return {!Array}
+ */
+function newArr() {
+  return [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
+}
