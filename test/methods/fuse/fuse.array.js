7e14ee5a3ce5a3be77794828727ea496a1ad1daf
Adam Smith <adamsmith@algorithmiv.com>
2015-11-23 18:48:32 -0800

    Added unit tests for vitals.fuse.array

---
+++ test/methods/fuse/fuse.array.js
@@ -0,0 +1,130 @@
+/**
+ * -----------------------------------------------------------------------------
+ * TEST - VITALS - JS METHOD - FUSE.ARRAY
+ * -----------------------------------------------------------------------------
+ * @see [vitals.fuse]{@link https://github.com/imaginate/vitals/blob/master/src/methods/fuse.js}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+describe('vitals.fuse.array (sections:js,base)', function() {
+  var title;
+
+  describe('basic tests', function() {
+
+    title = titleStr('should push new properties to dest array');
+    describe(title, function() {
+
+      title = callStr([], 5);
+      it(title, function() {
+        var arr = vitals.fuse.arr([], 5);
+        assert( arr.length === 1 );
+        assert( arr[0] === 5 );
+      });
+
+      title = callStr([], 5, true, null);
+      it(title, function() {
+        var arr = vitals.fuse.arr([], 5, true, null);
+        assert( arr.length === 2 );
+        assert( arr[0] === 5 );
+        assert( arr[1] === true );
+      });
+
+    });
+
+    title = titleStr('should concatenate arrays to dest array');
+    describe(title, function() {
+
+      title = callStr([], [ 5 ]);
+      it(title, function() {
+        var arr = vitals.fuse.arr([], [ 5 ]);
+        assert( arr.length === 1 );
+        assert( arr[0] === 5 );
+      });
+
+      title = callStr([], [ 5, true, null ]);
+      it(title, function() {
+        var arr = vitals.fuse.arr([], [ 5, true, null ]);
+        assert( arr.length === 3 );
+        assert( arr[0] === 5 );
+        assert( arr[1] === true );
+        assert( arr[2] === null );
+      });
+
+      title = callStr([], [ 5 ], [ 6 ], null);
+      it(title, function() {
+        var arr = vitals.fuse.arr([], [ 5 ], [ 6 ], null);
+        assert( arr.length === 2 );
+        assert( arr[0] === 5 );
+        assert( arr[1] === 6 );
+      });
+
+    });
+
+  });
+
+  describe('error tests', function() {
+    describe('should throw an error', function() {
+
+      title = callStr();
+      it(title, function() {
+        assert.throws(function() {
+          vitals.fuse.arr();
+        });
+      });
+
+      title = callStr([]);
+      it(title, function() {
+        assert.throws(function() {
+          vitals.fuse.arr([]);
+        });
+      });
+
+      title = callStr({}, 5);
+      it(title, function() {
+        assert.throws(function() {
+          vitals.fuse.arr({}, 5);
+        });
+      });
+
+      title = callStr(null, 5);
+      it(title, function() {
+        assert.throws(function() {
+          vitals.fuse.arr(null, 5);
+        });
+      });
+
+    });
+  });
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} shouldMsg
+ * @return {string}
+ */
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 4, true);
+}
+
+/**
+ * @private
+ * @param {...*} args
+ * @return {string}
+ */
+function callStr() {
+  return testCall('fuse.arr', arguments, 5, true);
+}
