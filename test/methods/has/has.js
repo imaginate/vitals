cdd02f9372aead99d6aecadc5b6e27881af3b018
Adam Smith <adamsmith@algorithmiv.com>
2015-11-23 23:14:46 -0800

    Added unit tests for vitals.has

---
+++ test/methods/has/has.js
@@ -0,0 +1,225 @@
+/**
+ * -----------------------------------------------------------------------------
+ * TEST - VITALS - JS METHOD - HAS
+ * -----------------------------------------------------------------------------
+ * @see [vitals.has]{@link https://github.com/imaginate/vitals/blob/master/src/methods/has.js}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+describe('vitals.has (sections:js,base)', function() {
+  var title;
+
+  describe('object tests', function() {
+
+    // newObj()= {
+    //   'a':  'd',
+    //   'b':  'e',
+    //   'c':  'f',
+    //   '1':   4,
+    //   '2':   5,
+    //   '3':   6,
+    //   'a1': '1',
+    //   'b2': '2',
+    //   'c3': '3'
+    // }
+
+    title = titleStr('should return true (key owned by obj)');
+    describe(title, function() {
+
+      title = callStr('<object>', 'a');
+      it(title, function() {
+        assert( vitals.has(newObj(), 'a') );
+      });
+
+      title = callStr('<object>', 1);
+      it(title, function() {
+        assert( vitals.has(newObj(), 1) );
+      });
+
+    });
+
+    title = titleStr('should return false (key not owned by obj)');
+    describe(title, function() {
+
+      title = callStr(null);
+      it(title, function() {
+        assert( !vitals.has(null, 'key') );
+      });
+
+      title = callStr('<object>', 'd');
+      it(title, function() {
+        assert( !vitals.has(newObj(), 'd') );
+      });
+
+    });
+
+  });
+
+  describe('array tests', function() {
+
+    // newArr()= [ "a", "b", "c", 1, 2, 3, "a1", "b2", "c3" ]
+
+    title = titleStr('should return true (value in array)');
+    describe(title, function() {
+
+      title = callStr('<array>', 3);
+      it(title, function() {
+        assert( vitals.has(newArr(), 3) );
+      });
+
+      title = callStr('<array>', 'a');
+      it(title, function() {
+        assert( vitals.has(newArr(), 'a') );
+      });
+
+    });
+
+    title = titleStr('should return false (value not in array)');
+    describe(title, function() {
+
+      title = callStr('<array>', 5);
+      it(title, function() {
+        assert( !vitals.has(newArr(), 5) );
+      });
+
+      title = callStr('<array>', '1');
+      it(title, function() {
+        assert( !vitals.has(newArr(), '1') );
+      });
+
+    });
+
+  });
+
+  describe('string tests', function() {
+
+    title = titleStr('should return true (substr match in string)');
+    describe(title, function() {
+
+      title = callStr('abc123a1b2c3', /[a-z]/);
+      it(title, function() {
+        assert( vitals.has('abc123a1b2c3', /[a-z]/) );
+      });
+
+      title = callStr('abc123a1b2c3', 1);
+      it(title, function() {
+        assert( vitals.has('abc123a1b2c3', 1) );
+      });
+
+      title = callStr('abc123a1b2c3', 'a');
+      it(title, function() {
+        assert( vitals.has('abc123a1b2c3', 'a') );
+      });
+
+    });
+
+    title = titleStr('should return false (substr match not in string)');
+    describe(title, function() {
+
+      title = callStr('abc123a1b2c3', /^[a-z]$/);
+      it(title, function() {
+        assert( !vitals.has('abc123a1b2c3', /^[a-z]$/) );
+      });
+
+      title = callStr('abc123a1b2c3', 5);
+      it(title, function() {
+        assert( !vitals.has('abc123a1b2c3', 5) );
+      });
+
+      title = callStr('abc123a1b2c3', 'd');
+      it(title, function() {
+        assert( !vitals.has('abc123a1b2c3', 'd') );
+      });
+
+    });
+
+  });
+
+  describe('error tests', function() {
+    describe('should throw an error', function() {
+
+      title = callStr();
+      it(title, function() {
+        assert.throws(function() {
+          vitals.has();
+        });
+      });
+
+      title = callStr({});
+      it(title, function() {
+        assert.throws(function() {
+          vitals.has({});
+        });
+      });
+
+      title = callStr('str');
+      it(title, function() {
+        assert.throws(function() {
+          vitals.has('str');
+        });
+      });
+
+    });
+  });
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} shouldMsg
+ * @return {string}
+ */
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 4, true);
+}
+
+/**
+ * @private
+ * @param {...*} args
+ * @return {string}
+ */
+function callStr() {
+  return testCall('has', arguments, 5, true);
+}
+
+/**
+ * @private
+ * @param {boolean=} keys
+ * @return {!Object}
+ */
+function newObj(keys) {
+  return keys
+    ? [ 'a', 'b', 'c', '1', '2', '3', 'a1', 'b2', 'c3' ]
+    : {
+      'a':  'd',
+      'b':  'e',
+      'c':  'f',
+      '1':   4,
+      '2':   5,
+      '3':   6,
+      'a1': '1',
+      'b2': '2',
+      'c3': '3'
+    };
+}
+
+/**
+ * @private
+ * @return {!Array}
+ */
+function newArr() {
+  return [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
+}
