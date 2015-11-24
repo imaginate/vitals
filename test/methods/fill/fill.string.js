4897792f10f3df3512d1075566033fa2e5ff420c
Adam Smith <adamsmith@algorithmiv.com>
2015-11-23 17:39:27 -0800

    Added unit tests for vitals.fill.string

---
+++ test/methods/fill/fill.string.js
@@ -0,0 +1,91 @@
+/**
+ * -----------------------------------------------------------------------------
+ * TEST - VITALS - JS METHOD - FILL.STRING
+ * -----------------------------------------------------------------------------
+ * @see [vitals.fill]{@link https://github.com/imaginate/vitals/blob/master/src/methods/fill.js}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+describe('vitals.fill.string (sections:js,base)', function() {
+  var title;
+
+  describe('basic tests', function() {
+
+    title = titleStr('should fill new string with val x times');
+    describe(title, function() {
+
+      title = callStr(5, 5);
+      it(title, function() {
+        var str = vitals.fill.str(5, 5);
+        assert( str === '55555' );
+      });
+
+      title = callStr(0, 5);
+      it(title, function() {
+        var str = vitals.fill.str(0, 5);
+        assert( str === '' );
+      });
+
+    });
+
+  });
+
+  describe('error tests', function() {
+    describe('should throw an error', function() {
+
+      title = callStr();
+      it(title, function() {
+        assert.throws(function() {
+          vitals.fill.str();
+        });
+      });
+
+      title = callStr(5);
+      it(title, function() {
+        assert.throws(function() {
+          vitals.fill.str(5);
+        });
+      });
+
+      title = callStr('fail', 'val');
+      it(title, function() {
+        assert.throws(function() {
+          vitals.fill.str('fail', 'val');
+        });
+      });
+
+    });
+  });
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} shouldMsg
+ * @return {string}
+ */
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 4, true);
+}
+
+/**
+ * @private
+ * @param {...*} args
+ * @return {string}
+ */
+function callStr() {
+  return testCall('fill.str', arguments, 5, true);
+}
