ce1e24b234f0fbe6f41a776231a5cdf4d5cea919
Adam Smith <adamsmith@algorithmiv.com>
2015-11-25 21:05:16 -0800

    Fixed invalid JSDoc link in fs vitals.get

--- src/methods/fs/get.js
+++ src/methods/fs/get.js
@@ -3,7 +3,7 @@
  * VITALS - FILE SYSTEM METHODS - GET
  * -----------------------------------------------------------------------------
  * @version 2.0.0
- * @see [vitals.get]{@link https://github.com/imaginate/vitals/blob/master/src/methods/get.js}
+ * @see [vitals.get]{@link https://github.com/imaginate/vitals/blob/master/src/methods/fs/get.js}
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
