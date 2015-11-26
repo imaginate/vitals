7e989380c8cf61ffe3bbe209ac573da757b24402
Adam Smith <adamsmith@algorithmiv.com>
2015-11-25 19:57:38 -0800

    Fixed invalid JSDoc in vitals.get

--- src/methods/get.js
+++ src/methods/get.js
@@ -1,9 +1,9 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS - JS SHORTCUTS - KEYS
+ * VITALS - BASE METHOD - GET
  * -----------------------------------------------------------------------------
  * @version 2.0.0
- * @see [vitals.keys]{@link https://github.com/imaginate/vitals/blob/master/src/methods/keys.js}
+ * @see [vitals.get]{@link https://github.com/imaginate/vitals/blob/master/src/methods/get.js}
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
