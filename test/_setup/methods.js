a02161b14da7bcfd4c754fb25972cbb7905e5464
Adam Smith <adamsmith@algorithmiv.com>
2016-01-25 15:56:27 -0800

    Updated tests setup: methods

--- test/_setup/methods.js
+++ test/_setup/methods.js
@@ -1,14 +1,14 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS TESTS: SETUP VITALS METHODS
+ * VITALS UNIT TESTS: ALL METHODS SETUP
  * -----------------------------------------------------------------------------
- * @see [vitals]{@link https://github.com/imaginate/vitals}
- *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
- * @copyright 2015 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
  * Supporting Libraries:
  * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
  *
  * Annotations:
  * @see [JSDoc3]{@link http://usejsdoc.org/}
@@ -17,69 +17,38 @@
 
 'use strict';
 
-var fs = require('fs');
-
-
-////////////////////////////////////////////////////////////////////////////////
-// SETUP GLOBAL HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-require('../_helpers/basics');
-require('../_helpers/display');
-
-/**
- * @global
- * @type {!Object}
- */
-global.assert = require('assert');
-
-
-////////////////////////////////////////////////////////////////////////////////
-// SETUP VITALS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @global
- * @type {!Object}
- */
-global.vitals = getVitals();
+global.vitals = loadVitalsMethods();
 
 /**
  * @private
  * @return {!Object}
  */
-function getVitals() {
+function loadVitalsMethods() {
 
+  /** @type {!Array<string>} */
+  var filenames;
+  /** @type {string} */
+  var filepath;
+  /** @type {string} */
+  var method;
   /** @type {!Object} */
   var vitals;
-  /** @type {!Array} */
-  var methods;
 
   vitals = {};
-  methods = getMethods();
-  each(methods, function(method) {
-    vitals[method] = require('../../src/methods/' + method + '.js');
+  filenames = get.filepaths('src/methods');
+  each(filenames, function(filename) {
+    method = stripJSExt(filename);
+    filepath = fuse('../../src/methods/', filename);
+    vitals[method] = require(filepath);
   });
   return vitals;
 }
 
 /**
  * @private
- * @return {!Array}
+ * @param {string} filename
+ * @return {string}
  */
-function getMethods() {
-
-  /** @type {!Array} */
-  var methods;
-  /** @type {string} */
-  var base;
-
-  base = 'src/methods/';
-  methods = fs.readdirSync(base);
-  methods = methods.filter(function(method) {
-    return is.file(base + method);
-  });
-  return methods.map(function(method) {
-    return method.replace(/\.js$/, '');
-  });
+function stripJSExt(filename) {
+  return cut(filename, /\.js$/);
 }
