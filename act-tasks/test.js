e2a7f5c83a10e949e9eab1ec7b7e55d78909ab75
Adam Smith <adamsmith@algorithmiv.com>
2016-01-25 23:21:51 -0800

    Added repo link to test act-task docs

--- act-tasks/test.js
+++ act-tasks/test.js
@@ -8,6 +8,7 @@
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
  * Supporting Libraries:
+ * @see [act]{@link https://github.com/imaginate/act}
  * @see [are]{@link https://github.com/imaginate/are}
  * @see [vitals]{@link https://github.com/imaginate/vitals}
  * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
