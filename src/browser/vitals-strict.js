d6b8e83060893691104f671b22053f2d8e1dc982
Adam Smith <adamsmith@algorithmiv.com>
2016-02-01 18:12:01 -0800

    Removed are from src docs

--- src/browser/vitals-strict.js
+++ src/browser/vitals-strict.js
@@ -34,9 +34,6 @@ _elems:"elements",_docs:"documents",_nilmap:"nullmap",_strmap:"stringmap",_numma
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
- *
  * Annotations:
  * @see [JSDoc3]{@link http://usejsdoc.org/}
  * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
--- src/sections/skeletons/fs.js
+++ src/sections/skeletons/fs.js
@@ -10,9 +10,6 @@
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
- *
  * Annotations:
  * @see [JSDoc3]{@link http://usejsdoc.org/}
  * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
