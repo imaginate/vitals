463add43571584273df1aa7c4bde940202d6c141
Adam Smith <adamsmith@algorithmiv.com>
2016-02-05 21:40:17 -0800

    Added dummy files for fs testing

---
+++ test/dummy/fake.js
@@ -0,0 +1,9 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS TESTS - FAKE FILE
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ */
+
+// fake
