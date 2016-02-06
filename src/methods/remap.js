271cbb732446ca453aecd84e22f6a40ec3904cf3
Adam Smith <adamsmith@algorithmiv.com>
2016-02-05 17:24:06 -0800

    Removed vitals.is ref from vitals.remap

--- src/methods/remap.js
+++ src/methods/remap.js
@@ -21,7 +21,6 @@ var _escape = require('./helpers/escape.js');
 var _own = require('./helpers/own.js');
 var copy = require('./copy.js');
 var _is = require('./helpers/is.js');
-var is = require('./is.js');
 
 
 ////////////////////////////////////////////////////////////////////////////////
