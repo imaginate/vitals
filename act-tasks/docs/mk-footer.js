5cc0a3e3edf97a2793f1bcd9646cd3cd61c52b2a
Adam Smith <adamsmith@algorithmiv.com>
2016-02-12 19:44:39 -0800

    Added docs helper: mkFooter

---
+++ act-tasks/docs/mk-footer.js
@@ -0,0 +1,32 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: mkFooter
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [act]{@link https://github.com/imaginate/act}
+ * @see [are]{@link https://github.com/imaginate/are}
+ * @see [vitals]{@link https://github.com/imaginate/vitals}
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var get = require('node-vitals')('fs').get;
+
+var TEMPLATE = get.file('act-tasks/docs/footer.md');
+
+/**
+ * @param {string} content
+ * @param {string=} fscontent
+ * @return {string}
+ */
+module.exports = function mkFooter(content, fscontent) {
+  return TEMPLATE;
+};
