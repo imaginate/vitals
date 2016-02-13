d83999d239f91a98af9faad488cecd74c1d2dada
Adam Smith <adamsmith@algorithmiv.com>
2016-02-12 19:42:25 -0800

    Added logic for act-task: docs.methods

--- act-tasks/docs.js
+++ act-tasks/docs.js
@@ -25,17 +25,13 @@ var cut    = vitals.cut;
 var each   = vitals.each;
 var fuse   = vitals.fuse;
 var get    = vitals.get;
-var has    = vitals.has;
-var remap  = vitals.remap;
 var to     = vitals.to;
 
-
-
 exports['desc'] = 'builds the wiki docs';
-exports['value'] = 'method';
-exports['default'] = '-build';
+exports['value'] = 'vitals-method';
+exports['default'] = '-methods';
 exports['methods'] = {
-  'build': {
+  'methods': {
     'desc': 'builds the wiki docs from JSDoc',
     'value': 'method',
     'method': buildDocs
@@ -45,3 +41,84 @@ exports['methods'] = {
     'method': versionDocs
   }
 };
+
+var WIKI = '../vitals.wiki';
+var BASE = './src/methods';
+
+var mkHeader = require('./docs/mk-header');
+var mkBody   = require('./docs/mk-body');
+var mkFooter = require('./docs/mk-footer');
+
+/**
+ * @public
+ * @param {string=} method - [default= "all"]
+ */
+function buildDocs(method) {
+
+  /** @type {!Array<string>} */
+  var files;
+  /** @type {string} */
+  var file;
+
+  if (!method || method === 'all') {
+    files = get.filepaths(BASE);
+    each(files, function(file) {
+      method = cut(file, /\.js$/);
+      buildDoc(method);
+    });
+  }
+  else {
+    file = fuse(BASE, '/', method, '.js');
+    if ( !is.file(file) ) throw new RangeError('invalid vitals method');
+    buildDoc(method);
+  }
+}
+
+/**
+ * @private
+ * @param {string} method
+ */
+function buildDoc(method) {
+
+  /** @type {string} */
+  var fscontent;
+  /** @type {string} */
+  var content;
+  /** @type {string} */
+  var fsfile;
+  /** @type {string} */
+  var file;
+
+  fsfile = fuse(BASE, '/fs/', method, '.js');
+  fscontent = is.file(fsfile)
+    ? get.file(fsfile)
+    : undefined;
+
+  file = fuse(BASE, '/', method, '.js');
+  content = get.file(file);
+
+  content = mkDoc(content, fscontent);
+  file = fuse(WIKI, '/method-', method, '.md');
+  to.file(content, file);
+}
+
+/**
+ * @private
+ * @param {string} content
+ * @param {string=} fscontent
+ * @return {string}
+ */
+function mkDoc(content, fscontent) {
+
+  /** @type {string} */
+  var header;
+  /** @type {string} */
+  var footer;
+  /** @type {string} */
+  var body;
+
+  header = mkHeader(content, fscontent);
+  body   = mkBody(content, fscontent);
+  footer = mkFooter(content, fscontent);
+  return fuse(header, body, footer);
+}
