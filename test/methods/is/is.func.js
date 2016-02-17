58e795bb7540550bef1fb6f18f4651de8f75b8cf
Adam Smith <adamsmith@algorithmiv.com>
2016-02-16 18:24:55 -0800

    Added unit tests for vitals.is.func

---
+++ test/methods/is/is.func.js
@@ -0,0 +1,89 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS: VITALS.IS.FUNC
+ * -----------------------------------------------------------------------------
+ * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [are]{@link https://github.com/imaginate/are}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+describe('vitals.is.func (section:base)', function() {
+  var title;
+
+  title = titleStr('should return true');
+  describe(title, function() {
+
+    title = callStr(function(){});
+    it(title, function() {
+      var result = vitals.is.func(function(){});
+      assert( result === true );
+    });
+
+    title = callStr(function(){}, function(){}, function(){});
+    it(title, function() {
+      var result = vitals.is.func(function(){}, function(){}, function(){});
+      assert( result === true );
+    });
+
+  });
+
+  title = titleStr('should return false');
+  describe(title, function() {
+
+    title = callStr(null);
+    it(title, function() {
+      var result = vitals.is.func(null);
+      assert( result === false );
+    });
+
+    title = callStr(function(){}, function(){}, {});
+    it(title, function() {
+      var result = vitals.is.func(function(){}, function(){}, {});
+      assert( result === false );
+    });
+
+  });
+
+  title = titleStr('should throw an error');
+  describe(title, function() {
+
+    title = callStr();
+    it(title, function() {
+      assert.throws(function() {
+        vitals.is.func();
+      });
+    });
+
+  });
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} shouldMsg
+ * @return {string}
+ */
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 2);
+}
+
+/**
+ * @private
+ * @param {...*} args
+ * @return {string}
+ */
+function callStr() {
+  return testCall('is.func', arguments, 3);
+}
