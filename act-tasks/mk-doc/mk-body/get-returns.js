fa5ac3f870d2fc181b0e9ba0e84cd507f4a0df1a
Adam Smith <adamsmith@algorithmiv.com>
2016-02-18 17:09:36 -0800

    Inserted getDescription for docs helper: getReturns

--- act-tasks/mk-doc/mk-body/get-returns.js
+++ act-tasks/mk-doc/mk-body/get-returns.js
@@ -19,18 +19,15 @@
 'use strict';
 
 var vitals = require('node-vitals')('base');
-var cut    = vitals.cut;
-var fill   = vitals.fill;
 var fuse   = vitals.fuse;
-var get    = vitals.get;
 var has    = vitals.has;
 var remap  = vitals.remap;
-var roll   = vitals.roll;
 var slice  = vitals.slice;
 var until  = vitals.until;
 
 var TYPE  = /^@returns? \{([^}]+)\}.*$/;
-var TRIM  = /^@returns? \{[^}]+\} *-? */;
+
+var getDescription = require('./get-description');
 
 /**
  * @param {!Array<string>} lines
@@ -45,7 +42,8 @@ module.exports = function getReturns(lines) {
 
   lines = pruneLines(lines);
   type  = remap(lines[0], TYPE, '$1');
-  desc  = getDescrip(lines, 0);
+  desc  = getDescription(lines, 0);
+  desc  = desc && fuse('\n', desc, '\n');
   return fuse('<i>', type, '</i>\n', desc);
 };
 
@@ -64,107 +62,4 @@ function pruneLines(lines) {
     return start === undefined;
   });
   return slice(lines, start, -1);
-}
-
-/**
- * @private
- * @param {!Array<string>} lines
- * @param {number} indents
- * @return {string}
- */
-function getDescrip(lines, indents) {
-
-  /** @type {string} */
-  var indent;
-  /** @type {string} */
-  var begin;
-  /** @type {string} */
-  var desc;
-
-  begin = cut(lines[0], TRIM);
-  lines = slice(lines, 1);
-  desc = parseLines(lines, indents);
-  desc = fuse(begin, desc);
-
-  if (!desc) return '';
-
-  indent = getIndent(indents);
-  return fuse('\n', indent, desc, '\n');
-}
-
-/**
- * @private
- * @param {!Array<string>} lines
- * @param {number} indents
- * @return {string}
- */
-function parseLines(lines, indents) {
-
-  /** @type {boolean} */
-  var sublist;
-  /** @type {string} */
-  var indent;
-  /** @type {number} */
-  var spaces;
-  /** @type {string} */
-  var space;
-  /** @type {boolean} */
-  var code;
-  /** @type {boolean} */
-  var list;
-
-  indent = getIndent(indents);
-  return roll.up('', lines, function(line) {
-
-    // </code>
-    if (code) {
-      line = slice(line, spaces);
-      code = !has(line, /^```$/);
-      return fuse(indent, line, '\n');
-    }
-
-    // <code>
-    if ( has(line, /^ *```/) ) {
-      space = get(line, /^ +/)[0] || '';
-      spaces = space.length;
-      line = slice(line, spaces);
-      line = line.length > 3 ? line : '```javascript';
-      code = true;
-      return fuse('\n\n', indent, line, '\n');
-    }
-
-    line = cut(line, /^ +/);
-
-    // <li>
-    if ( has(line, /^- /) ) {
-      if (!list) {
-        list = true;
-        indent = getIndent(sublist ? --indents : ++indents);
-        sublist = false;
-      }
-      return fuse('\n', indent, line);
-    }
-
-    // nested <li>
-    if ( has(line, /^-- /) ) {
-      if (list) {
-        list = false;
-        indent = getIndent(++indents);
-        sublist = true;
-      }
-      line = slice(line, 1);
-      return fuse('\n', indent, line);
-    }
-
-    return fuse(' ', line);
-  });
-}
-
-/**
- * @private
- * @param {number} indents
- * @return {string}
- */
-function getIndent(indents) {
-  return fill(indents, '  ');
 }
