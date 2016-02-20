00484936a5cdc29b6518ebcc7f7dacca40d3bf40
Adam Smith <adamsmith@algorithmiv.com>
2016-02-20 01:00:14 -0800

    Added act-task helper: getSections

---
+++ act-tasks/helpers/get-sections.js
@@ -0,0 +1,62 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: getSections
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var is = require('./is');
+var fs = require('fs');
+
+var BASE = './src/sections';
+var BWSR = './src/browser';
+
+/**
+ * @param {boolean=} fileform - "<section>.js" vs "<section>"
+ * @param {boolean=} browser - only get the browser sections
+ * @return {!Array<string>}
+ */
+module.exports = function getSections(fileform, browser) {
+
+  /** @type {!Array<string>} */
+  var filepaths;
+  /** @type {!Array<string>} */
+  var sections;
+  /** @type {string} */
+  var section;
+  /** @type {string} */
+  var base;
+  /** @type {number} */
+  var len;
+  /** @type {number} */
+  var i;
+
+  base = browser ? BWSR : BASE;
+  filepaths = fs.readdirSync(base);
+  sections = [];
+  len = filepaths.length;
+  i = -1;
+  while (++i < len) {
+    filepath = filepaths[i];
+    section  = fileform ? filepath : cutFileExt(filepath);
+    filepath = base + '/' + filepath;
+    if ( is.file(filepath) ) sections.push(section);
+  }
+  return sections;
+};
+
+/**
+ * @private
+ * @param {string} filepath
+ * @return {string}
+ */
+function cutFileExt(filepath) {
+  return filepath.replace(/\.js$/, '');
+}
