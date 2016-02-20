94d9601ae047838ed93ea3a55cda67bd6944ce4d
Adam Smith <adamsmith@algorithmiv.com>
2016-02-20 04:30:10 -0800

    Removed are & vitals from docs: mkDoc

--- act-tasks/helpers/mk-doc/index.js
+++ act-tasks/helpers/mk-doc/index.js
@@ -5,20 +5,12 @@
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
  *
- * Supporting Libraries:
- * @see [act]{@link https://github.com/imaginate/act}
- * @see [are]{@link https://github.com/imaginate/are}
- * @see [vitals]{@link https://github.com/imaginate/vitals}
- * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
- *
  * Annotations:
  * @see [JSDoc3]{@link http://usejsdoc.org/}
  * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
  */
 
 'use strict';
-
-var fuse = require('node-vitals')('fuse');
 
 var mkHeader = require('./mk-header');
 var mkBody   = require('./mk-body');
@@ -41,5 +33,5 @@ module.exports = function mkDoc(content, fscontent) {
   header = mkHeader(content, fscontent);
   body   = mkBody(content, fscontent);
   footer = mkFooter(content, fscontent);
-  return fuse(header, body, footer);
+  return header + body + footer;
 };
