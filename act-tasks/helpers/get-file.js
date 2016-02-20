35be551dd2f81a27190ce6ca6e6b0ffcf0f38005
Adam Smith <adamsmith@algorithmiv.com>
2016-02-20 01:00:23 -0800

    Added act-task helper: getFile

---
+++ act-tasks/helpers/get-file.js
@@ -0,0 +1,31 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: getFile
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+var fs = require('fs');
+
+/**
+ * @param {string} filepath
+ * @param {boolean=} buffer
+ * @return {(!Buffer|string)}
+ */
+module.exports = function getFile(filepath, buffer) {
+
+  /** @type {string} */
+  var content;
+
+  if (buffer) return fs.readFileSync(filepath);
+
+  content = fs.readFileSync(filepath, 'utf8');
+  return content && content.replace(/\r\n?/g, '\n');
+};
