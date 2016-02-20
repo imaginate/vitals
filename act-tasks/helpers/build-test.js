4bf1c4ff25f14a62d1b48d0873d50564bfab6f43
Adam Smith <adamsmith@algorithmiv.com>
2016-02-20 00:17:37 -0800

    Added act-task helper: buildTest

---
+++ act-tasks/helpers/build-test.js
@@ -0,0 +1,33 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: buildTest
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+/**
+ * @param {!Array<string>} sections
+ * @param {function} newTest
+ * @return {function}
+ */
+module.exports = function buildTest(sections, newTest) {
+
+  /** @type {function} */
+  var test;
+  /** @type {number} */
+  var len;
+  /** @type {number} */
+  var i;
+
+  len = sections.length;
+  i = -1;
+  while (++i < len) test = newTest(sections[i], test);
+  return test;
+};
