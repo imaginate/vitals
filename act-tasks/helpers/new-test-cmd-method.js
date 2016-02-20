810098b7794c90ac154f54988333f430e0c2f988
Adam Smith <adamsmith@algorithmiv.com>
2016-02-20 00:18:05 -0800

    Added act-task helper: newTestCmdMethod

---
+++ act-tasks/helpers/new-test-cmd-method.js
@@ -0,0 +1,58 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: newTestCmdMethod
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Supporting Libraries:
+ * @see [log-ocd]{@link https://github.com/imaginate/log-ocd}
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+/**
+ * @typedef {function} TestCmdMethod
+ */
+
+var log = require('log-ocd')();
+var is = require('./is');
+
+log.debug.setFormat({
+  'linesBefore': 2,
+  'linesAfter':  0
+});
+
+/**
+ * @param {boolean} start - start or close
+ * @param {string=} name - the name to log
+ * @param {?function=} callback
+ * @return {!TestCmdMethod}
+ */
+module.exports = function newTestCmdMethod(start, name, callback) {
+
+  /** @type {function} */
+  var logger;
+  /** @type {string} */
+  var msg;
+
+  if ( is.func(name) ) {
+    callback = name;
+    name = undefined;
+  }
+
+  if (name) {
+    logger = start ? log.debug : log.pass;
+    msg = start ? 'Starting' : 'Finished';
+    msg = msg + ' `' + name + '` tests';
+  }
+
+  return function testCmdMethod() {
+    msg && logger(msg);
+    callback && callback();
+  };
+};
