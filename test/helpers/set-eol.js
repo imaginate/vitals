14b57bcfd9b052edc197bea7604cfbdf9edd3b68
Adam Smith <adamsmith@algorithmiv.com>
2016-02-21 22:04:37 -0800

    Added test helper: setEol

---
+++ test/helpers/set-eol.js
@@ -0,0 +1,45 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS HELPER: setEol
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+module.exports = setEol;
+
+var own = require('./own');
+
+var EOL = {
+  'CR':   '\r',
+  'LF':   '\n',
+  'CRLF': '\r\n'
+};
+
+var FIND_EOL = {
+  'CR':   /\r?\n/g,
+  'LF':   /\r\n?/g,
+  'CRLF': /\r?\n|\r\n?/g
+};
+
+/**
+ * @global
+ * @param {string} str
+ * @param {string} eol
+ * @return {string}
+ */
+function setEol(str, eol) {
+
+  if (typeof str !== 'string') throw TypeError('invalid str');
+  if (typeof eol !== 'string') throw TypeError('invalid eol');
+
+  eol = eol.toUpperCase();
+
+  if ( !own(EOL, eol) ) throw RangeError('invalid eol (opts: LF, CR, CRLF)');
+
+  return str.replace(FIND_EOL[eol], EOL[eol]);
+}
