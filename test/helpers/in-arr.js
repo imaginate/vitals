e1178f09c3ca6532ce68016f7692da234961a242
Adam Smith <adamsmith@algorithmiv.com>
2016-02-21 22:30:20 -0800

    Added test helper: inArr

---
+++ test/helpers/in-arr.js
@@ -0,0 +1,35 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS HELPER: inArr
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+'use strict';
+
+module.exports = inArr;
+
+/**
+ * @param {!Array} source
+ * @param {*} val
+ * @return {boolean}
+ */
+function inArr(source, val) {
+
+  /** @type {number} */
+  var len;
+  /** @type {number} */
+  var i;
+
+  len = source.length;
+  i = -1;
+  while (++i < len) {
+    if (source[i] === val) return true;
+  }
+  return false;
+}
