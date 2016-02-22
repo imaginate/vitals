627879eaf6f9ea307878dd0b77dd0530307d2ee7
Adam Smith <adamsmith@algorithmiv.com>
2016-02-21 21:19:17 -0800

    Added test helper: isFile

---
+++ test/helpers/is-file.js
@@ -0,0 +1,31 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS HELPER: isFile
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+var fs = require('fs');
+
+/**
+ * @param {string} filepath
+ * @return {boolean}
+ */
+module.exports = unction isFile(filepath) {
+
+  if (typeof filepath !== 'string') throw new TypeError('invalid filepath (must be a string)');
+
+  if (!filepath) return false;
+
+  try {
+    return fs.statSync(filepath).isFile();
+  }
+  catch (err) {
+    return false;
+  }
+};
