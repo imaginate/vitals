664bacfc230b17db0efec137fc0f974e6283e4e3
Adam Smith <adamsmith@algorithmiv.com>
2016-02-21 22:28:40 -0800

    Added test helper: testTitle

---
+++ test/helpers/test-title.js
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS HELPER: testTitle
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+module.exports = testTitle;
+
+var is = require('./is');
+var breakStr = require('./break-str');
+
+/**
+ * @global
+ * @param {string} section
+ * @param {string} details
+ * @param {number} indent
+ * @return {string}
+ */
+function testTitle(section, details, indent) {
+
+  /** @type {string} */
+  var result;
+
+  if ( !is.str(section) ) throw new TypeError('invalid type for `section` param');
+  if ( !is.str(details) ) throw new TypeError('invalid type for `details` param');
+  if ( !is.num(indent)  ) throw new TypeError('invalid type for `indent` param');
+
+  result = section + ' tests: ' + details;
+  return breakStr(result, ++indent);
+}
