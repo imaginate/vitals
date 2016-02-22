ffe0399cef705f7be9e438a14b7b5dc0ad7aaf09
Adam Smith <adamsmith@algorithmiv.com>
2016-02-21 22:28:17 -0800

    Added test helper: indentStr

---
+++ test/helpers/indent-str.js
@@ -0,0 +1,38 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS HELPER: indentStr
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+module.exports = indentStr;
+
+var is = require('./is');
+
+/**
+ * @param {string} str
+ * @param {number} indents
+ * @return {string}
+ */
+function indentStr(str, indents) {
+
+  /** @type {string} */
+  var indent;
+
+  indents = indents || 0;
+
+  if ( !is.str(str)     ) throw new TypeError('invalid type for `str` param');
+  if ( !is.num(indents) ) throw new TypeError('invalid type for `indents` param');
+
+  if (!indents) return str;
+
+  indent = '';
+  while (--indents) indent += '  ';
+  indent = '\n' + indent;
+  return str.replace(/\n/g, indent);
+}
