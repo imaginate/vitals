570c22cc6a13c9bfe030432165f3902f53987355
Adam Smith <adamsmith@algorithmiv.com>
2016-02-21 21:19:10 -0800

    Added test helper: isDirectory

---
+++ test/helpers/is-directory.js
@@ -0,0 +1,31 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS HELPER: isDirectory
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
+ */
+
+var fs = require('fs');
+
+/**
+ * @param {string} dirpath
+ * @return {boolean}
+ */
+module.exports = function isDirectory(dirpath) {
+
+  if (typeof dirpath !== 'string') throw new TypeError('invalid dirpath (must be a string)');
+
+  if (!dirpath) return false;
+
+  try {
+    return fs.statSync(dirpath).isDirectory();
+  }
+  catch (err) {
+    return false;
+  }
+};
