37b69e0ee2cb85698930634bbcc6a13f091bd4f7
Adam Smith <adamsmith@algorithmiv.com>
2016-02-23 01:23:01 -0800

    Rebuilt tests for vitals.copy.directory

--- test/methods/copy/copy.directory.js
+++ test/methods/copy/copy.directory.js
@@ -1,14 +1,12 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS TESTS - COPY.DIRECTORY
+ * VITALS UNIT TESTS: vitals.copy.directory
  * -----------------------------------------------------------------------------
- * @see [vitals.copy]{@link https://github.com/imaginate/vitals/wiki/vitals.copy}
+ * @see [vitals.copy docs](https://github.com/imaginate/vitals/wiki/vitals.copy)
+ * @see [global test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3]{@link http://usejsdoc.org/}
@@ -17,105 +15,120 @@
 
 if (BROWSER_TESTS) return;
 
+var CONTENT = DUMMY.content; // content for dummy files
+var BASE = DUMMY.base.replace(/\/$/, ''); // base directory for dummy files
 var fs = require('fs');
 
 describe('vitals.copy.directory (section:fs)', function() {
   var title;
-  var fakes = [ 'fake1.js', 'fake2.js', 'fake3.js' ];
 
-  title = 'should copy files from dir to dir';
-  title = titleStr('basic', title);
+  title = titleStr('should shallowly copy files from dir to dir');
   describe(title, function() {
 
     before('setup dummy dirs and files', function() {
-      mkDummy({ 'root': fakes, 'subdir1': null });
+      var files = [ 'file1.js', 'file2.js', 'file3.js' ];
+      mkDummy({ 'root': files, 'subdir1': null });
     });
+    after('clean up dummy dirs and files', rmDummy);
 
     title = callStr('', 'subdir1');
     it(title, function() {
-      var src = addBase('');
-      var dest = addBase('subdir1');
-      var files = vitals.copy.dir(src, dest);
+      var dest  = addBase('subdir1');
+      var files = vitals.copy.dir(BASE, dest);
       assert( is.arr(files) );
       assert( files.length === 3 );
-      each(fakes, function(fake) {
-        assert( has(files, fake) );
-        fake = fuse(dest, '/', fake);
-        assert( is.file(fake) );
-        fake = fs.readFileSync(fake, 'utf8');
-        assert( fake === DUMMY.content );
-      });
+      assert( hasVal(files, 'file1.js') );
+      assert( hasVal(files, 'file2.js') );
+      assert( hasVal(files, 'file3.js') );
+      assert( validFile(dest + '/file1.js') );
+      assert( validFile(dest + '/file2.js') );
+      assert( validFile(dest + '/file3.js') );
     });
 
     title = callStr('', 'subdir2/');
     it(title, function() {
-      var src = addBase('');
       var dest = addBase('subdir2/');
-      var files = vitals.copy.dir(src, dest);
+      var files = vitals.copy.dir(BASE, dest);
       assert( is.arr(files) );
       assert( files.length === 3 );
-      each(fakes, function(fake) {
-        assert( has(files, fake) );
-        fake = fuse(dest, fake);
-        assert( is.file(fake) );
-        fake = fs.readFileSync(fake, 'utf8');
-        assert( fake === DUMMY.content );
-      });
+      assert( hasVal(files, 'file1.js') );
+      assert( hasVal(files, 'file2.js') );
+      assert( hasVal(files, 'file3.js') );
+      assert( validFile(dest + 'file1.js') );
+      assert( validFile(dest + 'file2.js') );
+      assert( validFile(dest + 'file3.js') );
     });
-
-    title = callStr('', 'subdir3/', true);
-    it(title, function() {
-      var src = addBase('');
-      var dest = addBase('subdir3/');
-      var dirs = [ '', 'subdir1/', 'subdir2/' ];
-      var files = vitals.copy.dir(src, dest, true);
-      assert( is.arr(files) );
-      assert( files.length === 9 );
-      each(dirs, function(dir) {
-        each(fakes, function(fake) {
-          fake = fuse(dir, fake);
-          assert( has(files, fake) );
-          fake = fuse(dest, fake);
-          assert( is.file(fake) );
-          fake = fs.readFileSync(fake, 'utf8');
-          assert( fake === DUMMY.content );
-        });
-      });
-    });
-
-    after('clean up dummy dirs and files', rmDummy);
-
   });
 
-  title = titleStr('error', 'should throw an error');
+  title = titleStr('should deeply copy files from dir to dir');
   describe(title, function() {
 
     before('setup dummy dirs and files', function() {
-      mkDummy({ 'root': fakes, 'subdir1': null });
+      var files = [ 'file1.js', 'file2.js', 'file3.js' ];
+      mkDummy({
+        'subdir1': files,
+        'subdir2': files,
+        'root':    files
+      });
     });
+    after('clean up dummy dirs and files', rmDummy);
+
+    title = callStr('', 'subdir/', true);
+    it(title, function() {
+      var dest = addBase('subdir/');
+      var files = vitals.copy.dir(BASE, dest, true);
+      assert( is.arr(files) );
+      assert( files.length === 9 );
+      assert( hasVal(files, 'file1.js') );
+      assert( hasVal(files, 'file2.js') );
+      assert( hasVal(files, 'file3.js') );
+      assert( hasVal(files, 'subdir1/file1.js') );
+      assert( hasVal(files, 'subdir1/file2.js') );
+      assert( hasVal(files, 'subdir1/file3.js') );
+      assert( hasVal(files, 'subdir2/file1.js') );
+      assert( hasVal(files, 'subdir2/file2.js') );
+      assert( hasVal(files, 'subdir2/file3.js') );
+      assert( validFile(dest + 'file1.js') );
+      assert( validFile(dest + 'file2.js') );
+      assert( validFile(dest + 'file3.js') );
+      assert( validFile(dest + 'subdir1/file1.js') );
+      assert( validFile(dest + 'subdir1/file2.js') );
+      assert( validFile(dest + 'subdir1/file3.js') );
+      assert( validFile(dest + 'subdir2/file1.js') );
+      assert( validFile(dest + 'subdir2/file2.js') );
+      assert( validFile(dest + 'subdir2/file3.js') );
+    });
+  });
+
+  title = titleStr('should throw an error');
+  describe(title, function() {
+
+    before('setup dummy dirs and files', function() {
+      var files = [ 'file1.js', 'file2.js', 'file3.js' ];
+      mkDummy({ 'root': files, 'subdir1': null });
+    });
+    after('clean up dummy dirs and files', rmDummy);
 
     title = callStr();
     it(title, function() {
       assert.throws(function() {
         vitals.copy.dir();
-      });
+      }, validTypeErr);
     });
 
     title = callStr('');
     it(title, function() {
       assert.throws(function() {
-        var src = addBase('');
-        vitals.copy.dir(src);
-      });
+        vitals.copy.dir(BASE);
+      }, validTypeErr);
     });
 
     title = callStr('', 'invalid');
     it(title, function() {
       assert.throws(function() {
-        var src = addBase('');
         var dest = addBase('invalid');
-        vitals.copy.dir(src, dest);
-      });
+        vitals.copy.dir(BASE, dest);
+      }, validTypeErr);
     });
 
     title = callStr('invalid', 'subdir1');
@@ -124,13 +137,9 @@ describe('vitals.copy.directory (section:fs)', function() {
         var src = addBase('invalid');
         var dest = addBase('subdir1');
         vitals.copy.dir(src, dest);
-      });
+      }, validTypeErr);
     });
-
-    after('clean up dummy dirs and files', rmDummy);
-
   });
-
 });
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -139,12 +148,11 @@ describe('vitals.copy.directory (section:fs)', function() {
 
 /**
  * @private
- * @param {string} section
  * @param {string} shouldMsg
  * @return {string}
  */
-function titleStr(section, shouldMsg) {
-  return testTitle(section, shouldMsg, 1);
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 2);
 }
 
 /**
@@ -152,11 +160,12 @@ function titleStr(section, shouldMsg) {
  * @param {...*} args
  * @return {string}
  */
-function callStr(args) {
-  args = remap(arguments, function(val, i) {
-    return i < 2 ? addBase(val) : val;
-  });
-  return testCall('copy.dir', args, 3);
+function callStr() {
+  if (arguments.length) {
+    if (arguments.length > 1) arguments[1] = addBase(arguments[1]);
+    arguments[0] = addBase(arguments[0]);
+  }
+  return testCall('copy.dir', arguments, 3);
 }
 
 /**
@@ -165,10 +174,21 @@ function callStr(args) {
  * @return {string}
  */
 function addBase(dir) {
+  return dir ? BASE + '/' + dir : BASE;
+}
+
+/**
+ * @private
+ * @param {string} filepath
+ * @return {boolean}
+ */
+function validFile(filepath) {
 
   /** @type {string} */
-  var base;
+  var content;
 
-  base = cut(DUMMY.base, /\/$/);
-  return dir ? fuse(base, '/', dir) : base;
+  if ( !isFile(filepath) ) return false;
+
+  content = fs.readFileSync(filepath, 'utf8');
+  return CONTENT === content;
 }
