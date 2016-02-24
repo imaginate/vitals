0d44a45b00627a11baab1ff5e49403ef8bbff7a2
Adam Smith <adamsmith@algorithmiv.com>
2016-02-24 02:04:23 -0800

    Rebuilt tests for vitals.cut.index

--- test/methods/cut/cut.index.js
+++ test/methods/cut/cut.index.js
@@ -1,14 +1,12 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - CUT.INDEX
+ * VITALS UNIT TESTS: vitals.cut.index
  * -----------------------------------------------------------------------------
- * @see [vitals.cut]{@link https://github.com/imaginate/vitals/wiki/vitals.cut}
+ * @see [vitals.cut docs](https://github.com/imaginate/vitals/wiki/vitals.cut)
+ * @see [global test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
@@ -18,101 +16,121 @@
 describe('vitals.cut.index (section:base)', function() {
   var title;
 
-  title = titleStr('basic', 'should splice array indexes from index to toIndex');
+  title = titleStr('should splice array indexes from start to end');
   describe(title, function() {
-
-    // newArr()= [ "a", "b", "c", 1, 2, 3, "a1", "b2", "c3" ]
 
     title = callStr('<array>', 1);
     it(title, function() {
-      var arr = vitals.cut.i(newArr(), 1);
-      var be = [ 'a', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
-      each(be, function(val, i) {
-        assert(arr[i] === val);
-      });
+      var arr1 = [ 1, 2, 3 ];
+      var arr2 = vitals.cut.i(arr1, 1);
+      assert( is.arr(arr2) );
+      assert( arr2 === arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 3 );
+      assert( arr2.length === 2 );
     });
 
     title = callStr('<array>', -1);
     it(title, function() {
-      var arr = vitals.cut.i(newArr(), -1);
-      var be = [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2' ];
-      each(be, function(val, i) {
-        assert(arr[i] === val);
-      });
+      var arr1 = [ 1, 2, 3 ];
+      var arr2 = vitals.cut.i(arr1, -1);
+      assert( is.arr(arr2) );
+      assert( arr2 === arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 2 );
+      assert( arr2.length === 2 );
     });
 
     title = callStr('<array>', 1, 3);
     it(title, function() {
-      var arr = vitals.cut.i(newArr(), 1, 3);
-      var be = [ 'a', 1, 2, 3, 'a1', 'b2', 'c3' ];
-      each(be, function(val, i) {
-        assert(arr[i] === val);
-      });
+      var arr1 = [ 1, 2, 3, 4, 5 ];
+      var arr2 = vitals.cut.i(arr1, 1, 3);
+      assert( is.arr(arr2) );
+      assert( arr2 === arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 4 );
+      assert( arr2[2] === 5 );
+      assert( arr2.length === 3 );
     });
 
-    title = callStr('<array>', 1, -3);
+    title = callStr('<array>', 1, -2);
     it(title, function() {
-      var arr = vitals.cut.i(newArr(), 1, -3);
-      var be = [ 'a', 'a1', 'b2', 'c3' ];
-      each(be, function(val, i) {
-        assert(arr[i] === val);
-      });
+      var arr1 = [ 1, 2, 3, 4, 5 ];
+      var arr2 = vitals.cut.i(arr1, 1, -2);
+      assert( is.arr(arr2) );
+      assert( arr2 === arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 4 );
+      assert( arr2[2] === 5 );
+      assert( arr2.length === 3 );
     });
 
     title = callStr('<array>', -1, -3);
     it(title, function() {
-      var arr = vitals.cut.i(newArr(), -1, -3);
-      var be = [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
-      each(be, function(val, i) {
-        assert(arr[i] === val);
-      });
+      var arr1 = [ 1, 2, 3, 4, 5 ];
+      var arr2 = vitals.cut.i(arr1, -1, -3);
+      assert( is.arr(arr2) );
+      assert( arr2 === arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 2 );
+      assert( arr2[2] === 3 );
+      assert( arr2[3] === 4 );
+      assert( arr2[4] === 5 );
+      assert( arr2.length === 5 );
     });
 
     title = callStr('<array>', -3, -1);
     it(title, function() {
-      var arr = vitals.cut.i(newArr(), -3, -1);
-      var be = [ 'a', 'b', 'c', 1, 2, 3, 'c3' ];
-      each(be, function(val, i) {
-        assert(arr[i] === val);
-      });
+      var arr1 = [ 1, 2, 3, 4, 5 ];
+      var arr2 = vitals.cut.i(arr1, -3, -1);
+      assert( is.arr(arr2) );
+      assert( arr2 === arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 2 );
+      assert( arr2[2] === 5 );
+      assert( arr2.length === 3 );
     });
-
   });
 
 
-  title = titleStr('error', 'should throw an error');
+  title = titleStr('should throw an error');
   describe(title, function() {
+
+    title = callStr();
+    it(title, function() {
+      assert.throws(function() {
+        vitals.cut.i();
+      }, validTypeErr);
+    });
 
     title = callStr([]);
     it(title, function() {
       assert.throws(function() {
         vitals.cut.i([]);
-      });
+      }, validTypeErr);
     });
 
     title = callStr([], 'a');
     it(title, function() {
       assert.throws(function() {
         vitals.cut.i([], 'a');
-      });
+      }, validTypeErr);
     });
 
     title = callStr({}, 1);
     it(title, function() {
       assert.throws(function() {
         vitals.cut.i({}, 1);
-      });
+      }, validTypeErr);
     });
 
     title = callStr(null, 1);
     it(title, function() {
       assert.throws(function() {
         vitals.cut.i(null, 1);
-      });
+      }, validTypeErr);
     });
-
   });
-
 });
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -121,12 +139,11 @@ describe('vitals.cut.index (section:base)', function() {
 
 /**
  * @private
- * @param {string} section
  * @param {string} shouldMsg
  * @return {string}
  */
-function titleStr(section, shouldMsg) {
-  return testTitle(section, shouldMsg, 1);
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 2);
 }
 
 /**
@@ -136,12 +153,4 @@ function titleStr(section, shouldMsg) {
  */
 function callStr() {
   return testCall('cut.i', arguments, 3);
-}
-
-/**
- * @private
- * @return {!Array}
- */
-function newArr() {
-  return [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
 }
