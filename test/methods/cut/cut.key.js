f093e7493909d83c3528a86263dbd9dab42f3f98
Adam Smith <adamsmith@algorithmiv.com>
2016-02-24 02:22:40 -0800

    Rebuilt tests for vitals.cut.key

--- test/methods/cut/cut.key.js
+++ test/methods/cut/cut.key.js
@@ -1,14 +1,12 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - CUT.KEY
+ * VITALS UNIT TESTS: vitals.cut.key
  * -----------------------------------------------------------------------------
- * @see [vitals.cut]{@link https://github.com/imaginate/vitals/wiki/vitals.cut}
+ * @see [vitals.cut docs](https://github.com/imaginate/vitals/wiki/vitals.cut)
+ * @see [global test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
@@ -18,91 +16,72 @@
 describe('vitals.cut.key (section:base)', function() {
   var title;
 
-  title = titleStr('basic', 'should delete prop from obj where key === val');
+  title = titleStr('should delete props from obj where obj owns key');
   describe(title, function() {
-
-    // newObj()= {
-    //   'a':  'd',
-    //   'b':  'e',
-    //   'c':  'f',
-    //   '1':   4,
-    //   '2':   5,
-    //   '3':   6,
-    //   'a1': '1',
-    //   'b2': '2',
-    //   'c3': '3'
-    // }
 
     title = callStr('<object>', 'a');
     it(title, function() {
-      var obj = vitals.cut.key(newObj(), 'a');
-      assert( !has(obj, 'a')  );
-      assert(  has(obj, 'b')  );
-      assert(  has(obj, 'c')  );
-      assert(  has(obj, '1')  );
-      assert(  has(obj, '2')  );
-      assert(  has(obj, '3')  );
-      assert(  has(obj, 'a1') );
-      assert(  has(obj, 'b2') );
-      assert(  has(obj, 'c3') );
+      var obj1 = { 'a': 1, 'b': 2, 'c': 3 };
+      var obj2 = vitals.cut.key(obj1, 'a');
+      assert( !hasOwn(obj2, 'a') );
+      assert(  hasOwn(obj2, 'b') );
+      assert(  hasOwn(obj2, 'c') );
+      assert( obj1 === obj2 );
     });
 
     title = callStr('<object>', 1);
     it(title, function() {
-      var obj = vitals.cut.key(newObj(), 1);
-      assert(  has(obj, 'a')  );
-      assert(  has(obj, 'b')  );
-      assert(  has(obj, 'c')  );
-      assert( !has(obj, '1')  );
-      assert(  has(obj, '2')  );
-      assert(  has(obj, '3')  );
-      assert(  has(obj, 'a1') );
-      assert(  has(obj, 'b2') );
-      assert(  has(obj, 'c3') );
+      var obj1 = {
+        'a': 1, 'b': 2,
+        '1': 3, '2': 4
+      };
+      var obj2 = vitals.cut.key(obj1, 1);
+      assert(  hasOwn(obj2, 'a') );
+      assert(  hasOwn(obj2, 'b') );
+      assert( !hasOwn(obj2, '1') );
+      assert(  hasOwn(obj2, '2') );
+      assert( obj1 === obj2 );
     });
 
     title = callStr('<object>', /a/);
     it(title, function() {
-      var obj = vitals.cut.key(newObj(), /a/);
-      assert(  has(obj, 'a')  );
-      assert(  has(obj, 'b')  );
-      assert(  has(obj, 'c')  );
-      assert(  has(obj, '1')  );
-      assert(  has(obj, '2')  );
-      assert(  has(obj, '3')  );
-      assert(  has(obj, 'a1') );
-      assert(  has(obj, 'b2') );
-      assert(  has(obj, 'c3') );
+      var obj1 = {
+        'a':  1, 'b':  2,
+        'a1': 3, 'b2': 4
+      };
+      var obj2 = vitals.cut.key(obj1, /a/);
+      assert( hasOwn(obj2, 'a') );
+      assert( hasOwn(obj2, 'b') );
+      assert( hasOwn(obj2, 'a1') );
+      assert( hasOwn(obj2, 'b2') );
+      assert( obj1 === obj2 );
     });
-
   });
 
-  title = titleStr('error', 'should throw an error');
+  title = titleStr('should throw an error');
   describe(title, function() {
 
     title = callStr();
     it(title, function() {
       assert.throws(function() {
         vitals.cut.key();
-      });
+      }, validTypeErr);
     });
 
     title = callStr({});
     it(title, function() {
       assert.throws(function() {
         vitals.cut.key({});
-      });
+      }, validErr);
     });
 
     title = callStr(null, 1);
     it(title, function() {
       assert.throws(function() {
         vitals.cut.key(null, 1);
-      });
+      }, validTypeErr);
     });
-
   });
-
 });
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -111,12 +90,11 @@ describe('vitals.cut.key (section:base)', function() {
 
 /**
  * @private
- * @param {string} section
  * @param {string} shouldMsg
  * @return {string}
  */
-function titleStr(section, shouldMsg) {
-  return testTitle(section, shouldMsg, 1);
+function titleStr(shouldMsg) {
+  return breakStr(shouldMsg, 2);
 }
 
 /**
@@ -126,22 +104,4 @@ function titleStr(section, shouldMsg) {
  */
 function callStr() {
   return testCall('cut.key', arguments, 3);
-}
-
-/**
- * @private
- * @return {!Object}
- */
-function newObj() {
-  return {
-    'a':  'd',
-    'b':  'e',
-    'c':  'f',
-    '1':   4,
-    '2':   5,
-    '3':   6,
-    'a1': '1',
-    'b2': '2',
-    'c3': '3'
-  };
 }
