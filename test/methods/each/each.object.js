9de22eef14927f0885a5d123b5ac5780681b1e51
Adam Smith <adamsmith@algorithmiv.com>
2016-02-26 04:08:57 -0800

    Rebuilt tests for vitals.each.object

--- test/methods/each/each.object.js
+++ test/methods/each/each.object.js
@@ -1,171 +1,108 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - EACH.OBJECT
+ * VITALS UNIT TESTS: vitals.each.object
  * -----------------------------------------------------------------------------
- * @see [vitals.each]{@link https://github.com/imaginate/vitals/wiki/vitals.each}
+ * @section base
+ * @see [vitals.each docs](https://github.com/imaginate/vitals/wiki/vitals.each)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.each.object (section:base)', function() {
-  var title;
+method('each.object', 'each.obj', function() {
 
-  // newObj()= {
-  //   'a':  'd',
-  //   'b':  'e',
-  //   'c':  'f',
-  //   '1':   4,
-  //   '2':   5,
-  //   '3':   6,
-  //   'a1': '1',
-  //   'b2': '2',
-  //   'c3': '3'
-  // }
+  should('iterate over all key => value pairs in object', function() {
 
-  describe('basic tests', function() {
-
-    title = titleStr('should iterate over every key => value pair');
-    describe(title, function() {
-
-      title = callStr('<object>', '<iteratee>');
-      it(title, function() {
-        var obj = newObj();
-        vitals.each.obj(newObj(), function(val, key) {
-          assert( has(obj, key)    );
-          assert( obj[key] === val );
-        });
+    test('<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var vals = [];
+      var keys = [];
+      vitals.each.obj(obj, function(val, key) {
+        vals.push(val);
+        keys.push(key);
       });
-
+      assert( hasVal(vals, 1) );
+      assert( hasVal(vals, 2) );
+      assert( hasVal(vals, 3) );
+      assert( vals.length === 3 );
+      assert( hasVal(keys, 'a') );
+      assert( hasVal(keys, 'b') );
+      assert( hasVal(keys, 'c') );
+      assert( keys.length === 3 );
     });
-
-    title = titleStr('should return the valid object');
-    describe(title, function() {
-
-      title = callStr('<object>', '<iteratee>');
-      it(title, function() {
-        var obj1 = newObj();
-        var obj2 = vitals.each.obj(obj1, function(val, key) {});
-        assert( obj1 === obj2 );
-      });
-
-      title = callStr('<object>', '<iteratee>');
-      it(title, function() {
-        var obj1 = newObj();
-        var obj2 = vitals.each.obj(obj1, function(val, key, obj) {});
-        assert( obj1 !== obj2 );
-        each(obj1, function(val, key) {
-          assert( has(obj2, key)    );
-          assert( obj2[key] === val );
-        });
-      });
-
-    });
-
-    title = titleStr('should bind the iteratee correctly');
-    describe(title, function() {
-
-      title = callStr('<object>', '<iteratee>', '<thisArg>');
-      it(title, function() {
-        var obj = newObj();
-        var thisArg = {};
-        vitals.each.obj(obj, function(val, key) {
-          this[key] = val;
-        }, thisArg);
-        each(obj, function(val, key) {
-          assert( has(thisArg, key)    );
-          assert( thisArg[key] === val );
-        });
-      });
-
-    });
-
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.obj();
-        });
-      });
-
-      title = callStr({});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.obj({});
-        });
-      });
-
-      title = callStr(5, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.obj(5, function(){});
-        });
-      });
-
-      title = callStr(null, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.obj(null, function(){});
-        });
-      });
-
-      title = callStr({}, function(){}, 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.obj({}, function(){}, 'fail');
-        });
-      });
-
-    });
-
   });
 
+  should('return the valid result', function() {
+
+    test('<object>', '<iteratee>', function() {
+      var obj1 = { a: 1, b: 2, c: 3 };
+      var obj2 = vitals.each.obj(obj1, function(val, key){});
+      assert( obj2 === obj1 );
+      assert( obj2.a === 1 );
+      assert( obj2.b === 2 );
+      assert( obj2.c === 3 );
+    });
+
+    test('<object>', '<iteratee>', function() {
+      var obj1 = { a: 1, b: 2, c: 3 };
+      var obj2 = vitals.each.obj(obj1, function(val, key, obj){});
+      assert( obj2 !== obj1 );
+      assert( obj2.a === 1 );
+      assert( obj2.b === 2 );
+      assert( obj2.c === 3 );
+    });
+  });
+
+  should('bind the iteratee correctly', function() {
+
+    test('<object>', '<iteratee>', '<this>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var self = {};
+      vitals.each.obj(obj, function(val, key) {
+        this[key] = val;
+      }, self);
+      assert( self.a === 1 );
+      assert( self.b === 2 );
+      assert( self.c === 3 );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.each.obj();
+      }, validTypeErr);
+    });
+
+    test({}, function() {
+      assert.throws(function() {
+        vitals.each.obj({});
+      }, validTypeErr);
+    });
+
+    test(5, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.each.obj(5, function(){});
+      }, validTypeErr);
+    });
+
+    test(null, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.each.obj(null, function(){});
+      }, validTypeErr);
+    });
+
+    test({}, '<iteratee>', 'fail', function() {
+      assert.throws(function() {
+        vitals.each.obj({}, function(){}, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('each.obj', arguments, 4);
-}
-
-/**
- * @private
- * @return {!Object}
- */
-function newObj() {
-  return {
-    'a':  'd',
-    'b':  'e',
-    'c':  'f',
-    '1':   4,
-    '2':   5,
-    '3':   6,
-    'a1': '1',
-    'b2': '2',
-    'c3': '3'
-  };
-}
