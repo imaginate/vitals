27c4774c7c9f8de73cb6812689ca5bfb3a16f11a
Adam Smith <adamsmith@algorithmiv.com>
2016-02-26 02:43:13 -0800

    Added main test setup script

---
+++ test/setup/index.js
@@ -0,0 +1,17 @@
+/**
+ * -----------------------------------------------------------------------------
+ * VITALS UNIT TESTS: setup
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+require('./helpers');
+require('./interface');
+require('./reporters');
