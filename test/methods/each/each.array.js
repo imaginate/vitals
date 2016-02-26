2bff89850b9a1a32e27d7d5bdc1a6a2ba956017f
Adam Smith <adamsmith@algorithmiv.com>
2016-02-26 03:59:16 -0800

    Rebuilt tests for vitals.each.array

--- test/methods/each/each.array.js
+++ test/methods/each/each.array.js
@@ -1,176 +1,209 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - EACH.ARRAY
+ * VITALS UNIT TESTS: vitals.each.array
  * -----------------------------------------------------------------------------
- * @see [vitals.each]{@link https://github.com/imaginate/vitals/wiki/vitals.each}
+ * @section base
+ * @see [vitals.each docs](https://github.com/imaginate/vitals/wiki/vitals.each)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.each.array (section:base)', function() {
-  var title;
+method('each.array', 'each.arr', function() {
 
-  // newArr()= [ "a", "b", "c", 1, 2, 3, "a1", "b2", "c3" ]
+  should('iterate over all index => value pairs in array', function() {
 
-  describe('basic tests', function() {
-
-    title = titleStr('should iterate over every index in order');
-    describe(title, function() {
-
-      title = callStr('<array>', '<iteratee>');
-      it(title, function() {
-        var arr = newArr();
-        var ii = 0;
-        vitals.each.arr(newArr(), function(val, i) {
-          assert( arr[i] === val );
-          assert( ii++ === i );
-        });
+    test('<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      var vals = [];
+      var keys = []; // ensures that indexes are visited in order
+      vitals.each.arr(arr, function(val, i) {
+        vals.push(val);
+        keys.push(i);
       });
-
+      assert( vals[0] === 1 );
+      assert( vals[1] === 2 );
+      assert( vals[2] === 3 );
+      assert( vals.length === 3 );
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
     });
-
-    title = titleStr('should convert string to array & correctly iterate');
-    describe(title, function() {
-
-      title = callStr('a,b,c,1,2,3,a1,b2,c3', '<iteratee>');
-      it(title, function() {
-        var arr = newArr();
-        var ii = 0;
-        vitals.each.arr('a,b,c,1,2,3,a1,b2,c3', function(val, i) {
-          assert( arr[i] == val );
-          assert( ii++ === i );
-        });
-      });
-
-    });
-
-    title = titleStr('should return the valid array');
-    describe(title, function() {
-
-      title = callStr('<array>', '<iteratee>');
-      it(title, function() {
-        var arr1 = newArr();
-        var arr2 = vitals.each.arr(arr1, function(val, i) {});
-        assert( arr1 === arr2 );
-      });
-
-      title = callStr('<array>', '<iteratee>');
-      it(title, function() {
-        var arr1 = newArr();
-        var arr2 = vitals.each.arr(arr1, function(val, i, arr) {});
-        assert( arr1 !== arr2 );
-        assert( arr1.join() === arr2.join() );
-      });
-
-      title = callStr('a,b,c,1,2,3,a1,b2,c3', '<iteratee>');
-      it(title, function() {
-        var arr1 = newArr();
-        var arr2 = vitals.each.arr('a,b,c,1,2,3,a1,b2,c3', function(val, i) {});
-        assert( arr1 !== arr2 );
-        assert( arr1.join() === arr2.join() );
-      });
-
-    });
-
-    title = titleStr('should bind the iteratee correctly');
-    describe(title, function() {
-
-      title = callStr('<array>', '<iteratee>', '<thisArg>');
-      it(title, function() {
-        var arr = newArr();
-        var thisArg = new Array(8);
-        vitals.each.arr(arr, function(val, i) {
-          this[i] = val;
-        }, thisArg);
-        assert( arr.join() === thisArg.join() );
-      });
-
-    });
-
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.arr();
-        });
-      });
-
-      title = callStr([]);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.arr([]);
-        });
-      });
-
-      title = callStr(5, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.arr(5, function(){});
-        });
-      });
-
-      title = callStr({}, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.arr({}, function(){});
-        });
-      });
-
-      title = callStr(null, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.arr(null, function(){});
-        });
-      });
-
-      title = callStr([], function(){}, 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.each.arr([], function(){}, 'fail');
-        });
-      });
-
-    });
-
   });
 
+  should('convert string to array & correctly iterate', function() {
+
+    test('1, 2, 3', '<iteratee>', function() {
+      var vals = [];
+      var keys = [];
+      vitals.each.arr('1, 2, 3', function(val, i) {
+        vals.push(val);
+        keys.push(i);
+      });
+      assert( vals[0] === '1' );
+      assert( vals[1] === '2' );
+      assert( vals[2] === '3' );
+      assert( vals.length === 3 );
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
+    });
+
+    test('1,2,3', '<iteratee>', function() {
+      var vals = [];
+      var keys = [];
+      vitals.each.arr('1,2,3', function(val, i) {
+        vals.push(val);
+        keys.push(i);
+      });
+      assert( vals[0] === '1' );
+      assert( vals[1] === '2' );
+      assert( vals[2] === '3' );
+      assert( vals.length === 3 );
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
+    });
+
+    test('1|2|3', '<iteratee>', function() {
+      var vals = [];
+      var keys = [];
+      vitals.each.arr('1|2|3', function(val, i) {
+        vals.push(val);
+        keys.push(i);
+      });
+      assert( vals[0] === '1' );
+      assert( vals[1] === '2' );
+      assert( vals[2] === '3' );
+      assert( vals.length === 3 );
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
+    });
+
+    test('1 2 3', '<iteratee>', function() {
+      var vals = [];
+      var keys = [];
+      vitals.each.arr('1 2 3', function(val, i) {
+        vals.push(val);
+        keys.push(i);
+      });
+      assert( vals[0] === '1' );
+      assert( vals[1] === '2' );
+      assert( vals[2] === '3' );
+      assert( vals.length === 3 );
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
+    });
+  });
+
+  should('return the valid result', function() {
+
+    test('<array>', '<iteratee>', function() {
+      var arr1 = [ 1, 2, 3 ];
+      var arr2 = vitals.each.arr(arr1, function(val, i){});
+      assert( is.arr(arr2) );
+      assert( arr2 === arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 2 );
+      assert( arr2[2] === 3 );
+      assert( arr2.length === 3 );
+    });
+
+    test('<array>', '<iteratee>', function() {
+      var arr1 = [ 1, 2, 3 ];
+      var arr2 = vitals.each.arr(arr1, function(val, i, arr){});
+      assert( is.arr(arr2) );
+      assert( arr2 !== arr1 );
+      assert( arr2[0] === 1 );
+      assert( arr2[1] === 2 );
+      assert( arr2[2] === 3 );
+      assert( arr2.length === 3 );
+    });
+
+    test('1,2,3', '<iteratee>', function() {
+      var arr = vitals.each.arr('1,2,3', function(val, i){});
+      assert( is.arr(arr) );
+      assert( arr[0] === '1' );
+      assert( arr[1] === '2' );
+      assert( arr[2] === '3' );
+      assert( arr.length === 3 );
+    });
+
+    test('1,2,3', '<iteratee>', function() {
+      var arr = vitals.each.arr('1,2,3', function(val, i, arr){});
+      assert( is.arr(arr) );
+      assert( arr[0] === '1' );
+      assert( arr[1] === '2' );
+      assert( arr[2] === '3' );
+      assert( arr.length === 3 );
+    });
+  });
+
+  should('bind the iteratee correctly', function() {
+
+    test('<array>', '<iteratee>', '<this>', function() {
+      var arr = [ 1, 2, 3 ];
+      var self = new Array(3);
+      vitals.each.arr(arr, function(val, i) {
+        this[i] = val;
+      }, self);
+      assert( is.arr(self) );
+      assert( self[0] === 1 );
+      assert( self[1] === 2 );
+      assert( self[2] === 3 );
+      assert( self.length === 3 );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.each.arr();
+      }, validTypeErr);
+    });
+
+    test([], function() {
+      assert.throws(function() {
+        vitals.each.arr([]);
+      }, validTypeErr);
+    });
+
+    test(5, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.each.arr(5, function(){});
+      }, validTypeErr);
+    });
+
+    test({}, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.each.arr({}, function(){});
+      }, validTypeErr);
+    });
+
+    test(null, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.each.arr(null, function(){});
+      }, validTypeErr);
+    });
+
+    test([], '<iteratee>', 'fail', function() {
+      assert.throws(function() {
+        vitals.each.arr([], function(){}, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('each.arr', arguments, 4);
-}
-
-/**
- * @private
- * @return {!Array}
- */
-function newArr() {
-  return [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
-}
