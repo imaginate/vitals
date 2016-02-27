6c40cda81ee4f8e57c4bc23b6e833ec9f700f374
Adam Smith <adamsmith@algorithmiv.com>
2016-02-26 16:49:00 -0800

    Rebuilt tests for vitals.fill.object

--- test/methods/fill/fill.object.js
+++ test/methods/fill/fill.object.js
@@ -1,148 +1,81 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - FILL.OBJECT
+ * VITALS UNIT TESTS: vitals.fill.object
  * -----------------------------------------------------------------------------
- * @see [vitals.fill]{@link https://github.com/imaginate/vitals/wiki/vitals.fill}
+ * @section base
+ * @see [vitals.fill docs](https://github.com/imaginate/vitals/wiki/vitals.fill)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.fill.object (section:base)', function() {
-  var title;
+method('fill.object', 'fill.obj', function() {
 
-  describe('basic tests', function() {
+  should('fill object properties with val', function() {
 
-    // newObj()= {
-    //   'a':  'd',
-    //   'b':  'e',
-    //   'c':  'f',
-    //   '1':   4,
-    //   '2':   5,
-    //   '3':   6,
-    //   'a1': '1',
-    //   'b2': '2',
-    //   'c3': '3'
-    // }
-
-    title = titleStr('should fill object properties with val');
-    describe(title, function() {
-
-      title = callStr('<object>', 5);
-      it(title, function() {
-        var keys = newObj(true);
-        var obj = vitals.fill.obj(newObj(), 5);
-        each(keys, function(key) {
-          assert( has(obj, key)  );
-          assert( obj[key] === 5 );
-        });
-      });
-
-      title = callStr('<object>', 'a,b,c', 5);
-      it(title, function() {
-        var obj1 = newObj();
-        var obj2 = vitals.fill.obj(newObj(), 'a,b,c', 5);
-        each(obj1, function(val, key) {
-          assert( has(obj2, key) );
-          if ( /^[a-z]$/.test(key) ) assert( obj2[key] === 5 );
-          else assert( obj2[key] === val );
-        });
-      });
-
-      title = callStr('<object>', [ 'x', 'y', 'z' ], 5);
-      it(title, function() {
-        var obj = vitals.fill.obj(newObj(), [ 'x', 'y', 'z' ], 5);
-        assert( obj.x === 5 );
-        assert( obj.y === 5 );
-        assert( obj.z === 5 );
-      });
-
+    test('<object>', 5, function() {
+      var obj1 = { a: 1, b: 2, c: 3 };
+      var obj2 = vitals.fill.obj(obj1, 5);
+      assert( obj2.a === 5 );
+      assert( obj2.b === 5 );
+      assert( obj2.c === 5 );
+      assert( obj2 === obj1 );
     });
 
-  });
+    test('<object>', 'a,b,c', 6, function() {
+      var obj1 = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+      var obj2 = vitals.fill.obj(obj1, 'a,b,c', 6);
+      assert( obj2.a === 6 );
+      assert( obj2.b === 6 );
+      assert( obj2.c === 6 );
+      assert( obj2.d === 4 );
+      assert( obj2.e === 5 );
+      assert( obj2 === obj1 );
+    });
 
-  describe('error tests', function() {
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.fill.obj();
-        });
-      });
-
-      title = callStr({});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.fill.obj({});
-        });
-      });
-
-      title = callStr(5, 5);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.fill.obj(5, 5);
-        });
-      });
-
-      title = callStr(null, 5);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.fill.obj(null, 5);
-        });
-      });
-
+    test('<object>', [ 'a', 'b', 'c' ], 6, function() {
+      var obj1 = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+      var keys = [ 'a', 'b', 'c' ];
+      var obj2 = vitals.fill.obj(obj1, keys, 6);
+      assert( obj2.a === 6 );
+      assert( obj2.b === 6 );
+      assert( obj2.c === 6 );
+      assert( obj2.d === 4 );
+      assert( obj2.e === 5 );
+      assert( obj2 === obj1 );
     });
   });
 
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.fill.obj();
+      }, validTypeErr);
+    });
+
+    test({}, function() {
+      assert.throws(function() {
+        vitals.fill.obj({});
+      }, validErr);
+    });
+
+    test(5, 5, function() {
+      assert.throws(function() {
+        vitals.fill.obj(5, 5);
+      }, validTypeErr);
+    });
+
+    test(null, 5, function() {
+      assert.throws(function() {
+        vitals.fill.obj(null, 5);
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('fill.obj', arguments, 4);
-}
-
-/**
- * @private
- * @param {boolean=} keys
- * @return {!Object}
- */
-function newObj(keys) {
-  return keys
-    ? [ 'a', 'b', 'c', '1', '2', '3', 'a1', 'b2', 'c3' ]
-    : {
-      'a':  'd',
-      'b':  'e',
-      'c':  'f',
-      '1':   4,
-      '2':   5,
-      '3':   6,
-      'a1': '1',
-      'b2': '2',
-      'c3': '3'
-    };
-}
