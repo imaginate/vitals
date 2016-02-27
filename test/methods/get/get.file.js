574587bf046b7d9c4d1edb7efe61ca0a740cfd36
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 00:34:31 -0800

    Rebuilt tests for vitals.get.file

--- test/methods/get/get.file.js
+++ test/methods/get/get.file.js
@@ -1,14 +1,14 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - GET.FILE
+ * VITALS UNIT TESTS: vitals.get.file
  * -----------------------------------------------------------------------------
- * @see [vitals.get]{@link https://github.com/imaginate/vitals/wiki/vitals.get}
+ * @section fs
+ * @see [vitals.get docs](https://github.com/imaginate/vitals/wiki/vitals.get)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
@@ -17,98 +17,75 @@
 
 if (BROWSER_TESTS) return;
 
+var addBase = DUMMY.addBase;
 var fs = require('fs');
 
-describe('vitals.get.file (section:fs)', function() {
-  var title;
+var CONTENT = DUMMY.content; // content for dummy files
+var BASE = DUMMY.base.replace(/\/$/, ''); // base directory for dummy files
 
-  describe('basic tests', function() {
+method('get.file', function() {
+  this.slow(25);
 
-    before('setup dummy dirs and files', function() {
-      mkDummy('fake.js');
-    });
-
-    title = titleStr('should return a buffer of the file');
-    describe(title, function() {
-
-      title = callStr('fake.js', true);
-      it(title, function() {
-        var file = addBase('fake.js');
-        var result = vitals.get.file(file, true);
-        assert( is.buffer(result) );
-        result = result.toString();
-        assert( result === DUMMY.content );
-        result = fs.readFileSync(file);
-        result = result.toString();
-        assert( result === DUMMY.content );
-      });
-
-    });
-
-    title = titleStr('should return a valid string of the file');
-    describe(title, function() {
-
-      title = callStr('fake.js');
-      it(title, function() {
-        var file = addBase('fake.js');
-        var result = vitals.get.file(file);
-        assert( result === DUMMY.content );
-        result = fs.readFileSync(file, 'utf8');
-        assert( result === DUMMY.content );
-      });
-
-      title = callStr('fake.js', { eol: 'CRLF' });
-      it(title, function() {
-        var file = addBase('fake.js');
-        var result = vitals.get.file(file, { eol: 'CRLF' });
-        var content = setEol(DUMMY.content, 'CRLF');
-        assert( result === content );
-        result = fs.readFileSync(file, 'utf8');
-        result = setEol(result, 'CRLF');
-        assert( result === content );
-      });
-
-    });
-
-    after('clean up dummy dirs and files', rmDummy);
-
+  before('setup dummy dirs and files', function() {
+    mkDummy('file.js');
   });
 
-  describe('error tests', function() {
-    describe('should throw an error', function() {
+  after('clean up dummy dirs and files', rmDummy);
 
-      before('setup dummy dirs and files', function() {
-        mkDummy('fake.js');
-      });
+  should('return a buffer of the file', function() {
 
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.get.file();
-        });
-      });
-
-      title = callStr('invalid.js');
-      it(title, function() {
-        assert.throws(function() {
-          var file = addBase('invalid.js');
-          vitals.get.file(file);
-        });
-      });
-
-      title = callStr('fake.js', 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          var file = addBase('fake.js');
-          vitals.get.file(file, 'fail');
-        });
-      });
-
-      after('clean up dummy dirs and files', rmDummy);
-
+    test('file.js', true, function() {
+      var file = addBase('file.js');
+      var result = vitals.get.file(file, true);
+      assert( isBuffer(result) );
+      result = result.toString();
+      assert( result === CONTENT );
+      assert( validFile(file) );
     });
   });
 
+  should('return a valid string of the file', function() {
+
+    test('file.js', function() {
+      var file = addBase('file.js');
+      var result = vitals.get.file(file);
+      assert( result === CONTENT );
+      assert( validFile(file, true) );
+    });
+
+    test('file.js', { eol: 'CRLF' }, function() {
+      var file = addBase('file.js');
+      var result = vitals.get.file(file, { eol: 'CRLF' });
+      var content = setEol(CONTENT, 'CRLF');
+      assert( result === content );
+      result = fs.readFileSync(file, 'utf8');
+      result = setEol(result, 'CRLF');
+      assert( result === content );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.get.file();
+      }, validTypeErr);
+    });
+
+    test('invalid.js', function() {
+      assert.throws(function() {
+        var file = addBase('invalid.js');
+        vitals.get.file(file);
+      }, validTypeErr);
+    });
+
+    test('file.js', 'fail', function() {
+      assert.throws(function() {
+        var file = addBase('file.js');
+        vitals.get.file(file, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -117,35 +94,20 @@ describe('vitals.get.file (section:fs)', function() {
 
 /**
  * @private
- * @param {string} shouldMsg
- * @return {string}
+ * @param {string} filepath
+ * @param {boolean=} encode
+ * @return {boolean}
  */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr(args) {
-  args = remap(arguments, function(val, i) {
-    return i ? val : addBase(val);
-  });
-  return testCall('get.file', args, 4);
-}
-
-/**
- * @private
- * @param {string} file
- * @return {string}
- */
-function addBase(file) {
+function validFile(filepath, encode) {
 
   /** @type {string} */
-  var base;
+  var content;
 
-  base = cut(DUMMY.base, /\/$/);
-  return fuse(base, '/', file);
+  if ( !isFile(filepath) ) return false;
+
+  content = encode
+    ? fs.readFileSync(filepath, 'utf8')
+    : fs.readFileSync(filepath).toString();
+  if (encode) content = setEol(content, 'LF');
+  return CONTENT === content;
 }
