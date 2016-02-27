4e459cafb69ba74ced3e943c7e2e0afe8d03dbc9
Adam Smith <adamsmith@algorithmiv.com>
2016-02-26 20:24:12 -0800

    Rebuilt tests for vitals.fuse.value

--- test/methods/fuse/fuse.value.js
+++ test/methods/fuse/fuse.value.js
@@ -1,180 +1,146 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - FUSE.VALUE
+ * VITALS UNIT TESTS: vitals.fuse.value
  * -----------------------------------------------------------------------------
- * @see [vitals.fuse]{@link https://github.com/imaginate/vitals/wiki/vitals.fuse}
+ * @section base
+ * @see [vitals.fuse docs](https://github.com/imaginate/vitals/wiki/vitals.fuse)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.fuse.value (section:base)', function() {
-  var title;
+method('fuse.value', 'fuse.val', function() {
 
-  describe('object tests', function() {
+  should('add new props to dest obj', function() {
 
-    title = titleStr('should add new props to dest obj');
-    describe(title, function() {
-
-      title = callStr({ a: 1 }, { a: 10, z: 10 });
-      it(title, function() {
-        var obj = { a: 10, z: 10 };
-        var dest = vitals.fuse.val({ a: 1 }, obj);
-        assert( dest.a === 1 );
-        assert( has(dest, obj) );
-        assert( dest[obj] === undefined );
-      });
-
-      title = callStr({ a: 1 }, 'z');
-      it(title, function() {
-        var obj = vitals.fuse.val({ a: 1 }, 'z');
-        assert( obj.a === 1 );
-        assert( has(obj, 'z') );
-        assert( obj.z === undefined );
-      });
-
-      title = callStr({ a: 1 }, 'a', 'b', 'c', null);
-      it(title, function() {
-        var obj = vitals.fuse.val({ a: 1 }, 'a', 'b', 'c', null);
-        each([ 'a','b','c','null' ], function(key) {
-          assert( has(obj, key) );
-          assert( obj[key] === undefined );
-        });
-      });
-
-      title = callStr({ a: 1 }, [ 'a', 'b', 'c' ]);
-      it(title, function() {
-        var arr = [ 'a', 'b', 'c' ];
-        var obj = vitals.fuse.val({ a: 1 }, arr);
-        assert( obj.a === 1 );
-        assert( has(obj, arr) );
-        assert( obj[arr] === undefined );
-      });
-
+    test({ a: 1 }, { a: 10, z: 10 }, function() {
+      var dest = { a: 1 };
+      var obj = vitals.fuse.val(dest, { a: 10, z: 10 });
+      assert( obj === dest );
+      assert(  hasOwn(dest, 'a') );
+      assert( !hasOwn(dest, 'z') );
+      assert(  hasOwn(dest, '[object Object]') );
+      assert( obj.a === 1 );
+      assert( obj['[object Object]'] === undefined );
     });
 
-  });
-
-  describe('array tests', function() {
-
-    title = titleStr('should push new properties to dest array');
-    describe(title, function() {
-
-      title = callStr([], 5);
-      it(title, function() {
-        var arr = vitals.fuse.val([], 5);
-        assert( arr.length === 1 );
-        assert( arr[0] === 5 );
-      });
-
-      title = callStr([], [ 5 ]);
-      it(title, function() {
-        var arr = [ 5 ];
-        var dest = vitals.fuse.val([], arr);
-        assert( dest.length === 1 );
-        assert( dest[0] === arr );
-      });
-
-      title = callStr([], 5, true, null);
-      it(title, function() {
-        var arr = vitals.fuse.val([], 5, true, null);
-        assert( arr.length === 3 );
-        assert( arr[0] === 5 );
-        assert( arr[1] === true );
-        assert( arr[2] === null );
-      });
-
+    test({ a: 1 }, 'z', function() {
+      var dest = { a: 1 };
+      var obj = vitals.fuse.val(dest, 'z');
+      assert( obj === dest );
+      assert( hasOwn(dest, 'a') );
+      assert( hasOwn(dest, 'z') );
+      assert( obj.a === 1 );
+      assert( obj.z === undefined );
     });
 
-  });
-
-  describe('string tests', function() {
-
-    title = titleStr('should append strings to dest string');
-    describe(title, function() {
-
-      title = callStr('', 5);
-      it(title, function() {
-        var str = vitals.fuse.val('', 5);
-        assert( str === '5' );
-      });
-
-      title = callStr('', 'a', 5);
-      it(title, function() {
-        var str = vitals.fuse.val('', 'a', 5);
-        assert( str === 'a5' );
-      });
-
-      title = callStr('', [ 'a', 5 ]);
-      it(title, function() {
-        var str = vitals.fuse.val('', [ 'a', 5 ]);
-        assert( str === 'a,5' );
-      });
-
-      title = callStr('', 5, [ 'a', 'b' ]);
-      it(title, function() {
-        var str = vitals.fuse.val('', 5, [ 'a', 'b' ]);
-        assert( str === '5a,b' );
-      });
-
+    test({ a: 1 }, 'a', 'b', null, function() {
+      var dest = { a: 1 };
+      var obj = vitals.fuse.val(dest, 'a', 'b', null);
+      assert( obj === dest );
+      assert( hasOwn(dest, 'a') );
+      assert( hasOwn(dest, 'b') );
+      assert( hasOwn(dest, 'null') );
+      assert( obj.a === undefined );
+      assert( obj.b === undefined );
+      assert( obj['null'] === undefined );
     });
 
-  });
-
-  describe('error tests', function() {
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.fuse.val();
-        });
-      });
-
-      title = callStr({});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.fuse.val({});
-        });
-      });
-
-      title = callStr(null, 5);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.fuse.val(null, 5);
-        });
-      });
-
+    test({ a: 1 }, [ 'a', 'b' ], function() {
+      var dest = { a: 1 };
+      var obj = vitals.fuse.val(dest, [ 'a', 'b' ]);
+      assert( obj === dest );
+      assert(  hasOwn(dest, 'a') );
+      assert( !hasOwn(dest, 'b') );
+      assert(  hasOwn(dest, 'a,b') );
+      assert( obj.a === 1 );
+      assert( obj['a,b'] === undefined );
     });
   });
 
+  should('push new properties to dest array', function() {
+
+    test([ 1 ], 5, function() {
+      var dest = [ 1 ];
+      var arr = vitals.fuse.val(dest, 5);
+      assert( is.arr(arr) );
+      assert( arr === dest );
+      assert( arr[0] === 1 );
+      assert( arr[1] === 5 );
+      assert( arr.length === 2 );
+    });
+
+    test([ 1 ], 5, true, null, function() {
+      var dest = [ 1 ];
+      var arr = vitals.fuse.val(dest, 5, true, null);
+      assert( is.arr(arr) );
+      assert( arr === dest );
+      assert( arr[0] === 1 );
+      assert( arr[1] === 5 );
+      assert( arr[2] === true );
+      assert( arr[3] === null );
+      assert( arr.length === 4 );
+    });
+
+    test([ 1 ], [ 5, true, null ], function() {
+      var dest = [ 1 ];
+      var prop = [ 5, true, null ];
+      var arr = vitals.fuse.val(dest, prop);
+      assert( is.arr(arr) );
+      assert( arr === dest );
+      assert( arr[0] === 1 );
+      assert( arr[1] === prop );
+      assert( arr.length === 2 );
+    });
+  });
+
+  should('append values to dest string', function() {
+
+    test('v', 5, function() {
+      var str = vitals.fuse.val('v', 5);
+      assert( str === 'v5' );
+    });
+
+    test('v', 'a', 5, function() {
+      var str = vitals.fuse.val('v', 'a', 5);
+      assert( str === 'va5' );
+    });
+
+    test('v', [ 'a', 5 ], function() {
+      var str = vitals.fuse.val('v', [ 'a', 5 ]);
+      assert( str === 'va,5' );
+    });
+
+    test('v', 5, [ 'a', 'b' ], function() {
+      var str = vitals.fuse.val('v', 5, [ 'a', 'b' ]);
+      assert( str === 'v5a,b' );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.fuse.val();
+      }, validErr);
+    });
+
+    test({}, function() {
+      assert.throws(function() {
+        vitals.fuse.val({});
+      }, validErr);
+    });
+
+    test(null, 5, function() {
+      assert.throws(function() {
+        vitals.fuse.val(null, 5);
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('fuse.val', arguments, 4);
-}
