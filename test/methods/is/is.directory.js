b5b0eb7fb8f2d758079fcdaa54117dfe12df622e
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:32:49 -0800

    Rebuilt tests for vitals.is.directory

--- test/methods/is/is.directory.js
+++ test/methods/is/is.directory.js
@@ -1,14 +1,14 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS.DIRECTORY
+ * VITALS UNIT TESTS: vitals.is.directory
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section fs
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
@@ -17,117 +17,61 @@
 
 if (BROWSER_TESTS) return;
 
-describe('vitals.is.directory (section:fs)', function() {
-  var title;
+var addBase = DUMMY.addBase;
 
-  describe('basic tests', function() {
+method('is.directory', 'is.dir', function() {
 
-    before('setup dummy dirs and files', function() {
-      mkDummy({ 'subdir1': null, 'subdir2': null, 'subdir3': null });
-    });
-
-    title = titleStr('should return true');
-    describe(title, function() {
-
-      title = callStr('subdir1');
-      it(title, function() {
-        var dir = addBase('subdir1');
-        var result = vitals.is.dir(dir);
-        assert( result === true );
-      });
-
-      title = callStr('subdir1', 'subdir2', 'subdir3');
-      it(title, function() {
-        var dir1 = addBase('subdir1');
-        var dir2 = addBase('subdir2');
-        var dir3 = addBase('subdir3');
-        var result = vitals.is.dir(dir1, dir2, dir3);
-        assert( result === true );
-      });
-
-    });
-
-    title = titleStr('should return false');
-    describe(title, function() {
-
-      title = callStr(null);
-      it(title, function() {
-        var result = vitals.is.dir(null);
-        assert( result === false );
-      });
-
-      title = callStr('invalid');
-      it(title, function() {
-        var dir = addBase('invalid');
-        var result = vitals.is.dir(dir);
-        assert( result === false );
-      });
-
-      title = callStr('subdir1', 'invalid', 'subdir3');
-      it(title, function() {
-        var dir1 = addBase('subdir1');
-        var dir2 = addBase('invalid');
-        var dir3 = addBase('subdir3');
-        var result = vitals.is.dir(dir1, dir2, dir3);
-        assert( result === false );
-      });
-
-    });
-
-    after('clean up dummy dirs and files', rmDummy);
-
+  before('setup dummy dirs and files', function() {
+    mkDummy({ 'subdir1': null, 'subdir2': null, 'subdir3': null });
   });
 
-  describe('error tests', function() {
-    describe('should throw an error', function() {
+  after('clean up dummy dirs and files', rmDummy);
 
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.is.dir();
-        });
-      });
+  should('return true', function() {
 
+    test('subdir1', function() {
+      var dir = addBase('subdir1');
+      var result = vitals.is.dir(dir);
+      assert( result === true );
+    });
+
+    test('subdir1', 'subdir2', 'subdir3', function() {
+      var dir1 = addBase('subdir1');
+      var dir2 = addBase('subdir2');
+      var dir3 = addBase('subdir3');
+      var result = vitals.is.dir(dir1, dir2, dir3);
+      assert( result === true );
     });
   });
 
+  should('return false', function() {
+
+    test(null, function() {
+      var result = vitals.is.dir(null);
+      assert( result === false );
+    });
+
+    test('invalid', function() {
+      var dir = addBase('invalid');
+      var result = vitals.is.dir(dir);
+      assert( result === false );
+    });
+
+    test('subdir1', 'invalid', 'subdir3', function() {
+      var dir1 = addBase('subdir1');
+      var dir2 = addBase('invalid');
+      var dir3 = addBase('subdir3');
+      var result = vitals.is.dir(dir1, dir2, dir3);
+      assert( result === false );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.is.dir();
+      }, validErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr(args) {
-  args = remap(arguments, function(val, i) {
-    return val && addBase(val);
-  });
-  return testCall('is.dir', args, 4);
-}
-
-/**
- * @private
- * @param {string=} dir
- * @return {string}
- */
-function addBase(dir) {
-
-  /** @type {string} */
-  var base;
-
-  base = cut(DUMMY.base, /\/$/);
-  return dir ? fuse(base, '/', dir) : base;
-}
