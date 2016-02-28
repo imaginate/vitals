58ffc076a308c2b6d742214cbd4f958030191c9e
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:34:15 -0800

    Rebuilt tests for vitals.is.regexp

--- test/methods/is/is.regexp.js
+++ test/methods/is/is.regexp.js
@@ -1,89 +1,54 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS.REGEXP
+ * VITALS UNIT TESTS: vitals.is.regexp
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section base
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.is.regexp (section:base)', function() {
-  var title;
+method('is.regexp', 'is.regex', function() {
 
-  title = titleStr('should return true');
-  describe(title, function() {
+  should('return true', function() {
 
-    title = callStr(/re/);
-    it(title, function() {
+    test(/re/, function() {
       var result = vitals.is.regex(/re/);
       assert( result === true );
     });
 
-    title = callStr(/re/, /re/, /re/);
-    it(title, function() {
+    test(/re/, /re/, /re/, function() {
       var result = vitals.is.regex(/re/, /re/, /re/);
       assert( result === true );
     });
-
   });
 
-  title = titleStr('should return false');
-  describe(title, function() {
+  should('return false', function() {
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       var result = vitals.is.regex(null);
       assert( result === false );
     });
 
-    title = callStr(/re/, /re/, {});
-    it(title, function() {
+    test(/re/, /re/, {}, function() {
       var result = vitals.is.regex(/re/, /re/, {});
       assert( result === false );
     });
-
   });
 
-  title = titleStr('should throw an error');
-  describe(title, function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.is.regex();
-      });
+      }, validErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('is.regex', arguments, 3);
-}
