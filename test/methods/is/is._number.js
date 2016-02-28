8c7daaa6d2d18bf380f4f467766cc66e12310ba2
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:34:54 -0800

    Rebuilt tests for vitals.is._number

--- test/methods/is/is._number.js
+++ test/methods/is/is._number.js
@@ -1,89 +1,54 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS._NUMBER
+ * VITALS UNIT TESTS: vitals.is._number
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section base
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.is._number (section:base)', function() {
-  var title;
+method('is._number', 'is._num', function() {
 
-  title = titleStr('should return true');
-  describe(title, function() {
+  should('return true', function() {
 
-    title = callStr(5);
-    it(title, function() {
+    test(5, function() {
       var result = vitals.is._num(5);
       assert( result === true );
     });
 
-    title = callStr(-1, 1, 5);
-    it(title, function() {
+    test(-1, 1, 5, function() {
       var result = vitals.is._num(-1, 1, 5);
       assert( result === true );
     });
-
   });
 
-  title = titleStr('should return false');
-  describe(title, function() {
+  should('return false', function() {
 
-    title = callStr(0);
-    it(title, function() {
+    test(0, function() {
       var result = vitals.is._num(0);
       assert( result === false );
     });
 
-    title = callStr(new Number(1), 1, 5);
-    it(title, function() {
+    test('<Number>', 1, 5, function() {
       var result = vitals.is._num(new Number(1), 1, 5);
       assert( result === false );
     });
-
   });
 
-  title = titleStr('should throw an error');
-  describe(title, function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.is._num();
-      });
+      }, validErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('is._num', arguments, 3);
-}
