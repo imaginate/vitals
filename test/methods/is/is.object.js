f4595af8ea886793d3b619a004c1a6e8e29b59ab
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:34:06 -0800

    Rebuilt tests for vitals.is.object

--- test/methods/is/is.object.js
+++ test/methods/is/is.object.js
@@ -1,89 +1,54 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS.OBJECT
+ * VITALS UNIT TESTS: vitals.is.object
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section base
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.is.object (section:base)', function() {
-  var title;
+method('is.object', 'is.obj', function() {
 
-  title = titleStr('should return true');
-  describe(title, function() {
+  should('return true', function() {
 
-    title = callStr({});
-    it(title, function() {
+    test({}, function() {
       var result = vitals.is.obj({});
       assert( result === true );
     });
 
-    title = callStr({}, {}, {});
-    it(title, function() {
+    test({}, {}, {}, function() {
       var result = vitals.is.obj({}, {}, {});
       assert( result === true );
     });
-
   });
 
-  title = titleStr('should return false');
-  describe(title, function() {
+  should('return false', function() {
 
-    title = callStr(function(){});
-    it(title, function() {
+    test(function(){}, function() {
       var result = vitals.is.obj(function(){});
       assert( result === false );
     });
 
-    title = callStr(null, {}, {});
-    it(title, function() {
+    test(null, {}, {}, function() {
       var result = vitals.is.obj(null, {}, {});
       assert( result === false );
     });
-
   });
 
-  title = titleStr('should throw an error');
-  describe(title, function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.is.obj();
-      });
+      }, validErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('is.obj', arguments, 3);
-}
