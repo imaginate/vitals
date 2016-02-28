7db0bc06550a7b33387c420f279b7f7006b9f9b7
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 20:37:29 -0800

    Rebuilt tests for vitals.has.enumerable

--- test/methods/has/has.enumerable.js
+++ test/methods/has/has.enumerable.js
@@ -1,142 +1,67 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - HAS.ENUMERABLE
+ * VITALS UNIT TESTS: vitals.has.enumerable
  * -----------------------------------------------------------------------------
- * @see [vitals.has]{@link https://github.com/imaginate/vitals/wiki/vitals.has}
+ * @section base
+ * @see [vitals.has docs](https://github.com/imaginate/vitals/wiki/vitals.has)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.has.enumerable (section:base)', function() {
-  var title;
+method('has.enumerable', 'has.enum', function() {
 
-  describe('basic tests', function() {
+  should('return true', function() {
 
-    // newObj()= {
-    //   'a':  'd',
-    //   'b':  'e', // not enumerable
-    //   'c':  'f',
-    //   '1':   4,
-    //   '2':   5,
-    //   '3':   6,
-    //   'a1': '1',
-    //   'b2': '2',
-    //   'c3': '3'
-    // }
-
-    title = titleStr('should return true');
-    describe(title, function() {
-
-      title = callStr('<object>', 'a');
-      it(title, function() {
-        assert( vitals.has.enum(newObj(), 'a') );
-      });
-
-      title = callStr('<object>', 1);
-      it(title, function() {
-        assert( vitals.has.enum(newObj(), 1) );
-      });
-
+    test('<object>', 'a', function() {
+      var result = vitals.has.enum({ a: 1 }, 'a');
+      assert( result === true );
     });
 
-    title = titleStr('should return false');
-    describe(title, function() {
-
-      title = callStr(null, 'key');
-      it(title, function() {
-        assert( !vitals.has.enum(null, 'key') );
-      });
-
-      title = callStr('<object>', 'b');
-      it(title, function() {
-        assert( !vitals.has.enum(newObj(), 'b') );
-      });
-
-    });
-
-  });
-
-  describe('error tests', function() {
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.has.enum();
-        });
-      });
-
-      title = callStr({});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.has.enum({});
-        });
-      });
-
-      title = callStr('str', 'key');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.has.enum('str', 'key');
-        });
-      });
-
+    test('<object>', 1, function() {
+      var result = vitals.has.enum({ '1': 'a' }, 1);
+      assert( result === true );
     });
   });
 
+  should('return false', function() {
+
+    test(null, 'a', function() {
+      var result = vitals.has.enum(null, 'a');
+      assert( result === false );
+    });
+
+    test('<object>', 'a', function() {
+      var obj = Object.defineProperty({ a: 1 }, 'a', { enumerable: false });
+      var result = vitals.has.enum(obj, 'a');
+      assert( result === false );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.has.enum();
+      }, validErr);
+    });
+
+    test({}, function() {
+      assert.throws(function() {
+        vitals.has.enum({});
+      }, validErr);
+    });
+
+    test('str', 'key', function() {
+      assert.throws(function() {
+        vitals.has.enum('str', 'key');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('has.enum', arguments, 4);
-}
-
-/**
- * @private
- * @param {boolean=} keys
- * @return {!Object}
- */
-function newObj(keys) {
-  return keys
-    ? [ 'a', 'b', 'c', '1', '2', '3', 'a1', 'b2', 'c3' ]
-    : Object.defineProperty({
-      'a':  'd',
-      'b':  'e',
-      'c':  'f',
-      '1':   4,
-      '2':   5,
-      '3':   6,
-      'a1': '1',
-      'b2': '2',
-      'c3': '3'
-    }, 'b', {
-      value: 'e',
-      writable: true,
-      enumerable: false,
-      configurable: true
-    });
-}
