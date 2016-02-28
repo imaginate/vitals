6d99953598bf6852163595599b403f87f4810250
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:33:02 -0800

    Rebuilt tests for vitals.is.element

--- test/methods/is/is.element.js
+++ test/methods/is/is.element.js
@@ -1,96 +1,61 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS.ELEMENT
+ * VITALS UNIT TESTS: vitals.is.element
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section base
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.is.element (section:base)', function() {
-  var title;
+method('is.element', 'is.elem', function() {
 
-  title = titleStr('should return true');
-  describe(title, function() {
+  should('return true', function() {
 
-    title = callStr({ nodeType: 1 });
-    it(title, function() {
+    test('<Element>', function() {
       var elem = { nodeType: 1 };
       var result = vitals.is.elem(elem);
       assert( result === true );
     });
 
-    title = callStr({ nodeType: 1 }, { nodeType: 1 }, { nodeType: 1 });
-    it(title, function() {
+    test('<Element>', '<Element>', '<Element>', function() {
       var elem1 = { nodeType: 1 };
       var elem2 = { nodeType: 1 };
       var elem3 = { nodeType: 1 };
       var result = vitals.is.elem(elem1, elem2, elem3);
       assert( result === true );
     });
-
   });
 
-  title = titleStr('should return false');
-  describe(title, function() {
+  should('return false', function() {
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       var result = vitals.is.elem(null);
       assert( result === false );
     });
 
-    title = callStr({ nodeType: 1 }, { nodeType: 1 }, { nodeType: 3 });
-    it(title, function() {
+    test('<Element>', '<Element>', '<Object>', function() {
       var elem1 = { nodeType: 1 };
       var elem2 = { nodeType: 1 };
       var misc3 = { nodeType: 3 };
       var result = vitals.is.elem(elem1, elem2, misc3);
       assert( result === false );
     });
-
   });
 
-  title = titleStr('should throw an error');
-  describe(title, function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.is.elem();
-      });
+      }, validErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('is.elem', arguments, 3);
-}
