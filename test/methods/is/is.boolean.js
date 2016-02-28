5a61bacfe3f59616f868df3d5642cee59e0561a3
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:32:30 -0800

    Rebuilt tests for vitals.is.boolean

--- test/methods/is/is.boolean.js
+++ test/methods/is/is.boolean.js
@@ -1,89 +1,54 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS.BOOLEAN
+ * VITALS UNIT TESTS: vitals.is.boolean
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section base
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.is.boolean (section:base)', function() {
-  var title;
+method('is.boolean', 'is.bool', function() {
 
-  title = titleStr('should return true');
-  describe(title, function() {
+  should('return true', function() {
 
-    title = callStr(false);
-    it(title, function() {
+    test(false, function() {
       var result = vitals.is.bool(false);
       assert( result === true );
     });
 
-    title = callStr(true, false, false);
-    it(title, function() {
+    test(true, false, false, function() {
       var result = vitals.is.bool(true, false, false);
       assert( result === true );
     });
-
   });
 
-  title = titleStr('should return false');
-  describe(title, function() {
+  should('return false', function() {
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       var result = vitals.is.bool(null);
       assert( result === false );
     });
 
-    title = callStr(true, false, 'true');
-    it(title, function() {
-      var result = vitals.is.bool(true, false, 'true');
+    test('<Boolean>', false, false, function() {
+      var result = vitals.is.bool(new Boolean(true), false, false);
       assert( result === false );
     });
-
   });
 
-  title = titleStr('should throw an error');
-  describe(title, function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.is.bool();
-      });
+      }, validErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('is.bool', arguments, 3);
-}
