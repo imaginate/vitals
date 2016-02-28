e3bf7e358fb1ffed652e8cdeec51e1e5d0c07bdf
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:33:25 -0800

    Rebuilt tests for vitals.is.file

--- test/methods/is/is.file.js
+++ test/methods/is/is.file.js
@@ -1,14 +1,14 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS.FILE
+ * VITALS UNIT TESTS: vitals.is.file
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section fs
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
@@ -17,117 +17,61 @@
 
 if (BROWSER_TESTS) return;
 
-describe('vitals.is.file (section:fs)', function() {
-  var title;
+var addBase = DUMMY.addBase;
 
-  describe('basic tests', function() {
+method('is.file', function() {
 
-    before('setup dummy dirs and files', function() {
-      mkDummy([ 'fake1.js', 'fake2.js', 'fake3.js' ]);
-    });
-
-    title = titleStr('should return true');
-    describe(title, function() {
-
-      title = callStr('fake1.js');
-      it(title, function() {
-        var file = addBase('fake1.js');
-        var result = vitals.is.file(file);
-        assert( result === true );
-      });
-
-      title = callStr('fake1.js', 'fake2.js', 'fake3.js');
-      it(title, function() {
-        var file1 = addBase('fake1.js');
-        var file2 = addBase('fake2.js');
-        var file3 = addBase('fake3.js');
-        var result = vitals.is.file(file1, file2, file3);
-        assert( result === true );
-      });
-
-    });
-
-    title = titleStr('should return false');
-    describe(title, function() {
-
-      title = callStr(null);
-      it(title, function() {
-        var result = vitals.is.file(null);
-        assert( result === false );
-      });
-
-      title = callStr('fail.js');
-      it(title, function() {
-        var file = addBase('fail.js');
-        var result = vitals.is.file(file);
-        assert( result === false );
-      });
-
-      title = callStr('fake1.js', 'fail.js', 'fake3.js');
-      it(title, function() {
-        var file1 = addBase('fake1.js');
-        var file2 = addBase('fail.js');
-        var file3 = addBase('fake3.js');
-        var result = vitals.is.file(file1, file2, file3);
-        assert( result === false );
-      });
-
-    });
-
-    after('clean up dummy dirs and files', rmDummy);
-
+  before('setup dummy dirs and files', function() {
+    mkDummy([ 'file1.js', 'file2.js', 'file3.js' ]);
   });
 
-  describe('error tests', function() {
-    describe('should throw an error', function() {
+  after('clean up dummy dirs and files', rmDummy);
 
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.is.file();
-        });
-      });
+  should('return true', function() {
 
+    test('file1.js', function() {
+      var file = addBase('file1.js');
+      var result = vitals.is.file(file);
+      assert( result === true );
+    });
+
+    test('file1.js', 'file2.js', 'file3.js', function() {
+      var file1 = addBase('file1.js');
+      var file2 = addBase('file2.js');
+      var file3 = addBase('file3.js');
+      var result = vitals.is.file(file1, file2, file3);
+      assert( result === true );
     });
   });
 
+  should('return false', function() {
+
+    test(null, function() {
+      var result = vitals.is.file(null);
+      assert( result === false );
+    });
+
+    test('fail.js', function() {
+      var file = addBase('fail.js');
+      var result = vitals.is.file(file);
+      assert( result === false );
+    });
+
+    test('file1.js', 'fail.js', 'file3.js', function() {
+      var file1 = addBase('file1.js');
+      var file2 = addBase('fail.js');
+      var file3 = addBase('file3.js');
+      var result = vitals.is.file(file1, file2, file3);
+      assert( result === false );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.is.file();
+      }, validErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr(args) {
-  args = remap(arguments, function(val, i) {
-    return val && addBase(val);
-  });
-  return testCall('is.file', args, 4);
-}
-
-/**
- * @private
- * @param {string} file
- * @return {string}
- */
-function addBase(file) {
-
-  /** @type {string} */
-  var base;
-
-  base = cut(DUMMY.base, /\/$/);
-  return fuse(base, '/', file);
-}
