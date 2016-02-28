26f146dffd3b51584714bc05f9db27d9af41fbf0
Adam Smith <adamsmith@algorithmiv.com>
2016-02-27 21:32:16 -0800

    Rebuilt tests for vitals.is.args

--- test/methods/is/is.args.js
+++ test/methods/is/is.args.js
@@ -1,103 +1,60 @@
 /**
  * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS: VITALS.IS.ARGS
+ * VITALS UNIT TESTS: vitals.is.args
  * -----------------------------------------------------------------------------
- * @see [vitals.is]{@link https://github.com/imaginate/vitals/wiki/vitals.is}
+ * @section base
+ * @see [vitals.is docs](https://github.com/imaginate/vitals/wiki/vitals.is)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.is.args (section:base)', function() {
-  var title;
+method('is.args', function() {
 
-  title = titleStr('should return true');
-  describe(title, function() {
+  should('return true', function() {
 
-    title = callStr( (function(){ return arguments; })() );
-    it(title, function() {
+    test('<args>', function() {
       var args = (function(){ return arguments; })();
       var result = vitals.is.args(args);
       assert( result === true );
     });
 
-    title = callStr(
-      (function(){ return arguments; })(),
-      (function(){ return arguments; })(),
-      (function(){ return arguments; })()
-    );
-    it(title, function() {
+    test('<args>', '<args>', '<args>', function() {
       var args1 = (function(){ return arguments; })();
       var args2 = (function(){ return arguments; })();
       var args3 = (function(){ return arguments; })();
       var result = vitals.is.args(args1, args2, args3);
       assert( result === true );
     });
-
   });
 
-  title = titleStr('should return false');
-  describe(title, function() {
+  should('return false', function() {
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       var result = vitals.is.args(null);
       assert( result === false );
     });
 
-    title = callStr(
-      (function(){ return arguments; })(),
-      (function(){ return arguments; })(),
-      []
-    );
-    it(title, function() {
+    test('<args>', '<args>', [], function() {
       var args1 = (function(){ return arguments; })();
       var args2 = (function(){ return arguments; })();
       var result = vitals.is.args(args1, args2, []);
       assert( result === false );
     });
-
   });
 
-  title = titleStr('should throw an error');
-  describe(title, function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.is.args();
-      });
+      }, validErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('is.args', arguments, 3);
-}
