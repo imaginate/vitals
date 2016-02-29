5e9bf5be4f3fb7598df81a5570ec13c4c3a33fbb
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 12:14:41 -0800

    Rebuilt tests for vitals.remap

--- test/methods/remap/remap.js
+++ test/methods/remap/remap.js
@@ -1,295 +1,220 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - REMAP
+ * VITALS UNIT TESTS: vitals.remap
  * -----------------------------------------------------------------------------
- * @see [vitals.remap]{@link https://github.com/imaginate/vitals/wiki/vitals.remap}
+ * @section base
+ * @see [vitals.remap docs](https://github.com/imaginate/vitals/wiki/vitals.remap)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.remap (section:base)', function() {
-  var title;
+method('remap', function() {
 
-  describe('object tests', function() {
+  should('make new object by iterating over all key => value pairs in source', function() {
 
-    // newObj()= {
-    //   'a':  'd',
-    //   'b':  'e',
-    //   'c':  'f',
-    //   '1':   4,
-    //   '2':   5,
-    //   '3':   6,
-    //   'a1': '1',
-    //   'b2': '2',
-    //   'c3': '3'
-    // }
-
-    title = titleStr('should iterate over every prop and return valid obj');
-    describe(title, function() {
-
-      title = callStr('<object>', '<iteratee>');
-      it(title, function() {
-        var obj = vitals.remap(newObj(), function(val, key) {
-          return val;
-        });
-        each(newObj(), function(val, key) {
-          assert( obj[key] === val );
-        });
+    test('<object>', '<iteratee>', function() {
+      var obj1 = { a: 1, b: 2, c: 3 };
+      var obj2 = vitals.remap(obj1, function(val) {
+        return ++val;
       });
-
-      title = callStr('<object>', '<iteratee>');
-      it(title, function() {
-        var obj = vitals.remap(newObj(), function(val, key) {
-          return val + key;
-        });
-        each(newObj(), function(val, key) {
-          val += key;
-          assert( obj[key] === val );
-        });
-      });
-
+      assert( obj2 !== obj1 );
+      assert( obj2.a === 2 );
+      assert( obj2.b === 3 );
+      assert( obj2.c === 4 );
     });
 
-    title = titleStr('should correctly clone the source');
-    describe(title, function() {
-
-      title = callStr('<object>', '<iteratee>');
-      it(title, function() {
-        var obj1 = newObj();
-        vitals.remap(obj1, function(val, key, obj) {
-          assert( obj !== obj1 )
-        });
+    test('<object>', '<iteratee>', function() {
+      var obj1 = { a: 1, b: 2, c: 3 };
+      var obj2 = vitals.remap(obj1, function(val, key) {
+        return key;
       });
-
-    });
-
-    title = titleStr('should correctly bind the iteratee');
-    describe(title, function() {
-
-      title = callStr('<object>', '<iteratee>', '<thisArg>');
-      it(title, function() {
-        var obj = newObj();
-        var thisArg = {};
-        vitals.remap(obj, function(val, key) {
-          this[key] = val;
-        }, thisArg);
-        each(obj, function(val, key) {
-          assert( has(thisArg, key)    );
-          assert( thisArg[key] === val );
-        });
-      });
-
-    });
-
-  });
-
-  describe('array tests', function() {
-
-    // newArr()= [ "a", "b", "c", 1, 2, 3, "a1", "b2", "c3" ]
-
-    title = titleStr('should iterate over every index in order');
-    describe(title, function() {
-
-      title = callStr('<array>', '<iteratee>');
-      it(title, function() {
-        var arr = newArr();
-        var ii = 0;
-        vitals.remap(newArr(), function(val, i) {
-          assert( arr[i] === val );
-          assert( ii++ === i );
-        });
-      });
-
-    });
-
-    title = titleStr('should iterate over every prop and return valid array');
-    describe(title, function() {
-
-      title = callStr('<array>', '<iteratee>');
-      it(title, function() {
-        var arr = vitals.remap(newArr(), function(val, i) {
-          return val;
-        });
-        each(newArr(), function(val, i) {
-          assert( arr[i] === val );
-        });
-      });
-
-      title = callStr('<array>', '<iteratee>');
-      it(title, function() {
-        var arr = vitals.remap(newArr(), function(val, i) {
-          return val + i;
-        });
-        each(newArr(), function(val, i) {
-          val += i;
-          assert( arr[i] === val );
-        });
-      });
-
-    });
-
-    title = titleStr('should correctly clone the source');
-    describe(title, function() {
-
-      title = callStr('<array>', '<iteratee>');
-      it(title, function() {
-        var arr1 = newArr();
-        vitals.remap(arr1, function(val, i, arr) {
-          assert( arr !== arr1 );
-        });
-      });
-
-    });
-
-    title = titleStr('should correctly bind the iteratee');
-    describe(title, function() {
-
-      title = callStr('<array>', '<iteratee>', '<thisArg>');
-      it(title, function() {
-        var arr = newArr();
-        var thisArg = new Array(9);
-        vitals.remap(arr, function(val, i) {
-          this[i] = val;
-        }, thisArg);
-        assert( arr.join() === thisArg.join() );
-      });
-
-    });
-
-  });
-
-  describe('string tests', function() {
-
-    title = 'should replace all patterns in source';
-    title = titleStr(title);
-    describe(title, function() {
-
-      title = callStr('abc123a1b2c3', 3, 5);
-      it(title, function() {
-        var str = vitals.remap('abc123a1b2c3', 3, 5);
-        assert( str === 'abc125a1b2c5' );
-      });
-
-      title = callStr('abc123a1b2c3', '*', '$&');
-      it(title, function() {
-        var str = vitals.remap.str('abc123a1b2c3', '*', '$&');
-        assert( str === 'abc123a1b2c3' );
-      });
-
-      title = callStr('abc123a1b2c3', /[a-z]/, 'z');
-      it(title, function() {
-        var str = vitals.remap('abc123a1b2c3', /[a-z]/, 'z');
-        assert( str === 'zbc123a1b2c3' );
-      });
-
-    });
-
-    title = titleStr('should correctly bind the replacer');
-    describe(title, function() {
-
-      title = callStr('abc123a1b2c3', /a/, '<replacer>', '<thisArg>');
-      it(title, function() {
-        var replacer = function(match) {
-          this[match] = true;
-          return match;
-        };
-        var thisArg = {};
-        vitals.remap('abc123a1b2c3', /a/, replacer, thisArg);
-        assert( thisArg.a === true );
-      });
-
-    });
-
-  });
-
-  describe('error tests', function() {
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.remap();
-        });
-      });
-
-      title = callStr({});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.remap({});
-        });
-      });
-
-      title = callStr(null, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.remap(null, function(){});
-        });
-      });
-
-      title = callStr({}, function(){}, 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.remap({}, function(){}, 'fail');
-        });
-      });
-
+      assert( obj2 !== obj1 );
+      assert( obj2.a === 'a' );
+      assert( obj2.b === 'b' );
+      assert( obj2.c === 'c' );
     });
   });
 
+  should('make new array by iterating over all index => value pairs in source', function() {
+
+    test('<array>', '<iteratee>', function() {
+      var arr1 = [ 1, 2, 3 ];
+      var arr2 = vitals.remap(arr1, function(val) {
+        return ++val;
+      });
+      assert( arr2 !== arr1 );
+      assert( arr2[0] === 2 );
+      assert( arr2[1] === 3 );
+      assert( arr2[2] === 4 );
+      assert( arr2.length === 3 );
+    });
+
+    test('<array>', '<iteratee>', function() {
+      var arr1 = [ 1, 2, 3 ];
+      var arr2 = vitals.remap(arr1, function(val, i) {
+        return i;
+      });
+      assert( arr2 !== arr1 );
+      assert( arr2[0] === 0 );
+      assert( arr2[1] === 1 );
+      assert( arr2[2] === 2 );
+      assert( arr2.length === 3 );
+    });
+  });
+
+  should('iterate over every index in order', function() {
+
+    test('<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      var vals = [];
+      var keys = [];
+      vitals.remap(arr, function(val, i) {
+        vals.push(val);
+        keys.push(i);
+      });
+      assert( vals[0] === 1 );
+      assert( vals[1] === 2 );
+      assert( vals[2] === 3 );
+      assert( vals.length === 3 );
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
+    });
+  });
+
+  should('replace all patterns in source', function() {
+
+    test('abc123', 3, 5, function() {
+      var str = vitals.remap('abc123', 3, 5);
+      assert( str === 'abc125' );
+    });
+
+    test('abc123abc123', 3, 5, function() {
+      var str = vitals.remap('abc123abc123', 3, 5);
+      assert( str === 'abc125abc125' );
+    });
+
+    test('abc123', 'a', 'z', function() {
+      var str = vitals.remap.str('abc123', 'a', 'z');
+      assert( str === 'zbc123' );
+    });
+
+    test('abc123', 'a', '$&', function() {
+      var str = vitals.remap.str('abc123', '*', '$&');
+      assert( str === 'abc123' );
+    });
+
+    test('abc123', /[a-z]/, 'z', function() {
+      var str = vitals.remap('abc123', /[a-z]/, 'z');
+      assert( str === 'zbc123' );
+    });
+
+    test('abc123', /[a-z]/g, 'z', function() {
+      var str = vitals.remap('abc123', /[a-z]/g, 'z');
+      assert( str === 'zzz123' );
+    });
+
+    test('abc123', '*', 'z', function() {
+      var str = vitals.remap.str('abc123', '*', 'z');
+      assert( str === 'abc123' );
+    });
+
+    test('abc123', '.*', 'z', function() {
+      var str = vitals.remap.str('abc123', '.*', 'z');
+      assert( str === 'abc123' );
+    });
+  });
+
+  should('correctly clone the source', function() {
+
+    test('<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      vitals.remap(obj, function(val, key, src) {
+        assert( obj !== src )
+      });
+    });
+
+    test('<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      vitals.remap(arr, function(val, i, src) {
+        assert( arr !== src );
+      });
+    });
+  });
+
+  should('correctly bind the iteratee', function() {
+
+    test('<object>', '<iteratee>', '<this>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var self = {};
+      vitals.remap(obj, function(val, key) {
+        this[key] = val;
+      }, self);
+      assert( self !== obj );
+      assert( self.a === 1 );
+      assert( self.b === 2 );
+      assert( self.c === 3 );
+    });
+
+    test('<array>', '<iteratee>', '<this>', function() {
+      var arr = [ 1, 2, 3 ];
+      var self = [];
+      vitals.remap(arr, function(val, i) {
+        this[i] = val;
+      }, self);
+      assert( self !== arr );
+      assert( self[0] === 1 );
+      assert( self[1] === 2 );
+      assert( self[2] === 3 );
+      assert( self.length === 3 );
+    });
+  });
+
+  should('correctly bind the replacer', function() {
+
+    test('abc123', /a|1/g, '<replacer>', '<this>', function() {
+      var self = {};
+      vitals.remap('abc123', /a|1/g, function(match) {
+        this[match] = true;
+        return match;
+      }, self);
+      assert( self['a'] === true );
+      assert( self['1'] === true );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.remap();
+      }, validTypeErr);
+    });
+
+    test({}, function() {
+      assert.throws(function() {
+        vitals.remap({});
+      }, validTypeErr);
+    });
+
+    test(null, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.remap(null, function(){});
+      }, validTypeErr);
+    });
+
+    test({}, '<iteratee>', 'fail', function() {
+      assert.throws(function() {
+        vitals.remap({}, function(){}, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('remap', arguments, 4);
-}
-
-/**
- * @private
- * @return {!Object}
- */
-function newObj() {
-  return {
-    'a':  'd',
-    'b':  'e',
-    'c':  'f',
-    '1':   4,
-    '2':   5,
-    '3':   6,
-    'a1': '1',
-    'b2': '2',
-    'c3': '3'
-  };
-}
-
-/**
- * @private
- * @return {!Array}
- */
-function newArr() {
-  return [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
-}
