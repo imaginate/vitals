72cd905a8daa036cd2c497151f93c9a47e6a9e99
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 14:00:47 -0800

    Rebuilt tests for vitals.slice.string

--- test/methods/slice/slice.string.js
+++ test/methods/slice/slice.string.js
@@ -1,115 +1,79 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - SLICE.STRING
+ * VITALS UNIT TESTS: vitals.slice.string
  * -----------------------------------------------------------------------------
- * @see [vitals.slice]{@link https://github.com/imaginate/vitals/wiki/vitals.slice}
+ * @section base
+ * @see [vitals.slice docs](https://github.com/imaginate/vitals/wiki/vitals.slice)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.slice.string (section:base)', function() {
-  var title;
+method('slice.string', 'slice.str', function() {
 
-  describe('string tests', function() {
+  should('return valid string', function() {
 
-    title = titleStr('should return a correctly copied string');
-    describe(title, function() {
-
-      title = callStr('abc123a1b2c3');
-      it(title, function() {
-        var str = vitals.slice.str('abc123a1b2c3');
-        assert( str === 'abc123a1b2c3' );
-      });
-
-      title = callStr('abc123a1b2c3', 1);
-      it(title, function() {
-        var str = vitals.slice.str('abc123a1b2c3', 1);
-        assert( str === 'bc123a1b2c3' );
-      });
-
-      title = callStr('abc123a1b2c3', -1);
-      it(title, function() {
-        var str = vitals.slice.str('abc123a1b2c3', -1);
-        assert( str === '3' );
-      });
-
-      title = callStr('abc123a1b2c3', 1, 3);
-      it(title, function() {
-        var str = vitals.slice.str('abc123a1b2c3', 1, 3);
-        assert( str === 'bc' );
-      });
-
-      title = callStr('abc123a1b2c3', 1, -3);
-      it(title, function() {
-        var str = vitals.slice.str('abc123a1b2c3', 1, -3);
-        assert( str === 'bc123a1b' );
-      });
-
-      title = callStr('abc123a1b2c3', -3, -1);
-      it(title, function() {
-        var str = vitals.slice.str('abc123a1b2c3', -3, -1);
-        assert( str === '2c' );
-      });
-
+    test('abc123', function() {
+      var str = vitals.slice.str('abc123');
+      assert( str === 'abc123' );
     });
 
-  });
+    test('abc123', 1, function() {
+      var str = vitals.slice.str('abc123', 1);
+      assert( str === 'bc123' );
+    });
 
-  describe('error tests', function() {
-    describe('should throw an error', function() {
+    test('abc123', -1, function() {
+      var str = vitals.slice.str('abc123', -1);
+      assert( str === '3' );
+    });
 
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.slice.str();
-        });
-      });
+    test('abc123', 1, 3, function() {
+      var str = vitals.slice.str('abc123', 1, 3);
+      assert( str === 'bc' );
+    });
 
-      title = callStr('str', 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.slice.str('str', 'fail');
-        });
-      });
+    test('abc123', 1, -3, function() {
+      var str = vitals.slice.str('abc123', 1, -3);
+      assert( str === 'bc' );
+    });
 
-      title = callStr('str', 1, 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.slice.str('str', 1, 'fail');
-        });
-      });
-
+    test('abc123', -3, -1, function() {
+      var str = vitals.slice.str('abc123', -3, -1);
+      assert( str === '12' );
     });
   });
 
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.slice.str();
+      }, validTypeErr);
+    });
+
+    test({}, function() {
+      assert.throws(function() {
+        vitals.slice.str({});
+      }, validTypeErr);
+    });
+
+    test('str', 'fail', function() {
+      assert.throws(function() {
+        vitals.slice.str('str', 'fail');
+      }, validTypeErr);
+    });
+
+    test('str', 1, 'fail', function() {
+      assert.throws(function() {
+        vitals.slice.str('str', 1, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('slice.str', arguments, 4);
-}
