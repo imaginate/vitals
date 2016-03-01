0f8db72ce9153accb0acf1d438b28a17de207347
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 18:24:48 -0800

    Rebuilt tests for vitals.to.regexp

--- test/methods/to/to.regexp.js
+++ test/methods/to/to.regexp.js
@@ -1,89 +1,57 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - TO.REGEXP
+ * VITALS UNIT TESTS: vitals.to.regexp
  * -----------------------------------------------------------------------------
- * @see [vitals.to]{@link https://github.com/imaginate/vitals/wiki/vitals.to}
+ * @section base
+ * @see [vitals.to docs](https://github.com/imaginate/vitals/wiki/vitals.to)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.to.regexp (section:base)', function() {
-  var title;
+method('to.regexp', 'to.regex', function() {
 
-  title = titleStr('should convert string to regex');
-  describe(title, function() {
+  should('convert string to regex', function() {
 
-    title = callStr('src');
-    it(title, function() {
+    test('src', function() {
       var result = vitals.to.regex('src');
       assert( is.regex(result) );
       assert( result.source === 'src' );
       assert( result.global === false );
     });
 
-    title = callStr('src', 'g');
-    it(title, function() {
+    test('src', 'g', function() {
       var result = vitals.to.regex('src', 'g');
       assert( is.regex(result) );
       assert( result.source === 'src' );
       assert( result.global === true  );
     });
-
   });
 
-  describe('should throw an error', function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.to.regex();
-      });
+      }, validErr);
     });
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       assert.throws(function() {
         vitals.to.regex(null);
-      });
+      }, validTypeErr);
     });
 
-    title = callStr('src', null);
-    it(title, function() {
+    test('src', null, function() {
       assert.throws(function() {
         vitals.to.regex('src', null);
-      });
+      }, validTypeErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('to.regex', arguments, 3);
-}
