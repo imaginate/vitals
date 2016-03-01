4d2a495280616249216a3f9ec36fc536e3ea1ac2
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 18:24:21 -0800

    Rebuilt tests for vitals.to.file

--- test/methods/to/to.file.js
+++ test/methods/to/to.file.js
@@ -1,14 +1,14 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - TO.FILE
+ * VITALS UNIT TESTS: vitals.to.file
  * -----------------------------------------------------------------------------
- * @see [vitals.to]{@link https://github.com/imaginate/vitals/wiki/vitals.to}
+ * @section fs
+ * @see [vitals.to docs](https://github.com/imaginate/vitals/wiki/vitals.to)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
@@ -17,190 +17,131 @@
 
 if (BROWSER_TESTS) return;
 
+var CONTENT = DUMMY.content; // content for dummy files
+var BASE = DUMMY.base; // base directory for dummy files
+
+var addBase = DUMMY.addBase;
 var fs = require('fs');
 
-describe('vitals.to.file (section:fs)', function() {
-  var title;
+method('to.file', function() {
 
-  describe('basic tests', function() {
-
-    before('setup dummy dirs and files', function() {
-      mkDummy('fake.js');
-    });
-
-    title = titleStr('should overwrite file with buffer');
-    describe(title, function() {
-
-      title = callStr('<Buffer>', 'fake.js');
-      it(title, function() {
-        var content = '// overwrite test 1';
-        var buf = new Buffer(content);
-        var file = addBase('fake.js');
-        var result = vitals.to.file(buf, file);
-        assert( is.buffer(result) );
-        result = result.toString();
-        assert( result === content );
-        result = fs.readFileSync(file);
-        result = result.toString();
-        assert( result === content );
-      });
-
-    });
-
-    title = titleStr('should make new file with buffer');
-    describe(title, function() {
-
-      title = callStr('<Buffer>', 'fake1.js');
-      it(title, function() {
-        var buf = new Buffer(DUMMY.content);
-        var file = addBase('fake1.js');
-        var result = vitals.to.file(buf, file);
-        assert( is.buffer(result) );
-        result = result.toString();
-        assert( result === DUMMY.content );
-        result = fs.readFileSync(file);
-        result = result.toString();
-        assert( result === DUMMY.content );
-      });
-
-      title = callStr('<Buffer>', 'fake2.js', 'utf8');
-      it(title, function() {
-        var buf = new Buffer(DUMMY.content);
-        var file = addBase('fake2.js');
-        var result = vitals.to.file(buf, file, 'utf8');
-        assert( is.buffer(result) );
-        result = result.toString();
-        assert( result === DUMMY.content );
-        result = fs.readFileSync(file, 'utf8');
-        assert( result === DUMMY.content );
-      });
-
-    });
-
-    title = titleStr('should overwrite file with string');
-    describe(title, function() {
-
-      title = callStr('// overwrite test 2', 'fake.js');
-      it(title, function() {
-        var content = '// overwrite test 2';
-        var file = addBase('fake.js');
-        var result = vitals.to.file(content, file);
-        assert( result === content );
-        result = fs.readFileSync(file, 'utf8');
-        assert( result === content );
-      });
-
-    });
-
-    title = titleStr('should make new file with string');
-    describe(title, function() {
-
-      title = callStr(DUMMY.content, 'fake3.js');
-      it(title, function() {
-        var content = DUMMY.content;
-        var file = addBase('fake3.js');
-        var result = vitals.to.file(content, file);
-        assert( result === content );
-        result = fs.readFileSync(file, 'utf8');
-        assert( result === content );
-      });
-
-      title = callStr(DUMMY.content, 'fake4.js');
-      it(title, function() {
-        var content = DUMMY.content;
-        var file = addBase('fake4.js');
-        var result = vitals.to.file(content, file, null);
-        assert( result === content );
-        result = fs.readFileSync(file);
-        result = result.toString();
-        assert( result === content );
-      });
-
-    });
-
-    after('clean up dummy dirs and files', rmDummy);
-
+  beforeEach('setup dummy dirs and files', function() {
+    mkDummy('file.js');
   });
 
-  describe('error tests', function() {
-    describe('should throw an error', function() {
+  afterEach('clean up dummy dirs and files', rmDummy);
 
-      before('setup dummy dirs and files', function() {
-        mkDummy('fake.js');
-      });
+  should('overwrite file with buffer', function() {
 
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.to.file();
-        });
-      });
-
-      title = callStr('content');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.to.file('content');
-        });
-      });
-
-      title = callStr(null, 'fake.js');
-      it(title, function() {
-        assert.throws(function() {
-          var file = addBase('fake.js');
-          vitals.to.file(null, file);
-        });
-      });
-
-      title = callStr('content', 'fake.js', false);
-      it(title, function() {
-        assert.throws(function() {
-          var file = addBase('fake.js');
-          vitals.to.file('content', file, false);
-        });
-      });
-
-      after('clean up dummy dirs and files', rmDummy);
-
+    test('<Buffer>', 'file.js', function() {
+      var content = '// overwrite test 1';
+      var buf = new Buffer(content);
+      var file = addBase('file.js');
+      var result = vitals.to.file(buf, file);
+      assert( isBuffer(result) );
+      result = result.toString();
+      assert( result === content );
+      assert( validFile(file, content) );
     });
   });
 
+  should('make new file with buffer', function() {
+
+    test('<Buffer>', 'file1.js', function() {
+      var buf = new Buffer(CONTENT);
+      var file = addBase('file1.js');
+      var result = vitals.to.file(buf, file);
+      assert( isBuffer(result) );
+      result = result.toString();
+      assert( result === CONTENT );
+      assert( validFile(file, CONTENT) );
+    });
+
+    test('<Buffer>', 'file2.js', 'utf8', function() {
+      var buf = new Buffer(CONTENT);
+      var file = addBase('file2.js');
+      var result = vitals.to.file(buf, file, 'utf8');
+      assert( isBuffer(result) );
+      result = result.toString();
+      assert( result === CONTENT );
+      assert( validFile(file, CONTENT, true) );
+    });
+  });
+
+  should('overwrite file with string', function() {
+
+    test('// overwrite test 2', 'file.js', function() {
+      var content = '// overwrite test 2';
+      var file = addBase('file.js');
+      var result = vitals.to.file(content, file);
+      assert( result === content );
+      assert( validFile(file, content, true) );
+    });
+  });
+
+  should('make new file with string', function() {
+
+    test(CONTENT, 'file3.js', function() {
+      var file = addBase('file3.js');
+      var result = vitals.to.file(CONTENT, file);
+      assert( result === CONTENT );
+      assert( validFile(file, CONTENT, true) );
+    });
+
+    test(CONTENT, 'file4.js', null, function() {
+      var file = addBase('file4.js');
+      var result = vitals.to.file(CONTENT, file, null);
+      assert( result === CONTENT );
+      assert( validFile(file, CONTENT) );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.to.file();
+      }, validTypeErr);
+    });
+
+    test('content', function() {
+      assert.throws(function() {
+        vitals.to.file('content');
+      }, validTypeErr);
+    });
+
+    test(null, 'file.js', function() {
+      assert.throws(function() {
+        var file = addBase('file.js');
+        vitals.to.file(null, file);
+      }, validTypeErr);
+    });
+
+    test('content', 'file.js', false, function() {
+      assert.throws(function() {
+        var file = addBase('file.js');
+        vitals.to.file('content', file, false);
+      }, validTypeErr);
+    });
+  });
 });
 
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
 /**
  * @private
- * @param {string} shouldMsg
- * @return {string}
+ * @param {string} filepath
+ * @param {string} result
+ * @param {boolean=} encode
+ * @return {boolean}
  */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr(args) {
-  args = remap(arguments, function(val, i) {
-    return i === 1 ? addBase(val) : val;
-  });
-  return testCall('to.file', args, 4);
-}
-
-/**
- * @private
- * @param {string} file
- * @return {string}
- */
-function addBase(file) {
+function validFile(filepath, result, encode) {
 
   /** @type {string} */
-  var base;
+  var content;
 
-  base = cut(DUMMY.base, /\/$/);
-  return fuse(base, '/', file);
+  if ( !isFile(filepath) ) return false;
+
+  content = encode
+    ? fs.readFileSync(filepath, 'utf8')
+    : fs.readFileSync(filepath).toString();
+  return result === content;
 }
