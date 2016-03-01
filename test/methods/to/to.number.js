c9904fb5273fee19616bae0959419befa5cc657e
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 18:24:41 -0800

    Rebuilt tests for vitals.to.number

--- test/methods/to/to.number.js
+++ test/methods/to/to.number.js
@@ -1,110 +1,74 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - TO.NUMBER
+ * VITALS UNIT TESTS: vitals.to.number
  * -----------------------------------------------------------------------------
- * @see [vitals.to]{@link https://github.com/imaginate/vitals/wiki/vitals.to}
+ * @section base
+ * @see [vitals.to docs](https://github.com/imaginate/vitals/wiki/vitals.to)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.to.number (section:base)', function() {
-  var title;
+method('to.number', 'to.num', function() {
 
-  title = titleStr('should convert value to number or NaN');
-  describe(title, function() {
+  should('convert value to number or NaN', function() {
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       var result = vitals.to.num(null);
       assert( result === 0 );
     });
 
-    title = callStr(true);
-    it(title, function() {
+    test(true, function() {
       var result = vitals.to.num(true);
       assert( result === 1 );
     });
 
-    title = callStr(false);
-    it(title, function() {
+    test(false, function() {
       var result = vitals.to.num(false);
       assert( result === 0 );
     });
 
-    title = callStr('10');
-    it(title, function() {
+    test('10', function() {
       var result = vitals.to.num('10');
       assert( result === 10 );
     });
 
-    title = callStr(10);
-    it(title, function() {
+    test(10, function() {
       var result = vitals.to.num(10);
       assert( result === 10 );
     });
-
   });
 
-  describe('should throw an error', function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.to.num();
-      });
+      }, validErr);
     });
 
-    title = callStr(undefined);
-    it(title, function() {
+    test(undefined, function() {
       assert.throws(function() {
         vitals.to.num(undefined);
-      });
+      }, validTypeErr);
     });
 
-    title = callStr('invalid');
-    it(title, function() {
+    test('invalid', function() {
       assert.throws(function() {
         vitals.to.num('invalid');
-      });
+      }, validRangeErr);
     });
 
-    title = callStr([ 1, 2, 3 ]);
-    it(title, function() {
+    test([ 1, 2, 3 ], function() {
       assert.throws(function() {
         vitals.to.num([ 1, 2, 3 ]);
-      });
+      }, validTypeErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('to.num', arguments, 3);
-}
