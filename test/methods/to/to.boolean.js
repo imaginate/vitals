75134fe9098f6d53aec359e1a02b254ec4182658
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 18:24:16 -0800

    Rebuilt tests for vitals.to.boolean

--- test/methods/to/to.boolean.js
+++ test/methods/to/to.boolean.js
@@ -1,138 +1,97 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - TO.BOOLEAN
+ * VITALS UNIT TESTS: vitals.to.boolean
  * -----------------------------------------------------------------------------
- * @see [vitals.to]{@link https://github.com/imaginate/vitals/wiki/vitals.to}
+ * @section base
+ * @see [vitals.to docs](https://github.com/imaginate/vitals/wiki/vitals.to)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.to.boolean (section:base)', function() {
-  var title;
+method('to.boolean', 'to.bool', function() {
 
-  title = titleStr('should convert value to boolean');
-  describe(title, function() {
+  should('convert value to boolean', function() {
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       var result = vitals.to.bool(null);
       assert( result === false );
     });
 
-    title = callStr(undefined);
-    it(title, function() {
+    test(undefined, function() {
       var result = vitals.to.bool(undefined);
       assert( result === false );
     });
 
-    title = callStr(true);
-    it(title, function() {
+    test(true, function() {
       var result = vitals.to.bool(true);
       assert( result === true );
     });
 
-    title = callStr('');
-    it(title, function() {
+    test('', function() {
       var result = vitals.to.bool('');
       assert( result === false );
     });
 
-    title = callStr('str');
-    it(title, function() {
+    test('str', function() {
       var result = vitals.to.bool('str');
       assert( result === true );
     });
 
-    title = callStr(0);
-    it(title, function() {
+    test(0, function() {
       var result = vitals.to.bool(0);
       assert( result === false );
     });
 
-    title = callStr(10);
-    it(title, function() {
+    test(10, function() {
       var result = vitals.to.bool(10);
       assert( result === true );
     });
 
-    title = callStr(NaN);
-    it(title, function() {
+    test(NaN, function() {
       var result = vitals.to.bool(NaN);
       assert( result === false );
     });
 
-    title = callStr({ a: 1, b: 2 });
-    it(title, function() {
+    test({ a: 1, b: 2 }, function() {
       var result = vitals.to.bool({ a: 1, b: 2 });
       assert( result === true );
     });
 
-    title = callStr([ 1, 2, 3 ]);
-    it(title, function() {
+    test([ 1, 2, 3 ], function() {
       var result = vitals.to.bool([ 1, 2, 3 ]);
       assert( result === true );
     });
 
-    title = callStr(function(){});
-    it(title, function() {
+    test('<function>', function() {
       var result = vitals.to.bool(function(){});
       assert( result === true );
     });
 
-    title = callStr(/regex/g);
-    it(title, function() {
+    test(/regex/g, function() {
       var result = vitals.to.bool(/regex/g);
       assert( result === true );
     });
 
-    title = callStr( (function(){ return arguments; })(1, 2, 3) );
-    it(title, function() {
+    test('<arguments>', function() {
       var args = (function(){ return arguments; })(1, 2, 3);
       var result = vitals.to.bool(args);
       assert( result === true );
     });
-
   });
 
-  describe('should throw an error', function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.to.bool();
-      });
+      }, validErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('to.bool', arguments, 3);
-}
