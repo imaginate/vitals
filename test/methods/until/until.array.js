f58876d00e05d19f919836954ced6f42570de2c1
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 17:50:13 -0800

    Rebuilt tests for vitals.until.array

--- test/methods/until/until.array.js
+++ test/methods/until/until.array.js
@@ -1,177 +1,134 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - UNTIL.ARRAY
+ * VITALS UNIT TESTS: vitals.until.array
  * -----------------------------------------------------------------------------
- * @see [vitals.until]{@link https://github.com/imaginate/vitals/wiki/vitals.until}
+ * @section base
+ * @see [vitals.until docs](https://github.com/imaginate/vitals/wiki/vitals.until)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.until.array (section:base)', function() {
-  var title;
+method('until.array', 'until.arr', function() {
 
-  describe('basic tests', function() {
+  should('return valid boolean', function() {
 
-    // newArr()= [ "a", "b", "c", 1, 2, 3, "a1", "b2", "c3" ]
-
-    title = titleStr('should iterate over every index in order');
-    describe(title, function() {
-
-      title = callStr(true, '<array>', '<iteratee>');
-      it(title, function() {
-        var arr = newArr();
-        var ii = 0;
-        vitals.until.arr(true, newArr(), function(val, i) {
-          assert( arr[i] === val );
-          assert( ii++ === i );
-        });
+    test(true, '<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      var pass = vitals.until.arr(true, arr, function(val, i) {
+        return i === 2;
       });
-
+      assert( pass === true );
     });
 
-    title = titleStr('should return valid boolean');
-    describe(title, function() {
-
-      title = callStr(true, '<array>', '<iteratee>');
-      it(title, function() {
-        var pass = vitals.until.arr(true, newArr(), function(val, i) {
-          return val === 3;
-        });
-        assert( pass === true );
+    test(3, '<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      var pass = vitals.until.arr(3, arr, function(val) {
+        return val;
       });
-
-      title = callStr(true, '<array>', '<iteratee>');
-      it(title, function() {
-        var fail = vitals.until.arr(true, newArr(), function(val, i) {
-          return val === '3';
-        });
-        assert( fail === false );
-      });
-
-      title = callStr(3, '<array>', '<iteratee>');
-      it(title, function() {
-        var pass = vitals.until.arr(3, newArr(), function(val, i) {
-          return val;
-        });
-        assert( pass === true );
-      });
-
-      title = callStr('3', '<array>', '<iteratee>');
-      it(title, function() {
-        var fail = vitals.until.arr('3', newArr(), function(val, i) {
-          return val;
-        });
-        assert( fail === false );
-      });
-
+      assert( pass === true );
     });
 
-    title = titleStr('should correctly clone the source');
-    describe(title, function() {
-
-      title = callStr(true, '<array>', '<iteratee>');
-      it(title, function() {
-        var arr1 = newArr();
-        vitals.until.arr(true, arr1, function(val, i, arr) {
-          assert( arr !== arr1 );
-        });
+    test(true, '<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      var fail = vitals.until.arr(true, arr, function(val, i) {
+        return i === 5;
       });
-
+      assert( fail === false );
     });
 
-    title = titleStr('should correctly bind the iteratee');
-    describe(title, function() {
-
-      title = callStr(true, '<array>', '<iteratee>', '<thisArg>');
-      it(title, function() {
-        var arr = newArr();
-        var thisArg = new Array(9);
-        var fail = vitals.until.arr(true, arr, function(val, i) {
-          this[i] = val;
-        }, thisArg);
-        assert( arr.join() === thisArg.join() );
-        assert( fail === false );
+    test(5, '<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      var fail = vitals.until.arr(5, arr, function(val) {
+        return val;
       });
-
+      assert( fail === false );
     });
-
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.arr();
-        });
-      });
-
-      title = callStr(true);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.arr(true);
-        });
-      });
-
-      title = callStr(true, []);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.arr(true, []);
-        });
-      });
-
-      title = callStr(true, null, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.arr(true, null, function(){});
-        });
-      });
-
-      title = callStr(true, [], function(){}, 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.arr(true, [], function(){}, 'fail');
-        });
-      });
-
-    });
-
   });
 
+  should('iterate from 0 to length over all props in array', function() {
+
+    test(true, '<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      var vals = [];
+      var keys = [];
+      vitals.until.arr(true, arr, function(val, i) {
+        vals.push(val);
+        keys.push(i);
+      });
+      assert( vals[0] === 1 );
+      assert( vals[1] === 2 );
+      assert( vals[2] === 3 );
+      assert( vals.length === 3 );
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
+    });
+  });
+
+  should('correctly clone the source', function() {
+
+    test(true, '<array>', '<iteratee>', function() {
+      var arr = [ 1, 2, 3 ];
+      vitals.until.arr(true, arr, function(val, i, src) {
+        assert( arr !== src );
+      });
+    });
+  });
+
+  should('correctly bind the iteratee', function() {
+
+    test(true, '<array>', '<iteratee>', '<this>', function() {
+      var arr = [ 1, 2, 3 ];
+      var self = [];
+      vitals.until.arr(true, arr, function(val, i) {
+        this[i] = val;
+      }, self);
+      assert( self !== arr );
+      assert( self[0] === 1 );
+      assert( self[1] === 2 );
+      assert( self[2] === 3 );
+      assert( self.length === 3 );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.until.arr();
+      }, validTypeErr);
+    });
+
+    test(true, function() {
+      assert.throws(function() {
+        vitals.until.arr(true);
+      }, validTypeErr);
+    });
+
+    test(true, [], function() {
+      assert.throws(function() {
+        vitals.until.arr(true, []);
+      }, validTypeErr);
+    });
+
+    test(true, null, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.until.arr(true, null, function(){});
+      }, validTypeErr);
+    });
+
+    test(true, [], '<iteratee>', 'fail', function() {
+      assert.throws(function() {
+        vitals.until.arr(true, [], function(){}, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('until.arr', arguments, 4);
-}
-
-/**
- * @private
- * @return {!Array}
- */
-function newArr() {
-  return [ 'a', 'b', 'c', 1, 2, 3, 'a1', 'b2', 'c3' ];
-}
