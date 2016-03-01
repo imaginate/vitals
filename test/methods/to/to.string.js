1c3b68b549e318238268d734f25ccc9f20e833ec
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 18:24:53 -0800

    Rebuilt tests for vitals.to.string

--- test/methods/to/to.string.js
+++ test/methods/to/to.string.js
@@ -1,144 +1,101 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - TO.STRING
+ * VITALS UNIT TESTS: vitals.to.string
  * -----------------------------------------------------------------------------
- * @see [vitals.to]{@link https://github.com/imaginate/vitals/wiki/vitals.to}
+ * @section base
+ * @see [vitals.to docs](https://github.com/imaginate/vitals/wiki/vitals.to)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.to.string (section:base)', function() {
-  var title;
+method('to.string', 'to.str', function() {
 
-  title = titleStr('should convert value to string');
-  describe(title, function() {
+  should('convert value to string', function() {
 
-    title = callStr(null);
-    it(title, function() {
+    test(null, function() {
       var result = vitals.to.str(null);
       assert( result === 'null' );
     });
 
-    title = callStr(undefined);
-    it(title, function() {
+    test(undefined, function() {
       var result = vitals.to.str(undefined);
       assert( result === 'undefined' );
     });
 
-    title = callStr(true);
-    it(title, function() {
+    test(true, function() {
       var result = vitals.to.str(true);
       assert( result === 'true' );
     });
 
-    title = callStr('str');
-    it(title, function() {
+    test('str', function() {
       var result = vitals.to.str('str');
       assert( result === 'str' );
     });
 
-    title = callStr(10);
-    it(title, function() {
+    test(10, function() {
       var result = vitals.to.str(10);
       assert( result === '10' );
     });
 
-    title = callStr(NaN);
-    it(title, function() {
+    test(NaN, function() {
       var result = vitals.to.str(NaN);
       assert( result === 'NaN' );
     });
 
-    title = callStr({ a: 1, b: 2 });
-    it(title, function() {
+    test({ a: 1, b: 2 }, function() {
       var result = vitals.to.str({ a: 1, b: 2 });
       assert( result === '[object Object]' );
     });
 
-    title = callStr([ 1, 2, 3 ]);
-    it(title, function() {
+    test([ 1, 2, 3 ], function() {
       var result = vitals.to.str([ 1, 2, 3 ]);
       assert( result === '1,2,3' );
     });
 
-    title = callStr(function(){});
-    it(title, function() {
+    test('<function>', function() {
       var result = vitals.to.str(function(){});
       assert( result === 'function (){}' );
     });
 
-    title = callStr(/regex/g);
-    it(title, function() {
+    test(/regex/g, function() {
       var result = vitals.to.str(/regex/g);
       assert( result === '/regex/g' );
     });
 
-    title = callStr( (function(){ return arguments; })(1, 2, 3) );
-    it(title, function() {
+    test('<arguments>', function() {
       var args = (function(){ return arguments; })(1, 2, 3);
       var result = vitals.to.str(args);
       assert( result === '[object Arguments]' );
     });
-
   });
 
-  title = titleStr('should join array with separator');
-  describe(title, function() {
+  should('join array with separator', function() {
 
-    title = callStr([ 1, 2, 3 ], '--');
-    it(title, function() {
+    test([ 1, 2, 3 ], '--', function() {
       var result = vitals.to.str([ 1, 2, 3 ], '--');
       assert( result === '1--2--3' );
     });
-
   });
 
-  describe('should throw an error', function() {
+  should('throw an error', function() {
 
-    title = callStr();
-    it(title, function() {
+    test(function() {
       assert.throws(function() {
         vitals.to.str();
-      });
+      }, validErr);
     });
 
-    title = callStr([], false);
-    it(title, function() {
+    test([], false, function() {
       assert.throws(function() {
         vitals.to.str([], false);
-      });
+      }, validTypeErr);
     });
-
   });
-
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 2);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('to.str', arguments, 3);
-}
