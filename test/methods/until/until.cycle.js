fd70888deec46776ab45c8f4773d5d1f559e56b9
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 17:50:20 -0800

    Rebuilt tests for vitals.until.cycle

--- test/methods/until/until.cycle.js
+++ test/methods/until/until.cycle.js
@@ -1,147 +1,98 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - UNTIL.CYCLE
+ * VITALS UNIT TESTS: vitals.until.cycle
  * -----------------------------------------------------------------------------
- * @see [vitals.until]{@link https://github.com/imaginate/vitals/wiki/vitals.until}
+ * @section base
+ * @see [vitals.until docs](https://github.com/imaginate/vitals/wiki/vitals.until)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.until.cycle (section:base)', function() {
-  var title;
+method('until.cycle', function() {
 
-  describe('basic tests', function() {
+  should('return valid boolean', function() {
 
-    title = titleStr('should call the iteratee x times');
-    describe(title, function() {
-
-      title = callStr(true, 8, '<iteratee>');
-      it(title, function() {
-        var times = 0;
-        vitals.until.time(true, 8, function() {
-          ++times;
-        });
-        assert( times === 8 );
+    test(true, 3, '<iteratee>', function() {
+      var pass = vitals.until.cycle(true, 3, function(i) {
+        return i === 1;
       });
-
-      title = callStr(true, 15, '<iteratee>');
-      it(title, function() {
-        var times = 0;
-        vitals.until.time(true, 15, function(time) {
-          assert( times++ === time );
-        });
-        assert( times === 15 );
-      });
-
+      assert( pass === true );
     });
 
-    title = titleStr('should return valid boolean');
-    describe(title, function() {
-
-      title = callStr(true, 5, '<iteratee>');
-      it(title, function() {
-        var pass = vitals.until.time(true, 5, function(time) {
-          return time === 3;
-        });
-        assert( pass === true );
+    test(true, 3, '<iteratee>', function() {
+      var fail = vitals.until.cycle(true, 3, function(i) {
+        return i === 5;
       });
-
-      title = callStr(true, 3, '<iteratee>');
-      it(title, function() {
-        var fail = vitals.until.time(true, 3, function(time) {
-          return time === 3;
-        });
-        assert( fail === false );
-      });
-
+      assert( fail === false );
     });
-
-    title = titleStr('should correctly bind the iteratee');
-    describe(title, function() {
-
-      title = callStr(true, 5, '<iteratee>', '<thisArg>');
-      it(title, function() {
-        var times = 0;
-        var thisArg = {};
-        var fail = vitals.until.time(true, 5, function() {
-          this.times = ++times;
-        }, thisArg);
-        assert( times === 5 );
-        assert( thisArg.times === 5 );
-        assert( fail === false );
-      });
-
-    });
-
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.time();
-        });
-      });
-
-      title = callStr(true);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.time(true);
-        });
-      });
-
-      title = callStr(true, 5);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.time(true, 5);
-        });
-      });
-
-      title = callStr(true, {}, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.time(true, {}, function(){});
-        });
-      });
-
-      title = callStr(true, 5, function(){}, 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.time(true, 5, function(){}, 'fail');
-        });
-      });
-
-    });
-
   });
 
+  should('call the iteratee x times', function() {
+
+    test(true, 3, '<iteratee>', function() {
+      var keys = [];
+      vitals.until.cycle(true, 3, function(i) {
+        keys.push(i);
+      });
+      assert( keys[0] === 0 );
+      assert( keys[1] === 1 );
+      assert( keys[2] === 2 );
+      assert( keys.length === 3 );
+    });
+  });
+
+  should('correctly bind the iteratee', function() {
+
+    test(true, 3, '<iteratee>', '<this>', function() {
+      var cycle = 0;
+      var self = [];
+      var fail = vitals.until.cycle(true, 3, function(i) {
+        this[i] = ++cycle;
+      }, self);
+      assert( self[0] === 1 );
+      assert( self[1] === 2 );
+      assert( self[2] === 3 );
+      assert( self.length === 3 );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.until.cycle();
+      }, validTypeErr);
+    });
+
+    test(true, function() {
+      assert.throws(function() {
+        vitals.until.cycle(true);
+      }, validTypeErr);
+    });
+
+    test(true, 5, function() {
+      assert.throws(function() {
+        vitals.until.cycle(true, 5);
+      }, validTypeErr);
+    });
+
+    test(true, {}, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.until.cycle(true, {}, function(){});
+      }, validTypeErr);
+    });
+
+    test(true, 5, '<iteratee>', 'fail', function() {
+      assert.throws(function() {
+        vitals.until.cycle(true, 5, function(){}, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('until.time', arguments, 4);
-}
