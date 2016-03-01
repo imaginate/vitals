4cb793f0d9c99901da30862024def5675c1369a0
Adam Smith <adamsmith@algorithmiv.com>
2016-02-29 17:50:26 -0800

    Rebuilt tests for vitals.until.object

--- test/methods/until/until.object.js
+++ test/methods/until/until.object.js
@@ -1,201 +1,133 @@
 /**
  * -----------------------------------------------------------------------------
- * TEST - VITALS - JS METHOD - UNTIL.OBJECT
+ * VITALS UNIT TESTS: vitals.until.object
  * -----------------------------------------------------------------------------
- * @see [vitals.until]{@link https://github.com/imaginate/vitals/wiki/vitals.until}
+ * @section base
+ * @see [vitals.until docs](https://github.com/imaginate/vitals/wiki/vitals.until)
+ * @see [test api](https://github.com/imaginate/vitals/blob/master/test/setup/interface.js)
+ * @see [test helpers](https://github.com/imaginate/vitals/blob/master/test/setup/helpers.js)
  *
  * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
  * @copyright 2016 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Supporting Libraries:
- * @see [are]{@link https://github.com/imaginate/are}
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
  * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
-describe('vitals.until.object (section:base)', function() {
-  var title;
+method('until.object', 'until.obj', function() {
 
-  describe('basic tests', function() {
+  should('return valid boolean', function() {
 
-    // newObj()= {
-    //   'a':  'd',
-    //   'b':  'e',
-    //   'c':  'f',
-    //   '1':   4,
-    //   '2':   5,
-    //   '3':   6,
-    //   'a1': '1',
-    //   'b2': '2',
-    //   'c3': '3'
-    // }
-
-    title = titleStr('should iterate over every prop');
-    describe(title, function() {
-
-      title = callStr(true, '<object>', '<iteratee>');
-      it(title, function() {
-        var obj = {};
-        vitals.until.obj(true, newObj(), function(val, key) {
-          obj[key] = val;
-        });
-        each(newObj(), function(val, key) {
-          assert( obj[key] === val );
-        });
+    test(true, '<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var pass = vitals.until.obj(true, obj, function(val) {
+        return val === 2;
       });
-
+      assert( pass === true );
     });
 
-    title = titleStr('should return valid boolean');
-    describe(title, function() {
-
-      title = callStr(true, '<object>', '<iteratee>');
-      it(title, function() {
-        var pass = vitals.until.obj(true, newObj(), function(val, key) {
-          return val === 5;
-        });
-        assert( pass === true );
+    test('b', '<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var pass = vitals.until.obj('b', obj, function(val, key) {
+        return key;
       });
-
-      title = callStr(true, '<object>', '<iteratee>');
-      it(title, function() {
-        var fail = vitals.until.obj(true, newObj(), function(val, key) {
-          return val === '5';
-        });
-        assert( fail === false );
-      });
-
-      title = callStr(5, '<object>', '<iteratee>');
-      it(title, function() {
-        var pass = vitals.until.obj(5, newObj(), function(val, key) {
-          return val;
-        });
-        assert( pass === true );
-      });
-
-      title = callStr('5', '<object>', '<iteratee>');
-      it(title, function() {
-        var fail = vitals.until.obj('5', newObj(), function(val, key) {
-          return val;
-        });
-        assert( fail === false );
-      });
-
+      assert( pass === true );
     });
 
-    title = titleStr('should correctly clone the source');
-    describe(title, function() {
-
-      title = callStr(true, '<object>', '<iteratee>');
-      it(title, function() {
-        var obj1 = newObj();
-        vitals.until.obj(true, obj1, function(val, key, obj) {
-          assert( obj !== obj1 );
-        });
+    test(true, '<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var fail = vitals.until.obj(true, obj, function(val) {
+        return val === 4;
       });
-
+      assert( fail === false );
     });
 
-    title = titleStr('should correctly bind the iteratee');
-    describe(title, function() {
-
-      title = callStr(true, '<object>', '<iteratee>', '<thisArg>');
-      it(title, function() {
-        var obj = newObj();
-        var thisArg = {};
-        var fail = vitals.until.obj(true, obj, function(val, key) {
-          this[key] = val;
-        }, thisArg);
-        each(obj, function(val, key) {
-          assert( has(thisArg, key)    );
-          assert( thisArg[key] === val );
-        });
-        assert( fail === false );
+    test('d', '<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var fail = vitals.until.obj('d', obj, function(val, key) {
+        return key;
       });
-
+      assert( fail === false );
     });
-
-    describe('should throw an error', function() {
-
-      title = callStr();
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.obj();
-        });
-      });
-
-      title = callStr(true);
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.obj(true);
-        });
-      });
-
-      title = callStr(true, {});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.obj(true, {});
-        });
-      });
-
-      title = callStr(true, null, function(){});
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.obj(true, null, function(){});
-        });
-      });
-
-      title = callStr(true, {}, function(){}, 'fail');
-      it(title, function() {
-        assert.throws(function() {
-          vitals.until.obj(true, {}, function(){}, 'fail');
-        });
-      });
-
-    });
-
   });
 
+  should('iterate over all key => value pairs in object', function() {
+
+    test(true, '<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var vals = [];
+      var keys = [];
+      vitals.until.obj(true, obj, function(val, key) {
+        vals.push(val);
+        keys.push(key);
+      });
+      assert( hasVal(vals, 1) );
+      assert( hasVal(vals, 2) );
+      assert( hasVal(vals, 3) );
+      assert( vals.length === 3 );
+      assert( hasVal(keys, 'a') );
+      assert( hasVal(keys, 'b') );
+      assert( hasVal(keys, 'c') );
+      assert( keys.length === 3 );
+    });
+  });
+
+  should('correctly clone the source', function() {
+
+    test(true, '<object>', '<iteratee>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      vitals.until.obj(true, obj, function(val, key, src) {
+        assert( obj !== src )
+      });
+    });
+  });
+
+  should('correctly bind the iteratee', function() {
+
+    test(true, '<object>', '<iteratee>', '<this>', function() {
+      var obj = { a: 1, b: 2, c: 3 };
+      var self = {};
+      vitals.until.obj(true, obj, function(val, key) {
+        this[key] = val;
+      }, self);
+      assert( self !== obj );
+      assert( self.a === 1 );
+      assert( self.b === 2 );
+      assert( self.c === 3 );
+    });
+  });
+
+  should('throw an error', function() {
+
+    test(function() {
+      assert.throws(function() {
+        vitals.until.obj();
+      }, validTypeErr);
+    });
+
+    test(true, function() {
+      assert.throws(function() {
+        vitals.until.obj(true);
+      }, validTypeErr);
+    });
+
+    test(true, {}, function() {
+      assert.throws(function() {
+        vitals.until.obj(true, {});
+      }, validTypeErr);
+    });
+
+    test(true, null, '<iteratee>', function() {
+      assert.throws(function() {
+        vitals.until.obj(true, null, function(){});
+      }, validTypeErr);
+    });
+
+    test(true, {}, '<iteratee>', 'fail', function() {
+      assert.throws(function() {
+        vitals.until.obj(true, {}, function(){}, 'fail');
+      }, validTypeErr);
+    });
+  });
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// PRIVATE HELPERS
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @private
- * @param {string} shouldMsg
- * @return {string}
- */
-function titleStr(shouldMsg) {
-  return breakStr(shouldMsg, 3);
-}
-
-/**
- * @private
- * @param {...*} args
- * @return {string}
- */
-function callStr() {
-  return testCall('until.obj', arguments, 4);
-}
-
-/**
- * @private
- * @return {!Object}
- */
-function newObj() {
-  return {
-    'a':  'd',
-    'b':  'e',
-    'c':  'f',
-    '1':   4,
-    '2':   5,
-    '3':   6,
-    'a1': '1',
-    'b2': '2',
-    'c3': '3'
-  };
-}
