cbbec38e1c1a1c12144718327c468531adad8401
Adam Smith <adam@imaginate.life>
2017-04-13 19:43:21 -0700

    Added task helper getRepoRoot

---
+++ act/helpers/get-repo-root.js
@@ -0,0 +1,38 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: getRepoRoot
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ *
+ * Annotations:
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+/**
+ * @note Older node.js versions such as v0.10 required a `path' parameter.
+ *   Where newer node.js versions such as v7.9 did not require a parameter.
+ * @see [node.js v0.10](https://nodejs.org/docs/v0.10.0/api/path.html#path_path_resolve_from_to)
+ * @see [node.js v7.9](https://nodejs.org/docs/v7.9.0/api/path.html#path_path_resolve_paths)
+ * @private
+ * @param {...string} path
+ * @return {string}
+ */
+var resolvePath = require('path').resolve;
+
+/**
+ * @private
+ * @const {string}
+ */
+var REPO_DIR = resolvePath(__dirname, '../..');
+
+/**
+ * @public
+ * @return {string}
+ */
+module.exports = function getRepoRoot() {
+  return REPO_DIR;
+};
