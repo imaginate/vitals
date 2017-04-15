dc3d766f801c57866f97c0c24fc7bee2c1c868b0
Adam Smith <adam@imaginate.life>
2017-04-14 23:48:43 -0700

    Added type checks to task helper: sliceArray

--- act/helpers/slice-array.js
+++ act/helpers/slice-array.js
@@ -13,6 +13,64 @@
 'use strict';
 
 /**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('./is.js');
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isArrayLike = IS.arrayLike;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNumber = IS.number;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isUndefined = IS.undefined;
+
+/**
+ * @private
+ * @param {number} val
+ * @return {boolean}
+ */
+var isWholeNumber = IS.wholeNumber;
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isGE = IS.greaterOrEqual;
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isGT = IS.greaterThan;
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isLT = IS.lessThan;
+
+/**
  * @public
  * @param {(!Object|function)} source
  * @param {number=} start - [default= 0]
@@ -30,28 +88,46 @@ module.exports = function sliceArray(source, start, end) {
   /** @type {number} */
   var i;
 
+  if ( !isArrayLike(source) )
+    throw new TypeError('invalid `source` type (must be an array like object)');
+  if ( !isUndefined(start) ) {
+    if ( !isNumber(start) )
+      throw new TypeError('invalid `start` type (must be a number or undefined)');
+    if ( !isWholeNumber(start) )
+      throw new RangeError('invalid `start` number (must be a whole number)');
+  }
+  if ( !isUndefined(end) ) {
+    if ( !isNumber(end) )
+      throw new TypeError('invalid `end` type (must be a number or undefined)');
+    if ( !isWholeNumber(end) )
+      throw new RangeError('invalid `end` number (must be a whole number)');
+  }
+
   len = source.length;
 
-  if (!start)
+  if ( isUndefined(start) )
     start = 0;
-  else if (start < 0) {
+  else if ( isLT(start, 0) ) {
     start = len + start;
-    if (start < 0)
+    if ( isLT(start, 0) )
       start = 0;
   }
 
-  if (end === undefined || end > len)
+  if ( isUndefined(end) || isGT(end, len) )
     end = len;
-  else if (end < 0)
+  else if ( isLT(end, 0) )
     end = len + end;
 
-  if (start >= end)
+  if ( isGE(start, end) )
     return [];
 
   arr = new Array(end - start);
-  ii = start - 1;
+  ii = start;
   i = 0;
-  while (++ii < end)
-    arr[i++] = source[ii];
+  while ( isLT(ii, end) ) {
+    arr[i] = source[ii];
+    ++ii;
+    ++i;
+  }
   return arr;
 };
