107cd52380449041a14a00f1b0068a265d3d3104
Adam Smith <adam@imaginate.life>
2017-04-15 01:07:19 -0700

    Cleaned up task helper: getFileContent

--- act/helpers/get-file.js
+++ act/helpers/get-file.js
@@ -2,30 +2,84 @@
  * -----------------------------------------------------------------------------
  * ACT TASK HELPER: getFile
  * -----------------------------------------------------------------------------
- * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
- * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
  *
  * Annotations:
  * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
 'use strict';
 
-var fs = require('fs');
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
 
 /**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var FS = require('fs');
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('./is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isFile = IS.file;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+/**
+ * @see [node.js v0.10](https://nodejs.org/docs/v0.10.0/api/fs.html#fs_fs_readfilesync_filename_options)
+ * @see [node.js v7.9](https://nodejs.org/docs/v7.9.0/api/fs.html#fs_fs_readfilesync_file_options)
+ * @private
+ * @param {string} filepath
+ * @param {string=} encoding
+ * @return {(string|!Buffer)} - If no encoding is provided a buffer is returned.
+ *   Otherwise a string of the file's content is returned.
+ */
+var readFile = FS.readFileSync;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
  * @param {string} filepath
  * @param {boolean=} buffer
  * @return {(!Buffer|string)}
  */
-module.exports = function getFile(filepath, buffer) {
+module.exports = function getFileContent(filepath, buffer) {
 
   /** @type {string} */
   var content;
 
-  if (buffer) return fs.readFileSync(filepath);
+  if ( !isString(filepath) )
+    throw new TypeError('invalid `filepath` type (must be a string)');
+  if ( !isFile(filepath) )
+    throw new Error('invalid `filepath` path (must be a readable file)');
 
-  content = fs.readFileSync(filepath, 'utf8');
+  if ( !!buffer )
+    return readFile(filepath);
+
+  content = readFile(filepath, 'utf8');
   return content && content.replace(/\r\n?/g, '\n');
 };
