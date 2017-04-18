e0df4283278681580f63519f8bda43b10d11c57f
Adam Smith <adam@imaginate.life>
2017-04-17 22:33:04 -0700

    Updated task helper: getRepoRoot

--- act/helpers/get-repo-root.js
+++ act/helpers/get-repo-root.js
@@ -12,15 +12,11 @@
 'use strict';
 
 /**
- * @note Older node.js versions such as v0.10 required a `path' parameter.
- *   Where newer node.js versions such as v7.9 did not require a parameter.
- * @see [node.js v0.10](https://nodejs.org/docs/v0.10.0/api/path.html#path_path_resolve_from_to)
- * @see [node.js v7.9](https://nodejs.org/docs/v7.9.0/api/path.html#path_path_resolve_paths)
  * @private
- * @param {...string} path
+ * @param {(!ArrayLike<string>|...string)=} path
  * @return {string}
  */
-var resolvePath = require('path').resolve;
+var resolvePath = require('./resolve-path.js');
 
 /**
  * @private
