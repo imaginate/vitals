e2e37d2db0c0d13349531ac96e913a616409e510
Adam Smith <adam@imaginate.life>
2017-04-19 07:08:38 -0700

    Added wiki BlockElement end: hr

---
+++ act/helpers/mk-doc/mk-body/parse-to-html/block-elements/hr/end.js
@@ -0,0 +1,26 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: getEndIndex
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {!Array<string>} lines
+ * @param {number} depth
+ * @return {number}
+ */
+module.exports = function getEndIndex(lines, depth) {
+  return 1;
+};
