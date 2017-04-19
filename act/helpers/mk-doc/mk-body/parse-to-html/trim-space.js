45a11dca82376c6f51dce070333501145c6b6721
Adam Smith <adam@imaginate.life>
2017-04-19 07:01:25 -0700

    Added wiki parse helper: trimSpace

---
+++ act/helpers/mk-doc/mk-body/parse-to-html/trim-space.js
@@ -0,0 +1,63 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: trimSpace
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var START = /^ +/;
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var END = / +$/;
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../../is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} source
+ * @return {string}
+ */
+function trimSpace(source) {
+
+  if ( !isString(source) )
+    throw new TypeError('invalid `source` type (must be a string)');
+
+  source = source.replace(START, '');
+  return source.replace(END, '');
+};
