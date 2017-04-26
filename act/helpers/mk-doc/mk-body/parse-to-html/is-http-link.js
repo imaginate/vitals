4f32d3651adb6a93828d790aad88de3accedf7d1
Adam Smith <adam@imaginate.life>
2017-04-25 17:00:32 -0700

    Added wiki helper: isHttpLink

---
+++ act/helpers/mk-doc/mk-body/parse-to-html/is-http-link.js
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: isHttpLink
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var LINK = /^https?:\/\/[a-zA-Z0-9_-\.]+\.[a-z]{2,}/;
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../../is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} src
+ * @return {boolean}
+ */
+function isHttpLink(src) {
+
+  if ( !isString(src) )
+    throw new TypeError('invalid `src` type (must be a string)');
+
+  return LINK.test(src);
+};
