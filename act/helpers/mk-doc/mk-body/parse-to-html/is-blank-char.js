367f10754c97c73dafb7628ef1eb1dd1085d53e9
Adam Smith <adam@imaginate.life>
2017-04-26 15:43:58 -0700

    Added wiki helper: isBlankChar

---
+++ act/helpers/mk-doc/mk-body/parse-to-html/is-blank-char.js
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: isBlankChar
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var BLANK = /^[ \t]$/;
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../../is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} ch
+ * @return {boolean}
+ */
+function isBlankChar(ch) {
+
+  if ( !isString(ch) )
+    throw new TypeError('invalid `ch` type (must be a string)');
+
+  return BLANK.test(ch);
+};
