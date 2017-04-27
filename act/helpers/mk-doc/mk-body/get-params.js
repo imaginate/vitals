771cf842754229b593be6f0290aefd363d68d3d1
Adam Smith <adam@imaginate.life>
2017-04-26 23:41:21 -0700

    Rebuilt wiki method: getParams

--- act/helpers/mk-doc/mk-body/get-params.js
+++ act/helpers/mk-doc/mk-body/get-params.js
@@ -11,23 +11,199 @@
 
 'use strict';
 
-var PARAM = /^@param \{[^}]+\} ([a-zA-Z.]+).*$/;
-var TYPE  = /^@param \{([^}]+)\}.*$/;
-var DEF   = /^@param .*?\[default= ([^\]]+)\].*$/;
-
-var getDescription = require('./get-description');
-
-/** @type {number} */
-var index;
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
 
 /**
+ * @private
+ * @const {!RegExp}
+ */
+var DFLT = /^=[ \t]*`([^`]+)`.*$/;
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var PARAM = /^[a-zA-Z_.$]+/;
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var PARAM_TAG = /^@param/;
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var PROP = /^@param[ \t]+\{[^}]+\}[ \t]+[a-zA-Z_$]+\./;
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var RETURN_TAG = /^@return/;
+
+/**
+ * @private
+ * @const {string}
+ */
+var TEMPLATE = require('../get-template.js')('param');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var TYPE = /[^{}]+(?=\})/;
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var TYPE_TRIM = /^[^}]+\}[ \t]+/;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} source
+ * @param {!RegExp} pattern
+ * @return {string}
+ */
+var getMatch = require('../../get-match.js');
+
+/**
+ * @private
+ * @param {string} src
+ * @param {string} tag
+ * @param {string} val
+ * @return {string}
+ */
+var insertTag = require('../insert-tag.js');
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isGT = IS.greaterThan;
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isLT = IS.lessThan;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNumber = IS.number;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isUndefined = IS.undefined;
+
+/**
+ * @private
+ * @param {(!Object|function)} source
+ * @param {number=} start = `0`
+ * @param {number=} end = `source.length`
+ * @return {!Array}
+ */
+var sliceArray = require('../../slice-array.js');
+
+/**
+ * @private
+ * @param {string} src
+ * @return {string}
+ */
+var trimSpace = require('./trim-space.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// METHODS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {!Array<string>} lines
+ * @param {?Object=} opts
+ * @param {number=} opts.depth = `0`
+ * @param {boolean=} opts.html = `false`
+ * @return {string}
+ */
+var getDescription = require('./get-description.js');
+
+/**
+ * @private
  * @param {!Array<string>} lines
  * @return {string}
  */
-module.exports = function getParams(lines) {
+function parseParam(lines) {
 
-  /** @type {!Array<!Array<string>>} */
-  var params;
+  /** @type {string} */
+  var result;
+  /** @type {string} */
+  var value;
+  /** @type {string} */
+  var line;
+
+  if ( !isGT(lines.length, 0) )
+    return '';
+
+  result = TEMPLATE;
+
+  value = isNumber(lines.id) && isGT(lines.id, 0)
+    ? lines.id.toString()
+    : '';
+  result = insertTag(result, 'index', value);
+
+  line = lines[0].replace(PARAM_TAG, '');
+  line = trimSpace(line);
+  value = getMatch(line, TYPE);
+  result = insertTag(result, 'type', value);
+
+  line = line.replace(TYPE_TRIM, '');
+  value = getMatch(line, PARAM);
+  result = insertTag(result, 'param', value);
+
+  line = line.replace(PARAM, '');
+  line = trimSpace(line);
+  value = DFLT.test(line)
+    ? line.replace(DFLT, '$1')
+    : '';
+  result = insertTag(result, 'dflt', value);
+
+  lines = sliceArray(lines, 1);
+  value = getDescription(lines, { html: true });
+  result = insertTag(result, 'desc', value);
+
+  return result;
+}
+
+/**
+ * @private
+ * @param {!Array<!Array<string>>} params
+ * @return {string}
+ */
+function parseParams(params) {
+
   /** @type {string} */
   var result;
   /** @type {number} */
@@ -35,42 +211,61 @@ module.exports = function getParams(lines) {
   /** @type {number} */
   var i;
 
-  index = 0;
-  lines = pruneLines(lines);
-  params = buildParams(lines);
   result = '';
+
   len = params.length;
   i = -1;
-  while (++i < len) result += getParam(params[i]);
+  while ( isLT(++i, len) )
+    result += parseParam(params[i]);
+
   return result;
-};
+}
 
 /**
+ * @private
  * @param {!Array<string>} lines
- * @return {string}
+ * @return {!Array<!Array<string>>}
  */
-function getParam(lines) {
+function prepParams(lines) {
 
-  /** @type {string} */
+  /** @type {!Array<!Array<string>>} */
+  var params;
+  /** @type {!Array<string>} */
   var param;
-  /** @type {string} */
-  var intro;
-  /** @type {boolean} */
-  var prop;
-  /** @type {string} */
-  var type;
-  /** @type {string} */
-  var desc;
-  /** @type {string} */
-  var def;
+  /** @type {number} */
+  var start;
+  /** @type {number} */
+  var len;
+  /** @type {number} */
+  var id;
+  /** @type {number} */
+  var i;
 
-  param = lines[0].replace(PARAM, '$1');
-  type  = lines[0].replace(TYPE,  '$1');
-  prop  = param.includes('.');
-  def   = getDefault(lines, prop ? 3 : 2);
-  desc  = getDescrip(lines, prop ? 3 : 2);
-  intro = prop ? '    - ' : '  ' + (++index) + '. ';
-  return intro + '**' + param + '**  <i>` ' + type + ' `</i>\n' + def + desc;
+  if ( !isGT(lines.length, 0) )
+    return [];
+
+  params = [];
+  start = 0;
+  id = 0;
+
+  len = lines.length;
+  i = 0;
+  while ( isLT(++i, len) ) {
+    if ( PARAM_TAG.test(lines[i]) ) {
+      param = sliceArray(lines, start, i);
+      if ( !PROP.test(param[0]) )
+        param.id = ++id;
+      params.push(param);
+      start = i;
+    }
+  }
+
+  param = sliceArray(lines, start);
+  if ( !PROP.test(param[0]) )
+    param.id = ++id;
+  params.push(param);
+
+  return params;
 }
 
 /**
@@ -86,100 +281,35 @@ function pruneLines(lines) {
   var end;
   /** @type {number} */
   var len;
-  /** @type {number} */
-  var i;
 
-  lines = lines.slice(0, -1);
-  len = lines.length;
-  i = -1;
-  while ( ++i < len && (start === undefined || end === undefined) ) {
-    if ( /^@public/.test(lines[i]) ) start = i + 1;
-    if ( /^@return/.test(lines[i]) ) end = i;
+  len = lines.length - 1;
+  end = 0;
+  while ( isLT(end, len) && !RETURN_TAG.test(lines[end]) ) {
+    if ( isUndefined(start) && PARAM_TAG.test(lines[end]) )
+      start = end;
+    ++end;
   }
-  return lines.slice(start, end);
+
+  return isUndefined(start)
+    ? []
+    : sliceArray(lines, start, end);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
 /**
- * @private
+ * @public
  * @param {!Array<string>} lines
- * @return {!Array<!Array<string>>}
+ * @return {string}
  */
-function buildParams(lines) {
+module.exports = function getParams(lines) {
 
   /** @type {!Array<!Array<string>>} */
   var params;
-  /** @type {!Array<string>} */
-  var param;
-  /** @type {number} */
-  var start;
-  /** @type {number} */
-  var len;
-  /** @type {number} */
-  var i;
 
-  params = [];
-  start = 0;
-  len = lines.length;
-  i = 0;
-  while (++i < len) {
-    if ( !/^@param/.test(lines[i]) ) continue;
-    param = lines.slice(start, i);
-    params.push(param);
-    start = i;
-  }
-  param = lines.slice(start);
-  params.push(param);
-  return params;
-}
-
-/**
- * @private
- * @param {!Array<string>} lines
- * @param {number} indents
- * @return {string}
- */
-function getDefault(lines, indents) {
-
-  /** @type {string} */
-  var indent;
-  /** @type {string} */
-  var def;
-
-  if ( !DEF.test(lines[0]) ) return '';
-
-  indent = getIndent(indents);
-  def = lines[0].replace(DEF, '$1');
-  return '\n' + indent + 'default value: ` ' + def + ' `\n';
-}
-
-/**
- * @private
- * @param {!Array<string>} lines
- * @param {number} indents
- * @return {string}
- */
-function getDescrip(lines, indents) {
-
-  /** @type {string} */
-  var desc;
-
-  desc = getDescription(lines, indents);
-  return desc && '\n' + desc + '\n';
-}
-
-/**
- * @private
- * @param {number} indents
- * @return {string}
- */
-function getIndent(indents) {
-
-  /** @type {string} */
-  var indent;
-
-  if (indents < 1) return '';
-
-  indent = '';
-  while (indents--) indent += '  ';
-  return indent;
-}
+  lines = pruneLines(lines);
+  params = prepParams(lines);
+  return parseParams(params);
+};
