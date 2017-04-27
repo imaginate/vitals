aec58881b0036c43c98c81341b0813e3428012ec
Adam Smith <adam@imaginate.life>
2017-04-27 00:32:13 -0700

    Rebuilt wiki method getReturns

--- act/helpers/mk-doc/mk-body/get-returns.js
+++ act/helpers/mk-doc/mk-body/get-returns.js
@@ -11,27 +11,163 @@
 
 'use strict';
 
-var TYPE  = /^@returns? \{([^}]+)\}.*$/;
-
-var getDescription = require('./get-description');
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
 
 /**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var RETURN = /^@returns?/;
+
+/**
+ * @private
+ * @const {string}
+ */
+var TEMPLATE = require('../get-template.js')('returns');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var TYPE = /[^{}]+(?=\})/;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} source
+ * @param {!RegExp} pattern
+ * @return {string}
+ */
+var getMatch = require('../../get-match.js');
+
+/**
+ * @private
+ * @param {string} src
+ * @param {string} tag
+ * @param {string} val
+ * @return {string}
+ */
+var insertTag = require('../insert-tag.js');
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isGT = IS.greaterThan;
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isLT = IS.lessThan;
+
+/**
+ * @private
+ * @param {*} line
+ * @return {boolean}
+ */
+function isReturn(line) {
+  return isString(line) && RETURN.test(line);
+}
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+/**
+ * @private
+ * @param {(!Object|function)} source
+ * @param {number=} start = `0`
+ * @param {number=} end = `source.length`
+ * @return {!Array}
+ */
+var sliceArray = require('../../slice-array.js');
+
+/**
+ * @private
+ * @param {string} src
+ * @return {string}
+ */
+var trimSpace = require('./trim-space.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// METHODS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {!Array<string>} lines
+ * @param {?Object=} opts
+ * @param {number=} opts.depth = `0`
+ * @param {boolean=} opts.html = `false`
+ * @return {string}
+ */
+var getDescription = require('./get-description.js');
+
+/**
+ * @private
  * @param {!Array<string>} lines
  * @return {string}
  */
-module.exports = function getReturns(lines) {
+function getType(lines) {
 
   /** @type {string} */
-  var type;
-  /** @type {string} */
-  var desc;
+  var line;
 
-  lines = pruneLines(lines);
-  type  = lines.length ? lines[0].replace(TYPE, '$1') : 'undefined';
-  desc  = getDescription(lines, 0);
-  desc  = desc && '\n' + desc + '\n';
-  return '<i>' + type + '</i>\n' + desc;
-};
+  if ( !isGT(lines.length, 0) )
+    return 'undefined';
+
+  line = lines[0];
+
+  if ( !isReturn(line) )
+    return 'undefined';
+
+  line = line.replace(RETURN, '');
+  line = trimSpace(line);
+  return getMatch(line, TYPE) || 'undefined';
+}
+
+/**
+ * @private
+ * @param {!Array<string>} lines
+ * @return {string}
+ */
+function parseReturns(lines) {
+
+  /** @type {string} */
+  var result;
+  /** @type {string} */
+  var value;
+
+  result = TEMPLATE;
+
+  value = getType(lines);
+  result = insertTag(result, 'type', value);
+
+  lines = sliceArray(lines, 1);
+  value = getDescription(lines, { html: true });
+  result = insertTag(result, 'description', value);
+
+  return result;
+}
 
 /**
  * @private
@@ -44,13 +180,25 @@ function pruneLines(lines) {
   var start;
   /** @type {number} */
   var len;
-  /** @type {number} */
-  var i;
 
   len = lines.length;
-  i = -1;
-  while (++i < len && start === undefined) {
-    if ( /^@return/.test(lines[i]) ) start = i;
-  }
-  return start === undefined ? [] : lines.slice(start, -1);
+  start = 0;
+  while ( isLT(start, len) && !isReturn(lines[start]) )
+    ++start;
+
+  return sliceArray(lines, start, -1);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {!Array<string>} lines
+ * @return {string}
+ */
+module.exports = function getReturns(lines) {
+  lines = pruneLines(lines);
+  return parseReturns(lines);
+};
