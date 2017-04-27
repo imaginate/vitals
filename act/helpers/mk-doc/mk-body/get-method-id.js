4747b102d1f5b05c7fd902e7ff9a403d12560c0c
Adam Smith <adam@imaginate.life>
2017-04-26 21:00:33 -0700

    Added wiki helper: getMethodID

---
+++ act/helpers/mk-doc/mk-body/get-method-id.js
@@ -0,0 +1,63 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: getMethodID
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var MAIN = /^[a-zA-Z0-9_]+\./;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} method
+ * @return {string}
+ */
+module.exports = function getMethodID(method) {
+
+  /** @type {string} */
+  var id;
+
+  if ( !isString(method) )
+    throw new TypeError('invalid `method` type (must be a string)');
+
+  if ( !MAIN.test(method) )
+    return 'main';
+
+  id = method.replace(MAIN, '');
+  return id.replace(/\./g, '-');
+};
