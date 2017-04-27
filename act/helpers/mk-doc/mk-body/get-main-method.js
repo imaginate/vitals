d531f511850a9e3016a55fa68a479b855313e939
Adam Smith <adam@imaginate.life>
2017-04-26 18:21:40 -0700

    Added wiki method: getMainMethod

---
+++ act/helpers/mk-doc/mk-body/get-main-method.js
@@ -0,0 +1,72 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: getMainMethod
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var MAIN = /^[a-z]+/;
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} source
+ * @param {!RegExp} pattern
+ * @return {string}
+ */
+var getMatch = require('../../get-match.js');
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} method
+ * @return {string}
+ */
+module.exports = function getMainMethod(method) {
+
+  /** @type {string} */
+  var main;
+
+  if ( !isString(method) )
+    throw new TypeError('invalid `method` type (must be a string)');
+
+  main = getMatch(method, MAIN);
+
+  if (!main)
+    throw new Error('no main method found for `' + method + '`');
+
+  return main;
+};
