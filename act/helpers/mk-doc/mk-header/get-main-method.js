c13524254b6828a4d4c6f648c4b36639c8271da1
Adam Smith <adam@imaginate.life>
2017-04-29 14:24:56 -0700

    Added wiki mkHeader method: getMainMethod

---
+++ act/helpers/mk-doc/mk-header/get-main-method.js
@@ -0,0 +1,72 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: getMainMethod
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var MAIN = /\b[a-z]+\b/;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} source
+ * @param {!RegExp} pattern
+ * @return {string}
+ */
+var getMatch = require('../../get-match.js');
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} content
+ * @return {string}
+ */
+module.exports = function getMainMethod(content) {
+
+  /** @type {string} */
+  var main;
+
+  if ( !isString(content) )
+    throw new TypeError('invalid `content` type (must be a string)');
+
+  main = getMatch(content, MAIN);
+
+  if (!main)
+    throw new Error('no main method found in `' + content + '`');
+
+  return main;
+};
