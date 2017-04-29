f9fcf67f2b2d329dcadf819b8522cf885662adb8
Adam Smith <adam@imaginate.life>
2017-04-29 14:10:40 -0700

    Added wiki mkHeader helper: trimEol

---
+++ act/helpers/mk-doc/mk-header/trim-eol.js
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: trimEol
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var EOL = /\n$/;
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} content
+ * @return {string}
+ */
+module.exports = function trimEol(content) {
+
+  if ( !isString(content) )
+    throw new TypeError('invalid `content` type (must be a string)');
+
+  return content.replace(EOL, '');
+};
