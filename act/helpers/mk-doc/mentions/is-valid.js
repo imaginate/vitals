b14a6e08c231125f42209e7f74aee7481ef0b226
Adam Smith <adam@imaginate.life>
2017-04-29 22:27:50 -0700

    Added wiki mentions helper: isValid

---
+++ act/helpers/mk-doc/mentions/is-valid.js
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: isValid
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var VALID = /^[a-zA-Z0-9_-$]+$/;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} ref
+ * @return {string}
+ */
+module.exports = function isValid(ref) {
+
+  if ( !isString(ref) )
+    throw new TypeError('invalid `ref` type (must be a string)');
+
+  return VALID.test(ref);
+};
