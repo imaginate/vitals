15934ee251157839ae54e5d73ba8143283c3a64b
Adam Smith <adam@imaginate.life>
2017-04-29 19:32:06 -0700

    Added wiki mkBody helper: trimParamTag

---
+++ act/helpers/mk-doc/mk-body/trim-param-tag.js
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: trimParamTag
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var PARAM = /^@param[ \t]+/;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} line
+ * @return {string}
+ */
+module.exports = function trimParamTag(line) {
+
+  if ( !isString(line) )
+    throw new TypeError('invalid `line` type (must be a string)');
+
+  return line.replace(PARAM, '');
+};
