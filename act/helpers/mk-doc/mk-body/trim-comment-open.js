e108ff48ad37e7ae12347e85b0ef4810bbd24dbf
Adam Smith <adam@imaginate.life>
2017-04-29 18:40:31 -0700

    Added wiki mkBody helper: trimCommentOpen

---
+++ act/helpers/mk-doc/mk-body/trim-comment-open.js
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: trimCommentOpen
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var OPEN = /\/\*\*\n +\* /;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} docs
+ * @return {string}
+ */
+module.exports = function trimCommentOpen(docs) {
+
+  if ( !isString(docs) )
+    throw new TypeError('invalid `docs` type (must be a string)');
+
+  return docs.replace(OPEN, '');
+};
