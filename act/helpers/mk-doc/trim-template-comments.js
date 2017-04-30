90216bdbc0653ef6ada833f9863fd99d2706c744
Adam Smith <adam@imaginate.life>
2017-04-29 18:10:53 -0700

    Added mkDoc helper: trimTemplateComments

---
+++ act/helpers/mk-doc/trim-template-comments.js
@@ -0,0 +1,70 @@
+/**
+ * -----------------------------------------------------------------------------
+ * ACT TASK HELPER: trimTemplateComments
+ * -----------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ *
+ * @see [JSDoc3](http://usejsdoc.org)
+ * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ */
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var END_COMMENT = /\n[ \t]*#[^\n]*$/;
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var INIT_COMMENT = /^[ \t]*#[^\n]*\n/;
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('../is.js');
+
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var SUB_COMMENT = /\n[ \t]*#[^\n]*(?=\n)/g;
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {string} content
+ * @return {string}
+ */
+module.exports = function trimTemplateComments(content) {
+
+  if ( !isString(content) )
+    throw new TypeError('invalid `content` type (must be a string)');
+
+  content = content.replace(SUB_COMMENT, '');
+  content = content.replace(INIT_COMMENT, '');
+  return content.replace(END_COMMENT, '');
+};
