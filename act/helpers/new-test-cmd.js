1f4d187659e5ed6a1c6f9f00f32df4d8b2005108
Adam Smith <adam@imaginate.life>
2017-05-02 01:38:21 -0700

    Cleaned up & optimized task helper: newTestCmd

--- act/helpers/new-test-cmd.js
+++ act/helpers/new-test-cmd.js
@@ -11,9 +11,15 @@
 
 'use strict';
 
+////////////////////////////////////////////////////////////////////////////////
+// TYPEDEFS
+////////////////////////////////////////////////////////////////////////////////
+
 /**
  * @typedef {function} TestCmdMethod
- *
+ */
+
+/**
  * @typedef {{
  *   __CMD:   boolean,
  *   start:   !TestCmdMethod,
@@ -27,118 +33,244 @@
  * }} TestCmd
  */
 
-var is = require('./is');
-
-var TESTS_DIR  = 'test/methods';
-
-var DEFAULT_REPORT = 'specky';
-var DEFAULT_SLOW   = 5; // ms
-var DEFAULT_SETUP  = 'methods';
-
-/**
- * @param {(?Object|TestCmd)} opts
- * @param {?TestCmdMethod=} opts.start  - [default= null]
- * @param {?TestCmdMethod=} opts.close  - [default= null]
- * @param {string=} opts.reporter  - [default= "specky"]
- * @param {number=} opts.slow      - [default= 5]
- * @param {string=} opts.setup     - [default= "methods"]
- * @param {string=} opts.method    - [default= ""] Test only a specific method.
- * @param {string=} opts.section   - [default= ""] Test only a specific section.
- * @param {string=} opts.submethod - [default= ""] Test only a specific submethod.
- * @return {!TestCmd}
- */
-module.exports = function newTestCmd(opts) {
-
-  /** @type {!TestCmd} */
-  var cmd;
-
-  if (opts && opts.__CMD) return opts;
-
-  opts = parseOpts(opts);
-  cmd = { '__CMD': true };
-  cmd.section = opts.section;
-  cmd.report = opts.reporter;
-  cmd.setup = addJSExt(opts.setup);
-  cmd.slow = String(opts.slow);
-  cmd.dir = !opts.submethod;
-  cmd = addCmdMethods(cmd, opts.start, opts.close);
-  cmd = addTest(cmd, opts.method, opts.submethod);
-  return cmd;
-};
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
 
 /**
  * @private
- * @param {Object} opts
- * @return {!Object}
+ * @const {string}
  */
-function parseOpts(opts) {
-  opts = opts || {};
-  opts.submethod = is.str(opts.submethod)  ? opts.submethod : '';
-  opts.reporter  = is.str(opts.reporter)   ? opts.reporter  : DEFAULT_REPORT;
-  opts.reporter  = opts.reporter           ? opts.reporter  : DEFAULT_REPORT;
-  opts.section   = is.str(opts.section)    ? opts.section   : '';
-  opts.method    = is.str(opts.method)     ? opts.method    : '';
-  opts.setup     = is.str(opts.setup)      ? opts.setup     : DEFAULT_SETUP;
-  opts.setup     = opts.setup              ? opts.setup     : DEFAULT_SETUP;
-  opts.slow      = is.num(opts.slow)       ? opts.slow      : DEFAULT_SLOW;
-  opts.slow      = opts.slow >= 1          ? opts.slow      : DEFAULT_SLOW;
-  return opts;
-}
+var DEFAULT_REPORTER = 'specky';
 
 /**
  * @private
- * @param {!TestCmd} cmd
- * @param {?TestCmdMethod=} start
- * @param {?TestCmdMethod=} close
- * @return {!TestCmd}
+ * @const {string}
  */
-function addCmdMethods(cmd, start, close) {
-  cmd.start = is.func(start) ? start : function start(){};
-  cmd.close = is.func(close) ? close : function close(){};
-  return cmd;
-}
+var DEFAULT_SETUP = 'methods';
 
 /**
  * @private
- * @param {!TestCmd} cmd
- * @param {?string} method
- * @param {?string} submethod
- * @return {!TestCmd}
+ * @const {number}
  */
-function addTest(cmd, method, submethod) {
-
-  /** @type {string} */
-  var path;
-
-  path = slashDir(TESTS_DIR);
-  path += method ? slashDir(method) : '';
-
-  if (submethod) {
-    path += addJSExt(submethod);
-    if ( !is.file(path) ) throw new ReferenceError('invalid test file');
-  }
-  else if ( !is.dir(path) ) throw new ReferenceError('invalid test directory');
-
-  cmd.test = path;
-  return cmd;
-}
+var DEFAULT_SLOW = 5; // ms
 
 /**
  * @private
- * @param {string} filepath
+ * @const {!Object<string, function>}
+ */
+var IS = require('./is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} path
  * @return {string}
  */
-function addJSExt(filepath) {
-  return /\.js$/.test(filepath)
-    ? filepath
-    : filepath + '.js';
-}
+var appendFileExtJS = require('./append-file-ext.js').construct('.js');
 
 /**
  * @private
  * @param {string} dirpath
  * @return {string}
  */
-function slashDir(dirpath) {
-  return dirpath.replace(/[^\/]$/, '$&/');
+var appendSlash = require('./append-slash.js');
+
+/**
+ * @private
+ * @param {string} path
+ * @return {boolean}
+ */
+var isDirectory = IS.directory;
+
+/**
+ * @private
+ * @param {string} path
+ * @return {boolean}
+ */
+var isFile = IS.file;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isFunction = IS.func;
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isLT = IS.lessThan;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNumber = IS.number;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isObject = IS.object;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+function isTestCmd(val) {
+  return isObject(val) && (val.__CMD === true);
 }
+
+/**
+ * @private
+ * @param {number} val
+ * @return {boolean}
+ */
+var isWholeNumber = IS.wholeNumber;
+
+/**
+ * @private
+ * @param {(!Array<string>|...string)=} path
+ * @return {string}
+ */
+var resolvePath = require('./resolve-path.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {string}
+ */
+var REPO_DIR = require('./get-repo-root.js')();
+
+/**
+ * @private
+ * @const {string}
+ */
+var TEST_DIR = appendSlash( resolvePath(REPO_DIR, './test/methods') );
+
+////////////////////////////////////////////////////////////////////////////////
+// METHODS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} method
+ * @param {string} submethod
+ * @return {string}
+ */
+function mkTestPath(method, submethod) {
+
+  /** @type {string} */
+  var path;
+
+  path = TEST_DIR;
+
+  if (method)
+    path += appendSlash(method);
+
+  if (submethod) {
+    path += appendFileExtJS(submethod);
+    if ( !isFile(path) )
+      throw new ReferenceError('invalid test file path `' + path + '`');
+  }
+  else if ( !isDirectory(path) )
+    throw new ReferenceError('invalid test directory path `' + path + '`');
+
+  return path;
+}
+
+/**
+ * @private
+ * @param {!Object} opts
+ * @return {!Object}
+ */
+function parseOpts(opts) {
+
+  if ( !isString(opts.submethod) )
+    opts.submethod = '';
+
+  if ( !isString(opts.reporter) || !opts.reporter )
+    opts.reporter = DEFAULT_REPORTER;
+
+  if ( !isString(opts.section) )
+    opts.section = '';
+
+  if ( !isString(opts.method) )
+    opts.method = '';
+
+  if ( !isString(opts.setup) || !opts.setup )
+    opts.setup = DEFAULT_SETUP;
+
+  if ( !isNumber(opts.slow) || !isWholeNumber(opts.slow) || isLT(opts.slow, 1) )
+    opts.slow = DEFAULT_SLOW;
+
+  if ( !isFunction(opts.start) )
+    opts.start = function start(){};
+
+  if ( !isFunction(opts.close) )
+    opts.close = function close(){};
+
+  return opts;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
+ * @param {(?Object|TestCmd)} opts
+ * @param {?TestCmdMethod=} opts.start = `null`
+ * @param {?TestCmdMethod=} opts.close = `null`
+ * @param {string=} opts.reporter = `"specky"`
+ * @param {number=} opts.slow = `5`
+ * @param {string=} opts.setup = `"methods"`
+ * @param {string=} opts.method = `""`
+ *   Test only a specific method.
+ * @param {string=} opts.section = `""`
+ *   Test only a specific section.
+ * @param {string=} opts.submethod = `""`
+ *   Test only a specific submethod.
+ * @return {!TestCmd}
+ */
+module.exports = function newTestCmd(opts) {
+
+  if ( isTestCmd(opts) )
+    return opts;
+
+  if ( !isObject(opts) )
+    opts = {};
+
+  opts = parseOpts(opts);
+  return {
+    '__CMD': true,
+    'section': opts.section,
+    'report': opts.reporter,
+    'start': opts.start,
+    'close': opts.close,
+    'setup': appendFileExtJS(opts.setup),
+    'test': mkTestPath(opts.method, opts.submethod),
+    'slow': opts.slow.toString(),
+    'dir': !opts.submethod
+  };
+};
