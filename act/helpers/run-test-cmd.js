e5031c7f068cde579163aad90c169e0921d5b96f
Adam Smith <adam@imaginate.life>
2017-05-01 20:59:35 -0700

    Cleaned up & optimized task helper: runTestCmd

--- act/helpers/run-test-cmd.js
+++ act/helpers/run-test-cmd.js
@@ -11,22 +11,15 @@
 
 'use strict';
 
-var cp = require('child_process');
-var log = require('log-ocd')();
-var getFile = require('./get-file');
-var getFiles = require('./get-filepaths');
-var newTestCmd = require('./new-test-cmd');
-
-var SECTION = /^[\s\S]+?@section ([a-z-]+)[\s\S]+$/;
-
-log.error.setConfig({
-  'throw': false,
-  'exit':  true
-});
+////////////////////////////////////////////////////////////////////////////////
+// TYPEDEFS
+////////////////////////////////////////////////////////////////////////////////
 
 /**
  * @typedef {function} TestCmdMethod
- *
+ */
+
+/**
  * @typedef {{
  *   __CMD:   boolean,
  *   start:   !TestCmdMethod,
@@ -40,54 +33,176 @@ log.error.setConfig({
  * }} TestCmd
  */
 
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
 /**
- * @param {(?Object|TestCmd)} opts
- * @param {?TestCmdMethod=} opts.start  - [default= null]
- * @param {?TestCmdMethod=} opts.close  - [default= null]
- * @param {string=} opts.reporter  - [default= "specky"]
- * @param {number=} opts.slow      - [default= 5]
- * @param {string=} opts.setup     - [default= "methods"]
- * @param {string=} opts.method    - [default= ""] Test only a specific method.
- * @param {string=} opts.section   - [default= ""] Test only a specific section.
- * @param {string=} opts.submethod - [default= ""] Test only a specific submethod.
+ * @private
+ * @const {!Object<string, function>}
  */
-module.exports = function runTestCmd(opts) {
+var CP = require('child_process');
 
-  /** @type {!ChildProcess} */
-  var child;
-  /** @type {!Array<string>} */
-  var files;
-  /** @type {!Array<string>} */
-  var args;
-  /** @type {!Object} */
-  var opts;
-  /** @type {!TestCmd} */
-  var cmd;
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('./is.js');
 
-  cmd = newTestCmd(opts);
-  files = mkTestFiles(cmd.test, cmd.section, cmd.dir);
-  args = [
-    './act/helpers/test-module.js',
-    cmd.report,
-    cmd.slow,
-    cmd.setup
-  ];
-  args = args.concat(files);
-  opts = { 'stdio': 'inherit' };
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var LOG = require('log-ocd')();
 
-  cmd.start();
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var NOT = /^!/;
 
-  try {
-    child = cp.spawn('node', args, opts);
-  }
-  catch (err) {
-    err.name = err.name || 'Error';
-    err.name = 'Internal `test` ' + err.name;
-    log.error(err);
-  }
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var SECTION = /^[\s\S]+?@section ([a-zA-Z-]+)[\s\S]+$/;
 
-  child.on('close', cmd.close);
-};
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} filepath
+ * @param {boolean=} buffer
+ * @return {(!Buffer|string)}
+ */
+var err = LOG.error;
+
+err.setConfig({
+  'throw': false,
+  'exit':  true
+});
+
+/**
+ * @private
+ * @param {string} filepath
+ * @param {boolean=} buffer
+ * @return {(!Buffer|string)}
+ */
+var getFileContent = require('./get-file-content.js');
+
+/**
+ * @private
+ * @param {string} dirpath
+ * @param {?Object|boolean=} opts
+ *   If a `boolean` then it is `opts.deep`.
+ * @param {?boolean=} opts.deep = `false`
+ *   Make a recursive search for valid filenames.
+ * @param {?boolean=} opts.full = `false`
+ *   Return absolute filepaths instead of relative.
+ * @param {?RegExp=} opts.valid
+ *   An alias for `opts.validFiles`.
+ * @param {?RegExp=} opts.invalid
+ *   An alias for `opts.invalidFiles`.
+ * @param {?RegExp=} opts.validFiles = `null`
+ *   A pattern for matching valid filenames. If `null` is given then no check is
+ *   performed.
+ * @param {?RegExp=} opts.invalidFiles = `null`
+ *   A pattern for matching invalid filenames. If `null` is given then no check
+ *   is performed.
+ * @param {?RegExp=} opts.validDirs = `null`
+ *   Only used when `opts.deep` is `true`. A pattern for matching valid dirnames.
+ *   If `null` is given then no check is performed.
+ * @param {?RegExp=} opts.invalidDirs = `/^\.git|\.bak|node_modules|vendor|tmp|logs?$/`
+ *   Only used when `opts.deep` is `true`. A pattern for matching invalid
+ *   dirnames. If `null` is given then no check is performed.
+ * @return {!Array<string>}
+ */
+var getFilepaths = require('./get-filepaths.js');
+
+/**
+ * @private
+ * @param {number} val1
+ * @param {number} val2
+ * @return {boolean}
+ */
+var isLT = IS.lessThan;
+
+/**
+ * @private
+ * @param {(!Array<string>|...string)=} path
+ * @return {string}
+ */
+var resolvePath = require('./resolve-path.js');
+
+/**
+ * @private
+ * @param {string} cmd
+ * @param {!Array<string>=} args = `[]`
+ * @param {!Object=} opts
+ * @param {string=} opts.cwd
+ * @param {!Object=} opts.env
+ * @param {string=} opts.argv0
+ * @param {(!Array<string>|string)=} opts.stdio
+ * @param {boolean=} opts.detached
+ * @param {number=} opts.uid
+ * @param {number=} opts.gid
+ * @param {(boolean|string)=} opts.shell
+ * @return {!ChildProcess}
+ */
+var spawnChild = CP.spawn;
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @const {string}
+ */
+var HELP_DIR = resolvePath(__dirname);
+
+////////////////////////////////////////////////////////////////////////////////
+// METHODS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {string} valid
+ *   The valid section(s).
+ * @return {function(string): boolean}
+ */
+function mkSectionTest(valid) {
+
+  /** @type {!RegExp} */
+  var pattern;
+  /** @type {boolean} */
+  var not;
+
+  if (!valid)
+    return function inSection(filepath) {
+      return true;
+    };
+
+  not = NOT.test(valid);
+  if (not)
+    valid = valid.slice(1);
+  pattern = new RegExp('^' + valid + '$');
+  return function inSection(filepath) {
+
+    /** @type {string} */
+    var content;
+    /** @type {string} */
+    var section;
+
+    content = getFileContent(filepath);
+    section = content.replace(SECTION, '$1');
+    return not
+      ? !pattern.test(section)
+      : pattern.test(section);
+  };
+}
 
 /**
  * @private
@@ -111,60 +226,93 @@ function mkTestFiles(base, section, deep) {
   /** @type {number} */
   var i;
 
-  if (!deep) return [ base ];
+  if (!deep)
+    return [ base ];
 
   inSection = mkSectionTest(section);
-  allFiles = getFiles(base, true);
+  allFiles = getFilepaths(base, true);
   files = [];
   len = allFiles.length;
   i = -1;
-  while (++i < len) {
+  while ( isLT(++i, len) ) {
     file = base + allFiles[i];
-    if ( inSection(file) ) files.push(file);
+    if ( inSection(file) )
+      files.push(file);
   }
   return files;
 }
 
 /**
  * @private
- * @param {string} valid - The valid section(s).
- * @return {function}
+ * @param {(?Object|TestCmd)} opts
+ * @param {?TestCmdMethod=} opts.start = `null`
+ * @param {?TestCmdMethod=} opts.close = `null`
+ * @param {string=} opts.reporter = `"specky"`
+ * @param {number=} opts.slow = `5`
+ * @param {string=} opts.setup = `"methods"`
+ * @param {string=} opts.method = `""`
+ *   Test only a specific method.
+ * @param {string=} opts.section = `""`
+ *   Test only a specific section.
+ * @param {string=} opts.submethod = `""`
+ *   Test only a specific submethod.
+ * @return {!TestCmd}
  */
-function mkSectionTest(valid) {
+var newTestCmd = require('./new-test-cmd.js');
 
-  /** @type {boolean} */
-  var not;
-  /** @type {!RegExp} */
-  var re;
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
 
-  if (!valid) {
-    /**
-     * @return {boolean}
-     */
-    return function inSection() {
-      return true;
-    };
+/**
+ * @public
+ * @param {(?Object|TestCmd)} opts
+ * @param {?TestCmdMethod=} opts.start = `null`
+ * @param {?TestCmdMethod=} opts.close = `null`
+ * @param {string=} opts.reporter = `"specky"`
+ * @param {number=} opts.slow = `5`
+ * @param {string=} opts.setup = `"methods"`
+ * @param {string=} opts.method = `""`
+ *   Test only a specific method.
+ * @param {string=} opts.section = `""`
+ *   Test only a specific section.
+ * @param {string=} opts.submethod = `""`
+ *   Test only a specific submethod.
+ */
+module.exports = function runTestCmd(opts) {
+
+  /** @type {!ChildProcess} */
+  var child;
+  /** @type {!Array<string>} */
+  var files;
+  /** @type {!Array<string>} */
+  var args;
+  /** @type {!Object} */
+  var opts;
+  /** @type {!TestCmd} */
+  var cmd;
+
+  cmd = newTestCmd(opts);
+  files = mkTestFiles(cmd.test, cmd.section, cmd.dir);
+  args = [
+    resolvePath(HELP_DIR, 'test-module.js'),
+    cmd.report,
+    cmd.slow,
+    cmd.setup
+  ];
+  args = args.concat(files);
+  opts = { 'stdio': 'inherit' };
+
+  cmd.start();
+
+  try {
+    child = spawnChild('node', args, opts);
+  }
+  catch (error) {
+    error.name = error.name || 'Error';
+    error.name = 'Internal `test` ' + error.name;
+    err(error);
   }
 
-  not = /^!/.test(valid);
-  if (not) valid = valid.slice(1);
-  re = new RegExp('^' + valid + '$');
-
-  /**
-   * @param {string} filepath
-   * @return {boolean}
-   */
-  return function inSection(filepath) {
-
-    /** @type {string} */
-    var content;
-    /** @type {string} */
-    var section;
-
-    content = getFile(filepath);
-    section = content.replace(SECTION, '$1');
-    return not
-      ? !re.test(section)
-      : re.test(section);
-  };
-}
+  child.on('close', cmd.close);
+};
