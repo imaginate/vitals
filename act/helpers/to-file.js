d6958a64f2a8a8ad32e573961490c8e60b8fde4f
Adam Smith <adam@imaginate.life>
2017-05-02 05:40:58 -0700

    Cleaned up & optimized task helper: toFile

--- act/helpers/to-file.js
+++ act/helpers/to-file.js
@@ -11,16 +11,70 @@
 
 'use strict';
 
-var is = require('./is');
-var fs = require('fs');
+////////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+////////////////////////////////////////////////////////////////////////////////
 
 /**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var FS = require('fs');
+
+/**
+ * @private
+ * @const {!Object<string, function>}
+ */
+var IS = require('./is.js');
+
+////////////////////////////////////////////////////////////////////////////////
+// HELPERS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isBuffer = IS.buffer;
+
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+
+/**
+ * @private
+ * @param {string} filepath
+ * @param {(string|!Buffer)} content
+ * @param {string=} encoding
+ * @return {(string|!Buffer)}
+ */
+var writeFile = FS.writeFileSync;
+
+////////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @public
  * @param {(!Buffer|string)} content
  * @param {string} filepath
  * @return {(!Buffer|string)}
  */
 module.exports = function toFile(content, filepath) {
-  if ( is.buffer(content) ) fs.writeFileSync(filepath, content);
-  else fs.writeFileSync(filepath, content, 'utf8');
+
+  if ( !isString(content) && !isBuffer(content) )
+    throw new TypeError('invalid `content` type (must be a string or buffer)');
+  if ( !isString(filepath) )
+    throw new TypeError('invalid `filepath` type (must be a string)');
+
+  if ( isBuffer(content) )
+    writeFile(filepath, content);
+  else
+    writeFile(filepath, content, 'utf8');
+
   return content;
 };
