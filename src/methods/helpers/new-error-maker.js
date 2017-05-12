23584cb2f467e05c1db2c0358f77626805910b12
Adam Smith <adam@imaginate.life>
2017-05-12 12:47:47 -0700

    Cleaned up ErrorMaker helpers

--- src/methods/helpers/new-error-maker.js
+++ src/methods/helpers/new-error-maker.js
@@ -1,76 +1,108 @@
 /**
- * -----------------------------------------------------------------------------
- * VITALS HELPER: newErrorMaker
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * $NEW-ERROR-MAKER HELPER
+ * ---------------------------------------------------------------------------
  * @version 4.1.3
  * @see [vitals](https://github.com/imaginate/vitals)
  *
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
- *
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
 'use strict';
 
-module.exports = newErrorMaker;
+module.exports = $newErrorMaker;
 
-
-////////////////////////////////////////////////////////////////////////////////
-// VITALS HELPER: newErrorMaker
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////// {{{2
+// $NEW-ERROR-MAKER HELPER
+//////////////////////////////////////////////////////////////////////////////
 
 /**
- * @param {string} main - A vitals method.
- * @return {function}
+ * @private
+ * @param {string} main
+ *   A vitals method.
+ * @return {!Function}
  */
-function newErrorMaker(main) {
+function $newErrorMaker(main) {
 
-  main = 'vitals.' + main;
+  /// {{{3
+  /// @const MAIN
+  /**
+   * @private
+   * @const {string}
+   */
+  var MAIN = 'vitals.' + main;
 
+  /// {{{3
+  /// @func cleanError
+  /**
+   * @private
+   * @param {!Error} err
+   * @param {string} name
+   * @param {string} msg
+   * @return {!Error} 
+   */
+  function errorMark(err, name, msg) {
+    err.__vitals = true;
+    err.vitals = true;
+    err.name = name;
+    err.message = msg;
+    err.msg = msg;
+    return err;
+  }
+
+  /// {{{3
+  /// @func error
   /**
    * @param {string} msg
    * @param {string=} method
    * @return {!Error} 
    */
-  var maker = function error(msg, method) {
+  function error(msg, method) {
 
     /** @type {!Error} */
     var err;
 
-    method = method ? main : main + '.' + method;
-    err = new Error(msg + ' for ' + method + ' call.');
-    err.__vitals = true;
-    err.vitals = true;
-    return err;
-  };
+    method = method
+      ? MAIN
+      : MAIN + '.' + method;
+    msg += ' for ' + method + ' call.';
+    err = new Error(msg);
+    return cleanError(err, 'Error', msg);
+  }
 
+  /// {{{3
+  /// @func typeError
   /**
    * @param {string} param
    * @param {string=} method
    * @return {!TypeError} 
    */
-  maker.type = function typeError(param, method) {
+  function typeError(param, method) {
 
     /** @type {!TypeError} */
     var err;
+    /** @type {string} */
+    var msg;
 
     param += ' param';
-    method = method ? main : main + '.' + method;
-    err = new TypeError('Invalid ' + param + ' in ' + method + ' call.');
-    err.__vitals = true;
-    err.vitals = true;
-    return err;
-  };
+    method = method
+      ? MAIN
+      : MAIN + '.' + method;
+    msg = 'Invalid ' + param + ' in ' + method + ' call.';
+    err = new TypeError(msg);
+    return cleanError(err, 'TypeError', msg);
+  }
 
+  /// {{{3
+  /// @func rangeError
   /**
    * @param {string} param
    * @param {string=} valid
    * @param {string=} method
    * @return {!RangeError} 
    */
-  maker.range = function rangeError(param, valid, method) {
+  function rangeError(param, valid, method) {
 
     /** @type {!RangeError} */
     var err;
@@ -78,14 +110,26 @@ function newErrorMaker(main) {
     var msg;
 
     param += ' param';
-    method = method ? main : main + '.' + method;
+    method = method
+      ? MAIN
+      : MAIN + '.' + method;
     msg = 'The '+ param +' was out-of-range for a '+ method +' call.';
-    msg += valid ? ' The valid options are: ' + valid : '';
+    if (valid)
+      msg += ' The valid options are: ' + valid;
     err = new RangeError(msg);
-    err.__vitals = true;
-    err.vitals = true;
-    return err;
-  };
+    return cleanError(err, 'RangeError', msg);
+  }
+  /// }}}3
 
-  return maker;
+  /** @type {!Function} */
+  var errorMaker;
+
+  errorMaker = error;
+  errorMaker.type = typeError;
+  errorMaker.range = rangeError;
+
+  return errorMaker;
 }
+/// }}}2
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
