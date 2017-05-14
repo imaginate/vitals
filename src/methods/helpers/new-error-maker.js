2c9571558178504c4a348ca0136c974358339f95
Adam Smith <adam@imaginate.life>
2017-05-13 19:45:10 -0700

    Added new params to $newErrorMaker helpers

--- src/methods/helpers/new-error-maker.js
+++ src/methods/helpers/new-error-maker.js
@@ -30,6 +30,30 @@ var $newErrorMaker = (function $newErrorMakerPrivateScope() {
   //////////////////////////////////////////////////////////
 
   /// {{{4
+  /// @const INDENT
+  /**
+   * @private
+   * @const {string}
+   */
+  var INDENT = '    ';
+
+  /// {{{4
+  /// @const MAP_TYPE
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var MAP_TYPE = /^\[object ([a-zA-Z0-9_\$]+)\]$/;
+
+  /// {{{4
+  /// @const LAST_SEP
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var LAST_SEP = /,\n$/;
+
+  /// {{{4
   /// @const OPEN_HASH
   /**
    * @private
@@ -44,6 +68,205 @@ var $newErrorMaker = (function $newErrorMakerPrivateScope() {
    * @const {!RegExp}
    */
   var OPEN_VITALS = /^vitals\./;
+
+  /// {{{4
+  /// @func _arrToStr
+  /**
+   * @private
+   * @param {(!Array|!Arguments)} val
+   * @param {number} depth
+   * @return {string}
+   */
+  function _arrToStr(val, depth) {
+
+    /** @type {string} */
+    var result;
+    /** @type {string} */
+    var indent;
+    /** @type {number} */
+    var len;
+    /** @type {number} */
+    var i;
+
+    if ( $is.empty(val) )
+      return '[]';
+
+    indent = _mkIndent(depth);
+    depth += 1;
+
+    result = '[\n';
+    len = val.length;
+    i = -1;
+    while (++i < len) {
+      result += indent + i + ': ';
+      result += _toStr(val[i], depth) + ',\n';
+    }
+    result = result.replace(LAST_SEP, '\n');
+    return result + ']';
+  }
+
+  /// {{{4
+  /// @func _escStr
+  /**
+   * @private
+   * @param {string} val
+   * @return {string}
+   */
+  function _escStr(val) {
+    val = val.replace(/\\/g, '\\\\');
+    val = val.replace(/\n/g, '\\n');
+    val = val.replace(/\r/g, '\\r');
+    val = val.replace(/\t/g, '\\t');
+    val = val.replace(/\v/g, '\\v');
+    val = val.replace(/\0/g, '\\0');
+    val = val.replace(/\b/g, '\\b');
+    val = val.replace(/\f/g, '\\f');
+    return val;
+  }
+
+  /// {{{4
+  /// @func _getMapType
+  /**
+   * @private
+   * @param {*} val
+   * @return {string}
+   */
+  function _getMapType(val) {
+
+    /** @type {string} */
+    var type;
+
+    if ( $is.fun(val) ) {
+      type = 'Function';
+      if (val.name)
+        type += '(' + val.name + ')';
+      return type;
+    }
+
+    type = _objToStr.call(val);
+    return MAP_TYPE.test(type)
+      ? type.replace(MAP_TYPE, '$1')
+      : 'UnknownObjectType';
+  }
+
+  /**
+   * @private
+   * @param {*} key
+   * @return {string}
+   */
+  function _keyToStr(key) {
+    return "'" + String(key) + "'";
+  }
+
+  /// {{{4
+  /// @func _mapToStr
+  /**
+   * @private
+   * @param {(!Object|!Function)} val
+   * @param {number} depth
+   * @return {string}
+   */
+  function _mapToStr(val, depth) {
+
+    /** @type {string} */
+    var result;
+
+    result = _getMapType(val) + ': ';
+    result += $is._arr(val)
+      ? _arrToStr(val, depth)
+      : _ownToStr(val, depth);
+    return result;
+  }
+
+  /// {{{4
+  /// @func _mkIndent
+  /**
+   * @private
+   * @param {number} depth
+   * @return {string}
+   */
+  function _mkIndent(depth) {
+
+    /** @type {string} */
+    var indent;
+
+    if (indent < 1)
+      return '';
+
+    indent = '';
+    while (depth--)
+      indent += INDENT;
+    return indent;
+  }
+
+  /// {{{4
+  /// @func _mkOptions
+  /**
+   * @private
+   * @param {!Array} opts
+   * @return {string}
+   */
+  function _mkOptions(opts) {
+
+    /** @type {string} */
+    var result;
+    /** @type {number} */
+    var len;
+    /** @type {number} */
+    var i;
+
+    result = '';
+
+    len = opts.length;
+    i = -1;
+    while (++i < len)
+      result += '\n- `' + _toStr(opts[i]) + '`';
+    return result;
+  }
+
+  /// {{{4
+  /// @func _objToStr
+  /**
+   * @private
+   * @this {!Object}
+   * @return {string}
+   */
+  var _objToStr = Object.prototype.toString;
+
+  /// {{{4
+  /// @func _ownToStr
+  /**
+   * @private
+   * @param {(!Object|!Function)} val
+   * @param {number} depth
+   * @return {string}
+   */
+  function _ownToStr(val, depth) {
+
+    /** @type {string} */
+    var result;
+    /** @type {string} */
+    var indent;
+    /** @type {string} */
+    var key;
+
+    if ( $is.empty(val) )
+      return '{}';
+
+    indent = _mkIndent(depth);
+    depth += 1;
+
+    result = '{\n';
+    for (key in val) {
+      if ( $own(val, key) ) {
+        result += indent;
+        result += _keyToStr(key) + ': ';
+        result += _toStr(val[key], depth) + ',\n';
+      }
+    }
+    result = result.replace(LAST_SEP, '\n');
+    return result + '}';
+  }
 
   /// {{{4
   /// @func _prepMainMethod
@@ -74,15 +297,45 @@ var $newErrorMaker = (function $newErrorMakerPrivateScope() {
   }
 
   /// {{{4
+  /// @func _primToStr
+  /**
+   * @private
+   * @param {*} val
+   * @return {string}
+   */
+  function _primToStr(val) {
+
+    if ( $is.bool(val) )
+      return val
+        ? 'true'
+        : 'false';
+
+    if ( $is.nil(val) )
+      return 'null';
+
+    if ( $is.none(val) )
+      return 'undefined';
+
+    if ( $is.nan(val) )
+      return 'NaN';
+
+    if ( $is.str(val) )
+      return '"' + _escStr(val) + '"';
+
+    return String(val);
+  }
+
+  /// {{{4
   /// @func _setErrorProps
   /**
    * @private
    * @param {!Error} err
    * @param {string} name
    * @param {string} msg
+   * @param {*=} val
    * @return {!Error} 
    */
-  function _setErrorProps(err, name, msg) {
+  function _setErrorProps(err, name, msg, val) {
     err.__vitals = true;
     err.vitals = true;
     err.name = name;
@@ -98,7 +351,26 @@ var $newErrorMaker = (function $newErrorMakerPrivateScope() {
     }
     err.message = msg;
     err.msg = msg;
+    if (arguments.length > 3)
+      err.val = val;
     return err;
+  }
+
+  /// {{{4
+  /// @func _toStr
+  /**
+   * @private
+   * @param {*} val
+   * @param {number=} depth
+   * @return {string}
+   */
+  function _toStr(val, depth) {
+    depth = depth || 0;
+    return $is._obj(val)
+      ? $is.regx(val)
+        ? val.toString();
+        : _mapToStr(val, depth)
+      : _primToStr(val);
   }
 
   ///////////////////////////////////////////////////// {{{3
@@ -151,40 +423,59 @@ var $newErrorMaker = (function $newErrorMakerPrivateScope() {
     /// @func typeError
     /**
      * @param {!TypeError} err
-     * @param {string} param
-     * @param {string=} method
+     * @param {string} paramName
+     * @param {*} paramVal
+     * @param {string} validTypes
+     * @param {string=} methodName
      * @return {!TypeError} 
      */
-    function typeError(err, param, method) {
+    function typeError(err, paramName, paramVal, validTypes, methodName) {
 
       /** @type {string} */
+      var method;
+      /** @type {string} */
+      var param;
+      /** @type {string} */
       var msg;
+      /** @type {string} */
+      var val;
 
-      param = _prepParam(param);
-      method = _prepMethod(method);
-      msg = 'invalid ' + param + ' data type for ' + method + ' call';
-      return _setErrorProps(err, 'TypeError', msg);
+      method = _prepMethod(methodName);
+      param = _prepParam(paramName);
+      val = _toStr(paramVal);
+      msg = 'invalid ' + param + ' data type for ' + method + ' call\n';
+      msg += 'valid data types: `' + validTypes + '`\n';
+      msg += 'actual ' + param + ' value: `' + val + '`';
+      return _setErrorProps(err, 'TypeError', msg, paramVal);
     }
 
     /// {{{4
     /// @func rangeError
     /**
      * @param {!RangeError} err
-     * @param {string} param
-     * @param {string=} valid
-     * @param {string=} method
+     * @param {string} paramName
+     * @param {(!Array<*>|string|undefined)=} validRange
+     *   An `array` of actual valid options or a `string` stating the valid
+     *   range. If `undefined` this option is skipped.
+     * @param {string=} methodName
      * @return {!RangeError} 
      */
-    function rangeError(err, param, valid, method) {
+    function rangeError(err, paramName, validRange, methodName) {
 
+      /** @type {string} */
+      var method;
+      /** @type {string} */
+      var param;
       /** @type {string} */
       var msg;
 
-      param = _prepParam(param);
-      method = _prepMethod(method);
+      method = _prepMethod(methodName);
+      param = _prepParam(paramName);
       msg = 'out-of-range ' + param + ' for ' + method + ' call';
-      if (valid)
-        msg += '\nvalid options:\n' + valid;
+      if ( $is.str(validRange) )
+        msg += '\nvalid range: `' + validRange + '`';
+      else if ( $is.arr(validRange) )
+        msg += '\nvalid options:' + _mkOptions(validRange);
       return _setErrorProps(err, 'RangeError', msg);
     }
     /// }}}4
