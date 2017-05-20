14cb5f985f361dd6b468054390bf10d131458c91
Adam Smith <adam@imaginate.life>
2017-05-20 10:00:26 -0700

    Cleaned, updated, & reformatted vitals.same docs & code

--- src/methods/same.js
+++ src/methods/same.js
@@ -1,27 +1,28 @@
 /**
- * -----------------------------------------------------------------------------
- * VITALS METHOD: same
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * VITALS.SAME
+ * ---------------------------------------------------------------------------
  * @section base
  * @version 4.1.3
  * @see [vitals.same](https://github.com/imaginate/vitals/wiki/vitals.same)
  *
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
- *
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
 'use strict';
 
-var newErrorMaker = require('./helpers/new-error-maker.js');
+var $newErrorMaker = require('./helpers/new-error-maker.js');
 
+///////////////////////////////////////////////////////////////////////// {{{1
+// VITALS.SAME
+//////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////
-// VITALS METHOD: same
-////////////////////////////////////////////////////////////////////////////////
-
+/**
+ * @public
+ * @const {!Function<string, !Function>}
+ * @dict
+ */
 var same = (function samePrivateScope() {
 
   //////////////////////////////////////////////////////////
@@ -30,8 +31,14 @@ var same = (function samePrivateScope() {
   // - same.loose (same.ish)
   //////////////////////////////////////////////////////////
 
+  /* {{{2 Same References
+   * @ref [equal]:(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness)
+   */
+
+  /// {{{2
+  /// @method same
   /**
-   * A functional representation of strict equality.
+   * A functional representation of [strict equality][equal].
    *
    * @public
    * @param {*} val1
@@ -40,42 +47,98 @@ var same = (function samePrivateScope() {
    */
   function same(val1, val2) {
 
-    if (arguments.length < 2) throw _error('Missing a val');
+    switch (arguments['length']) {
+      case 0:
+        throw $err(new Error, 'no #val1 defined');
+      case 1:
+        throw $err(new Error, 'no #val2 defined');
+    }
 
     return val1 === val2;
   }
 
+  /// {{{2
+  /// @method same.loose
+  /// @alias same.ish
   /**
-   * A functional representation of loose equality.
+   * A functional representation of [loose equality][equal].
    *
    * @public
    * @param {*} val1
    * @param {*} val2
    * @return {boolean}
    */
-  same.loose = function sameLoose(val1, val2) {
+  function sameLoose(val1, val2) {
 
-    if (arguments.length < 2) throw _error('Missing a val', 'loose');
+    switch (arguments['length']) {
+      case 0:
+        throw $err(new Error, 'no #val1 defined', 'loose');
+      case 1:
+        throw $err(new Error, 'no #val2 defined', 'loose');
+    }
 
     return val1 == val2;
-  };
-  // define shorthand
-  same.ish = same.loose;
+  }
+  same['loose'] = sameLoose;
+  same['ish'] = sameLoose;
 
-  //////////////////////////////////////////////////////////
-  // PRIVATE METHODS - GENERAL
+  ///////////////////////////////////////////////////// {{{2
+  // SAME HELPERS - ERROR MAKERS
   //////////////////////////////////////////////////////////
 
+  /// {{{3
+  /// @const ERROR_MAKER
   /**
    * @private
-   * @type {!ErrorAid}
+   * @const {!Object<string, !function>}
+   * @struct
    */
-  var _error = newErrorMaker('same');
+  var ERROR_MAKER = $newErrorMaker('same');
 
-  //////////////////////////////////////////////////////////
-  // END OF PRIVATE SCOPE FOR SAME
+  /// {{{3
+  /// @func $err
+  /**
+   * @private
+   * @param {!Error} err
+   * @param {string} msg
+   * @param {string=} method
+   * @return {!Error} 
+   */
+  var $err = ERROR_MAKER.error;
+
+  /// {{{3
+  /// @func $typeErr
+  /**
+   * @private
+   * @param {!TypeError} err
+   * @param {string} paramName
+   * @param {*} paramVal
+   * @param {string} validTypes
+   * @param {string=} methodName
+   * @return {!TypeError} 
+   */
+  var $typeErr = ERROR_MAKER.typeError;
+
+  /// {{{3
+  /// @func $rangeErr
+  /**
+   * @private
+   * @param {!RangeError} err
+   * @param {string} paramName
+   * @param {(!Array<*>|string|undefined)=} validRange
+   *   An `array` of actual valid options or a `string` stating the valid
+   *   range. If `undefined` this option is skipped.
+   * @param {string=} methodName
+   * @return {!RangeError} 
+   */
+  var $rangeErr = ERROR_MAKER.rangeError;
+  /// }}}2
+
+  // END OF PRIVATE SCOPE FOR VITALS.SAME
   return same;
 })();
-
+/// }}}1
 
 module.exports = same;
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
