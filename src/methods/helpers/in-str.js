576b36ea20422f5674a8398557a6f7ac07139a42
Adam Smith <adam@imaginate.life>
2017-05-24 19:32:12 -0700

    Cleaned & updated $inStr helper

--- src/methods/helpers/in-str.js
+++ src/methods/helpers/in-str.js
@@ -1,53 +1,92 @@
 /**
- * -----------------------------------------------------------------------------
- * VITALS HELPER: inStr
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * $IN-STR HELPER
+ * ---------------------------------------------------------------------------
  * @version 4.1.3
  * @see [vitals](https://github.com/imaginate/vitals)
  *
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
- *
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
 'use strict';
 
+///////////////////////////////////////////////////////////////////////// {{{2
+// $IN-STR HELPER
+//////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////
-// VITALS HELPER: inStr
-////////////////////////////////////////////////////////////////////////////////
+/**
+ * @private
+ * @param {string} source
+ * @param {*} pattern
+ * @return {boolean}
+ */
+var $inStr = (function $inStrPrivateScope() {
 
-var inStr = (function inStrPrivateScope() {
+  /* {{{3 $inStr References
+   * @ref [includes]:(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes)
+   */
 
+  /// {{{3
+  /// @func $inStr
   /**
-   * @private
+   * A cross-platform shortcut for [String.prototype.includes][includes].
+   *
    * @param {string} source
-   * @param {string} str
+   * @param {*} substr
    * @return {boolean}
    */
-  var baseInStr = !!String.prototype.includes
-    ? function baseInStr(source, str) { return source.includes(str); }
-    : function baseInStr(source, str) { return source.indexOf(str) !== -1; };
+  function $inStr(source, substr) {
 
-  /**
-   * A shortcut for `String.prototype.includes`.
-   * @param {string} source
-   * @param {*} str
-   * @return {boolean}
-   */
-  function inStr(source, str) {
-    str = String(str);
-    if (!source) return !str;
-    if (!str) return true;
-    return baseInStr(source, str);
+    if ( !$is.str(substr) )
+      substr = $is.regx(substr)
+        ? substr['toString']()
+        : String(substr);
+
+    return !source
+      ? !substr
+      : !substr
+        ? true
+        : _inStr(source, substr);
   }
 
-  ////////////////////////////////////////////////////
-  // PRIVATE SCOPE END: inStr
-  return inStr;
+  ///////////////////////////////////////////////////// {{{3
+  // $IN-STR HELPERS
+  //////////////////////////////////////////////////////////
+
+  /// {{{4
+  /// @func _inStr
+  /**
+   * Polyfills [String.prototype.includes][includes] if it does not exist.
+   *
+   * @private
+   * @param {string} source
+   * @param {string} substr
+   * @return {boolean}
+   */
+  var _inStr = (function _inStrPrivateScope() {
+
+    /**
+     * @private
+     * @const {!Object}
+     */
+    var PROTO = String['prototype'];
+
+    return 'includes' in PROTO && $is.fun(PROTO['includes'])
+      ? function _inStr(source, substr) {
+          return source['includes'](substr);
+        }
+      : function _inStr(source, substr) {
+          return source['indexOf'](substr) !== -1;
+        };
+  })();
+  /// }}}3
+
+  // END OF PRIVATE SCOPE FOR $IN-STR
+  return $inStr;
 })();
+/// }}}2
 
+module.exports = $inStr;
 
-module.exports = inStr;
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
