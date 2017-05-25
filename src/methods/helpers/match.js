0cb78906e39de438017d279ef259782bae9b1e8e
Adam Smith <adam@imaginate.life>
2017-05-24 18:37:41 -0700

    Cleaned & updated $match helper

--- src/methods/helpers/match.js
+++ src/methods/helpers/match.js
@@ -1,6 +1,6 @@
 /**
  * ---------------------------------------------------------------------------
- * MATCH HELPER
+ * $MATCH HELPER
  * ---------------------------------------------------------------------------
  * @version 4.1.3
  * @see [vitals](https://github.com/imaginate/vitals)
@@ -12,7 +12,7 @@
 'use strict';
 
 ///////////////////////////////////////////////////////////////////////// {{{2
-// MATCH HELPER
+// $MATCH HELPER
 //////////////////////////////////////////////////////////////////////////////
 
 /**
@@ -21,7 +21,7 @@
  * @param {*} pattern
  * @return {boolean}
  */
-var match = (function matchPrivateScope() {
+var $match = (function $matchPrivateScope() {
 
   /* {{{3 Match References
    * @ref [test]:(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test)
@@ -29,7 +29,7 @@ var match = (function matchPrivateScope() {
    */
 
   /// {{{3
-  /// @func match
+  /// @func $match
   /**
    * A cross-platform shortcut for [String.prototype.includes][includes] and
    * [RegExp.prototype.test][test].
@@ -38,17 +38,19 @@ var match = (function matchPrivateScope() {
    * @param {*} pattern
    * @return {boolean}
    */
-  function match(source, pattern) {
+  function $match(source, pattern) {
 
-    if ( isRegExp(pattern) )
-      return pattern.test(source);
+    if ( $is.regx(pattern) )
+      return pattern['test'](source);
 
-    pattern = toStr(pattern);
+    if ( !$is.str(pattern) )
+      pattern = String(pattern);
+
     return !source
       ? !pattern
       : !pattern
         ? true
-        : inStr(source, pattern);
+        : _inStr(source, pattern);
   }
 
   ///////////////////////////////////////////////////// {{{3
@@ -56,75 +58,38 @@ var match = (function matchPrivateScope() {
   //////////////////////////////////////////////////////////
 
   /// {{{4
-  /// @func hasIncludes
+  /// @func _inStr
   /**
-   * Checks if [String.prototype.includes][includes] exists.
+   * Polyfills [String.prototype.includes][includes] if it does not exist.
    *
-   * @private
-   * @return {boolean}
-   */
-  function hasIncludes() {
-    return 'includes' in String.prototype
-      && typeof String.prototype.includes === 'function';
-  }
-
-  /// {{{4
-  /// @func inStr
-  /**
    * @private
    * @param {string} source
    * @param {string} substr
    * @return {boolean}
    */
-  var inStr = hasIncludes()
-    ? function inStr(source, substr) {
-        return source.includes(substr);
-      }
-    : function inStr(source, substr) {
-        return source.indexOf(substr) !== -1;
-      };
+  var _inStr = (function _inStrPrivateScope() {
 
-  /// {{{4
-  /// @func isRegExp
-  /**
-   * @private
-   * @param {*} val
-   * @return {boolean}
-   */
-  function isRegExp(val) {
-    return !!val
-      && typeof val === 'object'
-      && objToStr.call(val) === '[object RegExp]';
-  }
+    /**
+     * @private
+     * @const {!Object}
+     */
+    var PROTO = String['prototype'];
 
-  /// {{{4
-  /// @func objToStr
-  /**
-   * @private
-   * @this {!Object}
-   * @return {string}
-   */
-  var objToStr = Object.prototype.toString;
-
-  /// {{{4
-  /// @func toStr
-  /**
-   * @private
-   * @param {*} val
-   * @return {string}
-   */
-  function toStr(val) {
-    return typeof val === 'string'
-      ? val
-      : String(val);
-  }
-
+    return 'includes' in PROTO && $is.fun(PROTO['includes'])
+      ? function _inStr(source, substr) {
+          return source['includes'](substr);
+        }
+      : function _inStr(source, substr) {
+          return source['indexOf'](substr) !== -1;
+        };
+  })();
   /// }}}3
-  // END OF PRIVATE SCOPE FOR MATCH
-  return match;
+
+  // END OF PRIVATE SCOPE FOR $MATCH
+  return $match;
 })();
 /// }}}2
 
-module.exports = match;
+module.exports = $match;
 
 // vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
