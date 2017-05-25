0d9ef17925dcb5c8b84e9fe2cd13ae9d52ebff4a
Adam Smith <adam@imaginate.life>
2017-05-24 19:06:31 -0700

    Cleaned & updated $inArr helper

--- src/methods/helpers/in-arr.js
+++ src/methods/helpers/in-arr.js
@@ -1,42 +1,43 @@
 /**
- * -----------------------------------------------------------------------------
- * VITALS HELPER: inArr
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * $IN-ARR HELPER
+ * ---------------------------------------------------------------------------
  * @version 4.1.3
  * @see [vitals](https://github.com/imaginate/vitals)
  *
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
- *
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
 'use strict';
 
-module.exports = inArr;
-
-
-////////////////////////////////////////////////////////////////////////////////
-// VITALS HELPER: inArr
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////// {{{2
+// $IN-ARR HELPER
+//////////////////////////////////////////////////////////////////////////////
 
 /**
+ * @private
  * @param {!Object} source
  * @param {*} val
  * @return {boolean}
  */
-function inArr(source, val) {
+function $inArr(source, val) {
 
   /** @type {number} */
   var len;
   /** @type {number} */
   var i;
 
-  len = source.length;
+  len = source['length'];
   i = -1;
   while (++i < len) {
-    if (source[i] === val) return true;
+    if (source[i] === val)
+      return true;
   }
   return false;
 }
+/// }}}2
+
+module.exports = $inArr;
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
