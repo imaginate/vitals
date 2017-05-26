11875cf132338e727cf7212bafaaa9e5ad1e1a05
Adam Smith <adam@imaginate.life>
2017-05-26 12:45:44 -0700

    Added $objStr helper

---
+++ src/helpers/obj-str.js
@@ -0,0 +1,43 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $OBJ-STR HELPER
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @helper $objStr
+/**
+ * @private
+ * @param {!Object} source
+ * @return {string}
+ */
+var $objStr = (function $objStrPrivateScope() {
+
+  /// #{{{ @func $objStr
+  /**
+   * @param {!Object} source
+   * @return {string}
+   */
+  function $objStr(source) {
+    return _objToStr['call'](source);
+  }
+  /// #}}} @func $objStr
+
+  /// #{{{ @func _objToStr
+  /**
+   * @private
+   * @this {!Object}
+   * @return {string}
+   */
+  var _objToStr = OBJ_PROTO['toString'];
+  /// #}}} @func _objToStr
+
+  return $objStr;
+})();
+/// #}}} @helper $objStr
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
