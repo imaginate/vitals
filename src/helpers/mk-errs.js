8d77d6de2425eaa2621b27945c965c8f2800c6e8
Adam Smith <adam@imaginate.life>
2017-05-26 15:42:13 -0700

    Replaced helper: $newErrorMaker -> $mkErrs

---
+++ src/helpers/mk-errs.js
@@ -0,0 +1,264 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $MK-ERRS HELPER
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @helper $mkErrs
+/**
+ * @private
+ * @param {string} superMethod
+ * @return {!Object<string, !function>}
+ */
+var $mkErrs = (function $mkErrsPrivateScope() {
+
+  /// #{{{ @group Constants
+
+  /// #{{{ @const _OPEN_HASH
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _OPEN_HASH = /^#/;
+  /// #}}} @const _OPEN_HASH
+
+  /// #{{{ @const _OPEN_VITALS
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _OPEN_VITALS = /^vitals\./;
+  /// #}}} @const _OPEN_VITALS
+
+  /// #{{{ @const _STRICT
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _STRICT = /^\!/;
+  /// #}}} @const _STRICT
+
+  /// #}}} @group Constants
+
+  /// #{{{ @group Helpers
+
+  /// #{{{ @func _mkOptions
+  /**
+   * @private
+   * @param {!Array} opts
+   * @return {string}
+   */
+  function _mkOptions(opts) {
+
+    /** @type {string} */
+    var result;
+    /** @type {number} */
+    var len;
+    /** @type {number} */
+    var i;
+
+    result = '';
+
+    len = opts['length'];
+    i = -1;
+    while (++i < len)
+      result += '\n- `' + $print(opts[i]) + '`';
+    return result;
+  }
+  /// #}}} @func _mkOptions
+
+  /// #{{{ @func _prepSuper
+  /**
+   * @private
+   * @param {string} name
+   * @return {string}
+   */
+  function _prepSuper(name) {
+    name = name['replace'](_OPEN_VITALS, '');
+    return 'vitals.' + name;
+  }
+  /// #}}} @func _prepSuper
+
+  /// #{{{ @func _prepParam
+  /**
+   * @private
+   * @param {string} name
+   * @return {string}
+   */
+  function _prepParam(name) {
+
+    if (!name)
+      return '';
+
+    if ( _STRICT['test'](name) )
+      return name['replace'](_STRICT, '');
+
+    name = name['replace'](_OPEN_HASH, '');
+    return '#' + name;
+  }
+  /// #}}} @func _prepParam
+
+  /// #{{{ @func _setErrorProps
+  /**
+   * @private
+   * @param {!Error} err
+   * @param {string} name
+   * @param {string} msg
+   * @param {*=} val
+   * @return {!Error}
+   */
+  function _setErrorProps(err, name, msg, val) {
+    err['__vitals'] = true;
+    err['vitals'] = true;
+    err['name'] = name;
+    switch (name) {
+      case 'TypeError':
+        err['__type'] = true;
+        err['type'] = true;
+        break;
+      case 'RangeError':
+        err['__range'] = true;
+        err['range'] = true;
+        break;
+    }
+    err['message'] = msg;
+    err['msg'] = msg;
+    if (arguments['length'] > 3) {
+      err['value'] = val;
+      err['val'] = val;
+    }
+    return err;
+  }
+  /// #}}} @func _setErrorProps
+
+  /// #}}} @group Helpers
+
+  /// #{{{ @func $mkErrs
+  /**
+   * @param {string} superMethod
+   * @return {!Object<string, !function>}
+   */
+  function $mkErrs(superMethod) {
+
+    /// #{{{ @const MK_ERR
+    /**
+     * @const {!Object<string, !function>}
+     * @struct
+     */
+    var MK_ERR = {
+      error: error,
+      typeError: typeError,
+      rangeError: rangeError
+    };
+    /// #}}} @const MK_ERR
+
+    /// #{{{ @const _SUPER
+    /**
+     * @private
+     * @const {string}
+     */
+    var _SUPER = _prepSuper(superMethod);
+    /// #}}} @const _SUPER
+
+    /// #{{{ @func _prepMethod
+    /**
+     * @private
+     * @param {(string|undefined)} method
+     * @return {string} 
+     */
+    function _prepMethod(method) {
+      method = method
+        ? _SUPER
+        : _SUPER + '.' + method;
+      return '`' + method + '`';
+    }
+    /// #}}} @func _prepMethod
+
+    /// #{{{ @func error
+    /**
+     * @param {!Error} err
+     * @param {string} msg
+     * @param {string=} method
+     * @return {!Error} 
+     */
+    function error(err, msg, method) {
+      method = _prepMethod(method);
+      msg += ' for ' + method + ' call';
+      return _setErrorProps(err, 'Error', msg);
+    }
+    /// #}}} @func error
+
+    /// #{{{ @func typeError
+    /**
+     * @param {!TypeError} err
+     * @param {string} paramName
+     * @param {*} paramVal
+     * @param {string} validTypes
+     * @param {string=} methodName
+     * @return {!TypeError} 
+     */
+    function typeError(err, paramName, paramVal, validTypes, methodName) {
+
+      /** @type {string} */
+      var method;
+      /** @type {string} */
+      var param;
+      /** @type {string} */
+      var msg;
+      /** @type {string} */
+      var val;
+
+      method = _prepMethod(methodName);
+      param = _prepParam(paramName);
+      val = $print(paramVal);
+      msg = 'invalid ' + param + ' data type for ' + method + ' call\n';
+      msg += 'valid data types: `' + validTypes + '`\n';
+      msg += 'actual ' + param + ' value: `' + val + '`';
+      return _setErrorProps(err, 'TypeError', msg, paramVal);
+    }
+    /// #}}} @func typeError
+
+    /// #{{{ @func rangeError
+    /**
+     * @param {!RangeError} err
+     * @param {string} paramName
+     * @param {(!Array<*>|string|undefined)=} validRange
+     *   An `array` of actual valid options or a `string` stating the valid
+     *   range. If `undefined` this option is skipped.
+     * @param {string=} methodName
+     * @return {!RangeError} 
+     */
+    function rangeError(err, paramName, validRange, methodName) {
+
+      /** @type {string} */
+      var method;
+      /** @type {string} */
+      var param;
+      /** @type {string} */
+      var msg;
+
+      method = _prepMethod(methodName);
+      param = _prepParam(paramName);
+      msg = 'out-of-range ' + param + ' for ' + method + ' call';
+      if ( $is.str(validRange) )
+        msg += '\nvalid range: `' + validRange + '`';
+      else if ( $is.arr(validRange) )
+        msg += '\nvalid options:' + _mkOptions(validRange);
+      return _setErrorProps(err, 'RangeError', msg);
+    }
+    /// #}}} @func rangeError
+
+    return MK_ERR;
+  }
+  /// #}}} @func $mkErrs
+
+  return $mkErrs;
+})();
+/// #}}} @helper $mkErrs
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
