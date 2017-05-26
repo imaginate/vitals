00dd7e4fb19246dce6c0c2a50976d7250bf871e6
Adam Smith <adam@imaginate.life>
2017-05-26 12:30:21 -0700

    Added constant refs for special js refs

---
+++ src/constants/special.js
@@ -0,0 +1,28 @@
+/**
+ * ---------------------------------------------------------------------------
+ * SPECIAL CONSTANTS
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @constant NIL
+/**
+ * @private
+ * @const {null}
+ */
+var NIL = null;
+/// #}}} @constant NIL
+
+/// #{{{ @constant VOID
+/**
+ * @private
+ * @const {undefined}
+ */
+var VOID = (function(){})();
+/// #}}} @constant VOID
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
