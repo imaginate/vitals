160fdcc64f4011916f7473c321beb9c7963c8d43
Adam Smith <adam@imaginate.life>
2017-05-26 15:07:41 -0700

    Added $print helper

---
+++ src/helpers/print.js
@@ -0,0 +1,297 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $PRINT HELPER
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @helper $print
+/**
+ * @private
+ * @param {*} val
+ * @param {number=} depth
+ * @return {string}
+ */
+var $print = (function $printPrivateScope() {
+
+  /// #{{{ @func $print
+  /**
+   * @param {*} val
+   * @param {number=} depth
+   * @return {string}
+   */
+  function _toStr(val, depth) {
+    depth = depth || 0;
+    return $is._obj(val)
+      ? $is.regx(val)
+        ? val['toString']();
+        : _mapToStr(val, depth)
+      : _primToStr(val);
+  }
+  /// #}}} @func $print
+
+  /// #{{{ @group constants
+
+  /// #{{{ @const _INDENT
+  /**
+   * @private
+   * @const {string}
+   */
+  var _INDENT = '    ';
+  /// #}}} @const _INDENT
+
+  /// #{{{ @const _MAP_TYPE
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _MAP_TYPE = /^\[object ([a-zA-Z0-9_\$]+)\]$/;
+  /// #}}} @const _MAP_TYPE
+
+  /// #{{{ @const _LAST_SEP
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _LAST_SEP = /,\n$/;
+  /// #}}} @const _LAST_SEP
+
+  /// #}}} @group constants
+
+  /// #{{{ @group Helpers
+
+  /// #{{{ @func _emptyHashMap
+  /**
+   * @private
+   * @param {(!Object|!Function)} val
+   * @return {boolean}
+   */
+  function _emptyHashMap(val) {
+
+    /** @type {string} */
+    var key;
+
+    for (key in val) {
+      if ( $own(val, key) )
+        return false;
+    }
+    return true;
+  }
+  /// #}}} @func _emptyHashMap
+
+  /// #{{{ @func _escStr
+  /**
+   * @private
+   * @param {string} val
+   * @return {string}
+   */
+  function _escStr(val) {
+    val = val['replace'](/\\/g, '\\\\');
+    val = val['replace'](/\n/g, '\\n');
+    val = val['replace'](/\r/g, '\\r');
+    val = val['replace'](/\t/g, '\\t');
+    val = val['replace'](/\v/g, '\\v');
+    val = val['replace'](/\0/g, '\\0');
+    val = val['replace'](/\b/g, '\\b');
+    val = val['replace'](/\f/g, '\\f');
+    return val;
+  }
+  /// #}}} @func _escStr
+
+  /// #{{{ @func _getMapType
+  /**
+   * @private
+   * @param {*} val
+   * @return {string}
+   */
+  function _getMapType(val) {
+
+    /** @type {string} */
+    var type;
+
+    if ( $is.fun(val) ) {
+      type = 'Function';
+      if (val['name'])
+        type += '(' + val['name'] + ')';
+      return type;
+    }
+
+    type = $objStr(val);
+    return _MAP_TYPE['test'](type)
+      ? type['replace'](_MAP_TYPE, '$1')
+      : 'UnknownObjectType';
+  }
+  /// #}}} @func _getMapType
+
+  /// #{{{ @func _mkIndent
+  /**
+   * @private
+   * @param {number} depth
+   * @return {string}
+   */
+  function _mkIndent(depth) {
+
+    /** @type {string} */
+    var indent;
+
+    if (indent < 1)
+      return '';
+
+    indent = '';
+    while (depth--)
+      indent += _INDENT;
+    return indent;
+  }
+  /// #}}} @func _mkIndent
+
+  /// #}}} @group Helpers
+
+  /// #{{{ @group Primitives
+
+  /// #{{{ @func _primToStr
+  /**
+   * @private
+   * @param {*} val
+   * @return {string}
+   */
+  function _primToStr(val) {
+
+    if ( $is.bool(val) )
+      return val
+        ? 'true'
+        : 'false';
+
+    if ( $is.nil(val) )
+      return 'null';
+
+    if ( $is.void(val) )
+      return 'undefined';
+
+    if ( $is.nan(val) )
+      return 'NaN';
+
+    if ( $is.str(val) )
+      return '"' + _escStr(val) + '"';
+
+    return $mkStr(val);
+  }
+  /// #}}} @func _primToStr
+
+  /// #}}} @group Primitives
+
+  /// #{{{ @group Objects
+
+  /// #{{{ @func _arrToStr
+  /**
+   * @private
+   * @param {(!Array|!Arguments)} val
+   * @param {number} depth
+   * @return {string}
+   */
+  function _arrToStr(val, depth) {
+
+    /** @type {string} */
+    var result;
+    /** @type {string} */
+    var indent;
+    /** @type {number} */
+    var len;
+    /** @type {number} */
+    var i;
+
+    len = val['length'];
+
+    if (len < 1)
+      return '[]';
+
+    indent = _mkIndent(depth);
+    depth += 1;
+
+    result = '[\n';
+    i = -1;
+    while (++i < len) {
+      result += indent + i + ': ';
+      result += $print(val[i], depth) + ',\n';
+    }
+    result = result['replace'](_LAST_SEP, '\n');
+    return result + ']';
+  }
+  /// #}}} @func _arrToStr
+
+  /// #{{{ @func _keyToStr
+  /**
+   * @private
+   * @param {*} key
+   * @return {string}
+   */
+  function _keyToStr(key) {
+    return "'" + $mkStr(key) + "'";
+  }
+  /// #}}} @func _keyToStr
+
+  /// #{{{ @func _mapToStr
+  /**
+   * @private
+   * @param {(!Object|!Function)} val
+   * @param {number} depth
+   * @return {string}
+   */
+  function _mapToStr(val, depth) {
+
+    /** @type {string} */
+    var result;
+
+    result = _getMapType(val) + ': ';
+    result += $is._arr(val)
+      ? _arrToStr(val, depth)
+      : _ownToStr(val, depth);
+    return result;
+  }
+  /// #}}} @func _mapToStr
+
+  /// #{{{ @func _ownToStr
+  /**
+   * @private
+   * @param {(!Object|!Function)} val
+   * @param {number} depth
+   * @return {string}
+   */
+  function _ownToStr(val, depth) {
+
+    /** @type {string} */
+    var result;
+    /** @type {string} */
+    var indent;
+    /** @type {string} */
+    var key;
+
+    if ( _emptyHashMap(val) )
+      return '{}';
+
+    indent = _mkIndent(depth);
+    depth += 1;
+
+    result = '{\n';
+    for (key in val) {
+      if ( $own(val, key) ) {
+        result += indent;
+        result += _keyToStr(key) + ': ';
+        result += $print(val[key], depth) + ',\n';
+      }
+    }
+    result = result['replace'](_LAST_SEP, '\n');
+    return result + '}';
+  }
+  /// #}}} @func _ownToStr
+
+  /// #}}} @group Objects
+
+  return $print;
+})();
+/// #}}} @helper $print
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
