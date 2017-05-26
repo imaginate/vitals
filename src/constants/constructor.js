99c56aae0b86e3b557e9b162fc6f72e102d19e34
Adam Smith <adam@imaginate.life>
2017-05-26 12:24:17 -0700

    Added constant refs for global constructors

---
+++ src/constants/constructor.js
@@ -0,0 +1,66 @@
+/**
+ * ---------------------------------------------------------------------------
+ * CONSTRUCTOR CONSTANTS
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @constant ARR
+/**
+ * @private
+ * @const {!Function}
+ * @constructor
+ */
+var ARR = Array;
+/// #}}} @constant ARR
+
+/// #{{{ @constant FUN
+/**
+ * @private
+ * @const {!Function}
+ * @constructor
+ */
+var FUN = Function;
+/// #}}} @constant FUN
+
+/// #{{{ @constant NUM
+/**
+ * @private
+ * @const {!Function}
+ * @constructor
+ */
+var NUM = Number;
+/// #}}} @constant NUM
+
+/// #{{{ @constant OBJ
+/**
+ * @private
+ * @const {!Function}
+ * @constructor
+ */
+var OBJ = Object;
+/// #}}} @constant OBJ
+
+/// #{{{ @constant STR
+/**
+ * @private
+ * @const {!Function}
+ * @constructor
+ */
+var STR = String;
+/// #}}} @constant STR
+
+/// #{{{ @constant REGX
+/**
+ * @private
+ * @const {!Function}
+ * @constructor
+ */
+var REGX = RegExp;
+/// #}}} @constant REGX
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
