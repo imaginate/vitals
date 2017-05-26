8add204abd1ae936a3ee2aaa725852cfc86e9b6c
Adam Smith <adam@imaginate.life>
2017-05-26 12:24:56 -0700

    Added constant refs for constructor prototypes

---
+++ src/constants/prototype.js
@@ -0,0 +1,60 @@
+/**
+ * ---------------------------------------------------------------------------
+ * PROTOTYPE CONSTANTS
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @constant ARR_PROTO
+/**
+ * @private
+ * @const {!Object}
+ */
+var ARR_PROTO = ARR['prototype'];
+/// #}}} @constant ARR_PROTO
+
+/// #{{{ @constant FUN_PROTO
+/**
+ * @private
+ * @const {!Object}
+ */
+var FUN_PROTO = FUN['prototype'];
+/// #}}} @constant FUN_PROTO
+
+/// #{{{ @constant NUM_PROTO
+/**
+ * @private
+ * @const {!Object}
+ */
+var NUM_PROTO = NUM['prototype'];
+/// #}}} @constant NUM_PROTO
+
+/// #{{{ @constant OBJ_PROTO
+/**
+ * @private
+ * @const {!Object}
+ */
+var OBJ_PROTO = OBJ['prototype'];
+/// #}}} @constant OBJ_PROTO
+
+/// #{{{ @constant STR_PROTO
+/**
+ * @private
+ * @const {!Object}
+ */
+var STR_PROTO = STR['prototype'];
+/// #}}} @constant STR_PROTO
+
+/// #{{{ @constant REGX_PROTO
+/**
+ * @private
+ * @const {!Object}
+ */
+var REGX_PROTO = REGX['prototype'];
+/// #}}} @constant REGX_PROTO
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
