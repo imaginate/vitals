2266d4756b2ababe6336b462092311f483c330c4
Adam Smith <adam@imaginate.life>
2017-05-26 16:03:43 -0700

    Added MK_ERR macro

---
+++ src/macros/mk-err.js
@@ -0,0 +1,52 @@
+/**
+ * ---------------------------------------------------------------------------
+ * MK_ERR MACRO
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @macro MK_ERR
+  /// #{{{ @func $err
+  /**
+   * @private
+   * @param {!Error} err
+   * @param {string} msg
+   * @param {string=} method
+   * @return {!Error} 
+   */
+  var $err = MK_ERR.error;
+  /// #}}} @func $err
+
+  /// #{{{ @func $typeErr
+  /**
+   * @private
+   * @param {!TypeError} err
+   * @param {string} paramName
+   * @param {*} paramVal
+   * @param {string} validTypes
+   * @param {string=} methodName
+   * @return {!TypeError} 
+   */
+  var $typeErr = MK_ERR.typeError;
+  /// #}}} @func $typeErr
+
+  /// #{{{ @func $rangeErr
+  /**
+   * @private
+   * @param {!RangeError} err
+   * @param {string} paramName
+   * @param {(!Array<*>|string|undefined)=} validRange
+   *   An `array` of actual valid options or a `string` stating the valid
+   *   range. If `undefined` this option is skipped.
+   * @param {string=} methodName
+   * @return {!RangeError} 
+   */
+  var $rangeErr = MK_ERR.rangeError;
+  /// #}}} @func $rangeErr
+/// #}}} @macro MK_ERR
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
