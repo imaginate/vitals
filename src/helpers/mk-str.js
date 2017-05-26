c289e2f4c84e64730cdf06db49955d77f7039bc3
Adam Smith <adam@imaginate.life>
2017-05-26 15:48:08 -0700

    Added $mkStr helper

---
+++ src/helpers/mk-str.js
@@ -0,0 +1,27 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $MK-STR HELPER
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @helper $mkStr
+/**
+ * @private
+ * @param {*} val
+ * @return {string}
+ */
+function $mkStr(val) {
+  return $is.str(val)
+    ? val
+    : $is.regx(val)
+      ? val['toString']()
+      : STR(val);
+}
+/// #}}} @helper $mkStr
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
