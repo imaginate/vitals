87117efb27aac940a939815d41c3b52f122a5f06
Adam Smith <adam@imaginate.life>
2017-05-31 23:13:05 -0700

    Added $mkdir helper

---
+++ src/helpers/mkdir.js
@@ -0,0 +1,44 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $MKDIR HELPER
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @helper $mkdir
+/**
+ * @private
+ * @param {string} path
+ * @return {void}
+ */
+var $mkdir = (function $mkdirPrivateScope() {
+
+  /// #{{{ @func _mkdir
+  /**
+   * @private
+   * @param {string} path
+   * @return {void}
+   */
+  var _mkdir = FS['mkdirSync'];
+  /// #}}} @func _mkdir
+
+  /// #{{{ @func $mkdir
+  /**
+   * @param {string} path
+   * @return {void}
+   */
+  function $mkdir(path) {
+    if ( !$is.dir(path) )
+      _mkdir(path);
+  }
+  /// #}}} @func $mkdir
+
+  return $mkdir;
+})();
+/// #}}} @helper $mkdir
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
