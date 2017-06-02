448ae1993bf7cd32703d7cd513ff2f768301f057
Adam Smith <adam@imaginate.life>
2017-06-02 00:18:01 -0700

    Updated tags & helpers for $escape helper

--- src/helpers/escape.js
+++ src/helpers/escape.js
@@ -9,47 +9,15 @@
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
  */
 
-'use strict';
-
-///////////////////////////////////////////////////////////////////////// {{{2
-// $ESCAPE HELPER
-//////////////////////////////////////////////////////////////////////////////
-
+/// #{{{ @helper $escape
 /**
  * @private
  * @param {string} source
  * @return {string}
  */
-var $escape = (function $escapePrivateScope() {
-
-  /// {{{3
-  /// @func $escape
-  /**
-   * @param {string} source
-   * @return {string}
-   */
-  function $escape(source) {
-    return source['replace'](CHARS, '\\$&');
-  }
-
-  ///////////////////////////////////////////////////// {{{3
-  // $ESCAPE HELPERS
-  //////////////////////////////////////////////////////////
-
-  /// {{{4
-  /// @const CHARS
-  /**
-   * @private
-   * @type {!RegExp}
-   */
-  var CHARS = /[\\^$.*+?|(){}[\]]/g;
-  /// }}}3
-
-  // END OF PRIVATE SCOPE FOR $ESCAPE
-  return $escape;
-})();
-/// }}}2
-
-module.exports = $escape;
+function $escape(source) {
+  return source['replace'](/[\\^$.*+?|(){}[\]]/g, '\\$&');
+}
+/// #}}} @helper $escape
 
 // vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
