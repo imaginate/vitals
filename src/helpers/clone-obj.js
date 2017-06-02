444eaf3e7aaf44b38807d4eb85c99cc33b347d12
Adam Smith <adam@imaginate.life>
2017-06-02 00:13:29 -0700

    Updated tags & helpers for $cloneObj helper

--- src/helpers/clone-obj.js
+++ src/helpers/clone-obj.js
@@ -9,59 +9,26 @@
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
  */
 
-'use strict';
-
-///////////////////////////////////////////////////////////////////////// {{{2
-// $CLONE-OBJ HELPER
-//////////////////////////////////////////////////////////////////////////////
-
+/// #{{{ @helper $cloneObj
 /**
  * @private
  * @param {!Object} obj
  * @return {!Object}
  */
-var $cloneObj = (function $cloneObjPrivateScope() {
+function $cloneObj(obj) {
 
-  /// {{{3
-  /// @func $cloneObj
-  /**
-   * @param {!Object} obj
-   * @return {!Object}
-   */
-  function $cloneObj(obj) {
+  /** @type {!Object} */
+  var clone;
+  /** @type {string} */
+  var key;
 
-    /** @type {!Object} */
-    var clone;
-    /** @type {string} */
-    var key;
-
-    clone = {};
-    for (key in obj) {
-      if ( _hasOwn['call'](obj, key) )
-        clone[key] = obj[key];
-    }
-    return clone;
+  clone = {};
+  for (key in obj) {
+    if ( $own(obj, key) )
+      clone[key] = obj[key];
   }
-
-  ///////////////////////////////////////////////////// {{{3
-  // $CLONE-OBJ HELPERS
-  //////////////////////////////////////////////////////////
-
-  /// {{{4
-  /// @func _hasOwn
-  /**
-   * @private
-   * @param {*} key
-   * @return {boolean}
-   */
-  var _hasOwn = Object['prototype']['hasOwnProperty'];
-  /// }}}3
-
-  // END OF PRIVATE SCOPE FOR $CLONE-OBJ
-  return $cloneObj;
-})();
-/// }}}2
-
-module.exports = $cloneObj;
+  return clone;
+}
+/// #}}} @helper $cloneObj
 
 // vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
