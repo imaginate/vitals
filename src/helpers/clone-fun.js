2665729620d3179b619d0645090afe4321d6af35
Adam Smith <adam@imaginate.life>
2017-06-06 10:58:55 -0700

    Added $cloneFun helper

---
+++ src/helpers/clone-fun.js
@@ -0,0 +1,35 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $CLONE-FUN HELPER
+ * ---------------------------------------------------------------------------
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @helper $cloneFun
+/**
+ * @private
+ * @param {!Function} func
+ * @return {!Function}
+ */
+function $cloneFun(func) {
+
+  /** @type {!Function} */
+  function funcCopy() {
+    return func['apply'](NIL, arguments);
+  }
+  /** @type {string} */
+  var key;
+
+  for (key in func) {
+    if ( $own(func, key) )
+      funcCopy[key] = func[key];
+  }
+  return funcCopy;
+}
+/// #}}} @helper $cloneFun
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
