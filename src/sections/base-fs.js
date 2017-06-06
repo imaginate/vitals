87ea142e1913c27b7bd67284f1bc5e7e3f98a358
Adam Smith <adam@imaginate.life>
2017-06-06 15:17:38 -0700

    Added base-fs section file

---
+++ src/sections/base-fs.js
@@ -0,0 +1,59 @@
+/**
+ * ---------------------------------------------------------------------------
+ * VITALS
+ * ---------------------------------------------------------------------------
+ * @section base
+ * @section fs
+ * @version 4.1.3
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #include @macro OPEN_WRAPPER ../macros/wrapper.js
+/// #include @core constants ../core/constants.js
+/// #include @core helpers ../core/helpers.js
+/// #include @helper $addSlash ../helpers/add-slash.js
+/// #include @helper $cloneArr ../helpers/clone-arr.js
+/// #include @helper $cloneFun ../helpers/clone-fun.js
+/// #include @helper $cloneObj ../helpers/clone-obj.js
+/// #include @helper $cloneRegx ../helpers/clone-regx.js
+/// #include @helper $escRegx ../helpers/esc-regx.js
+/// #include @helper $fixEol ../helpers/fix-eol.js
+/// #include @helper $getFlags ../helpers/get-flags.js
+/// #include @helper $hasOpt ../helpers/has-opt.js
+/// #include @helper $inArr ../helpers/in-arr.js
+/// #include @helper $inObj ../helpers/in-obj.js
+/// #include @helper $inStr ../helpers/in-str.js
+/// #include @helper $match ../helpers/match.js
+/// #include @helper $merge ../helpers/merge.js
+/// #include @helper $mkdir ../helpers/mkdir.js
+/// #include @helper $ownEnum ../helpers/own-enum.js
+/// #include @helper $pathname ../helpers/pathname.js
+/// #include @helper $readDir ../helpers/read-dir.js
+/// #include @helper $readFile ../helpers/read-file.js
+/// #include @helper $sliceArr ../helpers/slice-arr.js
+/// #include @helper $sliceStr ../helpers/slice-str.js
+/// #include @helper $splitKeys ../helpers/split-keys.js
+/// #include @helper $writeFile ../helpers/write-file.js
+/// #{{{ @section base
+/// #include @super is ../methods/is.js
+/// #include @super copy ../methods/copy.js
+/// #include @super cut ../methods/cut.js
+/// #include @super each ../methods/each.js
+/// #include @super fill ../methods/fill.js
+/// #include @super fuse ../methods/fuse.js
+/// #include @super get ../methods/get.js
+/// #include @super has ../methods/has.js
+/// #include @super remap ../methods/remap.js
+/// #include @super roll ../methods/roll.js
+/// #include @super same ../methods/same.js
+/// #include @super slice ../methods/slice.js
+/// #include @super to ../methods/to.js
+/// #include @super until ../methods/until.js
+/// #}}} @section base
+/// #include @macro EXPORT ../macros/export.js
+/// #include @macro CLOSE_WRAPPER ../macros/wrapper.js
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
