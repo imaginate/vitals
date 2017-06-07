f4034f106fa51bc5a6a49b6e01d4f0263bb50063
Adam Smith <adam@imaginate.life>
2017-06-07 10:21:46 -0700

    Added task helper: cloneObject

---
+++ act/helpers/clone-object.js
@@ -0,0 +1,105 @@
+/**
+ * ---------------------------------------------------------------------------
+ * CLONE-OBJECT HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = require('./is.js');
+/// #}}} @const IS
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func hasOwnProp
+/**
+ * @private
+ * @param {!Object} src
+ * @param {string} prop
+ * @return {boolean}
+ */
+var hasOwnProp = require('./has-own-property.js');
+/// #}}} @func hasOwnProp
+
+/// #{{{ @func isFunction
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isFunction = IS.func;
+/// #}}} @func isFunction
+
+/// #{{{ @func isNull
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNull = IS.nil;
+/// #}}} @func isNull
+
+/// #{{{ @func isObject
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isObject = IS.object;
+/// #}}} @func isObject
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func cloneObject
+/**
+ * @public
+ * @param {(?Object|?Function)} src
+ * @return {!Object}
+ */
+function cloneObject(src) {
+
+  /** @type {!Object} */
+  var clone;
+  /** @type {string} */
+  var key;
+
+  if ( isNull(src) )
+    return {};
+
+  if ( !isObject(src) && !isFunction(src) )
+    throw new TypeError('invalid `src` data type (must be `?Object|?Function`)');
+
+  clone = {};
+  for (key in src) {
+    if ( hasOwnProp(src, key) )
+      clone[key] = src[key];
+  }
+  return clone;
+}
+/// #}}} @func cloneObject
+
+module.exports = cloneObject;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
