42e95158e76a65b5f3123b9f639a251314e835f8
Adam Smith <adam@imaginate.life>
2017-06-07 08:59:52 -0700

    Added task helper: hasSlash

---
+++ act/helpers/has-slash.js
@@ -0,0 +1,72 @@
+/**
+ * ---------------------------------------------------------------------------
+ * HAS-SLASH HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const SLASH
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var SLASH = /\//;
+/// #}}} @const SLASH
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = require('./is.js');
+/// #}}} @const IS
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isString
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+/// #}}} @func isString
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func hasSlash
+/**
+ * @public
+ * @param {string} src
+ * @return {boolean}
+ */
+function hasSlash(src) {
+
+  if ( !isString(src) )
+    throw new TypeError('invalid `src` data type (must be a `string`)');
+
+  return !!src && SLASH.test(src);
+}
+/// #}}} @func hasSlash
+
+module.exports = hasSlash;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
