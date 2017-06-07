8f4fdcefff071316a0b8412c5c8a309050df4670
Adam Smith <adam@imaginate.life>
2017-06-07 11:45:03 -0700

    Cleaned up getRepoRoot task helper

--- act/helpers/get-repo-root.js
+++ act/helpers/get-repo-root.js
@@ -1,33 +1,59 @@
 /**
- * -----------------------------------------------------------------------------
- * ACT TASK HELPER: getRepoRoot
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * GET-REPO-ROOT HELPER
+ * ---------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
- *
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
  */
 
 'use strict';
 
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func resolvePath
 /**
  * @private
- * @param {(!ArrayLike<string>|...string)=} path
+ * @param {(!Array<string>|...string)=} path
  * @return {string}
  */
 var resolvePath = require('./resolve-path.js');
+/// #}}} @func resolvePath
+/// #}}} @group HELPERS
 
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const REPO_DIR
 /**
  * @private
  * @const {string}
  */
 var REPO_DIR = resolvePath(__dirname, '../..');
+/// #}}} @const REPO_DIR
+/// #}}} @group CONSTANTS
 
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func getRepoRoot
 /**
  * @public
  * @return {string}
  */
-module.exports = function getRepoRoot() {
+function getRepoRoot() {
   return REPO_DIR;
-};
+}
+/// #}}} @func getRepoRoot
+
+module.exports = getRepoRoot;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
