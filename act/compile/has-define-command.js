47e5ec04e7d71f04a7f1f95f214fe4f65281b354
Adam Smith <adam@imaginate.life>
2017-06-13 13:11:45 -0700

    Renamed compile helper: isMacroTag -> hasDefineCommand

--- act/compile/has-define-command.js
+++ act/compile/has-define-command.js
@@ -1,6 +1,6 @@
 /**
  * ---------------------------------------------------------------------------
- * IS-MACRO-TAG HELPER
+ * HAS-DEFINE-COMMAND HELPER
  * ---------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
  * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
@@ -22,13 +22,13 @@ var loadHelper = require('./load-helper.js');
 // CONSTANTS
 //////////////////////////////////////////////////////////////////////////////
 
-/// #{{{ @const TAG
+/// #{{{ @const CMD
 /**
  * @private
  * @const {!RegExp}
  */
-var TAG = /^[ \t]*\/\/\/[ \t]+#include[ \t]+@macro[ \t]/;
-/// #}}} @const TAG
+var CMD = /^[ \t]*\/\/\/[ \t]+#def(?:\{{3}|\}{3})[ \t]/;
+/// #}}} @const CMD
 
 /// #{{{ @const IS
 /**
@@ -59,22 +59,22 @@ var isString = IS.string;
 // EXPORTS
 //////////////////////////////////////////////////////////////////////////////
 
-/// #{{{ @func isMacroTag
+/// #{{{ @func hasDefineCommand
 /**
  * @public
- * @param {string} src
+ * @param {string} text
  * @return {boolean}
  */
-function isMacroTag(src) {
+function hasDefineCommand(text) {
 
-  if ( !isString(src) )
-    throw new TypeError('invalid `src` data type (valid types: `string`)');
+  if ( !isString(text) )
+    throw new TypeError('invalid `text` data type (valid types: `string`)');
 
-  return !!src && TAG.test(src);
+  return !!text && CMD.test(text);
 }
-/// #}}} @func isMacroTag
+/// #}}} @func hasDefineCommand
 
-module.exports = isMacroTag;
+module.exports = hasDefineCommand;
 
 /// #}}} @group EXPORTS
 
