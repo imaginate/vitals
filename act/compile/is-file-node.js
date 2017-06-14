91efb1255fe88267556bb0b3136e860f1aaa09e2
Adam Smith <adam@imaginate.life>
2017-06-13 17:21:13 -0700

    Added compile helper: isFileNode

---
+++ act/compile/is-file-node.js
@@ -0,0 +1,79 @@
+/**
+ * ---------------------------------------------------------------------------
+ * IS-FILE-NODE HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('./load-helper.js');
+/// #}}} @func loadHelper
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #{{{ @const FILE_TYPE_ID
+/**
+ * @private
+ * @const {!Object}
+ */
+var FILE_TYPE_ID = require('./type-ids.js').file;
+/// #}}} @const FILE_TYPE_ID
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isObject
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isObject = IS.object;
+/// #}}} @func isObject
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isFileNode
+/**
+ * @public
+ * @param {*} val
+ * @return {boolean}
+ */
+function isFileNode(val) {
+  return isObject(val) && 'type' in val && val.type === FILE_TYPE_ID;
+}
+/// #}}} @func isFileNode
+
+module.exports = isFileNode;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
