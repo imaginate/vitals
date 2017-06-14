5c0ca52ed1dfb2533f71ac9d3e6c4a99b59695bd
Adam Smith <adam@imaginate.life>
2017-06-13 17:28:55 -0700

    Added compile helper: isInsertNode

---
+++ act/compile/is-insert-node.js
@@ -0,0 +1,79 @@
+/**
+ * ---------------------------------------------------------------------------
+ * IS-INSERT-NODE HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('./load-helper.js');
+/// #}}} @func loadHelper
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #{{{ @const INS_TYPE_ID
+/**
+ * @private
+ * @const {!Object}
+ */
+var INS_TYPE_ID = require('./type-ids.js').insert;
+/// #}}} @const INS_TYPE_ID
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isObject
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isObject = IS.object;
+/// #}}} @func isObject
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isInsertNode
+/**
+ * @public
+ * @param {*} val
+ * @return {boolean}
+ */
+function isInsertNode(val) {
+  return isObject(val) && 'type' in val && val.type === INS_TYPE_ID;
+}
+/// #}}} @func isInsertNode
+
+module.exports = isInsertNode;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
