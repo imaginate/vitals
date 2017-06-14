56c433b6c031370b284e59d0759cc684e4da3e1d
Adam Smith <adam@imaginate.life>
2017-06-13 17:28:40 -0700

    Added compile helper: isIncludeNode

---
+++ act/compile/is-include-node.js
@@ -0,0 +1,79 @@
+/**
+ * ---------------------------------------------------------------------------
+ * IS-INCLUDE-NODE HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('./load-helper.js');
+/// #}}} @func loadHelper
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #{{{ @const INCL_TYPE_ID
+/**
+ * @private
+ * @const {!Object}
+ */
+var INCL_TYPE_ID = require('./type-ids.js').include;
+/// #}}} @const INCL_TYPE_ID
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isObject
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isObject = IS.object;
+/// #}}} @func isObject
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isIncludeNode
+/**
+ * @public
+ * @param {*} val
+ * @return {boolean}
+ */
+function isIncludeNode(val) {
+  return isObject(val) && 'type' in val && val.type === INCL_TYPE_ID;
+}
+/// #}}} @func isIncludeNode
+
+module.exports = isIncludeNode;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
