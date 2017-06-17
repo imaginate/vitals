d21df38752d260cffa13d541bce7fa212138b8ed
Adam Smith <adam@imaginate.life>
2017-06-17 00:26:14 -0700

    Added jspp helper: hasValidKey

---
+++ act/preprocessor/helpers/has-valid-key.js
@@ -0,0 +1,84 @@
+/**
+ * ---------------------------------------------------------------------------
+ * HAS-VALID-KEY HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @func loadTaskHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadTaskHelper = require('./load-task-helper.js');
+/// #}}} @func loadTaskHelper
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadTaskHelper('is');
+/// #}}} @const IS
+
+/// #{{{ @const KEY
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var KEY = /^[a-zA-Z0-9_\-\.]+:[a-zA-Z0-9_\-\.\$]+$/;
+/// #}}} @const KEY
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isString
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+/// #}}} @func isString
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func hasValidKey
+/**
+ * @public
+ * @param {string} key
+ * @return {boolean}
+ */
+function hasValidKey(key) {
+
+  if ( !isString(key) )
+    throw new TypeError('invalid `key` data type\n' +
+      '    valid-types: `string`');
+
+  return !!key && KEY.test(key);
+}
+/// #}}} @func hasValidKey
+
+module.exports = hasValidKey;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
