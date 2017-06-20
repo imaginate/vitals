3804e76202884181ae6b9ef692ee342af60495cc
Adam Smith <adam@imaginate.life>
2017-06-20 08:56:57 -0700

    Added jspp helper: replaceWildcards

---
+++ act/preprocessor/helpers/replace-wildcards.js
@@ -0,0 +1,77 @@
+/**
+ * ---------------------------------------------------------------------------
+ * REPLACE-WILDCARDS HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @func loadTaskHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadTaskHelper = require('./load-task-helper.js');
+/// #}}} @func loadTaskHelper
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadTaskHelper('is');
+/// #}}} @const IS
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isString
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+/// #}}} @func isString
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func replaceWildcards
+/**
+ * @public
+ * @param {string} src
+ * @return {boolean}
+ */
+function replaceWildcards(src) {
+
+  if ( !isString(src) )
+    throw new TypeError('invalid `src` data type\n' +
+      '    valid-types: `string`');
+
+  src = src.replace(/\*/g, '.*');
+  return src.replace(/\?/g, '.');
+}
+/// #}}} @func replaceWildcards
+
+module.exports = replaceWildcards;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
