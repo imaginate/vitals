4d212b441926097606c56be22315ef2c092201f3
Adam Smith <adam@imaginate.life>
2017-06-20 05:17:01 -0700

    Added jspp helper: isStateObject

---
+++ act/preprocessor/helpers/is-state-object.js
@@ -0,0 +1,117 @@
+/**
+ * ---------------------------------------------------------------------------
+ * IS-STATE-OBJECT HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @func loadTaskHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadTaskHelper = require('./load-task-helper.js');
+/// #}}} @func loadTaskHelper
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadTaskHelper('is');
+/// #}}} @const IS
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func hasOwnProperty
+/**
+ * @private
+ * @param {(!Object|!Function)} src
+ * @param {(string|number)} key
+ * @return {boolean}
+ */
+var hasOwnProperty = loadTaskHelper('has-own-property');
+/// #}}} @func hasOwnProperty
+
+/// #{{{ @func isBoolean
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isBoolean = IS.boolean;
+/// #}}} @func isBoolean
+
+/// #{{{ @func isBooleanMap
+/**
+ * @private
+ * @param {!Object} vals
+ * @return {boolean}
+ */
+var isBooleanMap = IS.booleanHashMap;
+/// #}}} @func isBooleanMap
+
+/// #{{{ @func isObject
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isObject = IS.object;
+/// #}}} @func isObject
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func isStateObject
+/**
+ * @public
+ * @param {*} src
+ * @return {boolean}
+ */
+function isStateObject(src) {
+
+  /** @type {string} */
+  var key;
+  /** @type {*} */
+  var val;
+
+  if ( !isObject(src) )
+    return false;
+
+  for (key in src) {
+    if ( hasOwnProperty(src, key) ) {
+      val = src[key];
+      if ( !isBoolean(val) ) {
+        if ( !isObject(val) || !isBooleanMap(val) )
+          return false;
+      }
+    }
+  }
+  return true;
+}
+/// #}}} @func isStateObject
+
+module.exports = isStateObject;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
