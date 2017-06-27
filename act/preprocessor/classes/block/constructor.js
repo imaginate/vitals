d5c99a4550b5f4cd2939be4ebe716ec275f59426
Adam Smith <adam@imaginate.life>
2017-06-27 15:13:08 -0700

    Added separate jspp block class constructor

---
+++ act/preprocessor/classes/block/constructor.js
@@ -0,0 +1,660 @@
+/**
+ * ---------------------------------------------------------------------------
+ * BLOCK CONSTRUCTOR
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @group LOADERS
+//////////////////////////////////////////////////////////////////////////////
+// LOADERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('./.load-helper.js');
+/// #}}} @func loadHelper
+
+/// #}}} @group LOADERS
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const BLK_TYPE_ID
+/**
+ * @private
+ * @const {!Object}
+ */
+var BLK_TYPE_ID = loadHelper('type-ids').BLK;
+/// #}}} @const BLK_TYPE_ID
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group STATE
+
+/// #{{{ @func capObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var capObject = loadHelper('cap-object');
+/// #}}} @func capObject
+
+/// #{{{ @func createObject
+/**
+ * @private
+ * @param {?Object} proto
+ * @return {!Object}
+ */
+var createObject = loadHelper('create-object');
+/// #}}} @func createObject
+
+/// #{{{ @func defineProperty
+/**
+ * @private
+ * @param {!Object} src
+ * @param {string} key
+ * @param {!Object} descriptor
+ * @return {!Object}
+ */
+var defineProperty = loadHelper('define-property');
+/// #}}} @func defineProperty
+
+/// #{{{ @func freezeObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var freezeObject = loadHelper('freeze-object');
+/// #}}} @func freezeObject
+
+/// #{{{ @func lockObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var lockObject = loadHelper('lock-object');
+/// #}}} @func lockObject
+
+/// #{{{ @func sealObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var sealObject = loadHelper('seal-object');
+/// #}}} @func sealObject
+
+/// #}}} @group STATE
+
+/// #{{{ @group GET
+
+/// #{{{ @func getIdComponent
+/**
+ * @private
+ * @param {(string|!Line)} text
+ * @return {string}
+ */
+var getIdComponent = loadHelper('get-id-component');
+/// #}}} @func getIdComponent
+
+/// #{{{ @func getTagComponent
+/**
+ * @private
+ * @param {(string|!Line)} text
+ * @return {string}
+ */
+var getTagComponent = loadHelper('get-tag-component');
+/// #}}} @func getTagComponent
+
+/// #}}} @group GET
+
+/// #{{{ @group HAS
+
+/// #{{{ @func hasOpen
+/**
+ * @private
+ * @param {string} val
+ * @return {boolean}
+ */
+var hasOpen = loadHelper('has-open-command');
+/// #}}} @func hasOpen
+
+/// #{{{ @func hasOwnProperty
+/**
+ * @private
+ * @param {(!Object|!Function)} src
+ * @param {(string|number)} key
+ * @return {boolean}
+ */
+var hasOwnProperty = loadHelper('has-own-property');
+/// #}}} @func hasOwnProperty
+
+/// #{{{ @func hasValidBlock
+/**
+ * @private
+ * @param {string} text
+ * @return {boolean}
+ */
+var hasValidBlock = loadHelper('has-valid-block');
+/// #}}} @func hasValidBlock
+
+/// #}}} @group HAS
+
+/// #{{{ @group IS
+
+/// #{{{ @func isBlkNode
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isBlkNode = loadHelper('is-block-node');
+/// #}}} @func isBlkNode
+
+/// #{{{ @func isCondNode
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isCondNode = loadHelper('is-conditional-node');
+/// #}}} @func isCondNode
+
+/// #{{{ @func isFileNode
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isFileNode = loadHelper('is-file-node');
+/// #}}} @func isFileNode
+
+/// #{{{ @func isInstanceOf
+/**
+ * @private
+ * @param {*} inst
+ * @param {!Function} constructor
+ * @return {boolean}
+ */
+var isInstanceOf = IS.instanceOf;
+/// #}}} @func isInstanceOf
+
+/// #{{{ @func isLineNode
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isLineNode = loadHelper('is-line-node');
+/// #}}} @func isLineNode
+
+/// #{{{ @func isNull
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNull = IS.nil;
+/// #}}} @func isNull
+
+/// #{{{ @func isUndefined
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isUndefined = IS.undefined;
+/// #}}} @func isUndefined
+
+/// #}}} @group IS
+
+/// #{{{ @group PATH
+
+/// #{{{ @func resolvePath
+/**
+ * @private
+ * @param {(!Array<string>|...string)=} path
+ * @return {string}
+ */
+var resolvePath = loadHelper('resolve-path');
+/// #}}} @func resolvePath
+
+/// #}}} @group PATH
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
+/**
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = loadHelper('set-error');
+/// #}}} @func setError
+
+/// #{{{ @func setCmdError
+/**
+ * @private
+ * @param {!SyntaxError} err
+ * @param {!Line} line
+ * @return {!SyntaxError}
+ */
+var setCmdError = setError.cmd;
+/// #}}} @func setCmdError
+
+/// #{{{ @func setIdError
+/**
+ * @private
+ * @param {!SyntaxError} err
+ * @param {!Line} line
+ * @return {!SyntaxError}
+ */
+var setIdError = setError.id;
+/// #}}} @func setIdError
+
+/// #{{{ @func setNewError
+/**
+ * @private
+ * @param {!SyntaxError} err
+ * @param {string} constructor
+ * @return {!SyntaxError}
+ */
+var setNewError = setError.new_;
+/// #}}} @func setNewError
+
+/// #{{{ @func setOpenError
+/**
+ * @private
+ * @param {!SyntaxError} err
+ * @param {!Line} line
+ * @return {!SyntaxError}
+ */
+var setOpenError = setError.open;
+/// #}}} @func setOpenError
+
+/// #{{{ @func setTagError
+/**
+ * @private
+ * @param {!SyntaxError} err
+ * @param {!Line} line
+ * @return {!SyntaxError}
+ */
+var setTagError = setError.tag;
+/// #}}} @func setTagError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group PATHS
+//////////////////////////////////////////////////////////////////////////////
+// PATHS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const DIR
+/**
+ * @private
+ * @const {string}
+ */
+var DIR = freezeObject({
+  MAIN: resolvePath(__dirname),
+  PROTO: resolvePath(__dirname, './methods')
+});
+/// #}}} @const DIR
+
+/// #}}} @group PATHS
+
+/// #{{{ @group CONSTRUCTORS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTORS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func Blk
+/**
+ * @public
+ * @param {!Line} open
+ * @param {!File} file
+ * @param {(?Blk|?Cond)=} parent
+ * @constructor
+ * @struct
+ */
+function Blk(open, file, parent) {
+
+  /// #{{{ @step verify-new-keyword
+
+  if ( !isInstanceOf(this, Blk) )
+    throw setNewError(new SyntaxError, 'Blk');
+
+  /// #}}} @step verify-new-keyword
+
+  /// #{{{ @step verify-parameters
+
+  if ( !isLineNode(open) )
+    throw setTypeError(new TypeError, 'open', '!Line');
+  if ( !isFileNode(file) )
+    throw setTypeError(new TypeError, 'file', '!File');
+  if (!isUndefined(parent)
+      && !isNull(parent)
+      && !isBlkNode(parent)
+      && !isCondNode(parent) )
+    throw setTypeError(new TypeError, 'parent', '(?Blk|?Cond)=');
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step set-constants
+
+  /// #{{{ @const PARENT
+  /**
+   * @private
+   * @const {(?Blk|?Cond)}
+   */
+  var PARENT = !!parent
+    ? parent
+    : null;
+  /// #}}} @const PARENT
+
+  /// #{{{ @const OPEN
+  /**
+   * @private
+   * @const {!Line}
+   */
+  var OPEN = open;
+  /// #}}} @const OPEN
+
+  /// #{{{ @const FILE
+  /**
+   * @private
+   * @const {!File}
+   */
+  var FILE = file;
+  /// #}}} @const FILE
+
+  /// #{{{ @const TEXT
+  /**
+   * @private
+   * @const {string}
+   */
+  var TEXT = OPEN.text;
+  /// #}}} @const TEXT
+
+  /// #{{{ @const TAG
+  /**
+   * @private
+   * @const {string}
+   */
+  var TAG = getTagComponent(TEXT);
+  /// #}}} @const TAG
+
+  /// #{{{ @const ID
+  /**
+   * @private
+   * @const {string}
+   */
+  var ID = getIdComponent(TEXT);
+  /// #}}} @const ID
+
+  /// #{{{ @const KEY
+  /**
+   * @private
+   * @const {string}
+   */
+  var KEY = TAG + ':' + ID;
+  /// #}}} @const KEY
+
+  /// #}}} @step set-constants
+
+  /// #{{{ @step verify-syntax
+
+  if (!TAG)
+    throw setTagError(new SyntaxError, OPEN);
+  if (!ID)
+    throw setIdError(new SyntaxError, OPEN);
+  if ( !hasOpen(TEXT) )
+    throw setOpenError(new SyntaxError, OPEN);
+  if ( !hasValidBlock(TEXT) )
+    throw setCmdError(new SyntaxError, OPEN);
+
+  /// #}}} @step verify-syntax
+
+  /// #{{{ @step set-members
+
+  /// #{{{ @member type
+  /**
+   * @public
+   * @const {!Object}
+   */
+  defineProperty(this, 'type', {
+    'value': BLK_TYPE_ID,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member type
+
+  /// #{{{ @member tag
+  /**
+   * @public
+   * @const {string}
+   */
+  defineProperty(this, 'tag', {
+    'value': TAG,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member tag
+
+  /// #{{{ @member id
+  /**
+   * @public
+   * @const {string}
+   */
+  defineProperty(this, 'id', {
+    'value': ID,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member id
+
+  /// #{{{ @member key
+  /**
+   * @public
+   * @const {string}
+   */
+  defineProperty(this, 'key', {
+    'value': KEY,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member key
+
+  /// #{{{ @member file
+  /**
+   * @public
+   * @const {!File}
+   */
+  defineProperty(this, 'file', {
+    'value': FILE,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member file
+
+  /// #{{{ @member open
+  /**
+   * @public
+   * @const {!Line}
+   */
+  defineProperty(this, 'open', {
+    'value': OPEN,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member open
+
+  /// #{{{ @member close
+  /**
+   * @public
+   * @type {?Line}
+   */
+  defineProperty(this, 'close', {
+    'value': null,
+    'writable': true,
+    'enumerable': true,
+    'configurable': true
+  });
+  /// #}}} @member close
+
+  /// #{{{ @member parent
+  /**
+   * @public
+   * @const {(?Blk|?Cond)}
+   */
+  defineProperty(this, 'parent', {
+    'value': PARENT,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member parent
+
+  /// #{{{ @member blks
+  /**
+   * @public
+   * @const {!Object<string, !Blk>}
+   */
+  defineProperty(this, 'blks', {
+    'value': {},
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member blks
+
+  /// #{{{ @member conds
+  /**
+   * @public
+   * @const {!Object<string, !Cond>}
+   */
+  defineProperty(this, 'conds', {
+    'value': {},
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member conds
+
+  /// #{{{ @member incls
+  /**
+   * @public
+   * @const {!Object<string, !Incl>}
+   */
+  defineProperty(this, 'incls', {
+    'value': {},
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member incls
+
+  /// #{{{ @member content
+  /**
+   * @public
+   * @const {!Array<(!Line|!Blk|!Cond|!Incl)>}
+   */
+  defineProperty(this, 'content', {
+    'value': [],
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member content
+
+  /// #}}} @step set-members
+
+  /// #{{{ @step lock-instance
+
+  capObject(this);
+  sealObject(this);
+
+  /// #}}} @step lock-instance
+}
+/// #}}} @func Blk
+
+/// #}}} @group CONSTRUCTORS
+
+/// #{{{ @group PROTOTYPE
+//////////////////////////////////////////////////////////////////////////////
+// PROTOTYPE
+//////////////////////////////////////////////////////////////////////////////
+
+Blk.prototype = createObject(null);
+
+defineProperty(Blk.prototype, 'constructor', {
+  'value': Blk,
+  'writable': false,
+  'enumerable': false,
+  'configurable': false
+});
+
+defineMethods(Blk.prototype, 'block', DIR.PROTO);
+capObject(Blk.prototype);
+
+/// #}}} @group PROTOTYPE
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = Blk;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
