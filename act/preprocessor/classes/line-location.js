5165806ab1402b2170beaef92e5a15aa8f4e3d16
Adam Smith <adam@imaginate.life>
2017-06-27 08:29:13 -0700

    Added jspp line-location class

---
+++ act/preprocessor/classes/line-location.js
@@ -0,0 +1,314 @@
+/**
+ * ---------------------------------------------------------------------------
+ * LINE-LOCATION CLASS
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @group LOADERS
+//////////////////////////////////////////////////////////////////////////////
+// LOADERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('./load-helper.js');
+/// #}}} @func loadHelper
+
+/// #}}} @group LOADERS
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const LINE_LOC_TYPE_ID
+/**
+ * @private
+ * @const {!Object}
+ */
+var LINE_LOC_TYPE_ID = loadHelper('type-ids').LINE_LOC;
+/// #}}} @const LINE_LOC_TYPE_ID
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group STATE
+
+/// #{{{ @func capObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var capObject = loadHelper('cap-object');
+/// #}}} @func capObject
+
+/// #{{{ @func createObject
+/**
+ * @private
+ * @param {?Object} proto
+ * @return {!Object}
+ */
+var createObject = loadHelper('create-object');
+/// #}}} @func createObject
+
+/// #{{{ @func defineProperty
+/**
+ * @private
+ * @param {!Object} src
+ * @param {string} key
+ * @param {!Object} descriptor
+ * @return {!Object}
+ */
+var defineProperty = loadHelper('define-property');
+/// #}}} @func defineProperty
+
+/// #{{{ @func freezeObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var freezeObject = loadHelper('freeze-object');
+/// #}}} @func freezeObject
+
+/// #{{{ @func lockObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var lockObject = loadHelper('lock-object');
+/// #}}} @func lockObject
+
+/// #{{{ @func sealObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var sealObject = loadHelper('seal-object');
+/// #}}} @func sealObject
+
+/// #}}} @group STATE
+
+/// #{{{ @group IS
+
+/// #{{{ @func isFileNode
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isFileNode = loadHelper('is-file-node');
+/// #}}} @func isFileNode
+
+/// #{{{ @func isInstanceOf
+/**
+ * @private
+ * @param {*} inst
+ * @param {!Function} constructor
+ * @return {boolean}
+ */
+var isInstanceOf = IS.instanceOf;
+/// #}}} @func isInstanceOf
+
+/// #{{{ @func isNumber
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNumber = IS.number;
+/// #}}} @func isNumber
+
+/// #{{{ @func isWholeNumber
+/**
+ * @private
+ * @param {number} val
+ * @return {boolean}
+ */
+var isWholeNumber = IS.wholeNumber;
+/// #}}} @func isWholeNumber
+
+/// #}}} @group IS
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
+/**
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = loadHelper('set-error');
+/// #}}} @func setError
+
+/// #{{{ @func setIndexError
+/**
+ * @private
+ * @param {!RangeError} err
+ * @param {string} param
+ * @param {number} index
+ * @param {number=} min = `0`
+ * @return {!RangeError}
+ */
+var setIndexError = setError.index;
+/// #}}} @func setIndexError
+
+/// #{{{ @func setNewError
+/**
+ * @private
+ * @param {!SyntaxError} err
+ * @param {string} constructor
+ * @return {!SyntaxError}
+ */
+var setNewError = setError.new_;
+/// #}}} @func setNewError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group CONSTRUCTORS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTORS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func LineLoc
+/**
+ * @public
+ * @param {number} linenum
+ * @param {!File} file
+ * @constructor
+ * @struct
+ */
+function LineLoc(linenum, file) {
+
+  /// #{{{ @step verify-new-keyword
+
+  if ( !isInstanceOf(this, LineLoc) )
+    throw setNewError(new SyntaxError, 'LineLoc');
+
+  /// #}}} @step verify-new-keyword
+
+  /// #{{{ @step verify-parameters
+
+  if ( !isNumber(linenum) )
+    throw setTypeError(new TypeError, 'linenum', 'number');
+  if ( !isWholeNumber(linenum) || linenum < 1 )
+    throw setIndexError(new RangeError, 'linenum', linenum, 1);
+  if ( !isFileNode(file) )
+    throw setTypeError(new TypeError, 'file', '!File');
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step set-members
+
+  /// #{{{ @member type
+  /**
+   * @public
+   * @const {!Object}
+   */
+  defineProperty(this, 'type', {
+    'value': LINE_LOC_TYPE_ID,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member type
+
+  /// #{{{ @member file
+  /**
+   * @public
+   * @const {!File}
+   */
+  defineProperty(this, 'file', {
+    'value': file,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member file
+
+  /// #{{{ @member linenum
+  /**
+   * @public
+   * @const {number}
+   */
+  defineProperty(this, 'linenum', {
+    'value': linenum,
+    'writable': false,
+    'enumerable': true,
+    'configurable': false
+  });
+  /// #}}} @member linenum
+
+  /// #}}} @step set-members
+
+  /// #{{{ @step freeze-instance
+
+  freezeObject(this);
+
+  /// #}}} @step freeze-instance
+}
+/// #}}} @func LineLoc
+
+/// #}}} @group CONSTRUCTORS
+
+/// #{{{ @group PROTOTYPE
+//////////////////////////////////////////////////////////////////////////////
+// PROTOTYPE
+//////////////////////////////////////////////////////////////////////////////
+
+LineLoc.prototype = createObject(null);
+LineLoc.prototype.constructor = LineLoc;
+
+/// #}}} @group PROTOTYPE
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = LineLoc;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
