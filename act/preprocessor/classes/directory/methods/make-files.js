39f73fd2fca80633d1cc32682b21df6a0f2df2b5
Adam Smith <adam@imaginate.life>
2017-06-29 14:58:27 -0700

    Added jspp directory method: makeFiles

---
+++ act/preprocessor/classes/directory/methods/make-files.js
@@ -0,0 +1,307 @@
+/**
+ * ---------------------------------------------------------------------------
+ * MAKE-FILES METHOD
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+'use strict';
+
+/// #{{{ @group LOADERS
+//////////////////////////////////////////////////////////////////////////////
+// LOADERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('./.load-helper.js');
+/// #}}} @func loadHelper
+
+/// #{{{ @func loadClass
+/**
+ * @private
+ * @param {string} name
+ * @return {!Function}
+ */
+var loadClass = loadHelper('load-class');
+/// #}}} @func loadClass
+
+/// #}}} @group LOADERS
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group CONSTRUCTORS
+
+/// #{{{ @func File
+/**
+ * @private
+ * @param {string} path
+ * @param {!Dir} parent
+ * @constructor
+ * @struct
+ */
+var File = loadClass('file');
+/// #}}} @func File
+
+/// #}}} @group CONSTRUCTORS
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
+/**
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = loadHelper('set-error');
+/// #}}} @func setError
+
+/// #{{{ @func setEmptyError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setEmptyError = setError.empty;
+/// #}}} @func setEmptyError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group FS
+
+/// #{{{ @func getFilePaths
+/**
+ * @private
+ * @param {string} dirpath
+ * @param {?Object|boolean=} opts
+ *   If the #opts is a `boolean`, the #opts.deep option is set to its value.
+ * @param {?boolean=} opts.deep = `false`
+ *   Make a recursive search for valid files.
+ * @param {?boolean=} opts.full = `false`
+ *   Return absolute file paths instead of relative file paths.
+ * @param {?boolean=} opts.extend = `false`
+ *   When supplying a valid or invalid pattern to check paths against, the
+ *   #opts.extend option allows you to supplement instead of overwrite the
+ *   default valid or invalid test. If the default value is `null`, this
+ *   option does not have any side effects.
+ * @param {?RegExp=} opts.valid = `null`
+ *   A pattern for matching valid file or directory paths. If #opts.valid is
+ *   `null`, no check is performed. If it is a `RegExp`, the source property
+ *   is checked for a forward slash, `"/"`. If it has a forward slash, the
+ *   path tree is tested against the #opts.valid pattern. Otherwise (i.e. if
+ *   it does not have a forward slash), the path name is tested against the
+ *   #opts.valid pattern.
+ * @param {?RegExp=} opts.invalid = `null`
+ *   A pattern for matching invalid file or directory paths. If #opts.invalid
+ *   is `null`, no check is performed. If it is a `RegExp`, the source
+ *   property is checked for a forward slash, `"/"`. If it has a forward
+ *   slash, the path tree is tested against the #opts.invalid pattern.
+ *   Otherwise (i.e. if it does not have a forward slash), the path name is
+ *   tested against the #opts.invalid pattern.
+ * @param {?RegExp=} opts.validDirs = `null`
+ *   Only used when #opts.deep is `true`. A pattern for matching valid
+ *   directory paths. If #opts.validDirs is `null`, no check is performed. If
+ *   it is a `RegExp`, the source property is checked for a forward slash,
+ *   `"/"`. If it has a forward slash, the path tree is tested against the
+ *   #opts.validDirs pattern. Otherwise (i.e. if it does not have a forward
+ *   slash), the path name is tested against the #opts.validDirs pattern.
+ * @param {?RegExp=} opts.invalidDirs = `/^(?:\.git|\.bak|node_modules|vendor|\.?te?mp|\.?logs?|.*~)$/i`
+ *   Only used when #opts.deep is `true`. A pattern for matching invalid
+ *   directory paths. If #opts.invalidDirs is `null`, no check is performed.
+ *   If it is a `RegExp`, the source property is checked for a forward slash,
+ *   `"/"`. If it has a forward slash, the path tree is tested against the
+ *   #opts.invalidDirs pattern. Otherwise (i.e. if it does not have a forward
+ *   slash), the path name is tested against the #opts.invalidDirs pattern.
+ * @param {?RegExp=} opts.validFiles = `null`
+ *   A pattern for matching valid file paths. If #opts.validFiles is `null`,
+ *   no check is performed. If it is a `RegExp`, the source property is
+ *   checked for a forward slash, `"/"`. If it has a forward slash, the path
+ *   tree is tested against the #opts.validFiles pattern. Otherwise (i.e. if
+ *   it does not have a forward slash), the path name is tested against the
+ *   #opts.validFiles pattern.
+ * @param {?RegExp=} opts.invalidFiles = `null`
+ *   A pattern for matching invalid file paths. If #opts.invalidFiles is
+ *   `null`, no check is performed. If it is a `RegExp`, the source property
+ *   is checked for a forward slash, `"/"`. If it has a forward slash, the
+ *   path tree is tested against the #opts.invalidFiles pattern. Otherwise
+ *   (i.e. if it does not have a forward slash), the path name is tested
+ *   against the #opts.invalidFiles pattern.
+ * @return {!Array<string>}
+ */
+var getFilePaths = loadHelper('get-filepaths');
+/// #}}} @func getFilePaths
+
+/// #}}} @group FS
+
+/// #{{{ @group OBJECT
+
+/// #{{{ @func createObject
+/**
+ * @private
+ * @param {?Object} proto
+ * @return {!Object}
+ */
+var createObject = loadHelper('create-object');
+/// #}}} @func createObject
+
+/// #{{{ @func freezeObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var freezeObject = loadHelper('freeze-object');
+/// #}}} @func freezeObject
+
+/// #{{{ @func setupOffProperty
+/**
+ * @private
+ * @param {!Object} src
+ * @param {string} key
+ * @param {*} value
+ * @param {boolean=} visible = `false`
+ * @return {!Object}
+ */
+var setupOffProperty = loadHelper('setup-off-property');
+/// #}}} @func setupOffProperty
+
+/// #}}} @group OBJECT
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func Dir.prototype.makeFiles
+/**
+ * @public
+ * @this {!Dir}
+ * @return {!Dir}
+ */
+function makeFiles() {
+
+  /// #{{{ @step declare-variables
+
+  /** @type {!Object<string, !File>} */
+  var files;
+  /** @type {!Array<string>} */
+  var paths;
+  /** @type {!File} */
+  var file;
+  /** @type {number} */
+  var len;
+  /** @type {number} */
+  var i;
+
+  /// #}}} @step declare-variables
+
+  /// #{{{ @step check-files-member
+
+  if (this.files)
+    return this;
+
+  /// #}}} @step check-files-member
+
+  /// #{{{ @step setup-files
+
+  files = createObject(null);
+
+  /// #}}} @step setup-files
+
+  /// #{{{ @step get-file-paths
+
+  paths = getFilePaths(this.path, {
+    'deep': false,
+    'full': true,
+    'validFiles': /\.js$/
+  });
+
+  /// #}}} @step get-file-paths
+
+  /// #{{{ @step make-file-nodes
+
+  len = paths.length;
+  i = -1;
+  while (++i < len) {
+    file = new File(paths[i], parent);
+    setupOffProperty(files, file.name, file, true);
+  }
+
+  /// #}}} @step make-file-nodes
+
+  /// #{{{ @step set-files-member
+
+  freezeObject(files);
+  setupOffProperty(this, 'files', files, true);
+
+  /// #}}} @step set-files-member
+
+  /// #{{{ @step return-instance
+
+  return this;
+
+  /// #}}} @step return-instance
+}
+/// #}}} @func Dir.prototype.makeFiles
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = makeFiles;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
