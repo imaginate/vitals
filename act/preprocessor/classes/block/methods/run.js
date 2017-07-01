ce4f19cd8ad53aeb91713d16df960be098022b15
Adam Smith <adam@imaginate.life>
2017-07-01 07:02:06 -0700

    Updated jspp block & conditional method: run

--- act/preprocessor/classes/block/methods/run.js
+++ act/preprocessor/classes/block/methods/run.js
@@ -56,69 +56,6 @@ var IS = loadHelper('is');
 var setError = loadHelper('set-error');
 /// #}}} @func setError
 
-/// #{{{ @func setCloseError
-/**
- * @private
- * @param {!SyntaxError} err
- * @param {!Line} line
- * @return {!SyntaxError}
- */
-var setCloseError = setError.close;
-/// #}}} @func setCloseError
-
-/// #{{{ @func setCmdError
-/**
- * @private
- * @param {!SyntaxError} err
- * @param {!Line} line
- * @return {!SyntaxError}
- */
-var setCmdError = setError.cmd;
-/// #}}} @func setCmdError
-
-/// #{{{ @func setEmptyError
-/**
- * @private
- * @param {!Error} err
- * @param {string} param
- * @return {!Error}
- */
-var setEmptyError = setError.empty;
-/// #}}} @func setEmptyError
-
-/// #{{{ @func setIdError
-/**
- * @private
- * @param {!SyntaxError} err
- * @param {!Line} line
- * @return {!SyntaxError}
- */
-var setIdError = setError.id;
-/// #}}} @func setIdError
-
-/// #{{{ @func setIndexError
-/**
- * @private
- * @param {!RangeError} err
- * @param {string} param
- * @param {number} index
- * @param {number=} min = `0`
- * @return {!RangeError}
- */
-var setIndexError = setError.index;
-/// #}}} @func setIndexError
-
-/// #{{{ @func setMatchError
-/**
- * @private
- * @param {!SyntaxError} err
- * @param {!Line} open
- * @param {!Line} close
- * @return {!SyntaxError}
- */
-var setMatchError = setError.match;
-/// #}}} @func setMatchError
-
 /// #{{{ @func setNoArgError
 /**
  * @private
@@ -128,48 +65,6 @@ var setMatchError = setError.match;
  */
 var setNoArgError = setError.noArg;
 /// #}}} @func setNoArgError
-
-/// #{{{ @func setNoCloseError
-/**
- * @private
- * @param {!SyntaxError} err
- * @param {!Line} line
- * @return {!SyntaxError}
- */
-var setNoCloseError = setError.noClose;
-/// #}}} @func setNoCloseError
-
-/// #{{{ @func setOpenError
-/**
- * @private
- * @param {!SyntaxError} err
- * @param {!Line} line
- * @return {!SyntaxError}
- */
-var setOpenError = setError.open;
-/// #}}} @func setOpenError
-
-/// #{{{ @func setOwnCmdError
-/**
- * @private
- * @param {!ReferenceError} err
- * @param {(!Line|!Blk|!Cond|!Incl)} node1
- * @param {(!Line|!Blk|!Cond|!Incl)} node2
- * @param {(?Blk|?Cond)=} scope = `null`
- * @return {!ReferenceError}
- */
-var setOwnCmdError = setError.ownCmd;
-/// #}}} @func setOwnCmdError
-
-/// #{{{ @func setTagError
-/**
- * @private
- * @param {!SyntaxError} err
- * @param {!Line} line
- * @return {!SyntaxError}
- */
-var setTagError = setError.tag;
-/// #}}} @func setTagError
 
 /// #{{{ @func setTypeError
 /**
@@ -182,156 +77,9 @@ var setTagError = setError.tag;
 var setTypeError = setError.type;
 /// #}}} @func setTypeError
 
-/// #{{{ @func setWholeError
-/**
- * @private
- * @param {!RangeError} err
- * @param {string} param
- * @param {number} value
- * @return {!RangeError}
- */
-var setWholeError = setError.whole;
-/// #}}} @func setWholeError
-
 /// #}}} @group ERROR
 
-/// #{{{ @group GET
-
-/// #{{{ @func getIdComponent
-/**
- * @private
- * @param {(string|!Line)} text
- * @return {string}
- */
-var getIdComponent = loadHelper('get-id-component');
-/// #}}} @func getIdComponent
-
-/// #{{{ @func getOwnedCommand
-/**
- * @private
- * @param {(!File|!Blk|!Cond)} src
- * @param {(string|!Blk|!Cond|!Incl)} node
- * @return {(?Blk|?Cond|?Incl)}
- */
-var getOwnedCommand = loadHelper('get-owned-command');
-/// #}}} @func getOwnedCommand
-
-/// #{{{ @func getTagComponent
-/**
- * @private
- * @param {(string|!Line)} text
- * @return {string}
- */
-var getTagComponent = loadHelper('get-tag-component');
-/// #}}} @func getTagComponent
-
-/// #}}} @group GET
-
-/// #{{{ @group HAS
-
-/// #{{{ @func hasBlock
-/**
- * @private
- * @param {string} val
- * @return {boolean}
- */
-var hasBlock = loadHelper('has-block-command');
-/// #}}} @func hasBlock
-
-/// #{{{ @func hasClose
-/**
- * @private
- * @param {string} val
- * @return {boolean}
- */
-var hasClose = loadHelper('has-close-command');
-/// #}}} @func hasClose
-
-/// #{{{ @func hasCommand
-/**
- * @private
- * @param {string} val
- * @return {boolean}
- */
-var hasCommand = loadHelper('has-command');
-/// #}}} @func hasCommand
-
-/// #{{{ @func hasConditional
-/**
- * @private
- * @param {string} val
- * @return {boolean}
- */
-var hasConditional = loadHelper('has-conditional-command');
-/// #}}} @func hasConditional
-
-/// #{{{ @func hasInclude
-/**
- * @private
- * @param {string} val
- * @return {boolean}
- */
-var hasInclude = loadHelper('has-include-command');
-/// #}}} @func hasInclude
-
-/// #{{{ @func hasOpen
-/**
- * @private
- * @param {string} val
- * @return {boolean}
- */
-var hasOpen = loadHelper('has-open-command');
-/// #}}} @func hasOpen
-
-/// #{{{ @func hasOwnProperty
-/**
- * @private
- * @param {(!Object|!Function)} src
- * @param {(string|number)} key
- * @return {boolean}
- */
-var hasOwnProperty = loadHelper('has-own-property');
-/// #}}} @func hasOwnProperty
-
-/// #{{{ @func hasValidBlock
-/**
- * @private
- * @param {string} text
- * @return {boolean}
- */
-var hasValidBlock = loadHelper('has-valid-block');
-/// #}}} @func hasValidBlock
-
-/// #}}} @group HAS
-
 /// #{{{ @group IS
-
-/// #{{{ @func isArray
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isArray = IS.array;
-/// #}}} @func isArray
-
-/// #{{{ @func isBlkNode
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isBlkNode = loadHelper('is-block-node');
-/// #}}} @func isBlkNode
-
-/// #{{{ @func isCondNode
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isCondNode = loadHelper('is-conditional-node');
-/// #}}} @func isCondNode
 
 /// #{{{ @func isFlagsNode
 /**
@@ -342,24 +90,6 @@ var isCondNode = loadHelper('is-conditional-node');
 var isFlagsNode = loadHelper('is-flags-node');
 /// #}}} @func isFlagsNode
 
-/// #{{{ @func isFileNode
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isFileNode = loadHelper('is-file-node');
-/// #}}} @func isFileNode
-
-/// #{{{ @func isInclNode
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isInclNode = loadHelper('is-include-node');
-/// #}}} @func isInclNode
-
 /// #{{{ @func isLineNode
 /**
  * @private
@@ -368,24 +98,6 @@ var isInclNode = loadHelper('is-include-node');
  */
 var isLineNode = loadHelper('is-line-node');
 /// #}}} @func isLineNode
-
-/// #{{{ @func isNull
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isNull = IS.nil;
-/// #}}} @func isNull
-
-/// #{{{ @func isNumber
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isNumber = IS.number;
-/// #}}} @func isNumber
 
 /// #{{{ @func isObject
 /**
@@ -396,89 +108,7 @@ var isNumber = IS.number;
 var isObject = IS.object;
 /// #}}} @func isObject
 
-/// #{{{ @func isUndefined
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isUndefined = IS.undefined;
-/// #}}} @func isUndefined
-
-/// #{{{ @func isWholeNumber
-/**
- * @private
- * @param {number} val
- * @return {boolean}
- */
-var isWholeNumber = IS.wholeNumber;
-/// #}}} @func isWholeNumber
-
 /// #}}} @group IS
-
-/// #{{{ @group OBJECT
-
-/// #{{{ @func capObject
-/**
- * @private
- * @param {(?Object|?Function)} src
- * @param {boolean=} deep = `false`
- * @return {(?Object|?Function)}
- */
-var capObject = loadHelper('cap-object');
-/// #}}} @func capObject
-
-/// #{{{ @func createObject
-/**
- * @private
- * @param {?Object} proto
- * @return {!Object}
- */
-var createObject = loadHelper('create-object');
-/// #}}} @func createObject
-
-/// #{{{ @func defineProperty
-/**
- * @private
- * @param {!Object} src
- * @param {string} key
- * @param {!Object} descriptor
- * @return {!Object}
- */
-var defineProperty = loadHelper('define-property');
-/// #}}} @func defineProperty
-
-/// #{{{ @func freezeObject
-/**
- * @private
- * @param {(?Object|?Function)} src
- * @param {boolean=} deep = `false`
- * @return {(?Object|?Function)}
- */
-var freezeObject = loadHelper('freeze-object');
-/// #}}} @func freezeObject
-
-/// #{{{ @func lockObject
-/**
- * @private
- * @param {(?Object|?Function)} src
- * @param {boolean=} deep = `false`
- * @return {(?Object|?Function)}
- */
-var lockObject = loadHelper('lock-object');
-/// #}}} @func lockObject
-
-/// #{{{ @func sealObject
-/**
- * @private
- * @param {(?Object|?Function)} src
- * @param {boolean=} deep = `false`
- * @return {(?Object|?Function)}
- */
-var sealObject = loadHelper('seal-object');
-/// #}}} @func sealObject
-
-/// #}}} @group OBJECT
 
 /// #}}} @group HELPERS
 
@@ -514,6 +144,15 @@ function run(flags, inclFiles, inclNodes) {
   /// #}}} @step declare-variables
 
   /// #{{{ @step verify-parameters
+
+  switch (arguments.length) {
+    case 0:
+      throw setNoArgError(new Error, 'flags');
+    case 1:
+      throw setNoArgError(new Error, 'inclFiles');
+    case 2:
+      throw setNoArgError(new Error, 'inclNodes');
+  }
 
   if ( !isFlagsNode(flags) )
     throw setTypeError(new TypeError, 'flags', '!Flags');
