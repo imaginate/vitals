655e69c04869baaf17243750d1dac4f6cbefa408
Adam Smith <adam@imaginate.life>
2017-07-21 03:07:39 -0700

    Added basic node externs for closure-compiler

---
+++ src/externs/node.js
@@ -0,0 +1,38 @@
+/**
+ * ---------------------------------------------------------------------------
+ * NODE EXTERNS
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ * @externs
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ */
+
+/// #{{{ @extern global
+/**
+ * @public
+ * @const {!Object}
+ */
+var global;
+/// #}}} @extern global
+
+/// #{{{ @extern module
+/**
+ * @public
+ * @const {!Object}
+ */
+var module;
+/// #}}} @extern module
+
+/// #{{{ @extern require
+/**
+ * @public
+ * @param {string} path
+ * @return {(!Object|!Function)}
+ */
+function require(path) {}
+/// #}}} @extern require
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
