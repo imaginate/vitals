9a3b29172ea220d72ab09de2ce278bff1738b989
Adam Smith <adam@imaginate.life>
2017-07-27 01:36:36 -0700

    Cleaned-up task helper: trimSlash

--- act/helpers/trim-slash.js
+++ act/helpers/trim-slash.js
@@ -1,66 +1,152 @@
 /**
- * -----------------------------------------------------------------------------
- * ACT TASK HELPER: trimSlash
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * TRIM-SLASH HELPER
+ * ---------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
- * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
- *
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
  */
 
 'use strict';
 
-////////////////////////////////////////////////////////////////////////////////
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
 // CONSTANTS
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
+/// #{{{ @const END_SLASH
 /**
  * @private
  * @const {!RegExp}
  */
-var END_SLASH = /\/$/;
+var END_SLASH = /\/+$/;
+/// #}}} @const END_SLASH
 
+/// #{{{ @const IS
 /**
  * @private
- * @const {!Object<string, function>}
+ * @const {!Object<string, !function>}
+ * @struct
  */
 var IS = require('./is.js');
+/// #}}} @const IS
 
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
 /**
  * @private
- * @const {!RegExp}
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
  */
-var UNIX_ROOT = /^\/$/;
+var setError = require('./set-error.js');
+/// #}}} @func setError
 
-////////////////////////////////////////////////////////////////////////////////
-// HELPERS
-////////////////////////////////////////////////////////////////////////////////
+/// #{{{ @func setEmptyError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setEmptyError = setError.empty;
+/// #}}} @func setEmptyError
 
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
+/// #{{{ @func isRootDirectory
+/**
+ * @private
+ * @param {string} path
+ * @return {boolean}
+ */
+var isRootDirectory = IS.rootDirectory;
+/// #}}} @func isRootDirectory
+
+/// #{{{ @func isString
 /**
  * @private
  * @param {*} val
  * @return {boolean}
  */
 var isString = IS.string;
+/// #}}} @func isString
 
-////////////////////////////////////////////////////////////////////////////////
-// EXPORTS
-////////////////////////////////////////////////////////////////////////////////
+/// #}}} @group IS
 
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func trimSlash
 /**
  * @public
  * @param {string} path
  * @return {string}
  */
-module.exports = function trimSlash(path) {
+function trimSlash(path) {
 
-  if ( !isString(path) )
-    throw new TypeError('invalid `path` type (must be a string)');
-  if ( !path )
-    throw new Error('invalid empty `path` string');
-  if ( UNIX_ROOT.test(path) )
-    throw new Error('invalid root `path`');
+  if (!arguments.length) {
+    throw setNoArgError(new Error, 'path');
+  }
+  if ( !isString(path) ) {
+    throw setTypeError(new TypeError, 'path', 'string');
+  }
+  if (!path) {
+    throw setEmptyError(new Error, 'path');
+  }
+  if ( isRootDirectory(path) ) {
+    throw setError(new Error,
+      'invalid root directory for `path` parameter\n' +
+      '    path-value: `' + path + '`');
+  }
 
   return path.replace(END_SLASH, '');
-};
+}
+/// #}}} @func trimSlash
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = trimSlash;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
