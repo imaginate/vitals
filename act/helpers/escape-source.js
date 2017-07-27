41cad37c8935a1a8cb6672e0640d1366f93eea83
Adam Smith <adam@imaginate.life>
2017-07-27 12:43:49 -0700

    Cleaned-up task helper: escapeSource

--- act/helpers/escape-source.js
+++ act/helpers/escape-source.js
@@ -3,7 +3,7 @@
  * ESCAPE-SOURCE HELPER
  * ---------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
- * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
  */
 
 'use strict';
@@ -17,6 +17,7 @@
 /**
  * @private
  * @const {!Object<string, !function>}
+ * @struct
  */
 var IS = require('./is.js');
 /// #}}} @const IS
@@ -28,6 +29,43 @@ var IS = require('./is.js');
 // HELPERS
 //////////////////////////////////////////////////////////////////////////////
 
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
+/**
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = require('./set-error.js');
+/// #}}} @func setError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
 /// #{{{ @func isString
 /**
  * @private
@@ -37,28 +75,40 @@ var IS = require('./is.js');
 var isString = IS.string;
 /// #}}} @func isString
 
+/// #}}} @group IS
+
 /// #}}} @group HELPERS
 
-/// #{{{ @group EXPORTS
+/// #{{{ @group METHODS
 //////////////////////////////////////////////////////////////////////////////
-// EXPORTS
+// METHODS
 //////////////////////////////////////////////////////////////////////////////
 
 /// #{{{ @func escapeSource
 /**
  * @public
  * @param {string} src
- * @return {boolean}
+ * @return {string}
  */
 function escapeSource(src) {
 
-  if ( !isString(src) )
-    throw new TypeError('invalid `src` data type\n' +
-      '    valid-types: `string`');
+  if (!arguments.length) {
+    throw setNoArgError(new Error, 'src');
+  }
+  if ( !isString(src) ) {
+    throw setTypeError(new TypeError, 'src', 'string');
+  }
 
   return src && src.replace(/[\\^$.*+?|(){}[\]]/g, '\\$&');
 }
 /// #}}} @func escapeSource
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
 
 module.exports = escapeSource;
 
