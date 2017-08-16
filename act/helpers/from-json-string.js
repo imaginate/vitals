4fa8dd672aa80219f598b49cceb489c35f0e76ae
Adam Smith <adam@imaginate.life>
2017-08-16 14:56:57 -0700

    Added task helper: fromJsonString

---
+++ act/helpers/from-json-string.js
@@ -0,0 +1,187 @@
+/**
+ * ---------------------------------------------------------------------------
+ * FROM-JSON-STRING HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+'use strict';
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ * @struct
+ */
+var IS = require('./is.js');
+/// #}}} @const IS
+
+/// #{{{ @const JSON3
+/**
+ * @private
+ * @const {!Object}
+ * @struct
+ */
+var JSON3 = require('json3').noConflict();
+/// #}}} @const JSON3
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
+/**
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = require('./set-error.js');
+/// #}}} @func setError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
+/// #{{{ @func isFunction
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isFunction = IS.func;
+/// #}}} @func isFunction
+
+/// #{{{ @func isNull
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNull = IS.nil;
+/// #}}} @func isNull
+
+/// #{{{ @func isString
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+/// #}}} @func isString
+
+/// #{{{ @func isUndefined
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isUndefined = IS.void;
+/// #}}} @func isUndefined
+
+/// #}}} @group IS
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func parse
+/**
+ * @private
+ * @param {string} src
+ * @param {(undefined|!function(string, *): *)=} filter
+ * @return {!Object}
+ */
+var parse = JSON3.parse;
+/// #}}} @func parse
+
+/// #{{{ @func fromJsonString
+/**
+ * @public
+ * @param {string} src
+ * @param {(?function(string, *): *)=} filter = `null`
+ * @return {!Object}
+ */
+function fromJsonString(src, filter) {
+
+  /// #{{{ @step verify-parameters
+
+  switch (arguments.length) {
+    case 0:
+      throw setNoArgError(new Error, 'src');
+    case 1:
+      filter = undefined;
+      break;
+    default:
+      if ( isUndefined(filter) || isNull(filter) ) {
+        filter = undefined;
+      }
+      else if ( !isFunction(filter) ) {
+        throw setTypeError(new TypeError, 'filter',
+          '(?function(string, *): *)=');
+      }
+  }
+
+  if ( !isString(src) ) {
+    throw setTypeError(new TypeError, 'src', 'string');
+  }
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step return-result
+
+  return isUndefined(filter)
+    ? parse(src)
+    : parse(src, filter);
+
+  /// #}}} @step return-result
+}
+/// #}}} @func fromJsonString
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = fromJsonString;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
