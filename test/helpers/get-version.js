7a18a25476f48bd147bcc47419259a007eae22df
Adam Smith <adam@imaginate.life>
2017-08-18 16:51:05 -0700

    Added test helper: getVersion

---
+++ test/helpers/get-version.js
@@ -0,0 +1,100 @@
+/**
+ * ---------------------------------------------------------------------------
+ * GET-VERSION HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+'use strict';
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group OBJECT
+
+/// #{{{ @func freezeObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {?Object}
+ */
+var freezeObject = require('./freeze-object.js');
+/// #}}} @func freezeObject
+
+/// #}}} @group OBJECT
+
+/// #{{{ @group PATH
+
+/// #{{{ @func resolvePath
+/**
+ * @private
+ * @param {(!Array<string>|!Arguments<string>|...string)=} path
+ * @return {string}
+ */
+var resolvePath = require('./resolve-path.js');
+/// #}}} @func resolvePath
+
+/// #}}} @group PATH
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group PATHS
+//////////////////////////////////////////////////////////////////////////////
+// PATHS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const DIR
+/**
+ * @private
+ * @const {!Object<string, string>}
+ * @struct
+ */
+var DIR = freezeObject({
+  REPO: resolvePath(__dirname, '../..')
+});
+/// #}}} @const DIR
+
+/// #{{{ @const FILE
+/**
+ * @private
+ * @const {!Object<string, string>}
+ * @struct
+ */
+var FILE = freezeObject({
+  PKG: resolvePath(DIR.REPO, 'package.json')
+});
+/// #}}} @const FILE
+
+/// #}}} @group PATHS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func getVersion
+/**
+ * @public
+ * @return {string}
+ */
+function getVersion() {
+  return require(FILE.PKG).version;
+}
+/// #}}} @func getVersion
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = getVersion;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
