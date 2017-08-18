acf18c07407700d28cadf097f9ab6b803ca8603b
Adam Smith <adam@imaginate.life>
2017-08-18 16:48:32 -0700

    Cleaned-up task helper: getVersion

--- act/helpers/get-version.js
+++ act/helpers/get-version.js
@@ -1,51 +1,100 @@
 /**
- * -----------------------------------------------------------------------------
- * ACT TASK HELPER: getVersion
- * -----------------------------------------------------------------------------
+ * ---------------------------------------------------------------------------
+ * GET-VERSION HELPER
+ * ---------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
- * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
- *
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
  */
 
 'use strict';
 
-////////////////////////////////////////////////////////////////////////////////
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
 // HELPERS
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
+/// #{{{ @group OBJECT
+
+/// #{{{ @func freezeObject
 /**
  * @private
- * @param {(!Array<string>|...string)=} path
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {?Object}
+ */
+var freezeObject = require('./freeze-object.js');
+/// #}}} @func freezeObject
+
+/// #}}} @group OBJECT
+
+/// #{{{ @group PATH
+
+/// #{{{ @func resolvePath
+/**
+ * @private
+ * @param {(!Array<string>|!Arguments<string>|...string)=} path
  * @return {string}
  */
 var resolvePath = require('./resolve-path.js');
+/// #}}} @func resolvePath
 
-////////////////////////////////////////////////////////////////////////////////
-// CONSTANTS
-////////////////////////////////////////////////////////////////////////////////
+/// #}}} @group PATH
 
+/// #}}} @group HELPERS
+
+/// #{{{ @group PATHS
+//////////////////////////////////////////////////////////////////////////////
+// PATHS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const DIR
 /**
  * @private
- * @const {string}
+ * @const {!Object<string, string>}
+ * @struct
  */
-var REPO_DIR = require('./get-repo-root.js')();
+var DIR = freezeObject({
+  REPO: require('./get-repo-root.js')()
+});
+/// #}}} @const DIR
 
+/// #{{{ @const FILE
 /**
  * @private
- * @const {string}
+ * @const {!Object<string, string>}
+ * @struct
  */
-var PACKAGE = resolvePath(REPO_DIR, './package.json');
+var FILE = freezeObject({
+  PKG: resolvePath(DIR.REPO, 'package.json')
+});
+/// #}}} @const FILE
 
-////////////////////////////////////////////////////////////////////////////////
-// EXPORTS
-////////////////////////////////////////////////////////////////////////////////
+/// #}}} @group PATHS
 
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func getVersion
 /**
  * @public
  * @return {string}
  */
-module.exports = function getVersion() {
-  return require(PACKAGE).version;
-};
+function getVersion() {
+  return require(FILE.PKG).version;
+}
+/// #}}} @func getVersion
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = getVersion;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
