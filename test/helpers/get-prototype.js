f600b339dce0f8c09fd59ccdcf17b80e0cc43ff4
Adam Smith <adam@imaginate.life>
2017-08-18 23:55:22 -0700

    Cleaned-up test helper: getPrototype

--- test/helpers/get-prototype.js
+++ test/helpers/get-prototype.js
@@ -1,30 +1,134 @@
 /**
- * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS HELPER: getPrototype
- * -----------------------------------------------------------------------------
- * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
- * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Annotations:
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ * ---------------------------------------------------------------------------
+ * GET-PROTOTYPE HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
  */
 
 'use strict';
 
-module.exports = getPrototype;
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
 
+/// #{{{ @const IS
 /**
  * @private
- * @param {!Object} obj
- * @return {Object}
+ * @const {!Object<string, !function>}
+ * @struct
  */
-var getProto = Object.getPrototypeOf;
+var IS = require('./is.js');
+/// #}}} @const IS
 
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
 /**
- * @param {!Object} obj
- * @return {Object}
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
  */
-function getPrototype(obj) {
-  return getProto(obj);
+var setError = require('./set-error.js');
+/// #}}} @func setError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
+/// #{{{ @func isHashMap
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isHashMap = IS.hashMap;
+/// #}}} @func isHashMap
+
+/// #}}} @group IS
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func _getPrototype
+/**
+ * @private
+ * @param {(!Object|!Function)} src
+ * @return {?Object}
+ */
+var _getPrototype = Object.getPrototypeOf;
+/// #}}} @func _getPrototype
+
+/// #{{{ @func getPrototype
+/**
+ * @public
+ * @param {(!Object|!Function)} src
+ * @return {?Object}
+ */
+function getPrototype(src) {
+
+  /// #{{{ @step verify-parameters
+
+  if (!arguments.length) {
+    throw setNoArgError(new Error, 'src');
+  }
+  if ( !isHashMap(src) ) {
+    throw setTypeError(new TypeError, 'src', '(!Object|!Function)');
+  }
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step return-prototype
+
+  return _getPrototype(src);
+
+  /// #}}} @step return-prototype
 }
+/// #}}} @func getPrototype
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = getPrototype;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
