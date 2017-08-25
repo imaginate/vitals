f7b59962e5b82624e621539f044fe88c60d4f216
Adam Smith <adam@imaginate.life>
2017-08-25 05:04:46 -0700

    Rebuilt test helper: setEol

--- test/helpers/set-eol.js
+++ test/helpers/set-eol.js
@@ -1,45 +1,204 @@
 /**
- * -----------------------------------------------------------------------------
- * VITALS UNIT TESTS HELPER: setEol
- * -----------------------------------------------------------------------------
- * @author Adam Smith <adam@imaginate.life> (https://github.com/imaginate)
- * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://github.com/imaginate)
- *
- * Annotations:
- * @see [JSDoc3](http://usejsdoc.org)
- * @see [Closure Compiler JSDoc Syntax](https://developers.google.com/closure/compiler/docs/js-for-compiler)
+ * ---------------------------------------------------------------------------
+ * SET-EOL HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
  */
 
-module.exports = setEol;
+'use strict';
 
-var own = require('./has-own');
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
 
+/// #{{{ @const EOL
+/**
+ * @private
+ * @const {!Object<string, string>}
+ * @dict
+ */
 var EOL = {
-  'CR':   '\r',
-  'LF':   '\n',
+  'CR': '\r',
+  'LF': '\n',
   'CRLF': '\r\n'
 };
+/// #}}} @const EOL
 
-var FIND_EOL = {
-  'CR':   /\r?\n/g,
-  'LF':   /\r\n?/g,
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ * @struct
+ */
+var IS = require('./is.js');
+/// #}}} @const IS
+
+/// #{{{ @const PATT
+/**
+ * @private
+ * @const {!Object<string, !RegExp>}
+ * @dict
+ */
+var PATT = {
+  'CR': /\r?\n/g,
+  'LF': /\r\n?/g,
   'CRLF': /\r?\n|\r\n?/g
 };
+/// #}}} @const PATT
 
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
 /**
- * @global
- * @param {string} str
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = require('./set-error.js');
+/// #}}} @func setError
+
+/// #{{{ @func setEmptyError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setEmptyError = setError.empty;
+/// #}}} @func setEmptyError
+
+/// #{{{ @func setEolError
+/**
+ * @private
+ * @param {!RangeError} err
+ * @param {string} param
+ * @param {string} val
+ * @return {!RangeError}
+ */
+var setEolError = setError.eol;
+/// #}}} @func setEolError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group HAS
+
+/// #{{{ @func hasOwnEnumProperty
+/**
+ * @private
+ * @param {(!Object|!Function)} src
+ * @param {(string|number)} key
+ * @return {boolean}
+ */
+var hasOwnEnumProperty = require('./has-own-enum-property.js');
+/// #}}} @func hasOwnEnumProperty
+
+/// #}}} @group HAS
+
+/// #{{{ @group IS
+
+/// #{{{ @func isString
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+/// #}}} @func isString
+
+/// #}}} @group IS
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func setEol
+/**
+ * @public
+ * @param {string} content
  * @param {string} eol
  * @return {string}
  */
-function setEol(str, eol) {
+function setEol(content, eol) {
 
-  if (typeof str !== 'string') throw TypeError('invalid str');
-  if (typeof eol !== 'string') throw TypeError('invalid eol');
+  /// #{{{ @step verify-parameters
+
+  switch (arguments.length) {
+    case 0:
+      throw setNoArgError(new Error, 'content');
+    case 1:
+      throw setNoArgError(new Error, 'eol');
+  }
+
+  if ( !isString(content) ) {
+    throw setTypeError(new TypeError, 'content', 'string');
+  }
+  if ( !isString(eol) ) {
+    throw setTypeError(new TypeError, 'eol', 'string');
+  }
+
+  if (!eol) {
+    throw setEmptyError(new Error, 'eol');
+  }
 
   eol = eol.toUpperCase();
 
-  if ( !own(EOL, eol) ) throw RangeError('invalid eol (opts: LF, CR, CRLF)');
+  if ( !hasOwnEnumProperty(EOL, eol) ) {
+    throw setEolError(new RangeError, 'eol', eol);
+  }
 
-  return str.replace(FIND_EOL[eol], EOL[eol]);
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step return-result
+
+  return content && content.replace(PATT[eol], EOL[eol]);
+
+  /// #}}} @step return-result
 }
+/// #}}} @func setEol
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = setEol;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
