3f3c9b1235972f81beec47e708a82e69cc57ab29
Adam Smith <adam@imaginate.life>
2017-08-28 20:51:16 -0700

    Rebuilt docs task helper: loadHelper

--- act/make-docs/helpers/load-helper.js
+++ act/make-docs/helpers/load-helper.js
@@ -3,10 +3,15 @@
  * LOAD-HELPER HELPER
  * ---------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
- * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
  */
 
 'use strict';
+
+/// #{{{ @group LOADERS
+//////////////////////////////////////////////////////////////////////////////
+// LOADERS
+//////////////////////////////////////////////////////////////////////////////
 
 /// #{{{ @func loadTaskHelper
 /**
@@ -17,66 +22,21 @@
 var loadTaskHelper = require('./load-task-helper.js');
 /// #}}} @func loadTaskHelper
 
-/// #{{{ @group INIT-HELPERS
-//////////////////////////////////////////////////////////////////////////////
-// INIT-HELPERS
-//////////////////////////////////////////////////////////////////////////////
-
-/// #{{{ @func resolvePath
-/**
- * @private
- * @param {(!Array<string>|...string)=} path
- * @return {string}
- */
-var resolvePath = loadTaskHelper('resolve-path');
-/// #}}} @func resolvePath
-
-/// #}}} @group INIT-HELPERS
+/// #}}} @group LOADERS
 
 /// #{{{ @group CONSTANTS
 //////////////////////////////////////////////////////////////////////////////
 // CONSTANTS
 //////////////////////////////////////////////////////////////////////////////
 
-/// #{{{ @const DIR_PATH
-/**
- * @private
- * @const {!RegExp}
- */
-var DIR_PATH = /^.*\//;
-/// #}}} @const DIR_PATH
-
-/// #{{{ @const DOCS_HELPER_DIR
-/**
- * @private
- * @const {string}
- */
-var DOCS_HELPER_DIR = resolvePath(__dirname);
-/// #}}} @const DOCS_HELPER_DIR
-
 /// #{{{ @const IS
 /**
  * @private
  * @const {!Object<string, !function>}
+ * @struct
  */
 var IS = loadTaskHelper('is');
 /// #}}} @const IS
-
-/// #{{{ @const JS_EXT
-/**
- * @private
- * @const {!RegExp}
- */
-var JS_EXT = /\.js$/;
-/// #}}} @const JS_EXT
-
-/// #{{{ @const TASK_HELPER_DIR
-/**
- * @private
- * @const {string}
- */
-var TASK_HELPER_DIR = resolvePath(__dirname, '../../helpers');
-/// #}}} @const TASK_HELPER_DIR
 
 /// #}}} @group CONSTANTS
 
@@ -85,23 +45,7 @@ var TASK_HELPER_DIR = resolvePath(__dirname, '../../helpers');
 // HELPERS
 //////////////////////////////////////////////////////////////////////////////
 
-/// #{{{ @func isFile
-/**
- * @private
- * @param {string} path
- * @return {boolean}
- */
-var isFile = IS.file;
-/// #}}} @func isFile
-
-/// #{{{ @func isString
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isString = IS.string;
-/// #}}} @func isString
+/// #{{{ @group ERROR
 
 /// #{{{ @func setError
 /**
@@ -134,6 +78,16 @@ var setEmptyError = setError.empty;
 var setFileError = setError.file;
 /// #}}} @func setFileError
 
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
 /// #{{{ @func setTypeError
 /**
  * @private
@@ -145,11 +99,100 @@ var setFileError = setError.file;
 var setTypeError = setError.type;
 /// #}}} @func setTypeError
 
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
+/// #{{{ @func isFile
+/**
+ * @private
+ * @param {string} path
+ * @return {boolean}
+ */
+var isFile = IS.file;
+/// #}}} @func isFile
+
+/// #{{{ @func isString
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+/// #}}} @func isString
+
+/// #}}} @group IS
+
+/// #{{{ @group PATH
+
+/// #{{{ @func getPathName
+/**
+ * @private
+ * @param {string} path
+ * @return {string}
+ */
+var getPathName = loadTaskHelper('get-path-name');
+/// #}}} @func getPathName
+
+/// #{{{ @func resolvePath
+/**
+ * @private
+ * @param {(!Array<string>|!Arguments<string>|...string)=} path
+ * @return {string}
+ */
+var resolvePath = loadTaskHelper('resolve-path');
+/// #}}} @func resolvePath
+
+/// #{{{ @func trimJsFileExtension
+/**
+ * @private
+ * @param {string} path
+ * @return {string}
+ */
+var trimJsFileExtension = loadTaskHelper('trim-file-extension').construct(
+  '.js');
+/// #}}} @func trimJsFileExtension
+
+/// #}}} @group PATH
+
+/// #{{{ @group OBJECT
+
+/// #{{{ @func freezeObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var freezeObject = loadTaskHelper('freeze-object');
+/// #}}} @func freezeObject
+
+/// #}}} @group OBJECT
+
 /// #}}} @group HELPERS
 
-/// #{{{ @group EXPORTS
+/// #{{{ @group PATHS
 //////////////////////////////////////////////////////////////////////////////
-// EXPORTS
+// PATHS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const DIR
+/**
+ * @private
+ * @const {!Object<string, string>}
+ * @struct
+ */
+var DIR = freezeObject({
+  DOCS: resolvePath(__dirname),
+  TASK: resolvePath(__dirname, '../../helpers')
+});
+/// #}}} @const DIR
+
+/// #}}} @group PATHS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
 //////////////////////////////////////////////////////////////////////////////
 
 /// #{{{ @func loadHelper
@@ -163,32 +206,46 @@ function loadHelper(name) {
   /** @type {string} */
   var path;
 
-  if ( !isString(name) )
+  if (!arguments.length) {
+    throw setNoArgError(new Error, 'name');
+  }
+  if ( !isString(name) ) {
     throw setTypeError(new TypeError, 'name', 'string');
-
-  name = name.replace(DIR_PATH, '');
-  name = name.replace(JS_EXT, '');
-
-  if (!name)
+  }
+  if (!name) {
     throw setEmptyError(new Error, 'name');
+  }
+
+  name = getPathName(name);
+  name = trimJsFileExtension(name);
+
+  if (!name) {
+    throw setEmptyError(new Error, 'name');
+  }
 
   name += '.js';
-  path = resolvePath(DOCS_HELPER_DIR, name);
+  path = resolvePath(DIR.DOCS, name);
+
+  if ( isFile(path) ) {
+    return require(path);
+  }
+
+  path = resolvePath(DIR.TASK, name);
 
   if ( !isFile(path) ) {
-    path = resolvePath(TASK_HELPER_DIR, name);
-
-    if ( !isFile(path) )
-      throw setError(new Error,
-        'invalid readable file path for helper `name`\n' +
-        '    file-name: `' + name + '`\n' +
-        '    task-path: `' + path + '`\n' +
-        '    docs-path: `' + resolvePath(DOCS_HELPER_DIR, name) + '`');
+    throw setFileError(new Error, 'name', path);
   }
 
   return require(path);
 }
 /// #}}} @func loadHelper
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
 
 module.exports = loadHelper;
 
