2896d541245a197d12beff08c23eda98ee2f6e15
Adam Smith <adam@imaginate.life>
2017-09-04 08:35:30 -0700

    Cleaned-up task helper: toCamelCase

--- act/helpers/to-camel-case.js
+++ act/helpers/to-camel-case.js
@@ -3,7 +3,7 @@
  * TO-CAMEL-CASE HELPER
  * ---------------------------------------------------------------------------
  * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
- * @copyright 2014-2017 Adam A Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
  */
 
 'use strict';
@@ -33,6 +33,7 @@ var DASH_END = /-+$/;
 /**
  * @private
  * @const {!Object<string, !function>}
+ * @struct
  */
 var IS = require('./is.js');
 /// #}}} @const IS
@@ -44,14 +45,7 @@ var IS = require('./is.js');
 // HELPERS
 //////////////////////////////////////////////////////////////////////////////
 
-/// #{{{ @func isString
-/**
- * @private
- * @param {*} val
- * @return {boolean}
- */
-var isString = IS.string;
-/// #}}} @func isString
+/// #{{{ @group ERROR
 
 /// #{{{ @func setError
 /**
@@ -62,6 +56,16 @@ var isString = IS.string;
  */
 var setError = require('./set-error.js');
 /// #}}} @func setError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
 
 /// #{{{ @func setTypeError
 /**
@@ -74,12 +78,62 @@ var setError = require('./set-error.js');
 var setTypeError = setError.type;
 /// #}}} @func setTypeError
 
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
+/// #{{{ @func isString
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isString = IS.string;
+/// #}}} @func isString
+
+/// #}}} @group IS
+
 /// #}}} @group HELPERS
 
-/// #{{{ @group EXPORTS
+/// #{{{ @group METHODS
 //////////////////////////////////////////////////////////////////////////////
-// EXPORTS
+// METHODS
 //////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func capitalizeCharacter
+/**
+ * @private
+ * @param {string} match
+ * @param {string} ch
+ * @return {string}
+ */
+function capitalizeCharacter(match, ch) {
+
+  /// #{{{ @step verify-parameters
+
+  switch (arguments.length) {
+    case 0:
+      throw setNoArgError(new Error, 'match');
+    case 1:
+      throw setNoArgError(new Error, 'ch');
+  }
+
+  if ( !isString(match) ) {
+    throw setTypeError(new TypeError, 'match', 'string');
+  }
+  if ( !isString(ch) ) {
+    throw setTypeError(new TypeError, 'ch', 'string');
+  }
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step return-result
+
+  return ch && ch.toUpperCase();
+
+  /// #}}} @step return-result
+}
+/// #}}} @func capitalizeCharacter
 
 /// #{{{ @func toCamelCase
 /**
@@ -89,17 +143,61 @@ var setTypeError = setError.type;
  */
 function toCamelCase(val) {
 
-  if ( !isString(val) )
+  /// #{{{ @step verify-parameters
+
+  if (!arguments.length) {
+    throw setNoArgError(new Error, 'val');
+  }
+  if ( !isString(val) ) {
     throw setTypeError(new TypeError, 'val', 'string');
+  }
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step check-empty-value
+
+  if (!val) {
+    return '';
+  }
+
+  /// #}}} @step check-empty-value
+
+  /// #{{{ @step normalize-dashes
 
   val = val.replace(DASH_BEGIN, '');
   val = val.replace(DASH_END, '');
   val = val.replace(/--+/g, '-');
-  return val && val.replace(/-([a-z])/g, function replacement(match, ch) {
-    return ch.toUpperCase();
-  });
+
+  /// #}}} @step normalize-dashes
+
+  /// #{{{ @step check-empty-value
+
+  if (!val) {
+    return '';
+  }
+
+  /// #}}} @step check-empty-value
+
+  /// #{{{ @step convert-value-to-camel-case
+
+  val = val.replace(/-([a-z])/g, capitalizeCharacter);
+
+  /// #}}} @step convert-value-to-camel-case
+
+  /// #{{{ @step return-result
+
+  return val;
+
+  /// #}}} @step return-result
 }
 /// #}}} @func toCamelCase
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
 
 module.exports = toCamelCase;
 
