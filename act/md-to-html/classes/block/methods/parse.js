4ac47c0b3744912f735baf8c343ecabd089e842c
Adam Smith <adam@imaginate.life>
2017-09-05 21:49:01 -0700

    Added html task method: Block.prototype.parse

---
+++ act/md-to-html/classes/block/methods/parse.js
@@ -0,0 +1,118 @@
+/**
+ * ---------------------------------------------------------------------------
+ * PARSE METHOD
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+'use strict';
+
+/// #{{{ @group LOADERS
+//////////////////////////////////////////////////////////////////////////////
+// LOADERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('../../../helpers/load-helper.js');
+/// #}}} @func loadHelper
+
+/// #}}} @group LOADERS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group OBJECT
+
+/// #{{{ @func freezeObject
+/**
+ * @private
+ * @param {(?Object|?Function)} src
+ * @param {boolean=} deep = `false`
+ * @return {(?Object|?Function)}
+ */
+var freezeObject = loadHelper('freeze-object');
+/// #}}} @func freezeObject
+
+/// #{{{ @func setConstantProperty
+/**
+ * @private
+ * @param {!Object} src
+ * @param {string} key
+ * @param {*} val
+ * @param {boolean=} visible = `true`
+ * @return {!Object}
+ */
+var setConstantProperty = loadHelper('set-constant-property');
+/// #}}} @func setConstantProperty
+
+/// #}}} @group OBJECT
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func Block.prototype.parse
+/**
+ * @public
+ * @this {!Block}
+ * @return {!Block}
+ */
+function parseBlock() {
+
+  /// #{{{ @step declare-variables
+
+  /** @type {string} */
+  var result;
+
+  /// #}}} @step declare-variables
+
+  /// #{{{ @step parse-block-lines
+
+  result = this.CLASS.parse(this.ROOT, this, this.ELEMS, this.LINES, this.LEN,
+    this.DEPTH);
+
+  /// #}}} @step parse-block-lines
+
+  /// #{{{ @step freeze-scoped-blocks
+
+  freezeObject(this.ELEMS);
+
+  /// #}}} @step freeze-scoped-blocks
+
+  /// #{{{ @step save-result
+
+  setConstantProperty(this, 'RESULT', result);
+
+  /// #}}} @step save-result
+
+  /// #{{{ @step return-instance
+
+  return this;
+
+  /// #}}} @step return-instance
+}
+/// #}}} @func Block.prototype.parse
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = parseBlock;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
