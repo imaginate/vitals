924e184bca036113e5c80b32b27a37a2ec93bf36
Adam Smith <adam@imaginate.life>
2017-09-06 01:21:54 -0700

    Added html task method: Paragraph.prototype.parse

---
+++ act/md-to-html/classes/paragraph/methods/parse.js
@@ -0,0 +1,442 @@
+/**
+ * ---------------------------------------------------------------------------
+ * PARSE METHOD
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+'use strict';
+
+/// #{{{ @group LOADERS
+//////////////////////////////////////////////////////////////////////////////
+// LOADERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func loadClass
+/**
+ * @private
+ * @param {string} id
+ * @return {!Function}
+ */
+var loadClass = require('../../../helpers/load-class.js');
+/// #}}} @func loadClass
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('../../../helpers/load-helper.js');
+/// #}}} @func loadHelper
+
+/// #}}} @group LOADERS
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const CLOSE_PATTERN
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var CLOSE_PATTERN = /!\$ *$/;
+/// #}}} @const CLOSE_PATTERN
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ * @struct
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #{{{ @const SPACE_BEGIN_PATTERN
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var SPACE_BEGIN_PATTERN = /^ +/;
+/// #}}} @const SPACE_BEGIN_PATTERN
+
+/// #{{{ @const SPACE_END_PATTERN
+/**
+ * @private
+ * @const {!RegExp}
+ */
+var SPACE_END_PATTERN = / +$/;
+/// #}}} @const SPACE_END_PATTERN
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
+/**
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = loadHelper('set-error');
+/// #}}} @func setError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setPIndentError
+/**
+ * @private
+ * @param {!SyntaxError} err
+ * @param {!Block} BLOCK
+ * @param {number} index
+ * @param {number} depth
+ * @return {!SyntaxError}
+ */
+function setPIndentError(err, BLOCK, index, depth) {
+
+  /// #{{{ @step declare-variables
+
+  /** @type {string} */
+  var indent;
+  /** @type {!Array<string>} */
+  var lines;
+  /** @type {string} */
+  var line;
+  /** @type {string} */
+  var msg;
+  /** @type {number} */
+  var end;
+  /** @type {number} */
+  var i;
+
+  /// #}}} @step declare-variables
+
+  /// #{{{ @step verify-parameters
+
+  switch (arguments.length) {
+    case 0:
+      throw setNoArgError(new Error, 'err');
+    case 1:
+      throw setNoArgError(new Error, 'BLOCK');
+    case 2:
+      throw setNoArgError(new Error, 'index');
+    case 3:
+      throw setNoArgError(new Error, 'depth');
+  }
+
+  if ( !isError(err) ) {
+    throw setTypeError(new TypeError, 'err', '!SyntaxError');
+  }
+  if ( !isBlock(BLOCK) ) {
+    throw setTypeError(new TypeError, 'BLOCK', '!Block');
+  }
+  if ( !isNumber(index) ) {
+    throw setTypeError(new TypeError, 'index', 'number');
+  }
+  if ( !isNumber(depth) ) {
+    throw setTypeError(new TypeError, 'depth', 'number');
+  }
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step make-error-message
+
+  index += BLOCK.INDEX;
+  indent = BLOCK.makeIndent(depth);
+
+  msg = 'invalid line `indent` in documentation `paragraph`\n'
+    + '    line-number-within-snippet: `' + (index + 1) + '`\n'
+    + '    valid-indent-value: `"' + indent + '"`\n'
+    + '    snippet-of-lines:';
+
+  lines = BLOCK.ROOT.LINES;
+  end = index + 9;
+  i = index - 9;
+  if (end > lines.length) {
+    end = lines.length;
+  }
+  if (i < 0) {
+    i = 0;
+  }
+  while (i < end) {
+    msg += '\n    ';
+    msg += i === index
+      ? '--> '
+      : '    ';
+    line = lines[i++] || ' ';
+    line = line.replace(/`/g, '\\`');
+    msg += i + ' `' + line + '`';
+  }
+
+  /// #}}} @step make-error-message
+
+  /// #{{{ @step set-error-name-property
+
+  if (err.name !== 'SyntaxError') {
+    err.name = 'SyntaxError';
+  }
+
+  /// #}}} @step set-error-name-property
+
+  /// #{{{ @step return-error
+
+  return setError(err, msg);
+
+  /// #}}} @step return-error
+}
+/// #}}} @func setPIndentError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
+/// #{{{ @func isArray
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isArray = IS.array;
+/// #}}} @func isArray
+
+/// #{{{ @func isBlankLine
+/**
+ * @private
+ * @param {string} line
+ * @return {boolean}
+ */
+var isBlankLine = loadHelper('is-blank-line');
+/// #}}} @func isBlankLine
+
+/// #{{{ @func isBlock
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isBlock = IS.block;
+/// #}}} @func isBlock
+
+/// #{{{ @func isError
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isError = IS.error;
+/// #}}} @func isError
+
+/// #{{{ @func isHtml
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isHtml = IS.html;
+/// #}}} @func isHtml
+
+/// #{{{ @func isNumber
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNumber = IS.number;
+/// #}}} @func isNumber
+
+/// #}}} @group IS
+
+/// #{{{ @group OBJECT
+
+/// #{{{ @func setConstantProperty
+/**
+ * @private
+ * @param {!Object} src
+ * @param {string} key
+ * @param {*} val
+ * @param {boolean=} visible = `true`
+ * @return {!Object}
+ */
+var setConstantProperty = loadHelper('set-constant-property');
+/// #}}} @func setConstantProperty
+
+/// #}}} @group OBJECT
+
+/// #{{{ @group SPECIAL
+
+/// #{{{ @func newContent
+/**
+ * @private
+ * @param {!Block} BLOCK
+ * @param {string} CONTENT
+ * @return {!Content}
+ */
+var newContent = loadClass('content').create;
+/// #}}} @func newContent
+
+/// #}}} @group SPECIAL
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func Paragraph.prototype.parse
+/**
+ * @public
+ * @this {!Paragraph}
+ * @param {!Html} ROOT
+ * @param {!Block} BLOCK
+ * @param {!Array<!Block>} ELEMS
+ * @param {!Array<string>} LINES
+ * @param {number} LEN
+ * @param {number} DEPTH
+ * @return {string}
+ */
+function parseParagraph(ROOT, BLOCK, ELEMS, LINES, LEN, DEPTH) {
+
+  /// #{{{ @step declare-variables
+
+  /** @type {string} */
+  var content;
+  /** @type {string} */
+  var line;
+  /** @type {number} */
+  var i;
+
+  /// #}}} @step declare-variables
+
+  /// #{{{ @step verify-parameters
+
+  switch (arguments.length) {
+    case 0:
+      throw setNoArgError(new Error, 'ROOT');
+    case 1:
+      throw setNoArgError(new Error, 'BLOCK');
+    case 2:
+      throw setNoArgError(new Error, 'ELEMS');
+    case 3:
+      throw setNoArgError(new Error, 'LINES');
+    case 4:
+      throw setNoArgError(new Error, 'LEN');
+    case 5:
+      throw setNoArgError(new Error, 'DEPTH');
+  }
+
+  if ( !isHtml(ROOT) || ROOT !== this.BLOCK.ROOT ) {
+    throw setTypeError(new TypeError, 'ROOT', '!Html');
+  }
+  if ( !isBlock(BLOCK) || BLOCK !== this.BLOCK ) {
+    throw setTypeError(new TypeError, 'BLOCK', '!Block');
+  }
+  if ( !isArray(ELEMS) || ELEMS !== BLOCK.ELEMS ) {
+    throw setTypeError(new TypeError, 'ELEMS', '!Array<!Block>');
+  }
+  if ( !isArray(LINES) || LINES !== BLOCK.LINES ) {
+    throw setTypeError(new TypeError, 'LINES', '!Array<string>');
+  }
+  if ( !isNumber(LEN) || LEN !== BLOCK.LEN ) {
+    throw setTypeError(new TypeError, 'LEN', 'number');
+  }
+  if ( !isNumber(DEPTH) || DEPTH !== BLOCK.DEPTH ) {
+    throw setTypeError(new TypeError, 'DEPTH', 'number');
+  }
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step prepare-content-source
+
+  /// #{{{ @const isIndented
+  /**
+   * @private
+   * @const {!function(string, number=): boolean}
+   */
+  var isIndented = ROOT.isIndented;
+  /// #}}} @const isIndented
+
+  content = '';
+  i = -1;
+  while (++i < LEN) {
+    line = LINES[i];
+    if ( isIndented(line, DEPTH) ) {
+      throw setPIndentError(new SyntaxError, BLOCK, i, DEPTH);
+    }
+    line = line.replace(SPACE_BEGIN_PATTERN, '');
+    line = line.replace(SPACE_END_PATTERN, '');
+    content += !!line
+      ? line + ' '
+      : '<br>';
+  }
+  content = content.replace(CLOSE_PATTERN, '');
+  content = content.replace(SPACE_END_PATTERN, '');
+
+  /// #}}} @step prepare-content-source
+
+  /// #{{{ @step parse-content
+
+  /// #{{{ @const CONTENT
+  /**
+   * @private
+   * @const {!Content}
+   */
+  var CONTENT = newContent(BLOCK, content);
+  /// #}}} @const CONTENT
+
+  /// #}}} @step parse-content
+
+  /// #{{{ @step save-content
+
+  setConstantProperty(BLOCK, 'CONTENT', CONTENT);
+
+  /// #}}} @step save-content
+
+  /// #{{{ @step return-parsed-result
+
+  return '<p>' + CONTENT.RESULT + '</p>';
+
+  /// #}}} @step return-parsed-result
+}
+/// #}}} @func Paragraph.prototype.parse
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = parseParagraph;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
