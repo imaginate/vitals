2164c790c01526b7f0a0734b6d401dd2415b3a4f
Adam Smith <adam@imaginate.life>
2017-09-05 22:07:11 -0700

    Added html task method: Heading.prototype.scope

---
+++ act/md-to-html/classes/heading/methods/scope.js
@@ -0,0 +1,233 @@
+/**
+ * ---------------------------------------------------------------------------
+ * SCOPE METHOD
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+'use strict';
+
+/// #{{{ @group LOADERS
+//////////////////////////////////////////////////////////////////////////////
+// LOADERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func loadHelper
+/**
+ * @private
+ * @param {string} name
+ * @return {(!Object|!Function)}
+ */
+var loadHelper = require('../../../helpers/load-helper.js');
+/// #}}} @func loadHelper
+
+/// #}}} @group LOADERS
+
+/// #{{{ @group CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @const IS
+/**
+ * @private
+ * @const {!Object<string, !function>}
+ * @struct
+ */
+var IS = loadHelper('is');
+/// #}}} @const IS
+
+/// #}}} @group CONSTANTS
+
+/// #{{{ @group HELPERS
+//////////////////////////////////////////////////////////////////////////////
+// HELPERS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @group ERROR
+
+/// #{{{ @func setError
+/**
+ * @private
+ * @param {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)} err
+ * @param {string} msg
+ * @return {(!Error|!RangeError|!ReferenceError|!SyntaxError|!TypeError)}
+ */
+var setError = loadTaskHelper('set-error');
+/// #}}} @func setError
+
+/// #{{{ @func setIndexError
+/**
+ * @private
+ * @param {!RangeError} err
+ * @param {string} param
+ * @param {number} index
+ * @param {number=} min = `0`
+ * @return {!RangeError}
+ */
+var setIndexError = setError.index;
+/// #}}} @func setIndexError
+
+/// #{{{ @func setNoArgError
+/**
+ * @private
+ * @param {!Error} err
+ * @param {string} param
+ * @return {!Error}
+ */
+var setNoArgError = setError.noArg;
+/// #}}} @func setNoArgError
+
+/// #{{{ @func setTypeError
+/**
+ * @private
+ * @param {!TypeError} err
+ * @param {string} param
+ * @param {string} types
+ * @return {!TypeError}
+ */
+var setTypeError = setError.type;
+/// #}}} @func setTypeError
+
+/// #{{{ @func setWholeError
+/**
+ * @private
+ * @param {!RangeError} err
+ * @param {string} param
+ * @param {number} value
+ * @return {!RangeError}
+ */
+var setWholeError = setError.whole;
+/// #}}} @func setWholeError
+
+/// #}}} @group ERROR
+
+/// #{{{ @group IS
+
+/// #{{{ @func isBlock
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isBlock = IS.block;
+/// #}}} @func isBlock
+
+/// #{{{ @func isHtml
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isHtml = IS.html;
+/// #}}} @func isHtml
+
+/// #{{{ @func isNumber
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+var isNumber = IS.number;
+/// #}}} @func isNumber
+
+/// #{{{ @func isWholeNumber
+/**
+ * @private
+ * @param {number} val
+ * @return {boolean}
+ */
+var isWholeNumber = IS.wholeNumber;
+/// #}}} @func isWholeNumber
+
+/// #}}} @group IS
+
+/// #}}} @group HELPERS
+
+/// #{{{ @group METHODS
+//////////////////////////////////////////////////////////////////////////////
+// METHODS
+//////////////////////////////////////////////////////////////////////////////
+
+/// #{{{ @func Heading.prototype.scope
+/**
+ * @public
+ * @this {!Heading}
+ * @param {!Html} ROOT
+ * @param {!Block} BLOCK
+ * @param {number} index
+ * @param {number} depth
+ * @return {!Block}
+ */
+function scopeHeading(ROOT, BLOCK, index, depth) {
+
+  /// #{{{ @step declare-variables
+
+  /** @type {string} */
+  var line;
+
+  /// #}}} @step declare-variables
+
+  /// #{{{ @step verify-parameters
+
+  switch (arguments.length) {
+    case 0:
+      throw setNoArgError(new Error, 'ROOT');
+    case 1:
+      throw setNoArgError(new Error, 'BLOCK');
+    case 2:
+      throw setNoArgError(new Error, 'index');
+    case 3:
+      throw setNoArgError(new Error, 'depth');
+  }
+
+  if ( !isHtml(ROOT, Html) || ROOT !== this.BLOCK.ROOT ) {
+    throw setTypeError(new TypeError, 'ROOT', '!Html');
+  }
+  if ( !isBlock(BLOCK, Block) || BLOCK !== this.BLOCK ) {
+    throw setTypeError(new TypeError, 'BLOCK', '!Block');
+  }
+  if ( !isNumber(index) ) {
+    throw setTypeError(new TypeError, 'index', 'number');
+  }
+  if ( !isNumber(depth) ) {
+    throw setTypeError(new TypeError, 'depth', 'number');
+  }
+
+  if ( !isWholeNumber(index) || index < 0 ) {
+    throw setIndexError(new RangeError, 'index', index, 0);
+  }
+  if ( !isWholeNumber(depth) ) {
+    throw setWholeError(new RangeError, 'depth', depth);
+  }
+
+  /// #}}} @step verify-parameters
+
+  /// #{{{ @step save-lines-in-scope
+
+  line = ROOT.LINES[index];
+  BLOCK.LINES.push(line);
+
+  /// #}}} @step save-lines-in-scope
+
+  /// #{{{ @step return-block-instance
+
+  return BLOCK;
+
+  /// #}}} @step return-block-instance
+}
+/// #}}} @func Heading.prototype.scope
+
+/// #}}} @group METHODS
+
+/// #{{{ @group EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+// EXPORTS
+//////////////////////////////////////////////////////////////////////////////
+
+module.exports = scopeHeading;
+
+/// #}}} @group EXPORTS
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
