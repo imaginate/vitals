7739b3603859a486ee12debdfcfdec7dec26cf2c
Adam Smith <adam@imaginate.life>
2017-10-11 16:22:54 -0700

    Added helper: $insHome

---
+++ src/helpers/ins-home.js
@@ -0,0 +1,76 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $INS-HOME HELPER
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+/// #{{{ @helper $insHome
+/**
+ * @private
+ * @param {string} path
+ * @return {string}
+ */
+var $insHome = (function __vitals$insHome__() {
+
+    /// #{{{ @const _END_SLASH
+    /**
+     * @private
+     * @const {!RegExp}
+     */
+    var _END_SLASH = /\/$/;
+    /// #}}} @const _END_SLASH
+
+  /// #{{{ @const _TILDE_ONLY
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _TILDE_ONLY = /^~$/;
+  /// #}}} @const _TILDE_ONLY
+
+  /// #{{{ @const _TILDE_START
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _TILDE_START = /^~\//;
+  /// #}}} @const _TILDE_START
+
+  /// #{{{ @func $insHome
+  /**
+   * @param {string} path
+   * @return {string}
+   */
+  function $insHome(path) {
+
+    /** @type {string} */
+    var home;
+
+    if ( _TILDE_ONLY['test'](path) ) {
+      return $homedir();
+    }
+
+    if ( !_TILDE_START['test'](path) ) {
+      return path;
+    }
+
+    home = $homedir();
+
+    if ( !!home && !_END_SLASH['test'](home) ) {
+      home += '/';
+    }
+
+    return path['replace'](_TILDE_START, home);
+  }
+  /// #}}} @func $insHome
+
+  return $insHome;
+})();
+/// #}}} @helper $insHome
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
