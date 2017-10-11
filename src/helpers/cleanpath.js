719e6f28607f7b249f4b8b51b080254c9c5ca051
Adam Smith <adam@imaginate.life>
2017-10-11 15:06:59 -0700

    Fixed missing ref, unc paths, & lower-case drives in $cleanpath

--- src/helpers/cleanpath.js
+++ src/helpers/cleanpath.js
@@ -15,10 +15,61 @@
  * @param {string} path
  * @return {string}
  */
-function $cleanpath(path) {
-  path['replace'](/\\+/g, '/');
-  return path['replace'](/\/\/+/g, '/');
-}
+var $cleanpath = (function __vitals$cleanpath__() {
+
+  /// #{{{ @const _LOW_DRIVE
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _LOW_DRIVE = /^[a-z](?=:)/;
+  /// #}}} @const _LOW_DRIVE
+
+  /// #{{{ @const _UNC
+  /**
+   * @private
+   * @const {!RegExp}
+   */
+  var _UNC = /^[\/\\][\/\\]+[^\/\\]/;
+  /// #}}} @const _UNC
+
+  /// #{{{ @func _capitalizeDrive
+  /**
+   * @private
+   * @param {string} drive
+   * @return {string}
+   */
+  function _capitalizeDrive(drive) {
+    return drive['toUpperCase']();
+  }
+  /// #}}} @func _capitalizeDrive
+
+  /// #{{{ @func $cleanpath
+  /**
+   * @param {string} path
+   * @return {string}
+   */
+  function $cleanpath(path) {
+
+    /** @type {string} */
+    var result;
+
+    result = path['replace'](/\\+/g, '/');
+    result = result['replace'](/\/\/+/g, '/');
+
+    if ( _UNC['test'](path) ) {
+      result = '/' + result;
+    }
+    else if ( _LOW_DRIVE['test'](path) ) {
+      result = result['replace'](_LOW_DRIVE, _capitalizeDrive);
+    }
+
+    return result;
+  }
+  /// #}}} @func $cleanpath
+
+  return $cleanpath;
+})();
 /// #}}} @helper $cleanpath
 
 // vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
