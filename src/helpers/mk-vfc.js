669d3b2162277d3f146454af8843469793001b44
Adam Smith <adam@imaginate.life>
2017-10-31 05:16:03 -0700

    Added new helper: $mkVfc

---
+++ src/helpers/mk-vfc.js
@@ -0,0 +1,25 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $MK-VFC HELPER
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+/// #{{{ @helper $mkVfc
+/**
+ * @private
+ * @param {string} path
+ * @param {(?Object|?undefined)=} opts
+ * @param {string=} opts.pwd
+ * @param {string=} opts.homedir
+ * @param {string=} opts.basedir
+ * @return {!VitalsFileClass}
+ */
+var $mkVfc = $VITALS['VitalsFileClass']['construct'];
+/// #}}} @helper $mkVfc
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
