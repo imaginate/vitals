a37cb528ba52a237ec53e04cc70b3a3b1ddcd185
Adam Smith <adam@imaginate.life>
2017-12-11 19:22:26 -0800

    Added helper: $isZeroStr

---
+++ src/helpers/is-zero-str.js
@@ -0,0 +1,23 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $IS-ZERO-STR HELPER
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+/// #{{{ @helper $isZeroStr
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+function $isZeroStr(val) {
+  return val === '';
+}
+/// #}}} @helper $isZeroStr
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
