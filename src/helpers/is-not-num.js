82b18359c1709ccd71888b0a4801cdd13c6d0ec5
Adam Smith <adam@imaginate.life>
2017-12-11 19:43:59 -0800

    Added helper: $isNotNum

---
+++ src/helpers/is-not-num.js
@@ -0,0 +1,23 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $IS-NOT-NUM HELPER
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+/// #{{{ @helper $isNotNum
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+function $isNotNum(val) {
+  return !$isNum(val);
+}
+/// #}}} @helper $isNotNum
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
