97c5e54ff3fa84ba38676024a223634cc572eefe
Adam Smith <adam@imaginate.life>
2017-12-11 18:14:22 -0800

    Added helper: $isDiff

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -19,6 +19,7 @@
  */
 
 /// #{{{ @core helpers
+/// #include @helper $isDiff ../helpers/is-diff.js
 /// #include @helper $isSame ../helpers/is-same.js
 /// #include @helper $objStr ../helpers/obj-str.js
 /// #include @helper $own ../helpers/own.js
---
+++ src/helpers/is-diff.js
@@ -0,0 +1,24 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $IS-DIFF HELPER
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+/// #{{{ @helper $isDiff
+/**
+ * @private
+ * @param {*} val1
+ * @param {*} val2
+ * @return {boolean}
+ */
+function $isDiff(val1, val2) {
+  return val1 !== val2;
+}
+/// #}}} @helper $isDiff
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
