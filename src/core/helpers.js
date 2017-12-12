42c348aeea63ed8d569dea29732f3a08a2ef6648
Adam Smith <adam@imaginate.life>
2017-12-11 18:07:47 -0800

    Added helper: $isBool

--- src/core/helpers.js
+++ src/core/helpers.js
@@ -27,6 +27,7 @@
 /// #include @helper $isNotNull ../helpers/is-not-null.js
 /// #include @helper $isVoid ../helpers/is-void.js
 /// #include @helper $isNotVoid ../helpers/is-not-void.js
+/// #include @helper $isBool ../helpers/is-bool.js
 /// #include @helper $is ../helpers/is.js
 /// #include @helper $mkStr ../helpers/mk-str.js
 /// #include @helper $print ../helpers/print.js
---
+++ src/helpers/is-bool.js
@@ -0,0 +1,23 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $IS-BOOL HELPER
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+/// #{{{ @helper $isBool
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+function $isBool(val) {
+  return $isSame($typeOf(val), 'boolean');
+}
+/// #}}} @helper $isBool
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
