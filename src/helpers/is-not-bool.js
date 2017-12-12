d1f45a31a7d11b4042586c2a3bcf3414b1ffca7e
Adam Smith <adam@imaginate.life>
2017-12-11 18:59:04 -0800

    Added helper: $isNotBool

---
+++ src/helpers/is-not-bool.js
@@ -0,0 +1,23 @@
+/**
+ * ---------------------------------------------------------------------------
+ * $IS-NOT-BOOL HELPER
+ * ---------------------------------------------------------------------------
+ * @version 5.0.0
+ * @see [vitals](https://github.com/imaginate/vitals)
+ *
+ * @author Adam Smith <adam@imaginate.life> (https://imaginate.life)
+ * @copyright 2014-2017 Adam A Smith <adam@imaginate.life>
+ */
+
+/// #{{{ @helper $isNotBool
+/**
+ * @private
+ * @param {*} val
+ * @return {boolean}
+ */
+function $isNotBool(val) {
+  return $typeof(val) !== 'boolean';
+}
+/// #}}} @helper $isNotBool
+
+// vim:ts=2:et:ai:cc=79:fen:fdm=marker:eol
